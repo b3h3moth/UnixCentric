e8fa29f427bc8cf307d948add71c2becce173029
Luca Suriano <l.s@cryptolab.net>
2010-10-23 17:41:14 +0200

    Inserite diverse informazioni a supporto delle funzioni

--- Standard-Input-Output-Library/01-tmpnamc.c
+++ Standard-Input-Output-Library/01-tmpnamc.c
@@ -3,51 +3,58 @@
 #include <errno.h>
 #include <string.h>
 
-#define MAX_BUF 512
+#define MAX_BUF 1024
+
+/* 
+ * Utilizzo di tmpnam() con argomento NULL - Prototipo: char *tmpnam(char *s); 
+ */
 
 int main(int argc, char *argv[])
 {
    /* La funzione tmpnam() ha il compito di generare il percorso di un nuovo
     * file temporaneo, il quale naturalmente ancora non esiste, in pratica
-	* genera una stringa; per temporaneo si intende che tale file sara' creato 
-	* nella directory /tmp */
+	* genera una stringa; tale file sara' creato nella directory /tmp */
 
    char *tmp_pathname;		/* Percorso del file temporaneo */
    FILE *ftmp = 0;			/* File temporaneo */
    char buf[MAX_BUF];		/* Servira' se tutto andra' bene, per contenere
-   							   che sara' scritto nel file temporaneo */
+   							   cio' che sara' scritto nel file temporaneo */
 
+   /* tmpnam() puo' accettare come argomento un puntatore a char oppure, come
+	* in questo caso, NULL, e il percorso sara' valido fino alla successiva 
+	* chiamata di tmpnam() proprio perche' non e' stato fornito un argomento */
    if (!(tmp_pathname = tmpnam(NULL)) ) {
       fprintf(stderr, "%s: generazione file temporaneo.\n", (char *)strerror(errno));
       exit(EXIT_FAILURE);
    }
 
-   /* tmp_pathname contiene un puntatore al percorso del file temporaneo */
+   /* tmp_pathname contiene il puntatore a un buffer, ovvero il percorso del
+	* file temporaneo */
    printf("File temporaneo: %s\n", tmp_pathname);
 
-   /* Se tutto e' andato a buon fine, mediante la fopen() si apre un file nella
-	* directory /tmp */
-   if ((ftmp = fopen(tmp_pathname,"w")) == NULL ) { 
+   /* Si chiama la fopen() per la creazione del file */
+   if ((ftmp = fopen(tmp_pathname, "w")) == NULL ) { 
       fprintf(stderr, "%s: creazione file temporaneo %s\n", (char *)strerror(errno), tmp_pathname);
       exit(EXIT_FAILURE);
    }
 
-   /* Scrivo sui file temporaneo */
+   /* Si scrive sui file temporaneo */
    fputs("Sto scrivendo sul file temporaneo appena creato.\n"
    		 "Da notare che la tmpnam() restituisce un puntatore\n", ftmp);
 
-   /* Riporto il puntatore all'inizio del file */
+   /* Si riporta il puntatore all'inizio del file */
    rewind(ftmp);
 
    /* Leggo dal file temporaneo */
-   if ( (fgets(buf, 512, ftmp)) == NULL) {
+   if ( (fgets(buf, sizeof(buf), ftmp)) != NULL) {
       fprintf(stderr, "%s: lettura da file temporaneo %s\n", (char *)strerror(errno), ftmp);
 	  exit(EXIT_FAILURE);
    }
 
+   /* Si stampa il contenuto del file */
    fputs(buf, stdout);
 
-
+   /* Il puntatore al file viene deallocato */
    fclose(ftmp);
 
    return(EXIT_SUCCESS);
