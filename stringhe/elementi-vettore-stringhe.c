060f28875acc107143c171d2d234cfeed87a0fe9
Luca Suriano <l.s@cryptolab.net>
2010-11-13 13:49:03 +0100

    Visualizzazione degli elementi di un vettore di stringhe

---
+++ stringhe/elementi-vettore-stringhe.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+ * Visualizzare gli elementi di un vettore di stringhe
+ */
+
+int main(int argc, char *argv[])
+{
+	char *array[] = {"campobasso","roma","napoli","venezia","verona","torino",
+		             "milano","genova","palermo","catania","bari","firenze"};
+	int x, y;
+
+	/* Calcola il numero degli elementi presenti nel vettore di stringhe */
+	int len = sizeof(array) / sizeof(char *);	
+
+
+	for (x=0; x<len; x++) {
+		for (y=0; y<=strlen(array[x]); y++)
+			printf("%c", array[x][y]);
+		printf("\n");
+	}
+
+	return(EXIT_SUCCESS);
+}
