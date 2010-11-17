dc13ad0baca189c000234d94ae80f2e37c422137
Luca Suriano <l.s@cryptolab.net>
2010-11-17 20:32:21 +0100

    Funzioni: argomenti e parametri, vettori unidimensionali passati come argomenti attuali

--- funzioni-e-struttura-dei-programmi/01-fondamenti-funzioni.c
+++ funzioni-e-struttura-dei-programmi/01-fondamenti-funzioni.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_LEN 10
+
 
 /* Prototipi delle funzioni */
 double Media(double val_a, double val_b);
 int Potenza(int x, int n);
-
+int SommaElementi(int vect[], int len);
 
 int main(int argc, char *argv[])
 {
 	double x = 10.8,  y = 89.90;
 	int val = 2, n = 5;
+	int vettore[MAX_LEN], tot_elem;
 
 	/* CONCETTI FONDAMENTALI SULLE FUNZIONI
 	 ***************************************************************************
@@ -100,13 +103,43 @@ int main(int argc, char *argv[])
 	 * valori passati come argomenti non saranno modificati.
 	 */
 
-	printf("La potenza di %d^%d = %d\n", val, n, Potenza(val, n));
+	printf("Potenza %d^%d = %d\n", val, n, Potenza(val, n));
 	printf("Valori degli argomenti nel main val=%d n=%d\n", val, n );
 
+	/* VETTORI COME ARGOMENTI
+	 ***************************************************************************
+	 * Quando il vettore passato come argomento e' unidimensionale si potrebbe
+	 * anche evitare di fornire la lunghezza, ma questo sara' comunque un
+	 * problema poiche' se dovesse servire la lunghezza del vettore si dovrebbe
+	 * gestire ulteriore codice per calcolarla.
+	 *
+	 * Un vettore unidimensionale puo' essere gestito in una funzione mediante
+	 * due argomenti, il vettore stesso e la lunghezza; nel prototipo e nella
+	 * definizione della funzione vanno inserite le quadre assieme al vettore,
+	 * nell'argomento attuale invece le quadre non sono necessarie; da notare
+	 * che la funzione non ha modo di verificare se la lunghezza fornita del
+	 * vettore sia esatta o meno, porre pertanto particolare attenzione.
+	 */
+
+	tot_elem = SommaElementi(vettore, MAX_LEN);
+	printf("Somma elementi vettore = %d\n", tot_elem);
+
+	/* Da notare che, mediante la funzione SommaElementi(), ciascun elemento del
+	 * vettore e' stato modificato, ma non si era detto che gli argomenti,
+	 * essendo passati per valore, non possono essere modificati? Certo, ma
+	 * ovviamente ci sono delle eccezioni, vettori e puntatori viaggiano in
+	 * stretto contatto, per cui e' facilmente intuibile perche' e' stato
+	 * possibile modificare i valori di un vettore. Tale argomento tuttavia
+	 * sara' ampiamente trattato allorquando ci si occupera' di funzioni e
+	 * puntatori.
+	 */
+
+	
 	return(EXIT_SUCCESS);
 }
 
 
+/* Si calcola la media aritmetica tra due numeri */
 double Media(double val_a, double val_b)
 {
 	return ( (val_a + val_b) / 2);
@@ -114,8 +147,7 @@ double Media(double val_a, double val_b)
 
 
 /* In questo caso la variabile n assumera' il valore -1 all'interno della
- * funzione, ma il corrispettivo argomento attuale non sara' modificato.
- */
+ * funzione, ma il corrispettivo argomento attuale non sara' modificato. */
 int Potenza(int x, int n)
 {
 	int risultato = 1;
@@ -128,19 +160,38 @@ int Potenza(int x, int n)
 	return risultato;
 }
 
+/* Somma degli elementi di un vettore e restituzione del risultato; il vettore
+ * dapprima viene riempito dopodiche' i valori vengono sommati. */
+int SommaElementi(int vect[], int len)
+{
+	int i, somma = 0;
 
-/* Alcune regole, stilistiche e non:
+	for (i=0; i<len; i++) {
+		/* Si assegna a ciascun elemento il valore della variabile <i> */
+		vect[i] = i;
+		somma += vect[i];
+	}
+
+	return somma;
+}
+
+
+/* DA RICORDARE
+ *******************************************************************************
  * - Inserire il tipo restituito sopra il nome della funzione migliora la
  *   leggibilita' allorquando dovessero essere utilizzati tipi di dato piuttosto
- *   lunghi, come ad esempio potrebbe esssere <unsigned long int>.
+ *   lunghi, come ad esempio potrebbe esssere <unsigned long int>;
  *
  * - Nella dichiarazione di una funzione e' possibile inserire solo il tipo di
- *   dato, senza il nome della variabile.
+ *   dato, senza il nome della variabile;
  *
- * - Il corpo di una funzione dichiarata void, puo' anche essere vuoto.
+ * - Il corpo di una funzione dichiarata void, puo' anche essere vuoto;
  *
  * - Il casting (void) e' un modo educato per dire che "non serve", se ci si
- *   trova nel caso in cui si debba scartare una funzione.
+ *   trova nel caso in cui si debba scartare una funzione;
  *
- * - Gli argomenti o argomenti attuali sono passati per valore.
+ * - Gli argomenti o argomenti attuali sono passati per valore;
+ *
+ * - Per la leggibilita' del codice e' una buona idea commentare ciascuna
+ *   funzione, indicando brevemente lo scopo per il quale e' stata scritta.
  */
