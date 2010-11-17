c310c4f9b317b63c0e2ca7f5e38244b20937a963
Luca Suriano <l.s@cryptolab.net>
2010-11-17 09:49:25 +0100

    Introduzione all'utilizzo delle funzioni nel linguaggio C-ANSI (c99)

---
+++ funzioni/funzioni.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	/* Le funzioni sono essenziali nel contesto di un programma C, poiche'
+	 * aiutano notevolmente alla realizzazione di programmi agili e
+	 * performanti; la definizione di una funzione e' caratterizzata nel modo
+	 * seguente:
+	 *
+	 * tipoRestituito IDfunzione( parametri )
+	 * {
+	 *     dichiarazioni
+	 *     istruzioni
+	 * }
+	
+	return(EXIT_SUCCESS);
+}
