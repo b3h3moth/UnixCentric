2ff57f7316249944a1af6d378839fb16b73fc546
Luca Suriano <l.s@cryptolab.net>
2010-11-18 18:05:09 +0100

    Modificato lo stato di uscita di talune funzioni, inserito in output il nome del file crittografato (ciphertext)

--- algoritmi-strutture-dati/crittografia/cesare.c
+++ algoritmi-strutture-dati/crittografia/cesare.c
@@ -3,8 +3,19 @@
 #include <errno.h>
 #include <string.h>
 
-#define MAX_BUF	1024
-#define CAESAR	3
+#define MAX_BUF			1024
+#define CAESAR_SEEK		3
+#define LEN_ALPHABET	23
+
+FILE *OpenCleartext(FILE *fp, char *inputfile);
+
+/*
+ * Implementazione del cifrario di Cesare, che prevede lo scostamento di tre
+ * carratteri in avanti per ogni lettera; e' stato uno dei primi algoritmi
+ * crittografici della storia. Saranno modificati solo caratteri alfanumerici,
+ * ossia solo numeri e lettere, in tal modo la struttura del documento restera'
+ * inalterata, ottenendo peraltro un effetto molto interessante.
+ */
 
 int main(int argc, char *argv[])
 {
@@ -18,16 +29,13 @@ int main(int argc, char *argv[])
 	/* Si verifica che si fornisca un argomento al programma  */
 	if (argc < 2) {
 		fprintf(stderr, "Uso: %s <file_da_cifrare>\n", argv[0]);
-		abort();
+		exit(EXIT_FAILURE);
 	}
 
 	/* Il file di input da cifrare e' fornito come argomento  */
-	if ( (f_cleartext = fopen(argv[1], "r")) == NULL) {
-		fprintf(stderr, "%s: Apertura testo in chiaro\n", (char *)strerror(errno));
-		abort();
-	}
+	f_cleartext = OpenCleartext(f_cleartext, argv[1]);
 
-	/* Conteggio righe del file da cifrare (cleantext) */
+	/* Conteggio righe del file da cifrare (cleartext) */
 	while ( (r_char = fgetc(f_cleartext)) != EOF) {
 		tot_rows++;
 	}
@@ -44,7 +52,7 @@ int main(int argc, char *argv[])
 
 	if ( (f_cleartext = fopen(argv[1], "r")) == NULL) {
 		fprintf(stderr, "%s: Apertura testo in chiaro\n", (char *)strerror(errno));
-		abort();
+		exit(EXIT_FAILURE);
 	}
 
 	/* Si copia ciascun carattere del file di input (cleartext) nel buf_rows
@@ -68,17 +76,33 @@ int main(int argc, char *argv[])
 	 * dall'inizio. */
 	for (i=0; i<tot_rows; i++) {
 		if ( (buf_rows[i] >= 88 && buf_rows[i] <= 90) || (buf_rows[i] >= 120 && buf_rows[i] <= 122) )
-			buf_rows[i] = buf_rows[i] - 23; 
+			buf_rows[i] = buf_rows[i] - LEN_ALPHABET; 
 		else if ( (buf_rows[i] >= 65 && buf_rows[i] <= 87) || (buf_rows[i] >= 97 && buf_rows[i] <= 120) )
-			buf_rows[i] += CAESAR;
-	
+			buf_rows[i] += CAESAR_SEEK;
+		else if ( (buf_rows[i] >= 48 && buf_rows[i] <= 54) )
+			buf_rows[i] += CAESAR_SEEK;
+		else if ( (buf_rows[i] >= 55 && buf_rows[i] <= 57) )
+			buf_rows[i] = buf_rows[i] - LEN_ALPHABET;
+
 		fprintf(f_ciphertext, "%c", buf_rows[i]);
 	}
 
-	printf("Il file %s e' stato cifrato con successo\n", argv[1]);
+	printf("Il file %s e' stato cifrato con successo: \"%s\"\n", argv[1], ciphertext);
 
 	fclose(f_ciphertext);
 	free(buf_rows);
 
 	return(EXIT_SUCCESS);
  }
+
+	
+FILE *OpenCleartext(FILE *fp, char *inputfile)
+{
+
+	if ( (fp = fopen(inputfile, "r")) == NULL) {
+		fprintf(stderr, "%s: Apertura testo in chiaro\n", (char *)strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
+	return fp;
+}
