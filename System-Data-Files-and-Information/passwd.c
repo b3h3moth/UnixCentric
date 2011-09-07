cbf6d8d1ca84b11436e4b6d905b2f19b2ae1cba4
Luca Suriano <l.s@cryptolab.net>
2011-09-07 17:31:44 +0200

    get info with getpwuid() SysCall through /etc/passwd file

--- System-Data-Files-and-Information/passwd.c
+++ System-Data-Files-and-Information/passwd.c
@@ -7,12 +7,15 @@ int main(void)
 {
 	struct passwd *pd;
 	uid_t uid = getuid();
-	pd = getpwuid(uid);
-	printf("The user name: %s\n", pd->pw_name);
-	printf("The user ID: %u\n", pd->pw_uid);
-	printf("The Group ID: %u\n", pd->pw_gid);
-	printf("The initial directory: %s\n", pd->pw_dir);
-	printf("The initial user program: %s\n", pd->pw_shell);
+	if ( (pd = getpwuid(uid)) < 0) 
+		exit(EXIT_FAILURE);
+	else {
+		printf("The user name: %s\n", pd->pw_name);
+		printf("The user ID: %u\n", pd->pw_uid);
+		printf("The Group ID: %u\n", pd->pw_gid);
+		printf("The initial directory: %s\n", pd->pw_dir);
+		printf("The initial user program: %s\n", pd->pw_shell);
+	}
 	
 	return(EXIT_SUCCESS);
 }
