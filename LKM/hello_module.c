5692cf32c961f2b8f100ffd32c15ca64b1fbaeb0
Luca Suriano <behemoth@autistici.org>
2011-11-19 01:48:07 +0100

    LKM; module is now complete

--- LKM/hello_module.c
+++ LKM/hello_module.c
@@ -2,28 +2,28 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
+/* MACRO for documentation purposes */
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Luca Suriano");
-MODULE_DESCRIPTION("A Hello, World module");
+MODULE_DESCRIPTION("Basic hello world module, just for learning the basics");
 
 /* init function, called when the modudle is loading */
 static int hello_init(void)
 {
-   printk(KERN_ALERT "Hello world module.\n");
-   printk(KERN_INFO "KERN_INFO ... hello\n");
+   printk(KERN_ALERT "Init: \"Hello World\" module is up\n");
    return 0;
 }
 
 /* the exit function, called when the module is rmoved */
 static void hello_exit(void)
 {
-   printk(KERN_ALERT "Exit hello world module.\n");
+   printk(KERN_ALERT "Exit: \"Hello World\" module is down\n");
 }
 
 module_init(hello_init);
 module_exit(hello_exit);
 
-/* Loglevel:
+/* Loglevel MACRO:
  * KERN_ALERT
  * KERN_INFO
  * KERN_NOTICE
