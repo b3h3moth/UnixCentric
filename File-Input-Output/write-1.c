2590976d5b6f090599b6c08b730dc30452ac0e94
B3h3m0th <behemoth@autistici.org>
2012-01-06 23:32:56 +0100

    Aggiunta una condizione alla stampa di stringhe con routine di basso livello

--- File-Input-Output/write-1.c
+++ File-Input-Output/write-1.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
 
-int main(int argc, char *argv[], char *envp[])
+int main(void)
 {
-   char stringa[] = "Linux Programming";
+   char buf[] = "UNIX Programming";
 
 
-   /* Questo metodo di 'stampare' sullo stdout e'
-    * piu' efficiente, poiche' lavora solo funzioni
-    * di basso livello, e oltre all'efficienza e'
-    * naturalmente piu' veloce della classica printf */
+   /* 
+    Questo metodo di 'stampare' sullo stdout e' piu' efficiente, poiche' lavora
+    mediante system call di basso livello, e oltre all'efficienza e'
+    naturalmente piu' veloce se si fosse utilizzata la classica printf() */
 
-   write(fileno(stdout), stringa, strlen(stringa));
+   write(fileno(stdout), buf, sizeof(buf));
 
+   printf("\n");
+
+   /*
+    Un'ulteriore esempio con  una espressione condizionale per verificare la
+    correttezza della buf in output, o meglio, del vettore di caratteri.
+   */
+   if (write(STDOUT_FILENO, buf, sizeof(buf)-1) != sizeof(buf)-1)
+      fprintf(stderr, "%d: %s err.\n", errno, strerror(errno));
 
    return(EXIT_SUCCESS);
 }
--- Process-Control/fork-2.c
+++ Process-Control/fork-2.c
@@ -1,33 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 #include <unistd.h>
 
-int glob = 6;
-char buf[] = "a write to stdout\n";
+int glob_var = 10;
+char buf[] = "UNIX Programming\n";
 
 int main(int argc, char *argv[], char *envp[])
 {
    int var;
-   pid_t pid;
-   var = 88;
+   pid_t pid = fork();
+   var = 100;
 
    if (write(STDOUT_FILENO, buf, sizeof(buf)-1) != sizeof(buf)-1)
-      printf("err...sys\n");
+      fprintf(stderr, "%d: %s err.\n", errno, strerror(errno));
 
-   printf("before fork\n");
+   printf("Prima della chiamata fork()\n");
 
-   if ( (pid == fork()) < 0)
-      printf("err...\n");
+   if (pid < 0)
+      fprintf(stderr, "%d: %s err.\n", errno, strerror(errno));
    else if ( pid == 0)
    {
-      glob++;
+      glob_var++;
       var++;
    }
    else
       sleep(2);
 
-   printf("pid = %d, glob = %d, var = %d\n", getpid(), glob, var);
-
+   printf("pid = %d, glob_var = %d, var = %d\n", getpid(), glob_var, var);
 
    return(EXIT_SUCCESS);
 }
