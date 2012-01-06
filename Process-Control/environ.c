b15334bee747fe96260cb23f1948966c8873dc0b
B3h3m0th <behemoth@autistici.org>
2012-01-06 20:50:23 +0100

    Modifica della inroduzione alle variabili d'ambiente

--- Process-Control/environ.c
+++ Process-Control/environ.c
@@ -4,17 +4,18 @@
 #include <unistd.h> 
 
 /*
- * Un processo lavoro all'interno di un sistema operativo, o meglio, gira
- * all'interno di esso, ovvero in un ambiente specifico; nei sistemi UNIX vi
- * sono diverse variabili d'ambiente utili per gli scopi piu' disparati.
- *
- * Le varaiabili d'ambiente sono caratterizzate dal seguente formato:
- * name = value
- * HOME = user
- *
- * L'indirizzo della lista delle variabili di ambiente e' passato attraverso la
- * variabilr globale 'environ'.
- */
+ Un processo lavora all'interno di un sistema operativo, o meglio, e' eseguito
+ all'interno di esso, in un ambiente ben definito, per cui vi devono essere 
+ delle specifiche informazioni che consentano di  lavorare all'interno 
+ dell'ambiente stesso.
+ 
+ Nei sistemi operativi UNIX vi sono diverse variabili d'ambiente, esse sono 
+ caratterizzate dal formato <name = value> (es. HOME = user).
+ 
+ L'indirizzo della lista delle variabili di ambiente e' passato attraverso la
+ variabile globale <environ>, che e' un puntatore ad un array di stringhe, in
+ cui ciascuna stringa indica per l'appunto una specifica variabile d'ambiente.
+*/
 
 extern char **environ;
 
