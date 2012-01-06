b9631604e267fc8d6add07a37a7574b96c16fcd5
B3h3m0th <behemoth@autistici.org>
2012-01-07 00:08:12 +0100

    Correzioni sulla variabile d'ambiente environ

--- Process-Control/environ.c
+++ Process-Control/environ.c
@@ -5,16 +5,17 @@
 
 /*
  Un processo lavora all'interno di un sistema operativo, o meglio, e' eseguito
- all'interno di esso, in un ambiente ben definito, per cui vi devono essere 
- delle specifiche informazioni che consentano di  lavorare all'interno 
+ nell'ambiente ben definito del sistema operativo stesso, per cui vi devono
+ essere delle specifiche informazioni che consentano di lavorare all'interno 
  dell'ambiente stesso.
  
  Nei sistemi operativi UNIX vi sono diverse variabili d'ambiente, esse sono 
  caratterizzate dal formato <name = value> (es. HOME = user).
  
- L'indirizzo della lista delle variabili di ambiente e' passato attraverso la
- variabile globale <environ>, che e' un puntatore ad un array di stringhe, in
- cui ciascuna stringa indica per l'appunto una specifica variabile d'ambiente.
+ L'indirizzo della lista delle variabili di ambiente e' ottenibile attraverso la
+ variabile globale <environ>, un puntatore ad un array di stringhe, all'interno
+ della quale ciascuna stringa indica per l'appunto una specifica variabile 
+ d'ambiente.
 */
 
 extern char **environ;
