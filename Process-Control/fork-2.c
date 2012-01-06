9eb9956e1d4d0c1e647e02276a19cccfaadb622d
B3h3m0th <behemoth@autistici.org>
2012-01-06 23:58:17 +0100

    Aggiunta una nota sull'esecuzione dei processi

--- Process-Control/fork-2.c
+++ Process-Control/fork-2.c
@@ -5,22 +5,36 @@
 #include <unistd.h>
 
 int glob_var = 10;
+char buf[] = "UNIX Programming\n";
 
 int main(int argc, char *argv[], char *envp[])
 {
    int var;
-   pid_t pid = fork();
+   pid_t pid;
    var = 100;
 
-   if (pid < 0)
+   if (write(STDOUT_FILENO, buf, sizeof(buf)-1) != sizeof(buf)-1)
+      fprintf(stderr, "%d: %s err.\n", errno, strerror(errno));
+
+   printf("Prima della chiamata fork()\n");
+
+   if ((pid = fork()) < 0)
       fprintf(stderr, "%d: %s err.\n", errno, strerror(errno));
    else if ( pid == 0)
    {
+      /* Child Process */
       glob_var++;
       var++;
    }
    else
+      /* Parent Process */
       sleep(2);
+
+   /* 
+    Da notare che in realta' non si sa se Child Process sara' eseguito prima del
+    Parent Process o viceversa, l'esecuzione di ciascun processo infatti e' 
+    gestita dall'algoritmo di scheduling implementato nel kernel.
+   */
 
    printf("pid = %d, glob_var = %d, var = %d\n", getpid(), glob_var, var);
 
