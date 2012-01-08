572b8117c48b56d0e82ea8877fc7c06dde3ea967
B3h3m0th <behemoth@autistici.org>
2012-01-08 20:25:48 +0100

    Aggiunta la possibilita' di inserire il file da scrivere sullo stout, come argomento, e varie modifiche utili alla didattica

--- IO-Unbuffered/01_open.c
+++ IO-Unbuffered/01_open.c
@@ -6,31 +6,39 @@
 #define MAX_BUF	1
 
 /*
- Il termine Unbuffered I/O (I/O di basso livello) indica che le varie routine
- (open, read, write, lseek, close, etc...) sono chiamate dirette al sistema
- operativo, per cui gestite direttamente dal Kernel, tecnicamente invece indica
- che l'accesso in lettura e scrittura sul disco comporta un 'semplice'
- trasferimento di byte, naturalmente nsenza formattazione.
+ Il termine Unbuffered I/O (definito anche I/O di basso livello) indica che le 
+ varie routine (open, read, write, lseek, close, etc...) sono chiamate dirette 
+ al sistema operativo e pertanto gestite direttamente dal Kernel; tecnicamente 
+ invece indica che l'accesso in lettura e in scrittura sul disco comporta un 
+ "semplice" trasferimento di byte, naturalmente senza formattazione.
 
- Non e' standard ISO C ma standard POSIX 1. e Single Unix Specification.
+ L'I/O Unbuffered non e' standard ISO C bensi' standard POSIX 1., nonche' Single
+ Unix Specification.
+
+ Comportamento del programma:
+ Il programma richiede l'inserimento del nome di un file in input, utilizzando 
+ un path assoluto, dopodiche' tale file sara' inviato allo standard output.
 */
 
-int main() {
-   char *filename="/etc/passwd";
+int main(int argc, char *argv[]) {
    int fd, n;
    char buf[MAX_BUF];
+
+   if (argc < 2) {
+      fprintf(stderr, "Uso: %s <file name>\n", argv[0]);
+      exit(EXIT_FAILURE);
+   }
    
-   if ( (fd = open(filename, O_RDONLY)) < 0) {
-      printf("Cannot open file ...");
+   if ( (fd = open(argv[1], O_RDONLY)) < 0) {
+      fprintf(stderr, "Err. open file\n");
       exit(EXIT_FAILURE);
    }
 
    /* Scrive il contenuto del file 'filename' in output, un carattere
     * per volta */
-   while ( (n = read(fd, buf, MAX_BUF)) > 0)
+   while ((n = read(fd, buf, MAX_BUF)) > 0)
       write (STDOUT_FILENO, buf, n);
 
    close(fd);
    return(EXIT_SUCCESS);
 }
-
