68b7393b4d7181e60b9f38364b08df154ae43d79
B3h3m0th <behemoth@autistici.org>
2012-01-08 17:14:41 +0100

    Aggiunta una descrizione alle routine getpid() getppid()

--- Process-Control/getpid.c
+++ Process-Control/getpid.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>  
 #include <unistd.h> 
 
-int main(int argc, char *argv[], char *envp[])
+int main(void)
 {
-   printf("PID processo corrente %d\n", getpid());
-   printf("PID processo padre %d\n", getppid());
+   /* 
+    Le due routine getpid() e getppid() servono per acquisire il PID del 
+    current process e il PID del parent process, rispettivamente; da notare che
+    il parent process del processo corrente risulta essere la shell in cui si
+    esegue il programma.
+
+    Si provi ad eseguire il seguente programma, dopodichè si esegua il comando
+    ps e si verifichi il PID della shell corrente, si noterà che che getppid()
+    restituirà lo stesso identificativo.
+   */
+   printf("PID Current process %d\n", getpid());
+   printf("PID Parent process %d\n", getppid());
 
    return(EXIT_SUCCESS);
 }
