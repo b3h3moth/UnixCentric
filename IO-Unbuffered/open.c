27b2c4a6664bd7992b534884b71fb52dbf3d792a
B3h3m0th <behemoth@autistici.org>
2012-01-08 17:41:10 +0100

    Introduzione all'I/O Unbuffered

--- IO-Unbuffered/open.c
+++ IO-Unbuffered/open.c
@@ -1,7 +1,17 @@
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
+#include <unistd.h>
+#include <fcntl.h> /* O_RDONLY */
+
+/*
+ Il termine Unbuffered I/O (I/O di basso livello) indica che le varie routine
+ (open, read, write, lseek, close, etc...) sono chiamate dirette al sistema
+ operativo, per cui gestite direttamente dal Kernel, tecnicamente invece indica
+ che l'accesso in lettura e scrittura sul disco comporta un 'semplice'
+ trasferimento di byte, naturalmente nsenza formattazione.
+
+ Non e' standard ISO C ma standard POSIX 1. e Single Unix Specification.
+*/
 
 int main() {
    char *filename="/etc/passwd";
@@ -10,7 +20,7 @@ int main() {
    
    if ( (fd = open(filename, O_RDONLY)) < 0) {
       printf("Cannot open file ...");
-      exit(1);
+      exit(EXIT_FAILURE);
    }
 
    /* Scrive il file /etc/passwd in output; 
@@ -18,8 +28,6 @@ int main() {
    while ( (n = read(fd, buf, 10)) > 0) {
       write (1,buf,n);
    }
-
-   printf(".");
 
    close(fd);
    return(EXIT_SUCCESS);
