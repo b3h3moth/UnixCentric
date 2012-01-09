6bd78f685a22c46c62a0e5c9ed8fe293ce083a97
B3h3m0th <behemoth@autistici.org>
2012-01-09 22:36:16 +0100

    revisione totale per rendere la lseek() più comprensibile

--- IO-Unbuffered/04_lseek-1.c
+++ IO-Unbuffered/04_lseek-1.c
@@ -4,34 +4,50 @@
 #include <unistd.h> 
 #include <string.h> 
 
+#define MAX_BUF 32
+#define PERMS 0644
 /*
- Crea il file passwd.txt, all'interno del quale inserisce il range di caratteri
- definito da lseek
+ Legge il contenuto del file 'input_file' e copia MAX_BUF byte in 'output_file'; 
+ mediante la lseek si individua l'offset da cui copiare, ossia l'inizio del
+ file.
 */
 
 int main(int argc, char *argv[], char *envp[])
 {
    int fd1, fd2;
-   char *filename = "/etc/passwd";
-   char *filename_out = "passwd.txt";
-   char buf[] = "luka:x";
+   char *input_file = "/etc/passwd";
+   char *output_file = "passwd.txt";
+   char buf[MAX_BUF];
 
-   if ( (fd1 = open(filename, O_RDONLY)) == -1)
-   {
-      printf("Err. apertura %s\n", filename);
-      exit(1);
+   if ( (fd1 = open(input_file, O_RDONLY)) < 0) {
+      printf("Err. apertura %s\n", input_file);
+      exit(EXIT_FAILURE);
    }
 
-   if ( (fd2 = open(filename_out, O_RDWR | O_CREAT, 0755)) == -1)
-   {
-      printf("Err. apertura %s\n", filename);
-      exit(1);
+   if ( (fd2 = open(output_file, O_RDWR | O_CREAT, PERMS)) < 0) {
+      printf("Err. apertura %s\n", input_file);
+      exit(EXIT_FAILURE);
    }
 
-   /* ESEMPIO PESSIMO */
-   lseek(fd1, 100, SEEK_SET );
-   read(fd1, buf, sizeof(buf));
-   write(fd2, &buf, sizeof(buf));
+   /* Calcola l'offset ad inizio del file */
+   if (lseek(fd1, 0, SEEK_SET ) == -1) {
+      fprintf(stderr, "Err. seek\n");
+      exit(EXIT_FAILURE);
+   }
+
+   /* 
+    Legge MAX_BUF byte da 'input_file' e li copia successivamente con la
+    write in 'output_file'; si lavora un byte per volta. 
+   */
+   if (read(fd1, &buf, MAX_BUF) < 0) {
+      fprintf(stderr, "Err. read\n");
+      exit(EXIT_FAILURE);
+   }
+
+   if (write(fd2, &buf, sizeof(buf)) < 0) {
+      fprintf(stderr, "Err. write\n");
+      exit(EXIT_FAILURE);
+   }
 
    close(fd1);
    close(fd2);
