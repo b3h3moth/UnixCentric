652434bd8c14960580bbb50ccf7e3623045d766a
B3h3m0th <behemoth@autistici.org>
2012-01-10 17:52:19 +0100

    Introduzione ai file descriptor e prototipo open()

--- IO-Unbuffered/01_open.c
+++ IO-Unbuffered/01_open.c
@@ -28,7 +28,31 @@ int main(int argc, char *argv[]) {
       fprintf(stderr, "Uso: %s <file name>\n", argv[0]);
       exit(EXIT_FAILURE);
    }
-   
+  
+   /*
+    int open(const char *pathnarme, int oflag, mode_t mode);
+    - pathname, il file da aprire o creare;
+    - oflag; serie di costanti, tra cui (O_RSONLY, O_WRONLY, O_RDWR, etc...);
+    - mode; usato solo se il file e' da creare, riguarda i permessi del file 
+    stesso.
+
+    Allorquando si crea/apre un file, il kernel rilascia un file descriptor al
+    processo, esso e' un intero non negativo che identifica un file aperto; nei 
+    sistemi UNIX ad ogni processo eseguito dalla shell sono associati 3 file
+    descriptor: standard input 0, standard output 1, standard error 2.
+
+    POISX peraltro ne specifica anche le costanti simboliche in:
+    - standard input  = 0 = STDIN_FILENO;
+    - stanbdar output = 1 = STDOUT_FILENO;
+    - standard error  = 2 = STDERR_FILENO;
+
+    Per poter iniziare a lavorare con un file e' necessario per prima cosa 
+    aprirlo, la open() ha questo compito, ed e' peralto in questo momento che 
+    sono associati ad esso anche i 3 file descriptor di default.
+
+    Nell'esempio, e' un file in sola lettura, pertanto non ha bisogno del terzo 
+    argomento.
+   */
    if ( (fd = open(argv[1], O_RDONLY)) < 0) {
       fprintf(stderr, "Err. open file\n");
       exit(EXIT_FAILURE);
