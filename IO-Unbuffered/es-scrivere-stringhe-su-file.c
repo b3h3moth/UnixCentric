a9cd3547a77fc635e352fb63c2d0b3f70311474c
B3h3m0th <behemoth@autistici.org>
2012-01-11 19:27:50 +0100

    Inserite solo funzioni di basso livello

--- IO-Unbuffered/es-scrivere-stringhe-su-file.c
+++ IO-Unbuffered/es-scrivere-stringhe-su-file.c
@@ -3,63 +3,50 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
 
-#define DIMBUF	128
+#define BUF_SIZE	128
+
 /*
- * Il programma riceve delle stringhe in input, tali stringhe dopo essere state
- * copiate sullo stdout sono copiate, senza spazi, in un file
- */
+ Il programma riceve delle stringhe in input, dopodiche' sono copiate nel file
+ filename.
+*/
 
-void inserisci(char b[], int fdw);
-void stampa(char b[], int fdr);
+void ins_str_to_file(char b[], int fdw);
 
 int main(int argc, char *argv[], char *envp[])
 {
-   char buf[128];
-   int fd1, fd2;
-   char *filename = "IO-Stringhe.txt";
+   char buf[BUF_SIZE];
+   int fd1;
+   char *filename = "test.txt";
 
-   if ( (fd1 = open(filename, O_WRONLY | O_CREAT, 0755)) == -1)
-   {
-      printf("Impossibile aprire il file '%s'\n", filename);
-      exit(1);
+   if ( (fd1 = open(filename, O_WRONLY | O_CREAT, 0755)) < 0) {
+      fprintf(stderr, "Err: (%d) - '%s'\n", errno, strerror(errno));
+      exit(errno);
    }
 
-   inserisci(buf, fd1);
+   ins_str_to_file(buf, fd1);
    close(fd1);
-
-   if ( (fd2 = open(filename, O_RDONLY)) == -1)
-   {
-      printf("Impossibile aprire il file '%s'\n", filename);
-      exit(1);
-   }
-
-   stampa(buf, fd2);
-   close(fd2);
 
    return(EXIT_SUCCESS);
 }
 
-void inserisci(char b[], int fdw)
+void ins_str_to_file(char b[], int fdw)
 {
    int i;
-   printf("Immettere il testo (digitare END per uscire)\n");
+   int n_read;
+   printf("Immettere il testo (digitare X per uscire)\n");
 
    /* Pulizia del buffer */
-   for (i=0; i<DIMBUF; i++)
+   for (i=0; i<BUF_SIZE; i++)
       b[i] = 0;
 
-   do
-   {
-      scanf("%s", b);
-      if (write(fdw, b, DIMBUF) != DIMBUF)
-      	 printf("Errore in scrittura\n");
-   } while (strcmp(b, "END"));
-
-}
-
-void stampa(char b[], int fdr)
-{
-   while( read(fdr, b, DIMBUF) != 0)
-      printf("%s\n", b);
+   while ((n_read = read(STDIN_FILENO, b, BUF_SIZE)) > 0) {
+      if (b[n_read-2] == 'X')
+      	 break;
+      if (write(fdw, b, n_read) != n_read) {
+      	 fprintf(stderr, "Err: (%d) - %s\n", errno, strerror(errno));
+	 exit(errno);
+      }
+   }
 }
