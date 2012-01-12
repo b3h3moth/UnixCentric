8921ce8ee2df82c8061c102021d0ed41384663a1
B3h3m0th <behemoth@autistici.org>
2012-01-12 12:42:44 +0100

    correzione refusi

--- IO-Unbuffered/es-KR-read-file.c
+++ IO-Unbuffered/es-KR-read-file.c
@@ -7,14 +7,14 @@ int main(int argc, char *argv[])
    char c;
 
    /*
-    * Come si puo notare, il parametro della read che prevederebbe la lunghezza
-    * del buffer, è di 1, ciò vuol dire che la lettura dell'input sarà carattere
-    * oer carattere.
+    * Come si puo' notare, il parametro della read() che prevede la lunghezza
+    * del buffer è impostato ad 1, ciò vuol dire che la lettura dell'input sarà
+    * carattere per carattere.
     *
     * Codice estremamente efficiente
     */
    while (read(0, &c, 1) > 0)
-      printf("%c", c);
+      write(STDOUT_FILENO, &c, sizeof(c));
 
    return(EXIT_SUCCESS);
 }
