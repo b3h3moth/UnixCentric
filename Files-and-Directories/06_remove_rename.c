8cbfff700e0ca9dd6abb5c0e09c33cdcde4f147d
B3h3m0th <behemoth@autistici.org>
2012-01-16 18:05:29 +0100

    Inserite talune regole sull'utilizzo di remove() e soprattutto rename()

--- Files-and-Directories/06_remove_rename.c
+++ Files-and-Directories/06_remove_rename.c
@@ -1,53 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/stat.h>
-
-
-int main(int argc, char **argv, char **envp) {
-   char *new_directory = "LinuxProgramming";
-   mode_t set_mode = 0755;
-
-   if (argc < 2) {
-   	   fprintf(stderr, "Uso: %s <dir_name>\n", argv[0]);
-	   exit(EXIT_FAILURE);
-   }
-
-   /* mkdir() crea fisicamente la nuova directory */
-   if ( (mkdir(argv[1], set_mode)) < 0) {
-   	   fprintf(stderr, "Err. nella creazione della directory %s\n", argv[1]);
-	   exit(EXIT_FAILURE);
-   }
-   
-   /* La directory viene prima rinominata ... */
-   if ( (rename(argv[1], new_directory)) < 0) {
-   	   fprintf(stderr, "Err. nel rinominare %s in %s\n", argv[1], new_directory);
-   	   exit(EXIT_FAILURE);
-   }
-   
-   /* E poi eliminata */
-   if ( (rmdir(new_directory)) < 0) {
-   	   fprintf(stderr, "Err. nella eliminazione di %s\n", new_directory);
-   	   exit(EXIT_FAILURE);
-   }
-
-   return(EXIT_SUCCESS);
-}
-#include <stdio.h>
-#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
 
 /*
-HEADER    : 
+HEADER    : <stdio.h>
 
-PROTOTYPE : 
+PROTOTYPE : int remove(const char *pathname);
+            int rename(const char *oldname, const char *newname):
 
-SEMANTICS : 
+SEMANTICS : remove() rimuove 'pathname' dal filesystem; rename() rinomina 
+            'oldname' in 'newname'.
 
 RETURNS   : 0 In caso di successo, -1 in caso di errore
 --------------------------------------------------------------------------------
+Come accennato nel sorgente relativo alla funzione unlink(), essa non puo' 
+essere adoperata per rimuovere le directory, si dovrebbe utilizzare infatti
+rmdir(), oppure la funzione remove(), che peraltro utilizza unlink() per 
+rimuovere i file e rmdir() per eliminare le directory. Lo standard ISO C 
+specifica che dovrebbe essere utilizzata la funzione remove() per cancellare i 
+file.
+
+La funzione rename() consente di rinominare sia file sia directory, il pathname
+'oldname' sara' rinominato con il pathname indicato da 'newname', vi sono 
+tuttavia regole ben precise da rispettare a seconda che si tratti di file o
+directory:
+
+- Se si lavora con i file, 'newname' non deve essere una directory; se 'newname'
+  e' un file esistente, sara' rimosso e 'oldname' rinominato in 'newname'.
+
+- Se 'oldname' e' una directory, allora 'newname' deve essere una directory 
+  vuota;
+
+- Se o 'oldname' o 'newname' e' un symbolic link, 'oldname' sara' rinominato
+  in 'newname', non sara' interessato il file a cui fa riferimento;
+
+- Se 'oldname' e 'newname' indicano lo stesso file, tutto restera' immutato,
+  senza errori di ritorno.
+
 */
 
-int main(int argc, char *argv[]) {
+int main(int argc, char **argv, char **envp) {
+   int fd;
+   char *new_name_file = "test_remove";
+   struct stat statbuf;
+
+   if (argc < 2) {
+      fprintf(stderr, "Uso: %s <filename>\nfilename da eliminare\n", argv[0]);
+      exit(EXIT_FAILURE);
+   }
+
+   if ((fd = open(argv[1], O_RDONLY)) < 0) {
+      fprintf(stderr, "Err.:(%d) - %s: %s\n", errno, strerror(errno), argv[1]);
+      exit(EXIT_FAILURE);
+   }
+
+   /* 
+    Si adopera una sleep() per avere il tempo materiale per poter verificare i 
+    cambiamenti dei pathname nel filesystem (e' sufficiente eseguire una serie 
+    di ls). 
+    */
+   sleep(5);
+
+   if (rename(argv[1], new_name_file) < 0) {
+      fprintf(stderr, "Err.:(%d) - %s: %s\n", errno, strerror(errno), argv[1]);
+      exit(EXIT_FAILURE);
+   }
+
+   sleep(5);
+
+   if (remove(new_name_file) <0) {
+      fprintf(stderr, "Err.:(%d) - %s: %s\n", errno, strerror(errno), 
+      	    new_name_file);
+      exit(EXIT_FAILURE);
+   }
+
+   close(fd);
 
    return(EXIT_SUCCESS);
 }
