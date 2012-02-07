7bc5d5b8cbd21a322b46c3887c5de0c267e068f2
B3h3m0th <behemoth@autistici.org>
2012-02-07 20:00:27 +0100

    Sistemato alla nuova versione

--- IO-Standard-Library/05_temporary_file_1_tmpnam.c
+++ IO-Standard-Library/05_temporary_file_1_tmpnam.c
@@ -6,44 +6,54 @@
 #define MAX_BUF 516
 
 /*
+HEADER    : <stdio.h>
 
-Prototipo: char *tmpnam(char *s);
+PROTOTYPE : char *tmpnam(char *str);
 
-Note:
-	Utilizzo di tmpnam() con buffer.
-	POSIX tuttavia consiglia di utilizzare mkstemp() invece di tmpnam()
+SEMANTICS : Restituisce il puntatore ad una stringa, che dev'essere un file name
+            valido e non esistente al momento della creazione.
 
+RETURNS   : Il puntatore al file temporaneo in caso di successo, NULL in caso di
+            esito negativo. Non sono definiti errori.
+--------------------------------------------------------------------------------
+Nota: L'uso di tmpnam() e' assolutamente non raccomandato; POSIX infatti 
+consiglia di utilizzare mkstemp()
+
+L'esempio a scopo didattico tuttavia deve essere proposto.
 */
 
-int main(int argc, char *argv[])
-{
-	/* La funzione tmpnam() ha il compito di generare il percorso di un nuovo
-    * file temporaneo, il quale naturalmente ancora non esiste, in pratica
-	* genera una stringa; tale file sara' creato nella directory /tmp */
-   	
-	/* A differenza dell'esempio 01-tmpnam.c, a tmpnam() viene fornito un
-	 * proprio buffer, che avra' vita per l'intera durata del programma */
-	char tmp_pathname[L_tmpnam];		/* Percorso del file temporaneo */
-	FILE *ftmp;							/* File temporaneo */
-	char buf[MAX_BUF];		/* Servira' se tutto andra' bene, per contenere
-   							   cio' che sara' scritto nel file temporaneo */
+int main(int argc, char *argv[]) {
+   char buf[MAX_BUF];     
+   char tmp_pathname[L_tmpnam];	
+   FILE *ftmp;		
+  
+   /* Oppure con NULL al posto di una stringa
+   char *tmpfile = tmpnam(NULL);
+   */
+   tmpnam(tmp_pathname);
 
-   /* tmpnam() puo' accettare come argomento un puntatore a char oppure NULL,
-	* in questo caso forniamo una stringa riempendo il buffer a disposizione */
-   if ((tmpnam(tmp_pathname)) == NULL ) {
-      fprintf(stderr, "%s: generazione file temporaneo.\n", (char *)strerror(errno));
-      exit(EXIT_FAILURE);
-   }
+   printf("1° file temporaneo: %s\n", tmp_pathname);
 
-   /* tmp_pathname contiene il puntatore a un buffer, ovvero il percorso del
-	* file temporaneo */
-   printf("File temporaneo: %s\n", tmp_pathname);
-
-   /* Si chiama la fopen() per la creazione del file */
    if ((ftmp = fopen(tmp_pathname, "w+")) == NULL ) { 
-      fprintf(stderr, "%s: creazione file temporaneo %s\n", (char *)strerror(errno), tmp_pathname);
+      fprintf(stderr, "%s: creazione file temporaneo %s\n", 
+      	              strerror(errno), tmp_pathname);
       exit(EXIT_FAILURE);
    }
+
+   /* Si scrive sul file temporaneo */
+   fputs("The art of UNIX programming", ftmp);
+
+   /* Si riporta la posizione corrente all'interno del file all'inizio, per
+    poter leggere il file */
+   fseek(ftmp, 0L, SEEK_SET);
+
+   /* Si legge la riga pocanzi scritta sull file temporaneo */
+   if (fgets(buf, sizeof(buf), ftmp) == NULL) {
+      fprintf(stderr, "Err. %s reading file temp\n", strerror(errno));
+      exit(EXIT_FAILURE);
+   }
+   
+   fputs(buf, stdout);
 
    fclose(ftmp);
 
