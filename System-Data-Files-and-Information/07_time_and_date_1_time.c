c58864170bf5cbde8661cb90240d7fd86d535fb6
B3h3m0th <behemoth@autistici.org>
2012-02-12 19:39:06 +0100

    Verifica del tempo con time()

--- System-Data-Files-and-Information/07_time_and_date_1_time.c
+++ System-Data-Files-and-Information/07_time_and_date_1_time.c
@@ -3,10 +3,13 @@
 #include <errno.h>
 #include <string.h>
 #include <time.h>
+#include <unistd.h>
 
 /*
 Calendar time: il numero di secondi a partire dalle 00:00 del 01 gennaio 1970
-ITC, Epoch; indica il tempo su a cui l'orologio del kernel fa riferimento.
+ITC, Epoch; indica il tempo a cui l'orologio del kernel fa riferimento, usato
+generalmente per le date dei file oppure per il calcolo di tempi con precisione
+di 1 secondo (troppo in realta' se si volesse calcolare un pezzo di codice).
 
 HEADER    : <time.h>
 PROTOTYPE : time_t time(time_t *tm);
@@ -18,15 +21,24 @@ RETURNS   : Il valore di time in n caso di successo, -1 in caso di errore
 */
 
 int main(int argc, char *argv[]) {
-   time_t current_time;
+   time_t start, end;
 
-   if (time(&current_time) < 0) {
-      fprintf(stderr, "Err.(%s) getting time\n", strerror(errno));
+   if ((start = time(&start)) < 0) {
+      fprintf(stderr, "Err.(%s) getting time, start\n", strerror(errno));
       exit(EXIT_FAILURE);
    }
 
-   printf("Dalle 00:00 del 01 gennaio 1970 sono trascorsi ");
-   printf("%ld secondi\n", current_time);
+   /* Sospendo l'esecuzione del programma per circa 1 secondo e mezzo, espresso
+    in millisecondi */
+   usleep(1500000);
+
+   if ((end = time(&end)) < 0) {
+      fprintf(stderr, "Err.(%s) getting time, end\n", strerror(errno));
+      exit(EXIT_FAILURE);
+   }
+
+   /* Come si potra' notare non sara' preciso il risultato */
+   printf("Tempo trascorso: %f secondi\n", difftime(start, end));
 
    return(EXIT_SUCCESS);
 }
