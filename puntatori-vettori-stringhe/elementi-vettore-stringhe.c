4a8d09ae2ab5287e774aa6afdac14ca15cc876a7
B3h3m0th <behemoth@autistici.org>
2012-02-15 11:01:25 +0100

    indentazione corretta

--- puntatori-vettori-stringhe/elementi-vettore-stringhe.c
+++ puntatori-vettori-stringhe/elementi-vettore-stringhe.c
@@ -8,19 +8,22 @@
 
 int main(int argc, char *argv[])
 {
-	char *array[] = {"campobasso","roma","napoli","venezia","verona","torino",
-		             "milano","genova","palermo","catania","bari","firenze"};
-	int x, y;
-
-	/* Calcola il numero degli elementi presenti nel vettore di stringhe */
-	int len = sizeof(array) / sizeof(char *);	
-
-
-	for (x=0; x<len; x++) {
-		for (y=0; y<=strlen(array[x]); y++)
-			printf("%c", array[x][y]);
-		printf("\n");
-	}
-
-	return(EXIT_SUCCESS);
+   char *array[] = {"campobasso",
+      "roma", "napoli", "venezia",
+      "verona","torino", "milano",
+      "genova","palermo","catania",
+      "bari","firenze"};
+   
+   int x, y;
+   
+   /* Calcola il numero degli elementi presenti nel vettore di stringhe */
+   int len = sizeof(array) / sizeof(char *);	
+   
+   for (x=0; x<len; x++) {
+      for (y=0; y<=strlen(array[x]); y++)
+      	 printf("%c", array[x][y]);
+      printf("\n");
+   }
+   
+   return(EXIT_SUCCESS);
 }
