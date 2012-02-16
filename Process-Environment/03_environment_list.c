56f7d76573ece580d67fc27cd06cc39929fb4225
B3h3m0th <behemoth@autistici.org>
2012-02-16 19:22:16 +0100

    environment list settata al login

--- Process-Environment/03_environment_list.c
+++ Process-Environment/03_environment_list.c
@@ -8,10 +8,15 @@ all'interno del quale ciascuna stringa (null-terminated) indica una variabile
 d'ambiente, nella forma "nome = valore"; ogni variabile d'ambiente contiene 
 specifiche informazioni sull'ambiente in cui il programma sta girando.
 
+Le variabili d'ambiente sono settate, generalmente, al login.
+
 E' possibile accedere alla lista delle variabili d'ambiente "environment list"
 mediante la variabile (globale) esterna 'environ', oppure utilizzando un terzo 
 argomento della funzione main(), envp[], altamente sconsigliato sia da ISO C 
 sia da POSIX.
+
+Vi sono diversi comandi che consentono di ottenere la lista delle variabili
+d'ambiente come 'env' o 'printenv'.
 */
 
 extern char **environ;
