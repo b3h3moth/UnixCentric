933d7d4e9f19c89b71b9b4920b969c4f244ef672
B3h3m0th <behemoth@autistici.org>
2012-02-19 19:42:45 +0100

    Studio della funzione wait()

---
+++ Process-Control/04_wait_1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+/*
+HEADER    : 
+PROTOTYPE : 
+SEMANTICS : 
+RETURNS   : 0 in caso di successo, -1 in caso di errore
+--------------------------------------------------------------------------------
+*/
+
+int main(int argc, char *argv[]) {
+
+   return(EXIT_SUCCESS);
+}
