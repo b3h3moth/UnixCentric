ba0c7800420b961b260f34555129703fdebf1b03
B3h3m0th <behemoth@autistici.org>
2012-02-29 17:11:05 +0100

    Aggiunto il terminatore della lista (char *)0

--- Process-Control/06_exec_functions_2_execl.c
+++ Process-Control/06_exec_functions_2_execl.c
@@ -7,14 +7,15 @@
 
 /*
 HEADER    : <unistd.h>
-PROTOTYPE : int execl(const char *path, const char *arg, ...);
+PROTOTYPE : int execl(const char *path, const char *arg0, ...);
 SEMANTICS : La funzione execl() esegue il programma 'path', a tale programma
-            puo' essere passara la lista di argomenti arg0 ... argN.
+            puo' essere passata la una lista di argomenti arg0
 RETURNS   : 0 in caso di successo, -1 in caso di errore
 --------------------------------------------------------------------------------
 Nota: La desinenza 'l' nel nome della funzione indica che il programma puo' 
-ricevere una lista di argomenti nella forma arg0, arg1, ... argN; l'eseguibile
-va cercato nel PATH indicato da 'path'.
+ricevere una lista di argomenti nella forma arg0, arg1, ... argN, tale lista 
+deve terminare con un puntatore nullo, possibilmente nella forma '(char *)0'; 
+l'eseguibile va cercato nel PATH indicato da 'path'.
 */
 
 /* Il programma stampa in output la lista dei file della directory corrente
@@ -29,14 +30,16 @@ int main(int argc, char *argv[]) {
 	    exit(EXIT_FAILURE);
 
 	case 0:
-	    if (execl("/bin/ls", "ls","-l",(char *)0) < 0) {
+	    if (execl("/bin/ls", "ls", "-l", (char *)0) < 0) {
 	    	fprintf(stderr, "Err.(%s) execl() failed\n", strerror(errno));
 		exit(EXIT_FAILURE);
 	    }
 
+	    exit(EXIT_SUCCESS);
+
 	default:
 	    waitpid(pid, NULL, 0);
-	    printf("padre: Lista del figlio completata\n");
+	    printf("In esecuzione il padre\n");
     }
 
    return(EXIT_SUCCESS);
