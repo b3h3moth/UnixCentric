674f831c788a4e7dd335bff9386232afe0c41c18
B3h3m0th <behemoth@autistici.org>
2012-03-01 13:53:06 +0100

    Nuova sezione sul controllo d'accesso

--- Process-Control/01_process_identifiers.c
+++ Process-Control/01_process_identifiers.c
@@ -44,6 +44,51 @@ Nota: Le due funzioni getpid() e getppid() servono per acquisire il PID del
 current process e il PID del parent process, ossia il PPID, questo perche' tutti
 i processi conservano, oltre al proprio PID anche il PID del genitore. 
 
+Controllo d'accesso:
+Un sistema unix-like e' basato su fondamenti di sicurezza imprescindibili,
+anzitutto vi e' una netta differenziazione tra il superuser (root o 
+amministratore) che gode del pieno controllo del sistema e gli utenti che 
+inevitabilmente hanno diverse restrizioni, vi e' inoltre il concetto di "utente"
+e di "gruppo".
+
+Il sistema associa un identificatore univoco ad ogni utente e gruppo, 
+lo user-ID (UID) e il group-ID (GID) rispettivamente, essi servono al kernel per
+identificare uno specifico utente o un gruppo di utenti, e grazie ad essi 
+verificare se godono delle autorizzazioni necessarie per eseguire i compiti 
+richiesti.
+
+Poiche' tutte le operazioni del sistema sono eseguite da uno o piu' processi,
+risulta ovvio che per poter fornire un controllo su tali operazioni e' 
+necessario essere a conoscenza dell'utente che ha lanciato il programma, ragion
+per cui anche un processo deve avere i propri UID e GID.
+
+I sistemi unix-like forniscono per lo meno due gruppi di identificatori:
+- real      , real user-ID / real group-ID
+              Sono impostati al login al valore dell'utente e del gruppo con cui
+	      si accede al sistema. Solitamente non vengono cambiati, potrebbe
+	      farlo tuttavia solo un processo che gode dei privilegi di 
+	      superuser, poiche' identificano l'utente.
+- effective , effective user-ID / effective group-ID
+              Ai due si aggiunge anche l'effective GID di altri eventuali gruppi
+	      di appartenenza.
+	      Sono utilizzati nelle verifiche dei permessi del processo e per il
+	      controllo d'accesso ai file.
+
+Nota: solitamente real ed effective sono identici, tranne nel caso in cui il
+      programma in esecuzione avesse i bit 'suid' o 'sgid' impostati, in tal
+      caso gli effective saranno impostati al nome dell'utente e del gruppo 
+      proprietari del file. Questo e' il caso in cui un programma puo' essere
+      eseguito da un utente con privilegi di superuser (o altri). Puo' essere
+      un serio problema di sicurezza.
+              
+I sistemi con kernel Linux ne aggiungono altri due:
+- saved     , saved user-ID / saved group-ID 
+              Solo se _POSIX_SAVED_IDS e' impostato.
+	      Sono copie dell'effective User-ID ed effettive group-ID del 
+	      processo padre, impostati da una delle funzioni exec all'avvio
+	      del processo. 
+- filesystem, Estensione di sicurezza NFS.
+
 Da notare infine che il parent process del processo corrente risulta essere la 
 shell in cui si esegue il programma.
 */
@@ -59,5 +104,5 @@ int main(int argc, char *argv[]) {
 
    return(EXIT_SUCCESS);
 }
-/* L'effettiva valenza e utilita0 di ciascuna funzione sara' estremamente chiara
+/* L'effettiva valenza e utilita' di ciascuna funzione sara' estremamente chiara
 negli esempi successivi a questo, andando avanti nello studio dei processi */
