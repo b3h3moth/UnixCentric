24bb2cff02c2a809953b2acb00f18d1f30285f1e
B3h3m0th <behemoth@autistici.org>
2012-03-06 22:07:18 +0100

    find and replace strings

---
+++ puntatori-vettori-stringhe/find_and_replace_strings/find_word_in_text_1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string.h>
+
+char *string_replace(char *original, char *pattern, char *replacement);
+
+int main(int argc, char *argv[]) {
+    
+    printf("%s\n", string_replace("Hello ciao", "ciao", "world"));
+
+    return(EXIT_SUCCESS);
+}
+
+char *string_replace(char *original, char *pattern, char *replacement)
+{
+  static char buffer[4096];
+  char *p_tmp;
+
+  /* Se non vi sono corrispondenze sara' ritornata la stringa patterninale */
+  if(!(p_tmp = strstr(original, pattern)))  
+    return original;
+
+  strncpy(buffer, original, p_tmp-original); 
+  buffer[p_tmp-original] = '\0';
+
+  sprintf(buffer+(p_tmp-original), "%s%s", replacement, p_tmp+strlen(pattern));
+
+  return buffer;
+}
