40453fc31e4d7e62cd7a82cc1ec8e4231db4a1bb
B3h3m0th <behemoth@autistici.org>
2012-03-19 18:54:22 +0100

    Aggiunta SIGINT e test esempio

--- Signals/02_signal.c
+++ Signals/02_signal.c
@@ -26,9 +26,10 @@ Nota: si e' utilizzata la typedef per rendere il prototipo della funzione
       meno ostico da leggere ed interpretare, il tipo di dato sighandler_t e'
       una estensione GNU delle glibc. Il prototipo della funzione pertanto e'
       caratterizzato da due argomenti, l'intero 'signum' e un puntatore ad una
-      funzione void che riceve come argomento un intero. Il secondo argomento
-      della funzione signal() e' definito "signal handler" - gestore del 
-      segnale.
+      funzione void che riceve come argomento un intero.
+
+Il kernel puo' essere informato su una funzione deputata al trattamento del
+segnale, tale funzione e' definita signal handler.
 
 'signum' e' il nome del segnale, mentre il valore della funzione handler puo' 
 essere: 
@@ -41,36 +42,76 @@ di "signal-catching function" - gestore del segnale o funzione per la cattura
 di un segnale.
 
 La funzione restituisce l'indirizzo dell'azione precedente, che puo' essere
-salvato per poi essere riutilizzato in un secondo tempo.
-
+salvato per poi essere riutilizzato in un secondo tempo, ovvero quando il 
+processo torna dalla funzione di gestione del segnale, ritorna proprio nel 
+punto in cui era avvenuta l'eccezione.
 */
 
-static void sig_usr(int);
+static void signal_handler(int signum);
 
+/* Semplice programma per l'intercettazione di 3 segnali, SIGUSR1, SIGUSR2 e
+SIGINT. Una volta compilato lo si esegue in background:
+$ ./a.out &
+[1] 5960
+
+Con il comando kill si inviano i primi due segnali:
+$ kill -USR1 5960
+Ricevuto il segnale: SIGUSR1 
+$ kill -USR2 5960
+Ricevuto il segnale: SIGUSR2
+
+Ora si porta il programma da background a foreground, e poiche' vi e' un ciclo 
+infinito il programma restera' in esecuzione, in attesa di essere terminato; 
+si prova a terminarlo con la combinazione di tasti Control-C
+$ fg %1
+./a.out
+^C Ricevuto il segnale: SIGINT
+^C Ricevuto il segnale: SIGINT
+
+Come si puo' ben notare il segnale SIGINT (^C) e' stato intercettato, a questo 
+punto per terminarlo non resta altro da fare che stopparlo nuovamente
+con Control-Z, dopodiche' terminarlo con il comando kill e il segnale SIGKILL:
+
+^Z
+[1]+ Stopped ./a.out
+$ kill -9 5960
+
+Nota: D'ora in avanti per indicare, ad esempio, Control-C, si utilizzera' ^C,
+      molto piu' chiaro.
+*/
 int main(int argc, char *argv[]) {
     
-    if (signal(SIGUSR1, sig_usr) == SIG_ERR) {
+    if (signal(SIGUSR1, signal_handler) == SIG_ERR) {
     	fprintf(stderr, "Err.(%s), cant' catch SIGUSR1\n", strerror(errno));
 	exit(EXIT_FAILURE);
     }
 
-    if (signal(SIGUSR2, sig_usr) == SIG_ERR) {
+    if (signal(SIGUSR2, signal_handler) == SIG_ERR) {
     	fprintf(stderr, "Err.(%s), cant' catch SIGUSR2\n", strerror(errno));
 	exit(EXIT_FAILURE);
     }
 
-    for(;;)
+    if (signal(SIGINT, signal_handler) == SIG_ERR) {
+    	fprintf(stderr, "Err.(%s), cant' catch SIGINT\n", strerror(errno));
+	exit(EXIT_FAILURE);
+    }
+
+    /* Il programma va in loop in modo tale da poter eseguire il segnale 
+    nel momento opportuno. */
+    while(1)
     	pause();
 
     return(EXIT_SUCCESS);
 }
 
-static void sig_usr(int signo)
+static void signal_handler(int signum)
 {
-    if (signo == SIGUSR1)
+    if (signum == SIGUSR1)
     	printf("Ricevuto il segnale: SIGUSR1\n");
-    else {
+    else if (signum == SIGUSR2)
+    	printf("Ricevuto il segnale: SIGUSR2\n");
+    else if (signum == SIGINT)
+    	printf("Ricevuto il segnale: SIGINT\n");
+    else
     	abort();
-	exit(EXIT_FAILURE);
-    }
 }
