f63db84c94edf13d2915d046556930d5d47046f1
Luca Suriano <behemoth@autistici.org>
2012-10-06 18:54:01 +0200

    openssl key and iv

---
+++ crittografia/openssl_bt.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <openssl/evp.h>
+
+/* Compila con:
+$ gcc bt.c -lcrypto 
+
+Genere le chiavi, visibili mediante key e iv.
+*/
+
+
+int main(int argc, char *argv[]) {
+    int i;
+    const EVP_CIPHER *cipher;
+    const EVP_MD *dgst = NULL;
+    unsigned char key[EVP_MAX_KEY_LENGTH], iv[EVP_MAX_IV_LENGTH];
+    const char *password = "password";
+    const unsigned char *salt = NULL;
+
+    OpenSSL_add_all_algorithms();
+
+    cipher = EVP_get_cipherbyname("aes-256-cbc");
+
+    if(!cipher) { 
+        fprintf(stderr, "Err.: No cipher\n"); 
+        return(EXIT_FAILURE);
+    }
+
+    dgst = EVP_get_digestbyname("md5");
+    
+    if(!dgst) { 
+        fprintf(stderr, "Err.: no such digest\n"); 
+        return(EXIT_FAILURE);
+    }
+
+    if(!EVP_BytesToKey(cipher, dgst, salt, (unsigned char*)password, 
+                       strlen(password), 1, key, iv)) {
+        fprintf(stderr, "Err.: EVP_BytesToKey failed\n");
+        return(EXIT_FAILURE);
+    }
+
+    printf("Key: "); 
+    for(i=0; i<cipher->key_len; ++i) { 
+        printf("%02x", key[i]); 
+    } printf("\n");
+    
+    printf("IV: "); 
+    for(i=0; i<cipher->iv_len; ++i) { 
+        printf("%02x", iv[i]); 
+    } printf("\n");
+
+    return(EXIT_SUCCESS);
+}
+
+/* Su debian GNU/Linux ho dovuto installare il pacchetto libssl-dev */
