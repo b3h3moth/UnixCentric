2f1d56af69ab09a494e1ee11cc220f08c6a40ef6
B3h3m0th <behemoth@autistici.org>
2012-11-27 13:40:49 +0100

    pthread_join(), esempio pratico, aggiunte studio della funzione

--- Thread/04_thread_join_and_cleanup_1.c
+++ Thread/04_thread_join_and_cleanup_1.c
@@ -5,7 +5,20 @@
 #include <pthread.h>
 #include <unistd.h>
 
-/* 
+#define MAX_VALUE 5
+
+/* La funzione pthread_join() si comporta come se fosse una funzione della 
+famiglia wait(), concenttualmente intendo, nel senso che se il main() dovvesse 
+terminare prima del thread, le strutture dati del thread sarebbero deallocate 
+con il thread stesso ancora in esecuzione, per cui e' necessario un meccanismo 
+che consenta di far attendere al main() la terminazione del thread.
+
+Il compito della funzione pthread_join() e' di terminare il thread corrente fino
+a quando il thread specificato non cessa l'esecuzione, essa inoltre unifica due
+thread in un unico thread, e' come se il nuovo ramo di esecuzione fosse 
+nuovamente riunito nel ramo principale, questo meccanismo e' estremamente 
+importante.
+
 HEADER    : <pthread.h>
 PROTOTYPE : int pthread_join(pthread_t thread, void **retval);
 SEMANTICS : La funzione pthread_join() attende la terminazione del thread 
@@ -15,12 +28,42 @@ RETURNS   : 0 in caso di successo, numero di errore in caso di errore
 --------------------------------------------------------------------------------
 */
 
+void *thr_func(void *arg);
+
 int main(int argc, char *argv[], char *envp[]) {
     pthread_t thrID;
-    int err;
+    int thr_err;
     
-    err = pthread_create(&thrID, NULL, &print, NULL);
+    if ((thr_err = pthread_create(&thrID, NULL, &thr_func, NULL)) != 0) {
+        fprintf(stderr, "Err. pthread_create() (%s)\n", strerror(thr_err));
+        exit(EXIT_FAILURE);
+    }
 
+    printf("Main Thread: %lu\n", pthread_self());
+    
+    /* Negli esempi precedenti e' stata inserita una sleep() per consentire
+    l'esecuzione di entrambi i thread, ora invece si una la funzione 
+    pthread_join() per far si che il main attenda l'esecuzione del nuovo thread;
+    in sostanza tale chiamata manda in sleep() il main() in attesa che la 
+    funzione thr_func() possa essere completamente eseguita, e non appena cio'
+    accadra' pthread_join() ritornera' e il programma avra' nuovamente un solo
+    thread. */
+    if (pthread_join(thrID, NULL) != 0) {
+        fprintf(stderr, "Err. pthread_join() (%s)\n", strerror(thr_err));
+        exit(EXIT_FAILURE);
+    }
 
-   return(EXIT_SUCCESS);
+    return(EXIT_SUCCESS);
+}
+
+void *thr_func(void *arg)
+{
+    int i = 0;
+    do {
+        sleep(1);
+        printf(" New Thread: %lu\n", pthread_self());
+        i++;
+    } while (i < MAX_VALUE);
+
+    return ((void*)0);
 }
