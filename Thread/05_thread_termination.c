d5d0bcad7f7e5b8b093a249ddb8c7c5fab6850cd
B3h3m0th <behemoth@autistici.org>
2012-11-27 20:13:21 +0100

    pthread_exit() info sulla terminazione dei thread

--- Thread/05_thread_termination.c
+++ Thread/05_thread_termination.c
@@ -6,16 +6,20 @@
 #include <pthread.h>
 
 /* Se un thread all'interno di un processo dovesse invocare exit(), _Exit(), 
-oppure _exit(), l'intero processo sara' terminato, per cui e' necessario un 
-meccanismo meno invasivo di terminazione; i thread contemplano 3 modalita' di 
-terminazione del loro flusso di esecuzione, senza peraltro terminare anche il 
-processo:
+oppure _exit(), l'intero processo sarebbe terminato, e questo non va bene
+perche' thread e processi dovrebbero essere indipendenti per cio' che concerne
+lo stato di uscita.
+
+I thread comtemplano un meccanismo di terminazaione meno invasivo rispetto alle
+funzioni precedenti, supportano infatti tre modalita' di terminazione del loro 
+flusso di esecuzione, senza peraltro terminare anche il processo:
+
 1 - Un thread semplicemente ritorna dalla routine di avvio;
 2 - Un thread puo' essere cancellato da un altro thread nello stesso processo;
 3 - Un thread puo' invocare la funzione pthread_exit().
 
-
-
+La funzione pthread_exit() termina il thread chiamante, ritornando, volendo, lo
+stato di terminazione.
 
 HEADER    : <pthread.h>
 PROTOTYPE : void pthread_exit(void *ret_val);
