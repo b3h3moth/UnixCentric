c5f1cda31f7f909e16b7babcd1cd81d354fe7ea7
B3h3m0th <behemoth@autistici.org>
2012-11-30 18:50:16 +0100

    accenno alla mutua esclusione, pthread_mutex_

--- Thread/06_dynamic_thread.c
+++ Thread/06_dynamic_thread.c
@@ -9,6 +9,7 @@
 /* variabile globale condivisa tra i thread */
 int global_var = 10;
 
+
 void *thr_func(void *thr_num);
 
 /* Il programma riceve come argomento il numero di thread da creare
@@ -47,6 +48,35 @@ int main(int argc, char *argv[]) {
 void *thr_func(void *thr_num)
 {
     pthread_t tid;
+
+    /* Attenzione, questo non e' il modo corretto, si potrebbe incorrere in
+    risultati inaspettati incrementando in tal modo una variabile globale; deve
+    essere chiaro che i thread lavorano in multitasking, in simultanea - certo,
+    su una macchina monoprocessore sara' una simulazione di multitasking, ma 
+    comunque ci sara' -, per cui e' possibile che uno o piu' thread leggano o 
+    scrivano un valore non corretto, o meglio, non congruo,
+    
+    Servono dei meccanismi specifici che consentano a ciascun thread di lavorare
+    in assoluta sicurezza sui dati, senza la preoccupazione che altri thread nel
+    frattempo possano modificarli, nel caso specifico la variabile globale 
+    'global_var' sarebbe dovuta essere stata modificata da ogni thread in mutua
+    esclusione. 
+    
+    A Titolo di esempio, il problema sarebbe risolto con l'utilizzo delle
+    funzioni della famiglia pthread_mutex_, in particolare con la variabile
+    globale:
+
+    pthread_mutex_t my_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+    e nel thread:
+    
+    pthread_mutex_lock(&my_mutex);
+    global_var++;
+    pthread_mutex_unlock(&my_mutex);
+
+    Ma non e' ancora il momento di parlare di mutua esclusione; per cui si
+    compili questo programma e si faccia attenzione ai risultati non congrui.
+    */
     global_var++;
 
     if ((tid = syscall(SYS_gettid)) == -1) { 
