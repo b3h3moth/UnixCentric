4e87bf51d2cb7e0284b12c6273a9a43e2ed7b1c2
B3h3m0th <behemoth@autistici.org>
2012-12-04 13:07:09 +0100

    rename files

---
+++ Thread/05_thread_termination_2.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+
+struct date  
+
+void *thr_func(void *thr_num);
+
+int main(void) {
+    pthread_t thrID1, thrIH2;
+    int thr_err;
+    struct foo *fp;
+
+    /* Si provvede alla creazione di MAX_THREAD thread */
+    for (i=0; i<MAX_THREAD; i++) {
+        
+        if ((thr_err = pthread_create(&thr[i],NULL, thr_func, (void*)i)) != 0) {
+            fprintf(stderr, "Err. pthread_create() %s\n", strerror(thr_err));
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    for (i=0; i<MAX_THREAD; i++) {
+        if (pthread_join(thr[i], NULL) != 0) {
+            fprintf(stderr, "Err. pthread_join() %s\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+    }
+    
+    return(EXIT_SUCCESS);
+}
+
+void *thr_func(void *thr_num)
+{
+    pthread_t tid;
+
+    if ((tid = syscall(SYS_gettid)) == -1) { 
+        fprintf(stderr, "Err. syscall() %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    printf("thread '%d' - TID %lu - Address 0x%x\n", 
+            (int)thr_num, tid, (unsigned int)tid);
+
+    /* La funzione pthread_exit() termina il thread chiamante */
+    pthread_exit((void*)0);
+}
