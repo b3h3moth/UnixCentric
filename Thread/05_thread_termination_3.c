be9c261bda8f431eb2c95045b57feefea058b6e2
B3h3m0th <behemoth@autistici.org>
2012-12-10 19:12:26 +0100

    migliorato l'output

--- Thread/05_thread_termination_3.c
+++ Thread/05_thread_termination_3.c
@@ -10,12 +10,15 @@ struct data {
     int var_a, var_b, var_c, var_d;
 };
 
-void print_data(const char *str, const stuct data *fp);
+void print_data(const char *str, const struct data *fp);
 void *thr_func1(void *arg);
 void *thr_func2(void *arg);
 
 void *thr_func(void *thr_num);
 
+/* Il programma mostra l'utilizzo di variabili allocate sullo stack usate come
+argomento della funzione pthread_exit() e come lo stack del secondo thread 
+venga sovrascritto dallo stack del primo thread. */
 int main(void) {
     pthread_t thrID1, thrID2;
     int thr_err;
@@ -33,7 +36,7 @@ int main(void) {
 
     sleep(1);
 
-    printf("Il genitore avvia il secondo thread\n");
+    printf("\nDal main si crea un nuovo thread\n\n");
 
     if ((thr_err = pthread_create(&thrID2, NULL, thr_func2, NULL)) != 0) {
         fprintf(stderr, "Err. pthread_create() 2 - %s\n", strerror(thr_err));
@@ -42,31 +45,31 @@ int main(void) {
 
     sleep(1);
 
-    printf("Genitore:\n", fp);
+    print_data("\n", fp);
     
     return(EXIT_SUCCESS);
 }
 
-void print_data(const char *str, const stuct data *fp)
+void print_data(const char *str, const struct data *fp)
 {
     printf(str);
-    printf(" struttura a 0x%x\n", (unsigned)fp);
-    printf(" data.var_a = %d\n", fp->var_a);
-    printf(" data.var_b = %d\n", fp->var_b);
-    printf(" data.var_c = %d\n", fp->var_c);
-    printf(" data.var_d = %d\n", fp->var_d);
+    printf("    struttura: 0x%x\n", (unsigned)fp);
+    printf("               data.var_a = %d\n", fp->var_a);
+    printf("               data.var_b = %d\n", fp->var_b);
+    printf("               data.var_c = %d\n", fp->var_c);
+    printf("               data.var_d = %d\n", fp->var_d);
 }
 
 void *thr_func1(void *arg)
 {
     struct data data = {1,2,3,4};
-    printf("thread 1:\n", &data);
+    print_data("thread 1:\n", &data);
 
     pthread_exit((void *)&data);
 }
 
 void *thr_func2(void *arg)
 {
-    printf("thread 2: ID %d\n", pthread_self());
+    printf("thread 2: ");
     pthread_exit((void *)0);
 }
