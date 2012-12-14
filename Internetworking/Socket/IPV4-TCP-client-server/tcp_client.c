6a8be80c209c723bc3d4dcc50c0f8b68e1333dc3
B3h3m0th <behemoth@autistici.org>
2012-12-14 18:50:33 +0100

    echo client completato

--- Internetworking/Socket/IPV4-TCP-client-server/tcp_client.c
+++ Internetworking/Socket/IPV4-TCP-client-server/tcp_client.c
@@ -9,13 +9,16 @@
 #include <arpa/inet.h>
 
 #define DEF_SERVER_PORT 7
+#define BUF_SIZE        512
 
 /* Implementazione di un echo client TCP per IPV4 */
 
 int main(int argc, char *argv[]) {
-    int sock;
-    int ret_val;
-    struct sockaddr_in server_address;
+    int sock;                               /* Il socket */
+    int ret_val;                            /* L'indirizzo convertito */
+    struct sockaddr_in server_address;      /* l'indirizzo del server */
+    ssize_t num_bytes;                      /* Il numero di byte inviati */
+    unsigned int total_byte_receveid = 0;   /* Il totale dei byte ricevuti */
 
     if (argc < 3 || argc > 4) {
         fprintf(stderr, "Uso: %s <indirizzo server> <echo> [<porta server>]\n",
@@ -28,6 +31,9 @@ int main(int argc, char *argv[]) {
     
     /* Secondo argomento: Stringa echo da inviare al server */
     char *echo_string = argv[2];
+
+    /* Si calcola la lunghezza della stringa di echo, sara' utile in seguito */
+    size_t echo_string_len = strlen(echo_string);
 
     /* Terzo argomento opzionale: La porta del server.
     Se la porta del server viene fornita la si converte in intero mediante la
@@ -49,8 +55,8 @@ int main(int argc, char *argv[]) {
     /* Si provvede al riempimento della struttura server_address: */
     server_address.sin_family = AF_INET;    /* Famiglia indirizzi IPv4 */
 
-    /* Conversioni degli indirizzi, la funzione pton() converte un indirizzo
-    IPv4 o IPv6 da testuale - notazione puntata - a binario 32 bit*/
+    /* Conversioni degli indirizzi, la funzione inet_pton() converte un 
+    indirizzo IPv4 o IPv6 da testuale - notazione puntata - a binario 32 bit */
     if ((ret_val = inet_pton(AF_INET, server_IP,
                     &server_address.sin_addr.s_addr)) == 0) {
         fprintf(stderr, "Err. inet_pton() str. invalida %s\n", strerror(errno));
@@ -63,15 +69,55 @@ int main(int argc, char *argv[]) {
     /* Conversione da host a network byte order (big-endian) */
     server_address.sin_port = htons(server_port);
 
-    /* Si stabilisce la connessine con il server echo */
+    /* Si stabilisce la connessione con il server echo */
     if (connect(sock, (struct sockaddr*)&server_address, 
                 sizeof(server_address)) < 0) {
         fprintf(stderr, "Err. connect() %s\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
 
+    /* Si provvede ad inviare la stringa al server mediante la funzione send(),
+    che in caso di esito positivo ritornera' il numero di byte inviati */
+    if ((num_bytes = send(sock, echo_string, echo_string_len, 0)) < 0) {
+        fprintf(stderr, "Err. send() fallita %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    } else if (num_bytes != echo_string_len) {
+        fprintf(stderr, "Err. send() %s numero byte errato\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
 
+    /* Il client riceve la stessa stringa inviata al server  */
+    fputs("Ricevuti: ", stdout);
 
+    while (total_byte_receveid < echo_string_len) {
+        char buf[BUF_SIZE];         /* I/O buffer */
+
+        /* Mediante la funzione recv() si ottiene la grandezza del buffer -1 per
+        il carattere nullo di terminazione stringa; per cui si sta ottenendo il
+        numero dei byte ricevuti */
+        if ((num_bytes = recv(sock, buf, BUF_SIZE -1, 0)) < 0) {
+            fprintf(stderr, "Err. recv() fallita %s\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        } else if (num_bytes == 0) {
+            fprintf(stderr, "Err. recv() conn. chiusa %s\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+
+        /* Si calcola il totale dei byte ricevuti */
+        total_byte_receveid += num_bytes;
+        
+        /* Si aggiunge il carattere nullo di terminazione stringa */
+        buf[num_bytes] = '\0';
+        
+        /* Si stampa sullo stdout la stringa ottenuta */
+        fputs(buf, stdout);
+    }
+
+    /* Si aggiunge l'ultimo carattere linefeed */
+    fputc('\n', stdout);
+
+    /* Si chiude il socket */
+    close(sock);
 
     return(EXIT_SUCCESS);
 }
