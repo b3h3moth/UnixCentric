acf026563dc2d3b70593f7c615686b6111650b42
B3h3m0th <behemoth@autistici.org>
2012-12-30 17:39:06 +0100

    info struttura protoent

--- Internetworking/Socket/02_getprotobyname.c
+++ Internetworking/Socket/02_getprotobyname.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 #include <sys/socket.h>
 #include <netdb.h> 
 
@@ -19,31 +20,37 @@ int main(int argc, char *argv[]) {
     /* Informazioni sul protocollo */
     struct protoent *proto_info;
     
-    if (argc < 2) {
+    if (argc != 2) {
     	fprintf(stderr, "Uso: %s <protocollo>\n", argv[0]);
 	exit(EXIT_FAILURE);
     }
     
     /* getprotobyname() restituisce un puntatore ad una struttura protoent
-    contenente campi con informazioni inerenti il protocollo */
-    proto_info = getprotobyname(argv[1]);
+    contenente campi con informazioni inerenti il protocollo, la lista con
+    tali informazioni e' collocata in /etc/protocols.
+
+    struct protoent {
+        char    *p_name;        il nome ufficiale del protocollo
+        char    **p_aliases;    lista di alias
+        int     p_proto;        numero del protocollo
+    } */
     
-    if (proto_info == NULL) {
-    	fprintf(stderr, "Err. getprotobyname()\n");
+    if ((proto_info = getprotobyname(argv[1])) == NULL) {
+        fprintf(stderr, "Err. getprotobyname()\n");
 	exit(EXIT_FAILURE);
     }
     
-    printf("Informazioni circa il protocollo:\n");
-    printf("nome: %s\nalias %s\nnumero protocollo %d\n", 
-    	    proto_info->p_name, *proto_info->p_aliases, proto_info->p_proto);
+    printf("Informazioni su \'%s\'\n"
+            "- nome: %s\n- alias: %s\n- numero protocollo: %d\n", argv[1],
+            proto_info->p_name, 
+            *proto_info->p_aliases, 
+            proto_info->p_proto);
     
     protocollo = proto_info->p_proto;
     
     /* Creazione della socket */
-    skt = socket(dominio, tipo, protocollo);
-    
-    if (skt < 0) {
-    	fprintf(stderr, "Err. socket()\n");
+    if ((skt = socket(dominio, tipo, protocollo)) < 0) {
+        fprintf(stderr, "Err. socket() - %s\n", strerror(errno));
 	exit(EXIT_FAILURE);
     }
     
