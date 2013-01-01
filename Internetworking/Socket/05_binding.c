d5fbc4c2e6ff9daa36121805b64166c2f407b4bc
B3h3m0th <behemoth@autistici.org>
2013-01-01 19:07:37 +0100

    funzione bind(), esempio di binding

--- Internetworking/Socket/05_binding.c
+++ Internetworking/Socket/05_binding.c
@@ -5,12 +5,16 @@
 #include <unistd.h>
 #include <netdb.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 
-/* Quando un socket e' creato mediante la funzione socket() non ancora gli
-viene assegnato un indirizzo, tale compito e' cura della funzione bind(),
-l'associazione tra socket ed indirizzo prende il nome di binding.
+#define PORT 7000
+#define SEC 30
 
-HEADER    : <sys/socket.h>
+/* Quando un socket e' creato mediante la funzione socket() non ancora gli si
+assegna un indirizzo, tale compito e' cura della funzione bind(), l'associazione
+tra socket ed indirizzo di trasporto prende il nome di binding.
+
+HEADER    : <sys/socket.h> <sys/types.h>
 PROTOTYPE : int bind(int sockfd, const struct sockaddr *addr, socklen_t len);
 SEMANTICS : La funzione bind() assegna l'indirizzo 'addr' al socket 'sockfd',
             'len' definisce la lunghezza dell'indirizzo puntato da 'addr'.
@@ -18,6 +22,44 @@ RETURNS   : 0 in caso di successo, -1 in caso di errore
 --------------------------------------------------------------------------------
 */
 
+/* Il programma associa un socket alla porta PORT  */
 int main(int argc, char *argv[]) {
+    struct sockaddr_in address;     /* Ipv4 internet address */
+    struct protoent *pr;            
+    int sock;
+
+    if (argc != 2) {
+        fprintf(stderr, "Uso: %s <protocollo>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+   
+    /* Si definisce il protocollo da adoperare */ 
+    if ((pr = getprotobyname(argv[1])) == NULL) {
+        fprintf(stderr, "Err. getprotobyname()\n");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Si crea il socket */
+    if ((sock = socket(AF_INET, SOCK_STREAM, pr->p_proto)) < 0) {
+        fprintf(stderr, "Err. socket() - %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    /* Si stabilisce l'indirizzo di trasporto con cui fare l'associazione;
+    se non si intende assegnare una specifica interfaccia si puo' utilizzare
+    INADDR_ANY.  */
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(sock, (void*)&address, sizeof(address)) < 0) {
+        fprintf(stderr, "Err. bind() - %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    sleep(SEC);
+
     return(EXIT_SUCCESS);
 }
+
+/* Utilizzando netstat si potra' verificare l'allocazione del socket */
