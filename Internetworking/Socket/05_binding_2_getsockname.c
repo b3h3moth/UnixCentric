f1c3a625e930f9917e22ebff1a5c37ee9e7f7379
B3h3m0th <behemoth@autistici.org>
2013-01-01 19:58:38 +0100

    binding

---
+++ Internetworking/Socket/05_binding_2_getsockname.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#define PORT 7000
+#define SEC 30
+
+/* Per capire a quale indirizzo di trasporto e' associato un socket, e'
+possibile utilizzare la funzione getsockname():
+
+HEADER    : <sys/socket.h>
+PROTOTYPE : int getsockname(int sockfd, struct sockaddr *addr, socklen_t *len);
+SEMANTICS : La funzione getsockname() restituisce l'indirizzo corrente al quale
+            il socket 'sockfd' e' associato, nel buffer puntato da 'addr'. 'len'
+            sara' inizializzato con la dimensione in byte dell'indirizzo del
+            socket.
+RETURNS   : 0 in caso di successo, -1 in caso di errore
+--------------------------------------------------------------------------------
+*/
+
+/* Il programma associa un socket ad una porta e ne stampa il numero  */
+int main(int argc, char *argv[]) {
+    struct sockaddr_in address;     /* Ipv4 internet address */
+    struct protoent *pr;            
+    int sock;
+
+    if (argc != 2) {
+        fprintf(stderr, "Uso: %s <protocollo>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+   
+    /* Si definisce il protocollo da adoperare */ 
+    if ((pr = getprotobyname(argv[1])) == NULL) {
+        fprintf(stderr, "Err. getprotobyname()\n");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Si crea il socket */
+    if ((sock = socket(AF_INET, SOCK_STREAM, pr->p_proto)) < 0) {
+        fprintf(stderr, "Err. socket() - %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    /* Si stabilisce l'indirizzo di trasporto con cui fare l'associazione;
+    se non si intende assegnare una specifica interfaccia si puo' utilizzare
+    INADDR_ANY.  */
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(sock, (void*)&address, sizeof(address)) < 0) {
+        fprintf(stderr, "Err. bind() - %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    sleep(SEC);
+
+    return(EXIT_SUCCESS);
+}
+
+/* Utilizzando netstat si potra' verificare l'allocazione del socket */
