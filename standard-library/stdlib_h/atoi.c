f40c4415be07194cdb63240736d46d321aec6739
B3h3m0th <behemoth@autistici.org>
2013-01-22 11:41:59 +0100

    Ottimizzazione atoi(), studio ed esempio

--- standard-library/stdlib_h/atoi.c
+++ standard-library/stdlib_h/atoi.c
@@ -2,20 +2,36 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_STR 32
+/*
+HEADER    : <stdlib.h>
+PROTOTYPE : int atoi(const char *str);
+SEMANTICS : La funzione atoi() converte la porzione iniziale della stringa 
+            puntata da 'str' in un intero.
+RETURNS   : La stringa convertita in intero
+--------------------------------------------------------------------------------
+*/
 
-int main(int argc, char **argv, char **envp) {
-   char buf[MAX_STR];
+int main(int argc, char *argv[]) {
+   char buf[] = "31/03/1973";
    int value;
-   strcpy(buf,"31337standard library 38928");
 
-   printf("%s\n", buf);
+   if (argc != 2) {
+       fprintf(stderr, "Uso: %s <intero>|<intero>+<stringa>\n", argv[0]);
+       exit(EXIT_FAILURE);
+   }
 
-   /* atoi() converte la porzione iniziale di una stringa in un intero
-	*/
+   printf("     Stringa iniziale: %s\n", buf);
+
    value = atoi(buf);
-   printf("%d\n", value);
+   printf("Stringa convertita in: %d\n", value);
 
+   /* Molto utile allorquando si deve lavorare coi parametri */
+   int choice;
+
+   choice = atoi(argv[1]);
+
+   printf("   Parametro iniziale: %s\n", argv[1]);
+   printf(" Parametro convertito: %d\n", choice);
 
    return(EXIT_SUCCESS);
 }
