949464b1a4ae06688c0e7b9e54dedffffd1af3ed
B3h3m0th <behemoth@autistici.org>
2013-01-29 22:17:02 +0100

    manutenzione: permessi file system

--- APIs_and_Libraries/libconfig/libconfig_intro.c
+++ APIs_and_Libraries/libconfig/libconfig_intro.c
@@ -28,6 +28,50 @@ $ gcc main.c `pkg-config --cflags --libs libconfig` -o main
 "pkg-config" e' una utility unix molto potente, ma soprattutto utile e semplice
 da usare.
 
+File di configurazione
+----------------------
+Il file di configurazione per la lettura e la scrittura consiste in gruppi di 
+impostazioni (settings) gerarchici e strutturati, nei quali si 
+associano coppie nome/valore. 
+
+Il valore puo' essere uno dei seguenti tipi di dato:
+- Uno scalare;
+- Un array;
+- Un gruppo, che consiste in una collezione di impostazioni;
+- Una lista, ossia una sequenza di valori di ciascun tipo.
+
+Esempio (negozio, libreria, libri, etc, sono le impostazioni):
+
+negozio
+{
+    libreria
+    {
+        libri 
+        {
+            titolo = "Libconfig programming tutorial";
+            autore = "B3h3m0th";
+            anno   = 2013;
+        };
+    };
+
+    pizzeria
+    {
+        pizze_da_asporto
+        {
+            { nome = "margherita";
+              prezzo = 4.00; }
+            { nome = "capricciosa";
+              prezzo = 4.50; }
+            { nome = "4 stagioni";
+              prezzo = 5.00; }
+        };
+    };
+};
+-------------------------------------------------------------------------------
+
+Un path identifica ciascuna impostazione, e' espresso dalla sequenza dei nomi
+iniziando dall'alto; ad esempio il path di autore e' :
+negozio.libreria.libri.autore
 
 */
 
