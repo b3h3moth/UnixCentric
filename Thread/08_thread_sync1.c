68c6540dd8b66fd1a266812822e583c4df27f09e
B3h3m0th <behemoth@autistici.org>
2013-04-23 19:12:05 +0200

    consistenza, migliorie

--- Thread/08_thread_sync1.c
+++ Thread/08_thread_sync1.c
@@ -4,8 +4,8 @@
 #include <pthread.h>
 
 /* Allorquando si lavora con thread multipli si possono verificare dei casi in
-cui alcuni di essi possano condividere la stessa variabile, e in tali contesti 
-la gestione della consistenza dei dati e' fondamentale.
+cui alcuni di essi condividano la stessa variabile, ed e' proprio in tali 
+contesti che la gestione della consistenza dei dati e' fondamentale.
 
 Cos'e' la consistenza dei dati? Poniamo il caso di avere due thread che 
 condividono la stessa variabile in sola lettura, in questo specifico ambito vi 
@@ -14,6 +14,9 @@ invece uno dei due thread avesse accesso alla variabile anche in scrittura e
 dovesse modificarne il valore, la variabile stessa si troverebbe in uno stato 
 inconsistente, perche' il thread in sola lettura leggerebbe prima un valore e 
 poi un altro. Vi dev'essere coerenza per ciascun thread.
+
+Per risolvere il problema si deve disporre di un meccanismo di lock, che
+consenta ad un thread per volta la modifica dei dati.
 
 HEADER    : 
 PROTOTYPE : 
