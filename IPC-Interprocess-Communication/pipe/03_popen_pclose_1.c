2c7fd3434442df29c350683163f5936a15352f2f
B3h3m0th <behemoth@autistici.org>
2013-04-30 18:52:54 +0200

    messaggio d'errore, correzione in pipe()

---
+++ IPC-Interprocess-Communication/pipe/03_popen_pclose_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+/* Il lavoro fatto dalla funzione pipe() viene svolto interamente dalle funzioni
+popen() e pclose().
+
+
+HEADER    : <stdio.h>
+PROTOTYPE : FILE *popen(const char *cmdstring, const char *type);
+SEMANTICS : 
+RETURNS   : Un puntatore in caso di successo, NULL in caso di errore
+--------------------------------------------------------------------------------
+*/
+
+int main(int argc, char *argv[]) {
+    return(EXIT_SUCCESS);
+}
