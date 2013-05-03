461505c801dcf7c59bc7165510b87813d8b5e9ec
B3h3m0th <behemoth@autistici.org>
2013-05-03 19:22:45 +0200

    semantica della funzione popen()

--- IPC-Interprocess-Communication/pipe/03_popen_pclose_1.c
+++ IPC-Interprocess-Communication/pipe/03_popen_pclose_1.c
@@ -8,11 +8,17 @@ popen() e pclose().
 
 HEADER    : <stdio.h>
 PROTOTYPE : FILE *popen(const char *cmdstring, const char *type);
-SEMANTICS : 
+SEMANTICS : La funzione popen() apre una pipe, crea un nuovo processo mediante
+            una fork ed esegue 'cmdstring' su una exec e ritorna un puntatore 
+            al file I/O standard; se 'type' equivale a r (read) il puntatore 
+            sara' connesso allo standard output di 'cmdstring', se 'type' sara'
+            w il puntatore sara' connesso allo standard input di 'cmdstring'.
 RETURNS   : Un puntatore in caso di successo, NULL in caso di errore
 --------------------------------------------------------------------------------
 */
 
 int main(int argc, char *argv[]) {
+
+
     return(EXIT_SUCCESS);
 }
