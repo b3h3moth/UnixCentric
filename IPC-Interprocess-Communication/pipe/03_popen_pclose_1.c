8468c55c30a02f03de441a93dd9caef37f6aced4
B3h3m0th <behemoth@autistici.org>
2013-05-03 19:45:08 +0200

    pipe: esempio completo su popen() e pclose()

--- IPC-Interprocess-Communication/pipe/03_popen_pclose_1.c
+++ IPC-Interprocess-Communication/pipe/03_popen_pclose_1.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <errno.h>
 
+#define PAGER "${PAGER:-less}"
+#define MAX_BUF 512
+
 /* Il lavoro fatto dalla funzione pipe() viene svolto interamente dalle funzioni
 popen() e pclose().
 
@@ -18,7 +21,44 @@ RETURNS   : Un puntatore in caso di successo, NULL in caso di errore
 */
 
 int main(int argc, char *argv[]) {
+    char buf[MAX_BUF];
+    FILE *fp_in, *fp_out;
 
+    if (argc != 2) {
+        fprintf(stderr, "Uso: %s <pathname>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    /* Apertura di un file in letura */
+    if ((fp_in = fopen(argv[1], "r")) == NULL) {
+        fprintf(stderr, "Err.: %d fopen() %s\n", errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    /* Crea un pipe, definendo il programma da aprire */
+    if ((fp_out = popen(PAGER, "w")) == NULL) {
+        fprintf(stderr, "Err.: %d popen() %s\n", errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    /* Copia il contenuto del file passato ad argv[1] al pager */
+    while (fgets(buf, MAX_BUF, fp_in) != NULL) {
+        if (fputs(buf, fp_out) == EOF) {
+            fprintf(stderr, "Err.: %d fputs() %s\n", errno, strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    /* Verifica degli errori */
+    if (ferror(fp_in)) {
+        fprintf(stderr, "Err.: %d ferror() %s\n", errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    if (pclose(fp_out) == -1) {
+        fprintf(stderr, "Err.: %d pclose() %s\n", errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
 
     return(EXIT_SUCCESS);
 }
