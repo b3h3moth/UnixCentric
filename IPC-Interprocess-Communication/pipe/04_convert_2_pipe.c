f2cd1b27da446a2fd412fe3ab38032168961b5f3
B3h3m0th <behemoth@autistici.org>
2013-05-07 16:48:10 +0200

    info sul programma che invochera' convert via pipe

---
+++ IPC-Interprocess-Communication/pipe/04_convert_2_pipe.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+/* Il programma invoca un altro programma - convert -mediante una pipe, che 
+utilizzera' per la conversione di caratteri */
+   
+int main(int argc, char *argv[]) {
+
+    return(EXIT_SUCCESS);
+}
