dda40e88913fbfa9b61ea8a1f333cb53f7623956
B3h3m0th <behemoth@autistici.org>
2013-05-07 17:03:37 +0200

    pipe che invoca convert

--- IPC-Interprocess-Communication/pipe/04_convert_2_pipe.c
+++ IPC-Interprocess-Communication/pipe/04_convert_2_pipe.c
@@ -5,10 +5,41 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+#define MAX_BUF 512
+
 /* Il programma invoca un altro programma - convert -mediante una pipe, che 
 utilizzera' per la conversione di caratteri */
    
 int main(int argc, char *argv[]) {
+    char buf[MAX_BUF];
+    FILE *fp;
+
+    /* Apertura della pipe e invocazione del programma convert */
+    if ((fp = popen("convert", "r")) == NULL) {
+        fprintf(stderr, "Err.: %d popen() - %s\n", errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    for ( ; ; ) {
+        fputs("> ", stdout);
+        fflush(stdout);
+
+        /* Lettura dalla pipe */
+        if (fgets(buf, MAX_BUF, fp) == NULL)
+            break;
+
+        if (fputs(buf, stdout) == EOF) {
+            fprintf(stderr, "Err.: %d fputs() - %s\n", errno, strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if (pclose(fp) == -1) {
+        fprintf(stderr, "Err.: %d pclose() - %s\n", errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    putchar('\n');
 
     return(EXIT_SUCCESS);
 }
