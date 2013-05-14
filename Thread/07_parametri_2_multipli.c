1d907bf63f74c86e242539851e555326eeed1a71
B3h3m0th <behemoth@autistici.org>
2013-05-14 18:58:23 +0200

    scopo del programma, info

---
+++ Thread/07_parametri_2_multipli.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <pthread.h>
+
+/* Allorquando gli argomenti da passare sono piu' d'uno e' consigliabile
+utilizzare una struttura in cui archiviare i dati; il programma infatti passa 
+multipli argomenti mediante una struttura e ciascun thread ne ricevera' un'unica
+istanza. */
+
+int main(int argc, char *argv[]) {
+
+    return(EXIT_SUCCESS);
+}
