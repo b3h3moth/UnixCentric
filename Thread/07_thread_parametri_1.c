a4a87f6a2bf54d1b0f14e09253183ed97dbfe90b
B3h3m0th <behemoth@autistici.org>
2013-05-14 13:29:46 +0200

    parametri thread_create(), esempio basico

--- Thread/07_thread_parametri_1.c
+++ Thread/07_thread_parametri_1.c
@@ -3,21 +3,68 @@
 #include <errno.h>
 #include <pthread.h>
 
+#define MAX_THREAD  10
+
+char *msg[MAX_THREAD];
+
 /* La funzione pthread_create() consente di passare uno o piu' argomenti alla
 funzione che consente l'avvio di un thread, ci sono tuttavia alcuni accorgimenti
 da considerare; anzitutto il caso piu' semplice, ossia un solo argomento, caso 
 in cui non ci sono particolari avvertenze, se invece gli argomenti dovessero 
 essere piu' d'uno si dovrebbe creare una struttura e all'interno della quale 
-inserire i vari argomenti, infine passare l'intera struttura come argomento, 
-questo faciliterebbe e di molto le cose.
-
-HEADER    : 
-PROTOTYPE : 
-SEMANTICS : 
-RETURNS   : 0 in caso di successo, -1 in caso di errore
---------------------------------------------------------------------------------
+inserire i vari argomenti, infine passare un puntatore all'intera struttura come
+argomento della funzione pthread_create().
 */
 
+void *thr_func(void *thr_id);
+
 int main(int argc, char *argv[]) {
+    pthread_t thread[MAX_THREAD];
+    int *msgs[MAX_THREAD];
+    int thr_ret, i;
+
+    msg[0] = "Assembly programming";
+    msg[1] = "C++ Programming";
+    msg[2] = "C Programming";
+    msg[3] = "Python Programming";
+    msg[4] = "Perl Programming";
+    msg[5] = "Ruby Programming";
+    msg[6] = "Shell Programming";
+    msg[7] = "GCC Compiling";
+    msg[8] = "GDB Debugging";
+    msg[9] = "Network programming";
+
+    for (i=0; i<MAX_THREAD; i++) {
+        msgs[i] = (int *)malloc(sizeof(int));
+        *msgs[i] = i;
+
+        printf("Creazione thread: %d\n", i);
+
+        thr_ret = pthread_create(&thread[i], NULL, thr_func, (void *)msgs[i]);
+        if (thr_ret != 0) {
+            fprintf(stderr, "Err. pthread_create() %s\n", strerror(thr_ret));
+            exit(EXIT_FAILURE);
+        }
+
+        if ((thr_ret = pthread_join(thread[i], NULL)) != 0) {
+            fprintf(stderr, "Err. pthread_join() %s\n", strerror(thr_ret));
+            exit(EXIT_FAILURE);
+        }
+    }
+
     return(EXIT_SUCCESS);
+}
+
+void *thr_func(void *thr_id)
+{
+    sleep(1);
+
+    int *p_id, n_msg;
+
+    p_id = (int *) thr_id;
+    n_msg = *p_id;
+
+    printf("Thread %d: %s\n", n_msg, msg[n_msg]);
+
+    pthread_exit(NULL);
 }
