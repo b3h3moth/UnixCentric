199cfebb4f15ccefb963fb06c226407f02865ad4
B3h3m0th <behemoth@autistici.org>
2013-06-08 18:06:16 +0200

    puntatori, intro

---
+++ puntatori/01_variabile_puntatore.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    /* Una variabile puntatore contiene l'indirizzo di una locazione di memoria
+    */
+    int n = 0;
+    int *ptr = &n;
+
+    /* ptr e' una variabile puntatore ad un intero 
+
+    Nota: Si consiglia di leggere le dichiarazioni di puntatori da destra a
+    sinistra. */
+
+    return(EXIT_SUCCESS);
+}
