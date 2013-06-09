783ef8a8ee64d6303e3f279757d4b9a66b6f6e4d
B3h3m0th <behemoth@autistici.org>
2013-06-09 18:32:18 +0200

    aritmetica puntatori, sottrazione e comparazione

--- puntatori/07_aritmetica_dei_puntatori.c
+++ puntatori/07_aritmetica_dei_puntatori.c
@@ -13,7 +13,8 @@ int main(int argc, char *argv[]) {
     double  8 byte
     */
 
-    /* Sommare un intero a un puntatore */
+    /* Sommare un intero a un puntatore 
+       --------------------------------*/
     printf("Somma\n");
     int vec[] = {98, 16, 24};
     int *ptr = vec; /* Non serve l'operatore di indirizzo perche' in questo
@@ -32,8 +33,9 @@ int main(int argc, char *argv[]) {
     p += 3; /* Il puntatore ora punta a se stesso */
     printf("\n%p v[3]\n%p p", &v[3], p);
 
-    /* Sottrarre un intero da un puntatore */
-    printf("\n\nSottrazione\n");
+    /* Sottrarre un intero da un puntatore 
+       -----------------------------------*/
+    printf("\n\nSottrazione tra intero e puntatore\n");
     int vect[] = {12, 56, 78};
     int *pt = vect + 2; /* Si fa puntatore all'ultimo elemento del vettore */
 
@@ -42,6 +44,26 @@ int main(int argc, char *argv[]) {
     printf("%d\n", *pt);
     pt -= 1;                /* 4 byte indietro */
     printf("%d\n", *pt);
+
+    /* Sottrarre puntatore a puntatore 
+       -------------------------------*/
+    printf("\n\nSottrazione  puntatori\n");
+    int *pt0 = vect;
+    int *pt1 = vect + 1;
+    int *pt2 = vect + 2;
+
+    printf("%d\n%d\n%d\n", pt2-pt0, pt2-pt1, pt0-pt1);
+
+    /* Confronto tra punatori 
+       ---------------------- 
+    Si lavora sul confronto tra gli indici dell'array - 0 false, 1 true */
+    printf("\nConfronto tra puntatori\n");
+    printf("pt0 > pt1 : %d\n", pt0 > pt1);
+    printf("pt0 < pt1 : %d\n", pt0 < pt1);
+    printf("pt2 > pt1 : %d\n", pt2 > pt1);
+    printf("pt2 < pt1 : %d\n", pt2 < pt1);
+    printf("pt0 > pt2 : %d\n", pt0 > pt1);
+    printf("pt0 < pt2 : %d\n", pt0 < pt1);
 
     return(EXIT_SUCCESS);
 }
