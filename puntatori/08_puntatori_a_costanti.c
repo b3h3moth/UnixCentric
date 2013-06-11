1e4838d7e59952070f6dad1f7df33e92297064ae
B3h3m0th <behemoth@autistici.org>
2013-06-11 10:25:28 +0200

    puntatore a costante, regole

---
+++ puntatori/08_puntatori_a_costanti.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    /* E' perfettamene lecito definire un puntatore a costante */
+    int num = 327;
+    int *ptr;
+    const int cnum = 89;
+    const int *cptr;    /* Puntatore a costante di tipo intero */
+
+    /* Si assegna l'indirizzo della variabile intera num a ptr */
+    ptr = &num;
+    
+    /* Si assegna l'indirizzo della costante intera cnum a cptr */
+    cptr = &cnum;
+
+    /* La dereferenziazione di un puntatore costante e' ammessa in sola lettura,
+    non e' lecito tuttavia la modifica del valore dereferenziato:
+    
+    *cptr = 200; 
+    Produrrebbe un errore in fase di compilazione: 
+    assignment of read-only location ‘*cptr’
+    */
+    printf("%d\n", *cptr);
+
+    /* Il valore del puntatore non e' costante, esso infatti puo' essere 
+    associato ad un'altra locazione di memoria. */
+    cptr = &num;
+    printf("%d\n", *cptr);
+
+    /* Regole per il puntatore costante 'cptr':
+    - Puo' puntatore a diversi valori interi costanti;
+    - Puo' puntatore a diversi valori interi non costanti;
+    - Puo' essere dereferenziato in sola lettura;
+    - Non puo' essere dereferenziato per la modifica del valore.
+    */
+    
+    return(EXIT_SUCCESS);
+}
