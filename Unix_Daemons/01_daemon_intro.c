b4e33929e077f190dca5cfa53f0e55349affd2f2
B3h3m0th <behemoth@autistici.org>
2013-06-13 18:50:32 +0200

    unix daemon; nuova sessione con setsid()

--- Unix_Daemons/01_daemon_intro.c
+++ Unix_Daemons/01_daemon_intro.c
@@ -3,6 +3,7 @@
 #include <errno.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/stat.h>
 
 /* I demoni sono processi che godono di una vita media sensibilmente maggiore 
 rispetto agli altri processi, solitamente sono eseguiti subito dopo la fase di 
@@ -20,11 +21,11 @@ sorta di caratteristica del sistema e sono scritti seguendo regole ben
 definite:
 
 - Creazione di un processo e chiusura del padre (fork);
-- Settaggio della maschera di creazione di file e directory (umask);
-- Creazione di una sessione (setsid);
+- Settaggio della maschera di creazione dei permessi dei file (umask);
+- Creazione di una nuova sessione per il processo figlio (setsid);
 - Cambiamento della working directory (chdir);
 - Chiusura di tutti i file descriptor;
-- Apertura del log per la scrittura;
+- Gestione del log (syslog);
 
 */
 
@@ -38,11 +39,23 @@ int main(void) {
 
 void daemonize(void)
 {
-    pid_t pid;
+    pid_t pid, sid;
 
-    /* Si provvede alla creazione di un nuovo processo, il padre tuttavia sara'
-    subito terminato in modo tale da lasciare in esecuzione solo e soltanto il
-    processo figlio. */
+    /* Si provvede ad una verifica iniziale, ovvero se la funzione getppid()
+    restituira' 1 vorra' dire che il PPID del programma in esecuzione sara'
+    init con PID 1 per l'appunto, e quindi si potra' proseguire tranquillamente,
+    questo perche' ciascun demone ha come genitore proprio init. E' una sorta
+    di verifica iniziale. */
+    
+    if (getppid() == 1) {
+        puts("init");
+        return;
+    } else
+        puts("ahi ahi ani");
+
+    /* E' necessario creare un nuovo processo, il padre tuttavia sara' subito 
+    terminato in modo tale da lasciare in esecuzione solo e soltanto il processo
+    figlio. */
 
     if ((pid = fork()) < 0)
         exit(EXIT_FAILURE);
@@ -50,17 +63,29 @@ void daemonize(void)
     if (pid > 0) 
         exit(EXIT_SUCCESS);
 
-    /* Se il programma prosegue a questo punto e' in esecuzione solo il 
-    processo figlio, ed e' proprio cio' che si desidera nei demoni. */
+    /* A questo punto del programma dovrebbe essere in esecuzione solo il 
+    processo figlio, cosi come si prevede bei demoni. */
 
     /* Quando un utente crea un file o una directory, essi sono settati con i
     permessi di default, tali permessi tuttavia possono essere modificati 
-    mediante la maschera di creazione dei file (file mode creation mask).
+    mediante la maschera di creazione dei permessi dei file.
     
     Per cio' che concerne un demone e' assolutamente necessario poter leggere
     e scrivere sui file, per cui impostando il valore di umask a 0 si avra' 
-    l'accesso completo alle risorse (file e directory) da parte del demone. */
+    l'accesso completo alle risorse da parte del demone. */
+    
     umask(0);
+
+    /* Il processo figlio, unico processo in esecuzione, deve ottenere un SID
+    (Session ID o ID di sessione) dal kernel per poter continuare l'esecuzione
+    come demone; si invoca la funzione setsid() che consente la creazione di una
+    nuova sessione e il processo chiamante, in questo caso il processo figlio,
+    diventera' leader della nuova sessione. */
+
+    if ((sid = setsid()) < 0) {
+        fprintf(stderr, "Err. %d setsid() %s\n", errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
 
     /* Numero massimo di file descriptors 
     if (getrlimit(RLIMIT_NOFILE, &rl) < 0) {
@@ -70,20 +95,12 @@ void daemonize(void)
     */
 
    /* Diventa leader di sessione */
-   if ((pid = fork()) < 0) {
-      puts("errore fork");
-      exit(-1);
-   } 
-   
-   /* Si chiude il parent process */
-   if (pid != 0)
-      exit(0);
-   
-   /* Si crea una nuova sessione */
+   /* Si crea una nuova sessione
    if ((sid = setsid()) < 0) {
       puts("erreo setsid");
       exit(-1);
    } 
+   */
 
 /* Si evita il terminale di controllo  
    sa.sa_handler = SIG_IGN;
@@ -101,11 +118,12 @@ void daemonize(void)
       exit(0);
 */
 
-   /* Si cambia la directory corrente con l'attuale */
+   /* Si cambia la directory corrente con l'attuale
    if (chdir("/") < 0) {
        puts("chdir error");
        exit(-1);
    }
+   */
   
 /* Si chiudono tutti i fd aperti
    if (rl.rlim_max == RLIM_INFINITY)
@@ -114,16 +132,18 @@ void daemonize(void)
        close(i);
 */
 
-   /* Si attaccano i fd 0,1,2 a /dev/null */
+   /* Si attaccano i fd 0,1,2 a /dev/null
    fd0 = open("/dev/null", O_RDWR);
    fd1 = dup(0);
    fd2 = dup(0);
+   */
 
-   /* Si inizializza il file di log */
+   /* Si inizializza il file di log
    openlog(cmd, LOG_CONS, LOG_DAEMON);
    if (fd0 != 0 || fd1 != 1 || fd2 != 2) {
        syslog(LOG_ERR, "fd inaspettato %d %d %d", fd0, fd1, fd2);
        exit(1);
    }
+   */
 
 }
