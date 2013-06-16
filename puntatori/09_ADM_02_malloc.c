76607cb77b55df9b633e68db1dee5c35133360a4
B3h3m0th <behemoth@autistici.org>
2013-06-16 11:06:52 +0200

    concat() concatena due stringhe, malloc() esempio

--- puntatori/09_ADM_02_malloc.c
+++ puntatori/09_ADM_02_malloc.c
@@ -1,55 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 
-/*
- * L'allocazione dinamica della memoria è usato di continuo con le stringhe.
- *
- * Prototipo malloc:
- *
- * void *malloc(size_t size);
- * 
- * Alloca un blocco di size byte di tipo size_t, ossia un intero senza segno, 
- * restituendo un puntatore a quest'ultimo.
- * 
- * Per allocare una stringa, in un certo senso, il lavoro è facilitato, poichè
- * per si dovrà utilizzare un char che corrisponde esattamente a 1byte, per cui,
- * una stringa di n caratteri, corrisponderà esattamente a sizeof(char) * n.
- *
- * Da non dimenticare di aggiungere un byte per il carattere di fine stringa \0.
- */
+/* L'allocazione dinamica della memoria è usata di continuo con le stringhe, in
+tal caso peraltro il lavoro sara' facilitato, poichè si dovrà utilizzare un char
+che corrisponde esattamente a 1 byte, per cui, una stringa di N caratteri sara'
+sizeof(char) * N; da non dimenticare di aggiungere un byte per il carattere di 
+fine stringa \0. */
 
-/*
- * Come esempio si crea una funzione che concatena due stringhe
- */
 
-char *concat(const char *s1, const char *s2);
+/* La funzione concat() concatena due stringhe */
+char *concat(const char *str1, const char *str2);
+
+/* Libera la memoria occupata dalle stringhe concatenate */
 void freeMemory(char *block);
 
-int main(int argc, char *argv[])
-{
-   char *stringa = concat("Learning"," C-ANSI (C99)");
-   printf("%s\n", stringa);
-   freeMemory(stringa);
-   printf("%s\n", stringa);
+int main(void) {
+   char *full_string = concat("Learning"," C-ANSI (C99)");
 
+   printf("%s\n", full_string);
+   
+   freeMemory(full_string);
 
    return(EXIT_SUCCESS);
 }
 
-char *concat(const char *s1, const char *s2)
+char *concat(const char *str1, const char *str2)
 {
    char *result;
 
-   result = malloc(strlen(s1) + strlen(s2) +1);
+   result = malloc(strlen(str1) + strlen(str2) +1);
 
    if (result == NULL) {
-      printf("Error: malloc failed in concat\n");
+      fprintf(stderr, "Err.:%d - malloc() %s\n", errno, strerror(errno));
       exit(EXIT_FAILURE);
    }
 
-   strcpy(result, s1);
-   strcat(result, s2);
+   strcpy(result, str1);
+   strcat(result, str2);
 
    return result;
 }
@@ -58,4 +47,5 @@ void freeMemory(char *block)
 {
    printf("-- FREE --\n");
    free(block);
+   block = NULL;
 }
