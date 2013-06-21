5a68130c14785bb4a6a5c854e6c343ac114abeea
B3h3m0th <behemoth@autistici.org>
2013-06-21 19:25:49 +0200

    dangling pointer, ulteriori specifiche

--- puntatori/12_free.c
+++ puntatori/12_free.c
@@ -46,9 +46,11 @@ int main(void) {
     p1 = (int*) malloc(sizeof(int));
     *p1 = 100;
     p2 = p1;
-    free(p1); 
-    *p2 = 100;
-    printf("p2 = %d\n", *p2);
+    free(p1); /* Si rilascia la memoria ... di entrambi, poiche' p2 = p1 */
+    *p2 = 100; /* Dangling pointer */
+    printf("p2 = %d\n", *p2);   /* Potrebbe anche essere visualizzato il valore
+                                   corretto, tuttavia resta comunque un dato
+                                   non conforme */
 
     return(EXIT_SUCCESS);
 }
