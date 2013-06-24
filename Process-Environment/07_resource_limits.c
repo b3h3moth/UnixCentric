1b5d96214f031ae041da0319b3910b91aa567f71
B3h3m0th <behemoth@autistici.org>
2013-06-24 20:08:06 +0200

    struttura rlimit

--- Process-Environment/07_resource_limits.c
+++ Process-Environment/07_resource_limits.c
@@ -9,9 +9,16 @@ proprie risorse, in particolare la funzione getrlimit() viene utilizzata per
 l'ottenimento delle informazioni mentre la funzione setrlimit() e' deputata
 al settaggio.
 
+Entrambe le funzioni puntano alla struttura rlimit:
+struct rlimit {
+    rlim_t rlim_cur;    // limite corrente; soft limit
+    rlim_t rlim_max;    // limite massimo; hard limit
+};
+
 HEADER    : <sys/resource.h>
 PROTOTYPE : int getrlimit(int resource, struct rlimit *prl);
-SEMANTICS : la funzione getrlimit() 
+SEMANTICS : la funzione getrlimit() ottiene informazioni su 'resource', che
+            punta alla struttura di tipo rlimit *prl.
 RETURNS   : 0 in caso di successo, non zero in caso di errore
 --------------------------------------------------------------------------------
 */
