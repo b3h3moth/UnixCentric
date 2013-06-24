fe6c837e53483245819e914ed6949be7371f4081
B3h3m0th <behemoth@autistici.org>
2013-06-24 20:35:17 +0200

    resource limits; RLIMIT_* macro

--- Process-Environment/07_resource_limits.c
+++ Process-Environment/07_resource_limits.c
@@ -27,8 +27,33 @@ SEMANTICS : La funzione setrlimit() setta i limiti di 'resource', che
             punta alla struttura di tipo rlimit *prl.
 RETURNS   : 0 in caso di successo, non zero in caso di errore
 --------------------------------------------------------------------------------
+resource puo' essere uno dei seguenti valori:
+- RLIMIT_AS - Massimo valore in byte della memoria;
+- RLIMIT_CORE - Massimo valore in byte del core file;
+- RLIMIT_CPU - Massimo ammontare del tempo di CPU in secondo;
+- RLIMIT_DATA - Massimo valore in byte del segmento dati o data segment;
+- RLIMIT_FSIZE - Massimo valore in byte di un file da creare;
+- RLIMIT_LOCKS (Linux 2.4 solo) - Massimo numero di file lock per processo;
+- RLIMIT_MEMLOCK - Massimo ammontare di memoria che un processo puo' bloccare
+                   in memoria usando  mlock();
+- RLIMIT_MSGQUEUE (Da Linux 2.6.8) - Limite massimo in byte per una coda di 
+                                     messaggi per un Real User ID del processo
+                                     chiamante;
+- RLIMIT_NICE (Da Linux 2.6.12) - valore di nice per processo;
+- RLIMIT_NOFILE - Massimo numero di file aperti per processo;
+- RLIMIT_NPROC - Massimo numero di processi figli per Real User ID;
+- RLIMIT_RSS - Limite massimo RSS;
+- RLIMIT_RTPRIO (Da Linux 2.6.12) - Limite massimo priorita' in real time;
+- RLIMIT_RTTIME (Da Linux 2.6.25) - Limite in microsecondi dell'ammontare del
+                                    tempo di CPU che un processo schedulato in
+                                    real time puo' consumare semza essere 
+                                    bloccato da una system call;
+- RLIMIT_SIGPENDING (Da Linux 2.6.8) - Limite sul numero dei segnali che puo'
+                                       essere messo in cosa per un Real user ID
+                                       del processo chiamante;
+- RLIMIT_STACK - Massimo valore in byte dello stack;
+- RLIMIT_VNEM - Sinonimo di RLIMIT_AS.
 */
-
 int main(int argc, char *argv[]) {
     return(EXIT_SUCCESS);
 }
