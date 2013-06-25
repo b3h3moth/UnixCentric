473c2696cfa4dbbb9e24e15010907dca7a40be94
B3h3m0th <behemoth@autistici.org>
2013-06-25 11:02:23 +0200

    resource limits; esempio completo

--- Process-Environment/07_resource_limits.c
+++ Process-Environment/07_resource_limits.c
@@ -2,7 +2,17 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#if defined(BSD) || defined(MACOS)
+#include <sys/time.h>
+#define FMT	"%10lld  "
+#else
+#define FMT	"%10ld  "
+#endif
 #include <sys/resource.h>
+
+#define	doit(name)	pr_limits(#name, name)
+
+static void	pr_limits(char *, int);
 
 /* Ciascun processo e' in grado sia di ottenere sia di settare i limiti delle
 proprie risorse, in particolare la funzione getrlimit() viene utilizzata per
@@ -54,6 +64,58 @@ resource puo' essere uno dei seguenti valori:
 - RLIMIT_STACK - Massimo valore in byte dello stack;
 - RLIMIT_VNEM - Sinonimo di RLIMIT_AS.
 */
-int main(int argc, char *argv[]) {
-    return(EXIT_SUCCESS);
+
+int main(void) {
+#ifdef	RLIMIT_AS
+    doit(RLIMIT_AS);
+#endif
+    doit(RLIMIT_CORE);
+    doit(RLIMIT_CPU);
+    doit(RLIMIT_DATA);
+    doit(RLIMIT_FSIZE);
+#ifdef  RLIMIT_LOCKS
+    doit(RLIMIT_LOCKS);
+#endif
+#ifdef	RLIMIT_MEMLOCK
+    doit(RLIMIT_MEMLOCK);
+#endif
+    doit(RLIMIT_NOFILE);
+#ifdef	RLIMIT_NPROC
+    doit(RLIMIT_NPROC);
+#endif
+#ifdef	RLIMIT_RSS
+    doit(RLIMIT_RSS);
+#endif
+#ifdef	RLIMIT_SBSIZE
+    doit(RLIMIT_SBSIZE);
+#endif
+    doit(RLIMIT_STACK);
+#ifdef	RLIMIT_VMEM
+    doit(RLIMIT_VMEM);
+#endif
+    exit(EXIT_SUCCESS);
+}
+
+static void pr_limits(char *name, int resource)
+{
+    struct rlimit	limit;
+
+    if (getrlimit(resource, &limit) < 0) {
+        fprintf(stderr, "Err. getrlimit() %s\n", name);
+        exit(EXIT_FAILURE);
+    }
+    
+    printf("%-14s  ", name);
+    
+    if (limit.rlim_cur == RLIM_INFINITY)
+        printf("(infinite)  ");
+    else
+        printf(FMT, limit.rlim_cur);
+    
+    if (limit.rlim_max == RLIM_INFINITY)
+        printf("(infinite)");
+    else
+        printf(FMT, limit.rlim_max);
+    
+    putchar((int)'\n');
 }
