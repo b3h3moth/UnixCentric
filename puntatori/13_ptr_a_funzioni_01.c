576a7f6227cc1aa2a7590bc13e0aa26687103a35
B3h3m0th <behemoth@autistici.org>
2013-07-08 20:04:54 +0200

    puntatori a funzione

---
+++ puntatori/13_ptr_a_funzioni_01.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* I puntatori a funzione sono utilizzati spesso per fornire menu', dove
+ciascuna opzione del menu' corrisponde ad una funzione. Le funzioni sono
+inserite in un vettore di puntatori a funzione. */
+
+int main(int argc, char *argv[]) {
+    return(EXIT_SUCCESS);
+}
