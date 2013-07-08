5914562c307e465af7e40fd125d94c8afc529c0d
B3h3m0th <behemoth@autistici.org>
2013-07-08 20:09:27 +0200

    prototipi voci menu'

--- puntatori/13_ptr_a_funzioni_01.c
+++ puntatori/13_ptr_a_funzioni_01.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Ogni funzione e' una voce del menu', accettano un argomento di tipo intero e
+restituisce void. */
+void voce1(int x);
+void voce2(int y);
+void voce3(int z);
+
 /* I puntatori a funzione sono utilizzati spesso per fornire menu', dove
 ciascuna opzione del menu' corrisponde ad una funzione. Le funzioni sono
 inserite in un vettore di puntatori a funzione. */
 
-int main(int argc, char *argv[]) {
+int main(void) {
+    /* Inizializzazione di un vettore di 3 puntatori a funzione, ciascuna di 
+    essere accetta un argomento di tipo intero e restituisce void
     return(EXIT_SUCCESS);
 }
