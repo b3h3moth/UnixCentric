85ea4e5dc5c5baf32c0d7c56c461106ef9e63b69
B3h3m0th <behemoth@autistici.org>
2013-10-05 18:54:08 +0200

    semantica della funzione mkfifo()

--- IPC_InterProcess_Communication/fifo/01_fifo.c
+++ IPC_InterProcess_Communication/fifo/01_fifo.c
@@ -10,11 +10,13 @@ vista come un file, per cui per utilizzarla possono essere adoperate le
 comuni funzioni per l'I/O di basso livello non bufferizzato (open, read, write,
 etc...).
 
-
+Creare una fifo e' come creare un file, essa infatti e' vista proprio come un
+file per cui puo' essere utilizzata da processi differenti.
 
 HEADER    : <sys/stat.h>
-PROTOTYPE : 
-SEMANTICS : 
+PROTOTYPE : int mkfifo(const char *pathname, mode_t mode);
+SEMANTICS : La funzione mkfifo() crea il file 'pathname' con modalita' di 
+            accesso definite da 'mode'.
 RETURNS   : 0 in caso di successo, -1 in caso di errore
 --------------------------------------------------------------------------------
 */
