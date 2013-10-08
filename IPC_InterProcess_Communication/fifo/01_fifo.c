0d663332cc7f00be6018e1bb80d8be62219bbb84
B3h3m0th <behemoth@autistici.org>
2013-10-08 17:53:58 +0200

    main() completo

--- IPC_InterProcess_Communication/fifo/01_fifo.c
+++ IPC_InterProcess_Communication/fifo/01_fifo.c
@@ -5,6 +5,9 @@
 #include <unistd.h>
 #include <sys/stat.h>
 
+#define BUF_SIZE    256
+#define PERMS   (S_IRUSR | S_IWUSR)
+
 /* Una fifo o altrimenti detta 'named pipe' - pipe con nome - a differenza di 
 una pipe e' identificata mediante un nome, gode di permessi specifici ed e' 
 vista a tutti gli effetti come fosse un file, per cui le  comuni funzioni per 
@@ -22,14 +25,37 @@ RETURNS   : 0 in caso di successo, -1 in caso di errore
 --------------------------------------------------------------------------------
 */
 
+
 int main(int argc, char *argv[]) {
     pid_t pid;
 
     /* Si verifica che vi sia un argomento fornito al programma */
     if (argc != 2) {
-        fprintf(stderr, "Uso: %s fifoname\n", argv[0]);
+        fprintf(stderr, "Uso: %s <nome della pipe>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
+
+    /* Creazione della FIFO o named pipe */
+    if (mkfifo(argv[1], PERMS) == -1) { 
+        fprintf(stderr, "Err: %d mkfifo()\n", errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    /* Creazione di un nuovo processo */
+    if ((pid = fork) == -1) {
+        fprintf(stderr, "Err: %d fork()\n", errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    if (pid == 0)
+        /* Processo figlio: si occupa di scrivere */
+        return fifo_child("Scrittura del processo figlio sulla FIFO");
+    else
+        /* Processo padre */
+        return fifo_parent(argv[1]);
+
+
+        
 
 
     return(EXIT_SUCCESS);
