cc5d56ba7b2facb6b6996e83ad1fb87dd6644877
B3h3m0th <behemoth@autistici.org>
2013-10-08 17:33:27 +0200

    Migliorata l'introduzione a mkfifo()

--- IPC_InterProcess_Communication/fifo/01_fifo.c
+++ IPC_InterProcess_Communication/fifo/01_fifo.c
@@ -4,14 +4,14 @@
 #include <string.h>
 #include <sys/stat.h>
 
-/* Una fifo e' chiamata talvolta 'named pipe' ossia 'pipe con nome', questo
-perche' a differenza di una pipe e' identificata mediante un nome, inoltre e'
-vista come un file, per cui per utilizzarla possono essere adoperate le
-comuni funzioni per l'I/O di basso livello non bufferizzato (open, read, write,
-etc...).
+/* Una fifo o altrimenti detta 'named pipe' - pipe con nome - a differenza di 
+una pipe e' identificata mediante un nome, gode di permessi specifici ed e' 
+vista a tutti gli effetti come fosse un file, per cui le  comuni funzioni per 
+l'I/O di basso livello non bufferizzato come open o read  possono essere 
+utilizzate.
 
-Creare una fifo e' come creare un file, essa infatti e' vista proprio come un
-file per cui puo' essere utilizzata da processi differenti.
+Creare una fifo e' come creare un file e puo' essere utilizzata da processi 
+differenti.
 
 HEADER    : <sys/stat.h>
 PROTOTYPE : int mkfifo(const char *pathname, mode_t mode);
@@ -21,6 +21,6 @@ RETURNS   : 0 in caso di successo, -1 in caso di errore
 --------------------------------------------------------------------------------
 */
 
-int main(int argc, char *argv[]) {
+int main(void) {
     return(EXIT_SUCCESS);
 }
