c98530cad1146d816abff29fdf427ee7cb278aa7
B3h3m0th <behemoth@autistici.org>
2013-10-08 18:27:39 +0200

    funzione fifo_child() completa, scrittura FIFO named pipe

--- IPC_InterProcess_Communication/fifo/01_fifo.c
+++ IPC_InterProcess_Communication/fifo/01_fifo.c
@@ -4,12 +4,13 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <fcntl.h>
 
 #define BUF_SIZE    256
 #define PERMS   (S_IRUSR | S_IWUSR)
 
-fifo_child(const char *file_name, const char *str);
-fifo_parent(const char *file_name);
+int fifo_child(const char *fifo_name, const char *id_str);
+int fifo_parent(const char *fifo_name);
 
 /* Una fifo o altrimenti detta 'named pipe' - pipe con nome - a differenza di 
 una pipe e' identificata mediante un nome, gode di permessi specifici ed e' 
@@ -48,7 +49,7 @@ int main(int argc, char *argv[]) {
     }
 
     /* Creazione di un nuovo processo */
-    if ((pid = fork) == -1) {
+    if ((pid = fork()) == -1) {
         fprintf(stderr, "Err: %d fork()\n", errno, strerror(errno));
         exit(EXIT_FAILURE);
     }
@@ -63,10 +64,51 @@ int main(int argc, char *argv[]) {
     return(EXIT_SUCCESS);
 }
 
-fifo_child(const char *file_name, const char *str)
+/* Lo scopo della funzione fifo_child() e' di consentire al processo figlio di
+scrivere su una named pipe. */
+int fifo_child(const char *fifo_name, const char *id_str)
 {
+    char buf[BUF_SIZE];
+    int fd, rval;
+    ssize_t str_size;
+
+    fprintf(stderr, "[%ld]: (processo figlio) apertura FIFO %s...", 
+            (long)getpid(), fifo_name);
+
+    while ((fd = open(fifo_name, O_WRONLY)) == -1) {
+        fprintf(stderr, "Err: %d open()\n", errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    /* Scrittura in buf del messaggio, compreso pid del processo */
+    rval = snprintf(buf, BUF_SIZE, "[%ld]:%s\n", (long)getpid(), id_str);
+
+    if (rval < 0) {
+        fprintf(stderr, "[%ld ]Err: scrittura stringa\n", (long)getpid());
+        exit(EXIT_FAILURE);
+    }
+
+    /* Calcolo della lunghezza del messaggio */
+    str_size = strlen(buf) + 1;
+
+    fprintf(stderr, "[%ld]: scrittura...", (long)getpid());
+
+    /* Si scrive 'buf' sulla FIFO */
+    if ((rval = write(fd, buf, str_size)) != str_size) {
+        fprintf(stderr, "[%ld ]Err: scrittura pipe\n", (long)getpid());
+        exit(EXIT_FAILURE);
+    }
+
+    fprintf(stderr, "[%ld]: conclusione...", (long)getpid());
+
+    return(EXIT_SUCCESS);
 }
 
-fifo_parent(const char *file_name)
+/* Lo scopo della funzione fifo_parent() e' di consentire al processo padre di
+leggere da una named pipe. */
+fifo_parent(const char *fifo_name)
 {
+    char buf[BUF_SIZE];
+    int fd, rval;
+    ssize_t str_size;
 }
