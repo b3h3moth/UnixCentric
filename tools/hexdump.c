24228abb026653cd5dd2145895ac6c5646ab672d
B3h3m0th <behemoth@autistici.org>
2014-11-19 13:00:52 +0100

    add buffer max size and a lot of corrections

--- tools/hexdump.c
+++ tools/hexdump.c
@@ -1,36 +1,41 @@
-#include <fcntl.h>
+/*
+ * hexadecimal dump.
+ *
+ * Usage: ./a.out <file>
+ */
+
 #include <stdio.h>
-#include <sys/stat.h>
-#include <sys/types.h>
+#include <stdlib.h>
+#include <fcntl.h>
 #include <unistd.h>
 
-int main (int argc, char* argv[])
-{
-  unsigned char buffer[16];
-  size_t offset = 0;
-  size_t bytes_read;
-  int i;
+#define MAX_BUF 20
 
-  /* Open the file for reading.  */
-  int fd = open (argv[1], O_RDONLY);
-  
-  /* Read from the file, one chunk at a time.  Continue until read
-     "comes up short", i.e. reads less than we asked for.  This
-     indicates that we've hit the end of the file.  */
-  do {
-    /* Read the next lines's worth of bytes.  */
-    bytes_read = read (fd, buffer, sizeof (buffer));
-    /* Print the offset in the file, followed by the bytes themselves.  */
-    printf ("0x%06x : ", offset);
-    for (i = 0; i < bytes_read; ++i)
-      printf ("%02x ", buffer[i]);
-    printf ("\n");
-    /* Keep count of our position in the file.  */
-    offset += bytes_read;
-  }
-  while (bytes_read == sizeof (buffer));
-
-  /* All done.  */
-  close (fd);
-  return 0;
+int main (int argc, char* argv[]) {
+    unsigned char buffer[MAX_BUF];
+    size_t offset = 0;
+    size_t bytes_read;
+    int i;
+    
+        
+    /* Open the file for reading.  */
+    int fd = open (argv[1], O_RDONLY);
+    
+    do {
+        bytes_read = read (fd, buffer, sizeof (buffer));
+        
+        /* Print the offset in the file. */
+        printf ("0x%06x : ", offset);
+        
+        for (i = 0; i < bytes_read; ++i)
+            printf ("%02x ", buffer[i]);
+        printf ("\n");
+        
+        /* Keep count of our position in the file.  */
+        offset += bytes_read;
+    } while (bytes_read == sizeof (buffer));
+    
+    close (fd);
+    
+    return(EXIT_SUCCESS);
 }
