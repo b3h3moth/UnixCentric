42c8fb4705b881dc6f9ef870314667b6ee82ee97
B3h3m0th <behemoth@autistici.org>
2015-02-20 10:35:23 +0100

    removed headers

--- IO-Unbuffered/01_open_2_flags.c
+++ IO-Unbuffered/01_open_2_flags.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
-#include <unistd.h>
 #include <errno.h>
 #include <string.h>
-#include <sys/stat.h>
 
 #ifndef BUF_SIZE
 #define BUF_SIZE 1024
