b6130473689c64e6ce2ad729543530715f4ace70
B3h3m0th <behemoth@autistici.org>
2015-04-09 16:51:08 +0200

    added conversion of argument, a

--- mathematics/eq2/main.c
+++ mathematics/eq2/main.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
+    int a,b,c = 0;
+
+    if (argc != 4) {
         fprintf(stderr, "Usage: %s <a><b><c>\n", argv[0]);
-        exi(EXIT_FAILURE);
+        exit(EXIT_FAILURE);
+    }
+
+    if ((a = strtol(argv[1], NULL, 10)) != 0 ) {
+        fprintf(stderr, "Err.: a argument %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
     }
 
     return(EXIT_SUCCESS);
