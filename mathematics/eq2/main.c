f7e048f6d548d1b2c4fb3639b02d0b8c8fe51fbc
B3h3m0th <behemoth@autistici.org>
2015-04-09 16:33:21 +0200

    added Usage info

--- mathematics/eq2/main.c
+++ mathematics/eq2/main.c
@@ -2,5 +2,10 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <a><b><c>\n", argv[0]);
+        exi(EXIT_FAILURE);
+    }
+
     return(EXIT_SUCCESS);
 }
