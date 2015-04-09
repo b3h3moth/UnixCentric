79219bc3d98c2c6185bd3076b7728c3fab746b59
B3h3m0th <behemoth@autistici.org>
2015-04-09 16:56:59 +0200

    each argument is verified

--- mathematics/eq2/main.c
+++ mathematics/eq2/main.c
@@ -11,10 +11,22 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    if ((a = strtol(argv[1], NULL, 10)) != 0 ) {
+    if ((a = strtol(argv[1], NULL, 10)) == 0 ) {
         fprintf(stderr, "Err.: a argument %s\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
+
+    if ((b = strtol(argv[2], NULL, 10)) == 0 ) {
+        fprintf(stderr, "Err.: b argument %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    if ((c = strtol(argv[3], NULL, 10)) == 0 ) {
+        fprintf(stderr, "Err.: c argument %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    printf ("a: %d\nb: %d\nc: %d\n", a,b,c);
 
     return(EXIT_SUCCESS);
 }
