da344c158bbb4fe4d5ae5cd5b65c5c5deeeef561
B3h3m0th <behemoth@autistici.org>
2015-04-09 17:25:44 +0200

    full example, how to solve quadratic equation

--- mathematics/eq2/main.c
+++ mathematics/eq2/main.c
@@ -2,9 +2,12 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include <math.h>
+
 
 int main(int argc, char *argv[]) {
-    int a,b,c = 0;
+    double a,b,c, x1, x2 = 0;
+    double delta = 0;
 
     if (argc != 4) {
         fprintf(stderr, "Usage: %s <a><b><c>\n", argv[0]);
@@ -26,7 +29,13 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    printf ("a: %d\nb: %d\nc: %d\n", a,b,c);
+    printf ("a: %f\nb: %f\nc: %f\n", a,b,c);
+    delta =  (pow(b, 2) - 4 * a * c);
+    printf("delta (b^2 -4ac) : %f\n", delta);
 
+    x1 = (-b + sqrt(delta)) / (2 * a * c);
+    x2 = (-b - sqrt(delta)) / (2 * a * c);
+
+    printf("x1: %f\nx2: %f\n", x1, x2);
     return(EXIT_SUCCESS);
 }
