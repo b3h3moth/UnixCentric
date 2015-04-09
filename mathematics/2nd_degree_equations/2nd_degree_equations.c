1387ae98329b7299f7eaf24535a967133ce43aa3
B3h3m0th <behemoth@autistici.org>
2015-04-09 18:29:42 +0200

    fixed double output, now I'm satisfied with the result

--- mathematics/2nd_degree_equations/2nd_degree_equations.c
+++ mathematics/2nd_degree_equations/2nd_degree_equations.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[]) {
     double delta = 0;
 
     if (argc != 4) {
-        fprintf(stderr, "Usage: %s <a><b><c>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <+/-num1><+/-num2><+/-num3>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
@@ -31,9 +31,9 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    printf ("Equation is: (%lfx² + %lfx + %lf = 0)\n", a,b,c);
-    delta =  (pow(b, 2) - (4 * a * c));
-    printf("DELTA: %lf\n", delta);
+    printf("Equation is: ((%.0lfx²) + (%.0lfx) + (%.0lf) = 0)\n", a,b,c);
+    delta = (pow(b, 2) - (4 * a * c));
+    printf("DELTA (discriminant): %.0lf\n", delta);
 
     if (delta > 0) {
         x1 = (-b + sqrt(delta)) / (2 * a);
@@ -45,6 +45,6 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    printf("x1: %lf\nx2: %lf\n", x1, x2);
+    printf("x1: %.0lf\nx2: %.0lf\n", x1, x2);
     return(EXIT_SUCCESS);
 }
