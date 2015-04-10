3de03ec245ec813a911561f4f89616052e832989
B3h3m0th <behemoth@autistici.org>
2015-04-10 10:34:33 +0200

    if branch to verify delya/4, small formula

--- mathematics/2nd_degree_equations/2nd_degree_equations.c
+++ mathematics/2nd_degree_equations/2nd_degree_equations.c
@@ -8,7 +8,8 @@
 /* how to solve 2nd degree equation (it's also called quadratic equation) */
 
 int main(int argc, char *argv[]) {
-    double a,b,c, x1, x2 = 0;
+    double a,b,c = 0;
+    double x1, x2 = 0;
     double delta, sum, product = 0;
 
     if (argc != 4) {
@@ -35,10 +36,15 @@ int main(int argc, char *argv[]) {
     delta = (pow(b, 2) - (4 * a * c));
     printf("Delta (discriminant):\n\t%.0lf\n", delta);
 
+    /*
+    if (delta >0 && (b % 2) == 0) {
+        x1 = (((-b/2) + sqrt(delta/4)) / a);
+        x2 = (((-b/2) - sqrt(delta/4)) / a);
+    } else */
     if (delta > 0) {
         x1 = (-b + sqrt(delta)) / (2 * a);
         x2 = (-b - sqrt(delta)) / (2 * a);
-    } else if (delta = 0)
+    } else if (delta == 0)
         x1 = x2 = -b / (2*a);
     else {
         fprintf(stderr, "(delta < 0) Equation is irrisolvible. Bye!\n");
