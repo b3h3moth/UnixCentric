f9c77ae59de5c0eaf0b70c9307ad5ba3e5e2b36f
B3h3m0th <behemoth@autistici.org>
2015-04-23 14:25:32 +0200

    first example of SDL library

--- APIs_and_Libraries/SDL/01_hello_world.c
+++ APIs_and_Libraries/SDL/01_hello_world.c
@@ -5,7 +5,29 @@
 int main(int argc, char *argv[]) {
     // Start SDL
     SDL_Init(SDL_INIT_EVERYTHING);
-    
+
+    // Image
+    SDL_Surface *image = NULL;
+    SDL_Surface *screen = NULL;
+
+    // Setup screen
+    screen = SDL_SetVideoMode(640, 480, 32, SDL_SWSURFACE);
+
+    // Load image
+    image = SDL_LoadBMP("hello.bmp");
+
+    // Apply image to screen
+    SDL_BlitSurface(image, NULL, screen, NULL);
+
+    // Update screen
+    SDL_Flip(screen);
+
+    // Pause
+    SDL_Delay(2000);
+
+    // Free loaded image
+    SDL_FreeSurface(image);
+
     // Quit SDL
     SDL_Quit();
     
