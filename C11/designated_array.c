4e47b0357962016bfbe72c1f3f2dd9ac5094c69f
B3h3m0th <behemoth@autistici.org>
2015-06-04 10:45:00 +0200

    designated array declaration, C11

--- C11/designated_array.c
+++ C11/designated_array.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
+#define MAX_ARR 10;
+
+int main(void) {
+
+    int numbers[MAX_ARR] = {
+        [0] = 100,
+        [2] = 200,
+        [4] = 600,
+        [8] = 800,
+        [9] = 1000,
+    };
+
+    for (int i=0; i<MAX_ARR; i++)
+        fprintf (stdout, "%d\n", numbers[i]);
+
     return(EXIT_SUCCESS);
 }
