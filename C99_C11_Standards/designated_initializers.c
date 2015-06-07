73cb190b091d0e121ccc08fa8a9bde9865fe8679
B3h3m0th <behemoth@autistici.org>
2015-06-07 17:10:40 +0200

    remove colon

---
+++ C99_C11_Standards/designated_initializers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_ARR 10
+
+int main(void) {
+
+    int numbers[MAX_ARR] = {
+        [1] = 100,
+        [3] = 200,
+        [5] = 600,
+        [7] = 800,
+        [9] = 1000,
+    };
+
+    for (int i=0; i<MAX_ARR; i++)
+        fprintf (stdout, "%d\n", numbers[i]);
+
+    return(EXIT_SUCCESS);
+}
