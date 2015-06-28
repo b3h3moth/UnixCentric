9a4bdc73ac1b6af1fa6960592a9b55aef4e205be
B3h3m0th <behemoth@autistici.org>
2015-06-28 23:27:28 +0200

    linked list with glib-2.0

--- APIs_and_Libraries/libGlib/linked_list.c
+++ APIs_and_Libraries/libGlib/linked_list.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <glib.h>
+#include <glib-2.0/glib.h>
+
+GList *list;
 
 int main(int argc, char *argv[]) {
+    list = g_list_append(list, "first");
+    list = g_list_append(list, "second");
+    list = g_list_append(list, "third");
+
+    while (list != NULL) {
+        printf("%s\n", (char *)list->data);
+        list = list->next;
+    }
+
     return(EXIT_SUCCESS);
 }
