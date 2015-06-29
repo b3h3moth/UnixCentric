19f415eec664231cea3bbeef47a7319302acac43
B3h3m0th <behemoth@autistici.org>
2015-06-29 12:03:31 +0200

    new element into list, check newline and remove it

--- APIs_and_Libraries/libGlib/linked_list.c
+++ APIs_and_Libraries/libGlib/linked_list.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <glib-2.0/glib.h>
+
+#define MAX_BUF 50
 
 GList *list;
 
 int main(int argc, char *argv[]) {
-    
+    char new_element[MAX_BUF];
+    int i=0;
+
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <string>\n", argv[0]);
         exit(EXIT_FAILURE);
@@ -14,7 +19,15 @@ int main(int argc, char *argv[]) {
     /* Add an element to list */
     list = g_list_append(list, argv[1]);
 
-    list = g_list_append(list, "second");
+    printf("Please give new element for list: ");
+    fgets(new_element, MAX_BUF+1, stdin);
+
+    for (i=0; i<strlen(new_element); i++) {
+        if (new_element[i] == '\n')
+            new_element[i] = '\0';
+    }
+
+    list = g_list_prepend(list, new_element);
     list = g_list_append(list, "third");
 
     while (list != NULL) {
