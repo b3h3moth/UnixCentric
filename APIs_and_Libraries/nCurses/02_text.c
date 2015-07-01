01640231373b4b2e47b7ed1998a3e80e4bbca4cf
B3h3m0th <behemoth@autistici.org>
2015-07-01 13:49:10 +0200

    addch() writes characters on the current screen

--- APIs_and_Libraries/nCurses/02_text.c
+++ APIs_and_Libraries/nCurses/02_text.c
@@ -3,8 +3,24 @@
 #include <ncurses.h>
 
 int main(void) {
+    char string[] = "Writing nCurses program";
+    char *pstr;
+    pstr  = string;
+
     initscr();
+
+    while(*pstr) {
+        /* addch() writes characters on the current screen, curstr */
+        addch(*pstr);
+        pstr++;
+    }
+
+    /* Update the screen */
     refresh();
+
+    /* Wait until the user types something */
+    getch();
+
     endwin();
     exit(EXIT_SUCCESS);
 }
