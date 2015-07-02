46c989350bd101a6f366ce759b949da406123bbe
B3h3m0th <behemoth@autistici.org>
2015-07-02 19:36:16 +0200

    addnstr() syntax and full example

--- APIs_and_Libraries/nCurses/02_writing_text_3_addnstr.c
+++ APIs_and_Libraries/nCurses/02_writing_text_3_addnstr.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <ncurses.h>
 
+/* It writes a string on the current screen 'curscr' with addnstr() function.
+
+int addnstr(const char *str, int n);
+write a string large exaclty n characters. */
+
 int main(void) {
+    char message[] = "Unix addicted\n";
+    char msg_help[] = "(type something to exit)\n";
+
     initscr();
+
+    addnstr(message, strlen(message));
+    addnstr(msg_help, strlen(msg_help));
+
+    refresh();
+    getch();
+
     endwin();
+  
     exit(EXIT_SUCCESS);
 }
