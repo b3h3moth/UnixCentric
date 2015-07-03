a8d5c14128874577db50b37523a6479f03334550
B3h3m0th <behemoth@autistici.org>
2015-07-03 18:13:52 +0200

    replaced size_t with char

--- APIs_and_Libraries/nCurses/03_formatting_text_with_printw.c
+++ APIs_and_Libraries/nCurses/03_formatting_text_with_printw.c
@@ -2,13 +2,16 @@
 #include <stdlib.h>
 #include <ncurses.h>
 
+/* The printw() function is the equivalent to the standard C printf() 
+function. */
+
 int main(void) {
-    size_t version = 5.7;
+    char version[] = "5.7";
     char os_name[] = "OpenBSD";
     
     initscr();
     printw("%s OS\n", os_name);
-    printw("version number is: %zu", version);
+    printw("version number is: %s", version);
     refresh();
     getch();
 
