4acc06baeaee2d91522120e8c3b62aaf4a823f1b
B3h3m0th <behemoth@autistici.org>
2015-07-07 18:39:19 +0200

    getch() testing

--- APIs_and_Libraries/nCurses/input_output/04_reading_text_1_getch.c
+++ APIs_and_Libraries/nCurses/input_output/04_reading_text_1_getch.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ncurses.h>
 
-int main(int argc, char *argv[]) {
-    return(EXIT_SUCCESS);
+int main(void) {
+    char ch;
+
+    initscr();
+    addstr("Please type x to quit");
+    refresh();
+
+    while( (ch = getch()) != 'x') {
+        printw("\ntyped: %c\n", ch);
+    }
+
+    endwin();
+    exit(EXIT_SUCCESS);
 }
