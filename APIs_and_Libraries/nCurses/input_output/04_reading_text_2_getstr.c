61752d4426f93c9d2993ae12c56d4dd324bf050d
B3h3m0th <behemoth@autistici.org>
2015-07-07 18:45:27 +0200

    addstr() get variables

--- APIs_and_Libraries/nCurses/input_output/04_reading_text_2_getstr.c
+++ APIs_and_Libraries/nCurses/input_output/04_reading_text_2_getstr.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ncurses.h>
 
-int main(int argc, char *argv[]) {
-    return(EXIT_SUCCESS);
+#define MAX_STR 30
+
+int main(void) {
+    char firstname[MAX_STR];
+    char lastname[MAX_STR];
+
+    initscr();
+    addstr("Firstname: ");
+    refresh();
+    getstr(firstname);
+
+    addstr("Lastname: ");
+    refresh();
+    getstr(lastname);
+
+    printw("Hi, %s, %s\n", firstname, lastname);
+    refresh();
+    getch();
+
+    endwin();
+    exit(EXIT_SUCCESS);
 }
