141e8357a2c350db1fffe8c1a703b80f5a478f2c
B3h3m0th <behemoth@autistici.org>
2015-07-08 22:30:26 +0200

    getnstr() syntax and full test

--- APIs_and_Libraries/nCurses/input_output/04_reading_text_3_getnstr.c
+++ APIs_and_Libraries/nCurses/input_output/04_reading_text_3_getnstr.c
@@ -2,8 +2,30 @@
 #include <stdlib.h>
 #include <ncurses.h>
 
+#define MAX_STR 10
+
 int main(void) {
+    char firstname[MAX_STR];
+    char lastname[MAX_STR];
+
+    /* getnstr(char *str, int n) 
+    reads from stdscr (default window) max 'n' characters, the resulting value
+    is placed into the array of characters pointed by 'str' */
+    
     initscr();
+    addstr("Firstname: ");
+    refresh();
+    getnstr(firstname, MAX_STR);
+
+    addstr("Lastname: ");
+    refresh();
+    getnstr(lastname, MAX_STR);
+
+
+    printw("Hi, %s, %s\n", firstname, lastname);
+    refresh();
+    getch();
+
     endwin();
     exit(EXIT_SUCCESS);
 }
