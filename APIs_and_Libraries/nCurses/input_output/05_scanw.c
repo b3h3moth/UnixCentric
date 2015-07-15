d69b68591785fabb127558b8667e5e59572da9b6
B3h3m0th <behemoth@autistici.org>
2015-07-15 10:30:33 +0200

    scanw() function tested

--- APIs_and_Libraries/nCurses/input_output/05_scanw.c
+++ APIs_and_Libraries/nCurses/input_output/05_scanw.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ncurses.h>
+#define MAX_STR 20
 
 int main(void) {
+    char firstname[MAX_STR];
+    char lastname[MAX_STR];
+    
     initscr();
+
+    /* scanw() is analogous to the scanf() functions family. */
+
+    printw("Please your firstname: ");
+    scanw("%s", firstname);
+
+    printw("Please your lastname: ");
+    scanw("%s", lastname);
+    
+    printw("Hi %s %s\n", firstname, lastname);
+
+    refresh();
+    getch();
+    
     endwin();
+    
     exit(EXIT_SUCCESS);
 }
