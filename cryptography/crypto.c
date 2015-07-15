624ef19c35b28d95c56f5eff165c365ee483e421
B3h3m0th <behemoth@autistici.org>
2015-07-15 09:56:20 +0200

    check if crypto fips is enabled

--- cryptography/crypto.c
+++ cryptography/crypto.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 #define CRYPTO_FIPS "/proc/sys/crypto/fips_enabled"
-int main(int argc, char *argv[])
-{
-FILE *fp;
-char c;
-fp = fopen( CRYPTO_FIPS, "r");
-if( fp == NULL)
-{
-printf("The OS doesn't support kernel crypto module!\n");
-return 1;
-}
-if(fread(&c, sizeof(c), 1, fp) <= 0){
-printf("failed to read from: %s\n", CRYPTO_FIPS);
-fclose( fp);
-return 1;
-}
-printf("FIPS_MODE: %c\n", c);
-fclose(fp);
-return 0;
+
+int main(int argc, char *argv[]) {
+    FILE *fp;
+    char c;
+    
+    if ( (fp = fopen( CRYPTO_FIPS, "r")) == NULL) {
+        printf("The OS doesn't support kernel crypto module!\n");
+        return(EXIT_FAILURE);
+    }
+    
+    if (fread(&c, sizeof(c), 1, fp) <= 0){
+        printf("failed to read from: %s\n", CRYPTO_FIPS);
+        fclose( fp);
+        return(EXIT_FAILURE);
+    }
+    
+    printf("FIPS_MODE: %c\n", c);
+    
+    fclose(fp);
+    
+    return(EXIT_SUCCESS);
 }
