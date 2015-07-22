1a34ee79cc753d2a29eaf5796be2936f1c872e90
B3h3m0th <behemoth@autistici.org>
2015-07-22 20:00:40 +0200

    removed comments

--- APIs_and_Libraries/nCurses/formatting_text/text_attributes.c
+++ APIs_and_Libraries/nCurses/formatting_text/text_attributes.c
@@ -2,13 +2,18 @@
 #include <stdlib.h>
 #include <ncurses.h>
 
-/*
-- attrset() sets text attributes;
--  attron() text attributes on;
-- attroff() text attributes off.
-*/
 int main(void) {
     initscr();
+
+    attron(A_BOLD);
+    attrset(A_UNDERLINE);
+    addstr("nCurses library is a good way to learn UNIX programming\n");
+    addstr("nCurses library is a good way to learn UNIX programming\n");
+    attrset(0);
+    addstr("nCurses library is a good way to learn UNIX programming\n");
+    refresh();
+    getch();
+
     endwin();
     exit(EXIT_SUCCESS);
 }
