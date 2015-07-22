7070b92b4abb5469600f1962a89a2a0e0528f565
B3h3m0th <behemoth@autistici.org>
2015-07-22 20:07:10 +0200

    attron(); attrset(); attroff() syntax

--- APIs_and_Libraries/nCurses/formatting_text/text_attributes.c
+++ APIs_and_Libraries/nCurses/formatting_text/text_attributes.c
@@ -5,15 +5,21 @@
 int main(void) {
     initscr();
 
+    /* attron() turns on the named attributes without affecting any others */
     attron(A_BOLD);
+
+    /* attrset() sets the current attributes of the given window */
     attrset(A_UNDERLINE);
-    addstr("nCurses library is a good way to learn UNIX programming\n");
-    addstr("nCurses library is a good way to learn UNIX programming\n");
-    attrset(0);
+    
     addstr("nCurses library is a good way to learn UNIX programming\n");
     
+    /* it turns off all attributes */
+    attrset(0);
+
+    addstr("I\'m working with a lot of nice functions\n");
+    
     /* attroff() turns off the named attributes without turning any other 
-    attributes on or off. */
+    attributes on or off */
     attroff(A_BOLD);
     
     refresh();
