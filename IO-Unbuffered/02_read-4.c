177fc815f36b34a890cf1b5aa7f5f5227c688324
B3h3m0th <behemoth@autistici.org>
2015-07-28 11:41:26 +0200

    new file on read() syscall added

---
+++ IO-Unbuffered/02_read-4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/socket.h>
+
+/*
+HEADER    : 
+PROTOTYPE : 
+SEMANTICS : 
+RETURNS   : 0 in caso di successo, -1 in caso di errore
+--------------------------------------------------------------------------------
+*/
+
+int main(int argc, char *argv[]) {
+    return(EXIT_SUCCESS);
+}
