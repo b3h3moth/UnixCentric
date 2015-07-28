49984056847f6fe361882586393d5d0bd483b68e
B3h3m0th <behemoth@autistici.org>
2015-07-28 12:02:04 +0200

    control arguments added

--- IO-Unbuffered/02_read-4.c
+++ IO-Unbuffered/02_read-4.c
@@ -8,10 +8,15 @@
 
 /* Leggere dati da input mediante read() e stamparli in output  */
 
-int main(void) {
+int main(int argc, char *argv[]) {
     char buf[MAX_BUF + 1];  /* E' necessario riservare spazio per la newline */
     ssize_t num_read;       /* Si usa ssize_t per la portabilita' */
-    
+
+    if (argc <= 1) {
+        fprintf(stderr, "Usage: %s <input data>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
     if ((num_read = read(STDIN_FILENO, buf, MAX_BUF)) == -1) {
         fprintf(stderr, "Err. reading file: %s\n", strerror(errno));
         exit(EXIT_FAILURE);
