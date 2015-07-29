4f70eeadb52413d49ca8948ee496bca09c66cc12
B3h3m0th <behemoth@autistici.org>
2015-07-29 18:54:57 +0200

    fixed indendation, stdlib added

--- IO-Unbuffered/04_lseek-holes-2.c
+++ IO-Unbuffered/04_lseek-holes-2.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <string.h>
@@ -6,62 +7,49 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-struct person
-{
-  char name[10];		/* first name */
-  char id[10];			/* ID number */
-  off_t pos;			/* position in file, for demonstration */
-} people[] =
-{
-  {
-  "luca", "123456789", 0},
-  {
-  "b3h3m0th", "987654321", 10240},
-  {
-"suriancic", "761234232", 81920},};
+struct person {
+    char name[10];		/* first name */
+    char id[10];		/* ID number */
+    off_t pos;			/* position in file, for demonstration */
+} people[] = { 
+    { "luca", "123456789", 0 },
+    { "b3h3m0th", "987654321", 10240 },
+    { "suriancic", "761234232", 81920 },
+};
 
-int
-main (int argc, char **argv)
-{
-  int fd;
-  int i, j;
-
-  if (argc < 2)
-    {
-      fprintf (stderr, "usage: %s file\n", argv[0]);
-      return 1;
+int main (int argc, char **argv) {
+    int fd;
+    int i, j;
+    
+    if (argc < 2) {
+        fprintf (stderr, "Usage: %s <file>\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
-
-  fd = open (argv[1], O_RDWR | O_CREAT | O_TRUNC, 0666);
-  if (fd < 0)
-    {
-      fprintf (stderr, "%s: %s: cannot open for read/write: %s\n",
-	       argv[0], argv[1], strerror (errno));
-      return 1;
+    
+    if ((fd = open (argv[1], O_RDWR | O_CREAT | O_TRUNC, 0666)) < 0) {
+        fprintf (stderr, "%s: %s: Cannot open for read/write: %s\n",\
+                argv[0], argv[1], strerror (errno));
+        exit(EXIT_FAILURE);
     }
-
-  j = sizeof (people) / sizeof (people[0]);	/* count of elements */
-
-  for (i = 0; i < j; i++)
-    {
-      if (lseek (fd, people[i].pos, SEEK_SET) < 0)
-	{
-	  fprintf (stderr, "%s: %s: seek error: %s\n",
-		   argv[0], argv[1], strerror (errno));
-	  (void) close (fd);
-	  return 1;
-	}
-
-      if (write (fd, &people[i], sizeof (people[i])) != sizeof (people[i]))
-	{
-	  fprintf (stderr, "%s: %s: write error: %s\n",
-		   argv[0], argv[1], strerror (errno));
-	  (void) close (fd);
-	  return 1;
-	}
+    
+    j = sizeof (people) / sizeof (people[0]);	/* count of elements */
+    
+    for (i=0; i<j; i++) {
+        if (lseek (fd, people[i].pos, SEEK_SET) < 0) {
+            fprintf (stderr, "%s: %s: seek error: %s\n",
+                    argv[0], argv[1], strerror (errno));
+            close(fd);
+            return(EXIT_FAILURE);
+        }
+        if (write(fd, &people[i], sizeof(people[i])) != sizeof(people[i])) {
+            fprintf (stderr, "%s: %s: write error: %s\n",
+                    argv[0], argv[1], strerror (errno));
+            close(fd);
+            return(EXIT_FAILURE);
+        }
     }
 
   /* all ok here */
-  (void) close (fd);
-  return 0;
+  close (fd);
+  return(EXIT_SUCCESS);
 }
