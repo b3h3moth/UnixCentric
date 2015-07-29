afff091a368e89a90ce8aeb3231c5a253c2ac856
B3h3m0th <behemoth@autistici.org>
2015-07-29 22:20:41 +0200

    fixed and improved code indendation

--- IO-Unbuffered/04_lseek-1.c
+++ IO-Unbuffered/04_lseek-1.c
@@ -6,11 +6,11 @@
 
 #define MAX_BUF     1024
 #define PERMS       0644
-#define NEW_OFFSET  70
+#define NEW_OFFSET  140
 
 /* Legge il contenuto del file 'input_file', dopodiche' il file-offset viene 
-aggiornato di 70 bytes rispetto all'inizio, da questo punto si copiera' tutto
-il contenuto sul fil 'output_file'.
+aggiornato di NEW_OFFSET bytes rispetto all'inizio, da questo punto si copiera'
+tutto il contenuto sul fil 'output_file'.
 */
 
 int main(int argc, char *argv[], char *envp[])
@@ -26,20 +26,17 @@ int main(int argc, char *argv[], char *envp[])
       exit(EXIT_FAILURE);
    }
 
-   if ( (fd2 = open(output_file, O_RDWR | O_CREAT, PERMS)) < 0) {
-      printf("Err. apertura %s\n", input_file);
-      exit(EXIT_FAILURE);
-   }
-
    /* Il file-offset del file in lettura e' ora aggiornato al byte 70 */
-   if (lseek(fd1, NEW_OFFSET, SEEK_SET) == -1) {
-      fprintf(stderr, "Err. lseek()\n");
-      exit(EXIT_FAILURE);
-   }
+   lseek(fd1, NEW_OFFSET, SEEK_SET);
 
    /* Legge input_file dal byte NEW_OFFSET */
    if ((num_read = read(fd1, &buf, MAX_BUF)) < 0) {
       fprintf(stderr, "Err. read\n");
+      exit(EXIT_FAILURE);
+   }
+
+   if ( (fd2 = open(output_file, O_RDWR | O_CREAT, PERMS)) < 0) {
+      printf("Err. apertura %s\n", input_file);
       exit(EXIT_FAILURE);
    }
 
