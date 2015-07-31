a4c243e0e3f6980ce68a0b51943006c6b22b7b2f
B3h3m0th <behemoth@autistici.org>
2015-07-31 20:53:43 +0200

    controllo degli argomenti e parametri

--- IO-Unbuffered/04_lseek-holes-3.c
+++ IO-Unbuffered/04_lseek-holes-3.c
@@ -7,5 +7,10 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
+    if (argc < 3 || strncmp(argv[1], "--help", 6) == 0) {
+        fprintf(stderr, "Usage: %s r|R|w|s\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
     return(EXIT_SUCCESS);
 }
