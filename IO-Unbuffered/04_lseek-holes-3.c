ccbe0828a0fc6c07919a9b3a9d486422085675e3
B3h3m0th <behemoth@autistici.org>
2015-08-01 12:47:17 +0200

    aperto il file con open(), rimossi headers

--- IO-Unbuffered/04_lseek-holes-3.c
+++ IO-Unbuffered/04_lseek-holes-3.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <ctype.h>
 #include <errno.h>
 #include <string.h>
-#include <unistd.h>
+#include <fcntl.h>
 
 int main(int argc, char *argv[]) {
-    if (argc < 3 || strncmp(argv[1], "--help", 6) == 0) {
-        fprintf(stderr, "Usage: %s r|R|w|s\n", argv[0]);
+    int fd;     /* File descriptor */
+
+    if (argc < 3 || strncmp(argv[1], "--help", 6)  == 0) {
+        fprintf(stderr, "Usage: %s <filename> [r|R|w!s]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    /* Apri un file in lettura/scrittura, se non esiste e' necessario crearlo,
+    dovra essere settato con i permessi impostati a rw-rw-rw */
+    if ((fd = open(argv[1], O_RDWR | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | \
+                                              S_IROTH | S_IWOTH)) == -1) {
+        fprintf(stderr, "Err. open(%s): %s\n", argv[1], strerror(errno));
         exit(EXIT_FAILURE);
     }
 
