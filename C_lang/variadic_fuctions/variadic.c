b3e59b037a92b1faec18f09963641617d2c76ea8
B3h3m0th <behemoth@autistici.org>
2015-08-02 18:41:17 +0200

    stdarg.h utilities

--- C_lang/variadic_fuctions/variadic.c
+++ C_lang/variadic_fuctions/variadic.c
@@ -2,6 +2,23 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+/* Il linguaggio C consente di definire funzioni che consentono un numero
+arbitrario di parametri, tipicamente sono le funzioni I/O; in tali funzioni il
+numero arbitrario di parametri si indica nel prototipo con l'ellissi "..." e
+deve sempre seguire obbligatoriamente un parametro fisso.
+
+L'header di riferimento e' <stdarg.h> che contiene una serie di utilities per 
+la standardizzazione sulll'uso delle funzioni con un numero arbitrario di 
+parametri.
+
+Le utilities fornite da <stdarg.h> (dipendenti dal sisistema utilizzato):
+- typedef char * va_list;
+- va_start();
+- va_arg();
+- va_end();
+- va_copy().
+*/
+
 void printout(int num, ...);
 
 int main(int argc, char *argv[]) {
