06fbf2b3af298d232388df2c33ab04bbc07d4269
B3h3m0th <behemoth@autistici.org>
2015-08-02 18:25:10 +0200

    invocazione chiamate a printout()

--- C_lang/variadic_fuctions/variadic.c
+++ C_lang/variadic_fuctions/variadic.c
@@ -5,6 +5,10 @@
 void printout(int num, ...);
 
 int main(int argc, char *argv[]) {
+    printout(0);
+    printout(1, "STR");
+    printout(2, "STR", "STR2");
+    printout(3, "STR", "STR2", 2015);
 
     return(EXIT_SUCCESS);
 }
