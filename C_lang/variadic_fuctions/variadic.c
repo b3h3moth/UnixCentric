7a6ebd1f6dea8bf4b34be107370f68faa2e5ca8b
B3h3m0th <behemoth@autistici.org>
2015-08-02 18:24:08 +0200

    implementazione della funzione printput()

--- C_lang/variadic_fuctions/variadic.c
+++ C_lang/variadic_fuctions/variadic.c
@@ -2,10 +2,26 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-void print(int num, ...);
+void printout(int num, ...);
 
 int main(int argc, char *argv[]) {
-    print(0);
 
     return(EXIT_SUCCESS);
 }
+
+void printout(int num, ...) {
+    va_list ap;
+
+    va_start(ap, num);
+
+    if (num >= 1)
+        printf("%s", va_arg(ap, char *));
+    if (num >= 2)
+        printf("%s", va_arg(ap, char *));
+    if (num >= 3)
+        printf("%d", va_arg(ap, int));
+    printf("\n");
+
+    va_end(ap);
+}
+
