187732047a2e56d1655132c8732a42013e80e7d6
B3h3m0th <behemoth@autistici.org>
2015-08-15 17:52:24 +0200

    RGB_color structure

--- C_lang/structures/01_declare_structures.c
+++ C_lang/structures/01_declare_structures.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
+struct RGB_color {
+    int read;
+    int green;
+    int blu;
+};
+
+int main(void) {
+
     return(EXIT_SUCCESS);
 }
