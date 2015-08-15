c28c2c609a28eea350d79d4877d71e8385ee0f98
B3h3m0th <behemoth@autistici.org>
2015-08-15 19:17:07 +0200

    aggiunta di MAX_STR, fissa l'ampiezza del vettore di caratteri set_name

--- C_lang/structures/RGB_color.h
+++ C_lang/structures/RGB_color.h
@@ -1,10 +1,13 @@
 #ifndef STRUCTURE
 #define STRUCTURE
 
+#define MAX_STR 10
+
 struct RGB_color {
-    int set_red;
-    int set_green;
-    int set_blu;
+    int  set_red;
+    int  set_green;
+    int  set_blu;
+    char set_name[MAX_STR];
 };
 
 #endif
