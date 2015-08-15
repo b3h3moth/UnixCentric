99efac4237e6839c11175e4abb140fbdcc4dfb46
B3h3m0th <behemoth@autistici.org>
2015-08-15 18:01:19 +0200

    memory allocation comment

--- C_lang/structures/01_declare_and_initialize.c
+++ C_lang/structures/01_declare_and_initialize.c
@@ -8,6 +8,9 @@ struct RGB_color {
 };
 
 int main(void) {
+
+    /* Inizializza la struttura, ora sara' anche creato lo spazio di memoria
+    necessario a contenere la variabile struct 'black' */
     struct RGB_color black = {0, 0, 0};
 
     return(EXIT_SUCCESS);
