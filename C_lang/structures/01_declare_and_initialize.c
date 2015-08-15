3652082691d10986009e1e2f485efb43e66a0990
B3h3m0th <behemoth@autistici.org>
2015-08-15 19:17:47 +0200

    inizializzato anche il vettore di caratteri set_name

--- C_lang/structures/01_declare_and_initialize.c
+++ C_lang/structures/01_declare_and_initialize.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "RGB_color.h" /* Si include la definizione della struttura */
 
 int main(void) {
@@ -7,7 +8,7 @@ int main(void) {
     /* Dichiara ed inizializza la struttura subito dopo la definizione. 
     Sara' anche creato lo spazio di memoria necessario a contenere la 
     variabile struct 'black' */
-    struct RGB_color black = {0, 0, 0};
+    struct RGB_color black = {0, 0, 0, "nero"};
 
     /* Prima dichiara la variabile struct white, dopodiche' ciascun membro
     sara' inizializzato */
@@ -15,6 +16,7 @@ int main(void) {
     white.set_red = 255;
     white.set_green = 255;
     white.set_blu = 255;
+    strncpy(white.set_name, "bianco", 6);
 
     return(EXIT_SUCCESS);
 }
