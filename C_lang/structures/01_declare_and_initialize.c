ed2bad4272958da75bea040057523cb02c5c7bc5
B3h3m0th <behemoth@autistici.org>
2015-08-15 18:04:10 +0200

    new struct variable added, white, fixed structure member

--- C_lang/structures/01_declare_and_initialize.c
+++ C_lang/structures/01_declare_and_initialize.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 
 struct RGB_color {
-    int read;
-    int green;
-    int blu;
+    int set_red;
+    int set_green;
+    int set_blu;
 };
 
 int main(void) {
@@ -12,6 +12,11 @@ int main(void) {
     /* Inizializza la struttura, ora sara' anche creato lo spazio di memoria
     necessario a contenere la variabile struct 'black' */
     struct RGB_color black = {0, 0, 0};
+
+    struct RGB_color white;
+    white.set_red = 255;
+    white.set_green = 255;
+    white.set_blu = 255;
 
     return(EXIT_SUCCESS);
 }
