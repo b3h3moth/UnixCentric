79ad98c265c6a4a2c2bd34f5a169236408ca8e50
B3h3m0th <behemoth@autistici.org>
2015-08-18 13:54:11 +0200

    inizializzazione vettore a seconda degli elementi della lista di inizializzazione

--- C_lang/array_and_strings/array_initialization.c
+++ C_lang/array_and_strings/array_initialization.c
@@ -5,20 +5,26 @@
 
 int main(void) {
 
+    // Il vettore e' inizializzato col numero degli inizializzatori
+    // In questo caso si tratta di un vettore di 5 elementi
+    int vector_a[] = {10, 20, 30, 40, 50};
+
     // I primi 5 elelmenti sono inizializzati, il resto sara' posto a 0
-    int vector_a[SIZE] = {10, 20, 30, 40, 50};
+    int vector_b[SIZE] = {10, 20, 30, 40, 50};
 
     // Un elemento in piu', warning (GCC)
     // warning: excess elements in array initializer
-    int vector_b[SIZE] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110};
+    int vector_c[SIZE] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110};
 
     // Tutti gli elementi inizializzati a 0.
     // Vale il discorso fatto per il primo vettore
-    int vector_c[SIZE] = {0};
+    int vector_d[SIZE] = {0};
 
     // Lista di inizializzazione vuota, warning (GCC)
     // warning: ISO C forbids empty initializer braces
-    int vector_d[SIZE] = { };
+    int vector_e[SIZE] = { };
+
+
 
     return(EXIT_SUCCESS);
 }
