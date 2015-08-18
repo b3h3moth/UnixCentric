f0fee3ccbfcd5c0bf3b380c3a2226e7fe0c54bd4
B3h3m0th <behemoth@autistici.org>
2015-08-18 11:59:33 +0200

    standard C89, C99 e C11, dichiarazione di variabili

--- C_lang/variables_and_types/variable_declarations_c89_c99.c
+++ C_lang/variables_and_types/variable_declarations_c89_c99.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
+int main(void) {
+    /* Nello standard C89 una variabile doveva essere dichiarata prima di
+    essere utilizzata e non dovevano esserci altri tipi di istruzioni tra di
+    esse;, dallo standard C99 invece una variabile puo' essere dichiarata
+    ovunque.
+
+    Si compili il seguente codice con:
+    -gcc -pedantic -Wall -std=c89
+    -gcc -pedantic -Wall -std=c99
+
+    Naturalmente anche lo standard C11 (-std=c11) compila con la nuova regola
+    stabilita dallo standard C99.
+    */
+    int first = 10;
+
+    printf("%d\n", first);
+
+    int second = 20;
+    printf("%d\n", second);
+
+    for (int i=0; i<first; i++)
+        printf("%d ", i);
+
     return(EXIT_SUCCESS);
 }
