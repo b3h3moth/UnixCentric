f3e5b83aaf648cc138c999b4ffe28bd743456e50
B3h3m0th <behemoth@autistici.org>
2015-08-18 13:41:41 +0200

    primo metodo di inizializzazione

--- C_lang/array_and_strings/array_initialization.c
+++ C_lang/array_and_strings/array_initialization.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
+#define SIZE 10
+
+int main(void) {
+
+    // I primi 5 elelmenti sono inizializzati, il resto sara' posto a 0
+    int vector_a[SIZE] = {10, 20, 30, 40, 50};
     return(EXIT_SUCCESS);
 }
