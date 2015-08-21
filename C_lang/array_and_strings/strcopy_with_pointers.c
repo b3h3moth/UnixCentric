01830e6018b948adcf5e79e238b6a107589f2cb3
B3h3m0th <behemoth@autistici.org>
2015-08-21 13:22:28 +0200

    variabili per il conteggio dei caratteri della stringa

--- C_lang/array_and_strings/strcopy_with_pointers.c
+++ C_lang/array_and_strings/strcopy_with_pointers.c
@@ -5,5 +5,9 @@ int main(int argc, char *argv[]) {
     char *default_string = "Take a walk on the wild side";
     char *ptr = default_string;
 
+    int defstr_size  = 0;
+    char *bkp_copy = NULL;
+
+    // Conta il numero dei caratteri della stringa
     return(EXIT_SUCCESS);
 }
