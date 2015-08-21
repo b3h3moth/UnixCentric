5dd36f9065b487c4d12150872b6cb9ab03815814
B3h3m0th <behemoth@autistici.org>
2015-08-21 15:59:17 +0200

    nuovo file su array bidimensionali

---
+++ C_lang/array/bidimentional_array.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ROWS    2
+#define COLS    3
+
+int main(void) {
+
+    // Array bidimensionale e' sinonimo di matrice
+    return(EXIT_SUCCESS);
+}
