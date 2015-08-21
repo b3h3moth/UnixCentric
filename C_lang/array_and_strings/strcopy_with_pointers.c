9744f94d619ae9e50a90baf671fd832ee0f7fdec
B3h3m0th <behemoth@autistici.org>
2015-08-21 13:21:33 +0200

    stringa di default e puntatore ad essa

--- C_lang/array_and_strings/strcopy_with_pointers.c
+++ C_lang/array_and_strings/strcopy_with_pointers.c
@@ -2,5 +2,8 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+    char *default_string = "Take a walk on the wild side";
+    char *ptr = default_string;
+
     return(EXIT_SUCCESS);
 }
