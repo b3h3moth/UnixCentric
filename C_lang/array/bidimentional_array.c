ee617984a27b10eb9b5da99e229a57adbb0c56a5
B3h3m0th <behemoth@autistici.org>
2015-08-21 16:08:47 +0200

    inizializzazione matrice

--- C_lang/array/bidimentional_array.c
+++ C_lang/array/bidimentional_array.c
@@ -7,7 +7,15 @@
 int main(void) {
 
     // Array bidimensionale e' sinonimo di matrice
-    int matrix[ROWS][COLS];
+    // Dichiarazione di una matrice di tipo intero
+    int matrix_a[ROWS][COLS];
+
+    // Inizializzazione contestualmente alla dichiarazione
+    // Valgono comunque le regole per i vettori
+    int matrix_b[ROWS][COLS] = { {10, 20, 30},
+                                 {40, 50, 60} };
+
+
 
     return(EXIT_SUCCESS);
 }
