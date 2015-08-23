7a42cd8d95521eb4583d5de38bc882ac4db44ec3
B3h3m0th <behemoth@autistici.org>
2015-08-23 18:08:23 +0200

    show_float() e show_pointer()

--- C_lang/variables_and_types/byte_representation.c
+++ C_lang/variables_and_types/byte_representation.c
@@ -8,7 +8,7 @@
 
 typedef unsigned char *byte_pointer;
 
-// Fornisce l'indirizzo della sequanza di byte di 'start'
+// Fornisce ciascun byte in esadecimale
 void show_bytes(byte_pointer start, int len) {
     int i;
     for (i=0; i<len; i++)
@@ -18,6 +18,14 @@ void show_bytes(byte_pointer start, int len) {
 
 void show_int(int x) {
     show_bytes((byte_pointer) &x, sizeof(int));
+}
+
+void show_float(float x) {
+    show_bytes((byte_pointer) &x, sizeof(float));
+}
+
+void show_pointer(void *x) {
+    show_bytes((byte_pointer) &x, sizeof(void *));
 }
 
 int main(void) {
