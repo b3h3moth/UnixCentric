ebd31877862e9f5e4077a02e7fa8de47118b5caa
B3h3m0th <behemoth@autistici.org>
2015-08-23 17:42:52 +0200

    scopo del programma

--- C_lang/variables_and_types/byte_representation.c
+++ C_lang/variables_and_types/byte_representation.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
+/*
+    Lo scopo del programma e' di fornire la 'byte representation' di differenti
+    programmi oggetto 
+*/
+
+typedef unsigned chat *byte_pointer;
+
+int main(void) {
+    
     return(EXIT_SUCCESS);
 }
