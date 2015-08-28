3fc826e39ca19d542dc05db91a3f46809b82a39b
B3h3m0th <behemoth@autistici.org>
2015-08-28 19:03:31 +0200

    ~ not e & AND

--- C_lang/bitwise_operators/bit_op.c
+++ C_lang/bitwise_operators/bit_op.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
+int main(void) {
+    int num = 0x00;
+    int not_num = ~num;
+    int and_num = 0x1011 & 0x0010;
+
+    printf(" num: 0x%x, %d (dec)\n", num, num);
+    printf("~num: 0x%x, %d (dec)\n", not_num, not_num);
+    printf("0x1011 & 0x0010 = 0x%x, %d (dec)\n", and_num, and_num);
+
     return(EXIT_SUCCESS);
 }
