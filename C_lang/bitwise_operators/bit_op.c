082e2cbcc00ba6dfa31f65bc99141fb9818fc324
B3h3m0th <behemoth@autistici.org>
2015-08-28 19:11:33 +0200

    risultati hex|dec migliorati in output

--- C_lang/bitwise_operators/bit_op.c
+++ C_lang/bitwise_operators/bit_op.c
@@ -4,11 +4,13 @@
 int main(void) {
     int num = 0x00;
     int not_num = ~num;
-    int and_num = 0x1011 & 0x0010;
+    int num_a = 0x1010;
+    int num_b = 0x1111;
+    int and_num_ab = num_a & num_b;
 
-    printf(" num: 0x%x, %d (dec)\n", num, num);
-    printf("~num: 0x%x, %d (dec)\n", not_num, not_num);
-    printf("0x1011 & 0x0010 = 0x%x, %d (dec)\n", and_num, and_num);
+    printf("num: 0x%x, %d (dec)\n", num, num);
+    printf("~num(0x%x), %d (dec)\n", not_num, not_num);
+    printf("num_a(0x%x) & num_b(0x%x) = 0x%x\n", num_a, num_b, and_num_ab);
 
     return(EXIT_SUCCESS);
 }
