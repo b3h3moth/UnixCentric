29efd92a74d45ec65d445066c506844680a2f2ad
B3h3m0th <behemoth@autistici.org>
2015-08-28 20:08:49 +0200

    XOR logico (^)

--- C_lang/bitwise_operators/bit_op.c
+++ C_lang/bitwise_operators/bit_op.c
@@ -8,10 +8,12 @@ int main(void) {
     int not_num = ~num;
     int and_num = num_a & num_b;
     int or_num = num_a | num_b;
+    int xor_num = num_a ^ num_b;
 
     printf("(NOT) ~num(0x%x) = 0x%x\n", num, not_num);
     printf("(AND) num_a(0x%x) & num_b(0x%x) = 0x%x\n", num_a, num_b, and_num);
     printf(" (OR) num_a(0x%x) | num_b(0x%x) = 0x%x\n", num_a, num_b, or_num);
+    printf("(XOR) num_a(0x%x) ^ num_b(0x%x) = 0x%x\n", num_a, num_b, xor_num);
 
     return(EXIT_SUCCESS);
 }
