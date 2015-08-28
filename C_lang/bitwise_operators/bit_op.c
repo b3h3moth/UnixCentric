a7109d7a0e8f5ed08e9c68d1f2e553d6116b17a0
B3h3m0th <behemoth@autistici.org>
2015-08-28 20:06:01 +0200

    OR logico (|)

--- C_lang/bitwise_operators/bit_op.c
+++ C_lang/bitwise_operators/bit_op.c
@@ -2,15 +2,16 @@
 #include <stdlib.h>
 
 int main(void) {
-    int num = 0x00;
-    int not_num = ~num;
+    int num = 0x00000000;
     int num_a = 0x1010;
     int num_b = 0x1111;
-    int and_num_ab = num_a & num_b;
+    int not_num = ~num;
+    int and_num = num_a & num_b;
+    int or_num = num_a | num_b;
 
-    printf("num: 0x%x, %d (dec)\n", num, num);
-    printf("~num(0x%x), %d (dec)\n", not_num, not_num);
-    printf("num_a(0x%x) & num_b(0x%x) = 0x%x\n", num_a, num_b, and_num_ab);
+    printf("(NOT) ~num(0x%x) = 0x%x\n", num, not_num);
+    printf("(AND) num_a(0x%x) & num_b(0x%x) = 0x%x\n", num_a, num_b, and_num);
+    printf(" (OR) num_a(0x%x) | num_b(0x%x) = 0x%x\n", num_a, num_b, or_num);
 
     return(EXIT_SUCCESS);
 }
