2be60b1d437eefb632ec19559b362ca6ffe9253f
B3h3m0th <behemoth@autistici.org>
2015-08-29 18:33:54 +0200

    migliorato il commento inerente la specifica dell'array tridimensionale

--- C_lang/array/multidimentional_array.c
+++ C_lang/array/multidimentional_array.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define NUM_PAGES 4
-#define NUM_ROWS 2
-#define NUM_COLS 2
+#define NUM_PAGES 2
+#define NUM_ROWS 3
+#define NUM_COLS 4
 
 /* Un array con piu' di due dimensioni puo' essere visto come  una sorta di
-pila di tabelle una sopra l'altra; la prima dimensione indica la pagina la
-seconda e la terza righe e colonne. */
+pila di tabelle o pagine una sopra l'altra; la prima dimensione indica la 
+pagina la seconda e la terza sono le righe e le colonne. */
 
 int main(void) {
 
-    // Array multidimensionale, tridimensionale
+    /* Array multidimensionale, tridimensionale
+    Una pila di 3 tabelle/pagine, ciscuna di esse formata da 3 righe e 4 
+    colonne */
     int array[NUM_PAGES][NUM_ROWS][NUM_COLS];
+
 
     return(EXIT_SUCCESS);
 }
