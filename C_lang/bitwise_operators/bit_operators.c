a522b1b92517062e5033a7c24db9735a7b83236c
B3h3m0th <behemoth@autistici.org>
2015-09-02 20:22:32 +0200

    inseriti i binari di ciascun valore ed operazione bit a bit

--- C_lang/bitwise_operators/bit_operators.c
+++ C_lang/bitwise_operators/bit_operators.c
@@ -4,20 +4,25 @@
 void bit_swap(int *x, int *y);
 
 int main(void) {
-    unsigned char num_a = 0; // binario: 00000000
-    unsigned char num_b = 1;
-    unsigned char num_c = 10;
+    unsigned char num_a = 1;    // binario 00000001
+    unsigned char num_b = 10;   // binario 00001010
+    unsigned char num_c = 100;  // binario 01100100
 
     // ~
     // One's complement operator (Operatore di completamento a uno)
     // inverte tutti i bit dell'operando su cui agisce
-    unsigned char res_not = ~num_a; // binario 11111111;
+    unsigned char res_not_a = ~num_a; // binario 11111110
+    unsigned char res_not_b = ~num_b; // binario 11110101
+    unsigned char res_not_c = ~num_c; // binario 10011011
 
-    unsigned char and_num = num_b & num_c;
-    unsigned char or_num = num_b | num_c;
+    unsigned char and_num = num_a & num_b;
+    unsigned char or_num = num_b | num_c; 
     unsigned char xor_num = num_b ^ num_c;
 
-    printf("(~) ~num(%d) = hex:0x%x - dec.: %d\n", num_a, res_not, res_not);
+    printf("  ~%d = %d - hex.0x%x\n", num_a, res_not_a, res_not_a);
+    printf(" ~%d = %d - hex.0x%x\n", num_b, res_not_b, res_not_b);
+    printf("~%d = %d - hex.0x%x\n", num_c, res_not_c, res_not_c);
+
     printf("(AND) %d & %d = %d - hex.0x%x\n", num_b, num_c, and_num, and_num);
     printf(" (OR) %d | %d = %d - hex.0x%x\n", num_b, num_c, or_num, or_num);
     printf("(XOR) %d ^ %d = %d - hex.0x%x\n", num_b, num_c, xor_num, xor_num);
