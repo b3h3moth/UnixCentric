1648961ce483073da2cd181f86a81f9421bdda56
B3h3m0th <behemoth@autistici.org>
2015-09-02 18:50:31 +0200

    cambiato il tipo da int ad unsigned char

--- C_lang/bitwise_operators/bit_operators.c
+++ C_lang/bitwise_operators/bit_operators.c
@@ -4,22 +4,23 @@
 void bit_swap(int *x, int *y);
 
 int main(void) {
-    int num = 0x00000000;
+    unsigned char num = 10;
     int num_a = 0x1010;
     int num_b = 0x1111;
-    int not_num = ~num;
+
+    // ~
+    // One's complement operator (Operatore di completamento a uno)
+    // inverte tutti i bit dell'operando su cui agisce
+    unsigned char not_num = ~num; // Diventa 0x11111111;
+
     int and_num = num_a & num_b;
     int or_num = num_a | num_b;
     int xor_num = num_a ^ num_b;
 
-    printf("(NOT) ~num(0x%x) = 0x%x\n", num, not_num);
+    printf("(NOT) ~num(0x%x) = 0x%x - Decimal: %d\n", num, not_num, not_num);
     printf("(AND) num_a(0x%x) & num_b(0x%x) = 0x%x\n", num_a, num_b, and_num);
     printf(" (OR) num_a(0x%x) | num_b(0x%x) = 0x%x\n", num_a, num_b, or_num);
     printf("(XOR) num_a(0x%x) ^ num_b(0x%x) = 0x%x\n", num_a, num_b, xor_num);
-
-    printf("before swapping - a = %d, b = %d\n", a, b);
-    bit_swap(&a, &b);
-    printf(" after swapping - a = %d, b = %d\n", a, b);
 
     return(EXIT_SUCCESS);
 }
