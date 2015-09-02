2248c2962d2a7d78bf92f1246c6fb5f12752347a
B3h3m0th <behemoth@autistici.org>
2015-09-02 20:08:55 +0200

    refactoring, migliorata la stampa

--- C_lang/bitwise_operators/bit_operators.c
+++ C_lang/bitwise_operators/bit_operators.c
@@ -5,22 +5,22 @@ void bit_swap(int *x, int *y);
 
 int main(void) {
     unsigned char num_a = 0; // binario: 00000000
-    unsigned char num_c = 1;
-    unsigned char num_b = 10;
+    unsigned char num_b = 1;
+    unsigned char num_c = 10;
 
     // ~
     // One's complement operator (Operatore di completamento a uno)
     // inverte tutti i bit dell'operando su cui agisce
     unsigned char res_not = ~num_a; // binario 11111111;
 
-    unsigned char and_num = num_c & num_b;
-    unsigned char or_num = num_c | num_b;
-    unsigned char xor_num = num_c ^ num_b;
+    unsigned char and_num = num_b & num_c;
+    unsigned char or_num = num_b | num_c;
+    unsigned char xor_num = num_b ^ num_c;
 
-    printf("(NOT) ~num(%4d) = hex:0x%x - dec.: %d\n", num_a, res_not, res_not);
-    printf("(AND) num_c(0x%x) & num_b(0x%x) = 0x%x\n", num_c, num_b, and_num);
-    printf(" (OR) num_c(0x%x) | num_b(0x%x) = 0x%x\n", num_c, num_b, or_num);
-    printf("(XOR) num_c(0x%x) ^ num_b(0x%x) = 0x%x\n", num_c, num_b, xor_num);
+    printf("(~) ~num(%d) = hex:0x%x - dec.: %d\n", num_a, res_not, res_not);
+    printf("(AND) %d & %d = %d - hex.0x%x\n", num_b, num_c, and_num, and_num);
+    printf(" (OR) %d | %d = %d - hex.0x%x\n", num_b, num_c, or_num, or_num);
+    printf("(XOR) %d ^ %d = %d - hex.0x%x\n", num_b, num_c, xor_num, xor_num);
 
     return(EXIT_SUCCESS);
 }
