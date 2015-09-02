ca9b0f185d4d144996c966f4efcf8ceaf9fe07df
B3h3m0th <behemoth@autistici.org>
2015-09-02 19:05:18 +0200

    fixed type

--- C_lang/bitwise_operators/bit_operators.c
+++ C_lang/bitwise_operators/bit_operators.c
@@ -4,18 +4,18 @@
 void bit_swap(int *x, int *y);
 
 int main(void) {
-    unsigned char num = 10;
-    int num_a = 0x1010;
-    int num_b = 0x1111;
+    unsigned char num = 0;
+    unsigned char num_a = 1;
+    unsigned char num_b = 10;
 
     // ~
     // One's complement operator (Operatore di completamento a uno)
     // inverte tutti i bit dell'operando su cui agisce
     unsigned char not_num = ~num; // Diventa 0x11111111;
 
-    int and_num = num_a & num_b;
-    int or_num = num_a | num_b;
-    int xor_num = num_a ^ num_b;
+    unsigned char and_num = num_a & num_b;
+    unsigned char or_num = num_a | num_b;
+    unsigned char xor_num = num_a ^ num_b;
 
     printf("(NOT) ~num(0x%x) = 0x%x - Decimal: %d\n", num, not_num, not_num);
     printf("(AND) num_a(0x%x) & num_b(0x%x) = 0x%x\n", num_a, num_b, and_num);
