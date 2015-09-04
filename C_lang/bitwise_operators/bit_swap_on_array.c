108f81c4aec96b79771b93b512c620b28b3d3a6c
B3h3m0th <behemoth@autistici.org>
2015-09-04 13:55:17 +0200

    commento, scopo del programma

--- C_lang/bitwise_operators/bit_swap_on_array.c
+++ C_lang/bitwise_operators/bit_swap_on_array.c
@@ -7,6 +7,10 @@
 void bit_swap(int *x, int *y);
 void reverse_array(int array[], int size);
 
+/* Lo scopo del programma e' di verificare il diverso risultato, rispetto a cio'
+che ci si aspetterebbe, nell'invocare la funzione reverse_array() con un numero
+di elementi prima pari poi dispari */
+
 int main(void) {
     int numbers_a[EVEN_SIZE] = {1,2,3,4,5,6,7,9,10};
     int  numbers_b[ODD_SIZE] = {1,2,3,4,5,6,7,9};
