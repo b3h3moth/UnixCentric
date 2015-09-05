b956dc956cd1fc5324361a0f316c640fcafc04e4
B3h3m0th <behemoth@autistici.org>
2015-09-05 14:39:43 +0200

    revisione dell'esempio, %.*s

--- C_lang/stuff/printf_width_precision.c
+++ C_lang/stuff/printf_width_precision.c
@@ -3,8 +3,18 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-    char str[] = "C Language Programming";
-    printf("%.*s", strlen(str), str);
+    char *unix_skills[] = { "C99 lang", "C11 lang", "C++11 lang", "Perl lang", 
+                            "Awk lang", "Python lang", "Bash scripting",
+                            "Zsh scripting", "System Administration", 
+                            "Newtork Administration" };
+
+    // Non e' una soluzione molto elegante questa, ne' portabile, per 
+    // calcolare il totale degli elementi dell'array di stringhe
+    unsigned int size = sizeof(unix_skills) / sizeof(*unix_skills);
+
+    // Indica la la lunghezza del dato
+    for (int i=0; i<size-1; i++)
+        printf("%.*s\n", strlen(unix_skills[i]), unix_skills[i]);
 
     return(EXIT_SUCCESS);
 }
