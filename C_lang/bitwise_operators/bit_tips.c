7d2f67d2e1c28a2e2efc309279d1e89ec7c55f2a
B3h3m0th <behemoth@autistici.org>
2015-09-07 19:55:07 +0200

    aggiunte considerazioni sui compilatori GCC e CLang (LLVM)

--- C_lang/bitwise_operators/bit_tips.c
+++ C_lang/bitwise_operators/bit_tips.c
@@ -5,14 +5,19 @@ int main(void) {
     /* Si imposta ciascun bit a 1 con l'operatore di complemento, si sta
     operando su un unsigned short di due byte, ovvero 16 bit. Nel caso
     specifico quindi si sta impostando il valore massimo permesso da quel tipo
-    di dato, ovverp 65535. */
-    unsigned short us_num1 = ~0;
+    di dato, ovvero 65535. */
+    unsigned short us_num1 = ~0; // da 0000000000000000 a 1111111111111111
 
-    /* Sia il compilatore GCC sia Clang imposteranno a 0 il risultato, poiche'
-    si e' inserito un numero maggiore rispetto al massimo consentito. */
+    /* E se provassi ad assegnare ad una variabile 'unsigned short' un valore 
+    maggiore di una sola unita' rispetto al massimo consentito? 
+    O uno o cento non fa differenza, si sara' superato il limite massimo
+    (overflow), per cui sia il compilatore GCC sia il compilatore Clang (LLVM)
+    troncheranno il valore della variabile impostandolo 0, il tutto condito da
+    un messaggio di warning sullo stdout. */
     unsigned short us_num2 = 65536;
     unsigned short us_num3 = us_num1 + 1;
 
     printf("%d %d %d\n", us_num1, us_num2, us_num3);
+    
     return(EXIT_SUCCESS);
 }
