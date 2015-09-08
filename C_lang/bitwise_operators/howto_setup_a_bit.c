55c6129c4d1c877b8d50a573a0de76afbecbf585
B3h3m0th <behemoth@autistici.org>
2015-09-08 14:12:03 +0200

    OR inclusivo per settare un bit, stampa del risultato

--- C_lang/bitwise_operators/howto_setup_a_bit.c
+++ C_lang/bitwise_operators/howto_setup_a_bit.c
@@ -5,10 +5,10 @@ int main(void) {
     /* Settare un bit
     Modifica del  bit numero 7 di una variabile di tipo 'unsigned short' da 16
     bit */
-    unsigned short num1 = 0x0030;   // 0000000000110000 |
-    num1 |= 0x40;                   // 0000000001000000 =
-                                    // 0000000001110000
-    printf("%d\n", num1);
+    unsigned short num1 = 0x0030;   // 0000000000110000 |   (dec. 48)
+    num1 |= 0x0040;                 // 0000000001000000 =   (dec. 64)
+                                    // 0000000001110000     (dec. 112)
+    printf("0x00%x |= 0x00%x = 0x00%x\n", 0x30, 0x40, num1);
     
     return(EXIT_SUCCESS);
 }
