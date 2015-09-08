cb146304ffb0795f1844e289bcdbbee7cd0db0ba
B3h3m0th <behemoth@autistici.org>
2015-09-08 14:26:29 +0200

    nuovo metodo con left-shift per settaggio bit

--- C_lang/bitwise_operators/howto_setup_a_bit.c
+++ C_lang/bitwise_operators/howto_setup_a_bit.c
@@ -8,7 +8,13 @@ int main(void) {
     unsigned short num1 = 0x0030;   // 0000000000110000 |   (dec. 48)
     num1 |= 0x0040;                 // 0000000001000000 =   (dec. 64)
                                     // 0000000001110000     (dec. 112)
-    printf("0x00%x |= 0x00%x = 0x00%x\n", 0x30, 0x40, num1);
-    
+    printf("0x%x |= 0x%x = 0x%x\n", 0x30, 0x40, num1);
+   
+    // Oppure con uno shift-left
+    unsigned short num2 = 0x0000;
+    unsigned short i = 4;
+    num2 |= 7 << i;
+    printf("0x%x\n", num2);
+
     return(EXIT_SUCCESS);
 }
