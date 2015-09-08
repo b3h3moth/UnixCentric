c3263b3411d0d5df41061f05ad97f7e32898fef7
B3h3m0th <behemoth@autistici.org>
2015-09-08 14:03:43 +0200

    setup a bit

--- C_lang/bitwise_operators/howto_setup_a_bit.c
+++ C_lang/bitwise_operators/howto_setup_a_bit.c
@@ -2,22 +2,13 @@
 #include <stdlib.h>
 
 int main(void) {
-    /* Si imposta ciascun bit di un 'unsigned short' - 2 byte, 16 bit - a 1
-    mediante l'operatore di complemento. Nel caso specifico trattasi del valore
-    massimo permesso da quel tipo di dato, ovvero 65535. */
-    unsigned short us_num1 = ~0; // da 0000000000000000 a 1111111111111111
-
-    /* Se si tentasse di assegnare ad una variabile di tipo 'unsigned short'
-    un valore maggiore di appena una sola unita' rispetto al massimo
-    consentito, cosa succedderebbe?
-    Zero, uno o cento non fa differenza, sara' superato il limite massimo
-    (overflow) consentito da quel tipo di dato, per cui sia il compilatore GCC
-    sia il compilatore Clang (LLVM) troncheranno il valore della variabile
-    impostandolo 0, il tutto condito da un messaggio di warning sullo stdout.*/
-    unsigned short us_num2 = 65536;
-    unsigned short us_num3 = us_num1 + 1;
-
-    printf("%d %d %d\n", us_num1, us_num2, us_num3);
+    /* Settare un bit
+    Modifica del  bit numero 7 di una variabile di tipo 'unsigned short' da 16
+    bit */
+    unsigned short num1 = 0x0030;   // 0000000000110000 |
+    num1 |= 0x40;                   // 0000000001000000 =
+                                    // 0000000001110000
+    printf("%d\n", num1);
     
     return(EXIT_SUCCESS);
 }
