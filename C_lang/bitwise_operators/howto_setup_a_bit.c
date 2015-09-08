0df5d3ad8891cbcc48ad275bd1f87e850214bb9d
B3h3m0th <behemoth@autistici.org>
2015-09-08 19:36:07 +0200

    setup a bit, fixed shift-left and mask

--- C_lang/bitwise_operators/howto_setup_a_bit.c
+++ C_lang/bitwise_operators/howto_setup_a_bit.c
@@ -2,21 +2,22 @@
 #include <stdlib.h>
 
 int main(void) {
-    /* Settare un bit (OR, |)
-    Modifica del bit numero 7 di una variabile di tipo 'unsigned short' da 16
-    bit */
+    /* Settare un bit
+    Modifica del bit numero 6 di una variabile di tipo 'unsigned short' da 16
+    bit, mediante un OR e una maschera con il bit nella posizione 6 impostato a
+    1 e i restanti bit a 0. */
     unsigned short num1 = 0x0030; // 0000000000110000 |   (dec. 48)
     num1 |= 0x0040;               // 0000000001000000 =   (dec. 64)
                                   // 0000000001110000     (dec. 112)
     printf("0x%x |= 0x%x - 0x%x\n", 0x30, 0x40, num1);
    
-    // Oppure con uno shift-left, si ottiene il medesimo risultato
-    unsigned short num2 = 0x0000; 
-    unsigned short i = 4;
+    // Oppure con uno shift-left
+    unsigned short num2 = 0x0030; 
+    unsigned short i = 6;
 
-    num2 |= 7 << i;
+    num2 |= 1 << i;
     
-    printf("num |= %d << %d - 0x%x\n", 7, i, num2);
+    printf("0x%X |= %d << %d - 0x%x\n", 0x00, 1, i, num2);
 
     return(EXIT_SUCCESS);
 }
