08aabe9cfa92d5cc330811e57a9b290fb53348f2
B3h3m0th <behemoth@autistici.org>
2015-09-11 18:53:19 +0200

    modificare un campo di bit

---
+++ C_lang/bitwise_operators/howto_change_a_bit_field.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    /* Modificare un campo di bit
+    Si vogliono modificare i bit della variabile 'num' dal bit 4 al bit 6 */
+
+    return(EXIT_SUCCESS);
+}
