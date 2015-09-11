eb879d5e62ebc6b391b3ea721058e3e25f6e4ebb
B3h3m0th <behemoth@autistici.org>
2015-09-11 20:26:45 +0200

    test su campi di bit, modifica

--- C_lang/bitwise_operators/howto_change_a_bit_field.c
+++ C_lang/bitwise_operators/howto_change_a_bit_field.c
@@ -4,6 +4,9 @@
 int main(void) {
     /* Modificare un campo di bit
     Si vogliono modificare i bit della variabile 'num' dal bit 4 al bit 6 */
+    unsigned int num = 0;
+    num = num & ~0x0070 | 0x0050;
+    printf("%d:0x%x\n", num, num);
 
     return(EXIT_SUCCESS);
 }
