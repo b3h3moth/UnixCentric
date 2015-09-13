b64a470b9d351bb59cb5024b3d05e2f0757c1a00
B3h3m0th <behemoth@autistici.org>
2015-09-13 18:24:20 +0200

    fixed output

--- C_lang/bitwise_operators/howto_change_a_bit_field.c
+++ C_lang/bitwise_operators/howto_change_a_bit_field.c
@@ -5,13 +5,13 @@ int main(void) {
     /* Modificare un campo di bit
     Si vogliono modificare i bit della variabile 'num' dal bit 4 al bit 6 */
     unsigned int num = 0;
-    num = num & ~0x0070 | 0x0050; /* L'operatore bitwise AND '&' ha una 
+    num = num & ~0x0070 | 0x0050; /* L'operatore bitwise AND '&' ha una
                                      precedenza maggiore rispetto all'operatore
-                                     bitwise OR '|', quindi le parentesi in 
-                                     questo caso non sono necessarie, tuttavia 
-                                     inserirle per migliorare la leggibilita' 
+                                     bitwise OR '|', quindi le parentesi in
+                                     questo caso non sono necessarie, tuttavia
+                                     inserirle per migliorare la leggibilita'
                                      non sarebbe affatto un reato */
-    printf("%d:0x%x\n", num, num);
+    printf("(dec.) %d - (hex) 0x%x\n", num, num);
 
     return(EXIT_SUCCESS);
 }
