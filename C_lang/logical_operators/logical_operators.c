9564bbc27bc32527d9b6d7f09df4be10df42e5ef
B3h3m0th <behemoth@autistici.org>
2015-09-15 17:51:59 +0200

    operatori logici

---
+++ C_lang/logical_operators/logical_operators.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    /* Gli operatori logici in C sono: '||' (OR), '&&' (AND) e '!' (NOT), essi
+    restituiscono 1 (TRUE) o 0 (FALSE) e non devono essere confusi con gli
+    operatori bitwise. */
+    return(EXIT_SUCCESS);
+}
