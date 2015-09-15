70f519f444613dd14a9dfece5b086f7b2a0032d0
B3h3m0th <behemoth@autistici.org>
2015-09-15 19:07:26 +0200

    bitwise and logical operations in the same expression

--- C_lang/logical_operators/logical_operators.c
+++ C_lang/logical_operators/logical_operators.c
@@ -6,8 +6,8 @@ int main(void) {
     restituiscono 1 (TRUE) o 0 (FALSE) e non devono essere confusi con gli
     operatori bitwise. */
 
-    unsigned int num1 = 0x66;   // 1100110 - 65 (dec.)
-    unsigned int num2 = 0x39;   //  111001  - 57 (dec.)
+    unsigned int num1 = 0x66;   // 01100110
+    unsigned int num2 = 0x39;   // 00111001
     
     unsigned int bit_res1 = num1 & num2;
     unsigned int log_res1 = num1 && num2;
@@ -15,6 +15,8 @@ int main(void) {
     unsigned int log_res2 = num1 || num2;
     unsigned int bit_res3 = ~num1 | ~num2;
     unsigned int log_res3 = !num1 || !num2;
+    unsigned int bit_res4 = num1 & !num2;
+    unsigned int log_res4 = num1 && ~num2;
 
     printf("(bitwise)  0x%x  &  0x%x = 0x%x\n", num1, num2, bit_res1);
     printf("(logical)  0x%x &&  0x%x = 0x%x\n", num1, num2, log_res1);
@@ -22,6 +24,8 @@ int main(void) {
     printf("(logical)  0x%x ||  0x%x = 0x%x\n", num1, num2, log_res2);
     printf("(bitwise) ~0x%x |  ~0x%x = 0x%x\n", num1, num2, bit_res3);
     printf("(logical) !0x%x || !0x%x = 0x%x\n", num1, num2, log_res3);
+    printf("(bitwise)  0x%x  & !0x%x = 0x%x\n", num1, num2, bit_res4);
+    printf("(logical)  0x%x && ~0x%x = 0x%x\n", num1, num2, log_res4);
 
     return(EXIT_SUCCESS);
 }
