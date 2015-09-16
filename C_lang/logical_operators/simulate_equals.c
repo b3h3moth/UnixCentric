df7d89ef390630c52e5ba911d2931674290fa596
B3h3m0th <behemoth@autistici.org>
2015-09-16 19:20:48 +0200

    simulazione del == con NOT logico e XOR bitwise

--- C_lang/logical_operators/simulate_equals.c
+++ C_lang/logical_operators/simulate_equals.c
@@ -2,5 +2,24 @@
 #include <stdlib.h>
 
 int main(void) {
+    unsigned int num1 = 0x20;
+    unsigned int num2 = 0x30;
+    unsigned int num3 = 32;
+
+    if (!(num1 ^ num2))
+        printf("0x%x and 0x%x are equals\n", num1, num2);
+    else
+        printf("0x%x and 0x%x are not equals\n", num1, num2);
+
+    if (!(num2 ^ num3))
+        printf("0x%x and 0x%x are equals\n", num2, num3);
+    else
+        printf("0x%x and 0x%x are not equals\n", num2, num3);
+
+    if (!(num1 ^ num3))
+        printf("0x%x and 0x%x are equals\n", num1, num3);
+    else
+        printf("0x%x and 0x%x are not equals\n", num1, num3);
+    
     return(EXIT_SUCCESS);
 }
