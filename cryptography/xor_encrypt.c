26404cf7b05da92d4a7da34dd8cd3bd7e0957b95
B3h3m0th <behemoth@autistici.org>
2015-09-20 18:34:57 +0200

    test cifratura XOR completo

--- cryptography/xor_encrypt.c
+++ cryptography/xor_encrypt.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define KEY_BASE '%'
+
 int main(void) {
+    int original_char, new_char;
+
+    while((original_char = getchar()) != EOF) {
+        new_char = original_char ^ KEY_BASE;
+
+        if (isprint(original_char) && isprint(new_char))
+            putchar(new_char);
+        else
+            putchar(original_char);
+    }
+
     return(EXIT_SUCCESS);
 }
