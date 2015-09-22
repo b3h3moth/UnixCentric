996412805c263c6a29d2ee944812b5ca4fa9dbf2
B3h3m0th <behemoth@autistici.org>
2015-09-22 19:20:16 +0200

    recupero campo di bit con right-shift

--- C_lang/bitwise_operators/howto_get_a_bit_field.c
+++ C_lang/bitwise_operators/howto_get_a_bit_field.c
@@ -5,10 +5,14 @@ int main(void) {
     /* Recuperare un valore da un campo di bit
     In questo caso si recuperano i 3 bit meno significativi, ovvero 0,1,2 */
     unsigned int num1 = 0xAF; // 10101111
-    unsigned int num2 = num1 & 0x7;
-    // num2 reecupera i 3 bit meso significativi e vale 00000111, 0x7
 
-    printf("num1:0x%X num2:0x%X\n", num1, num2);
+    // num2 reecupera i 3 bit meso significativi e vale 00000111, 0x07
+    unsigned int num2 = num1 & 0x07;
+
+    // num3 recupera i bit 4,5,6 mediante shift, e vale 00000010, 0x02
+    unsigned int num3 = (num1 >> 4) & 0x07;
+ 
+    printf("num1:0x%X num2:0x%X num3:0x%X\n", num1, num2, num3);
 
     return(EXIT_SUCCESS);
 }
