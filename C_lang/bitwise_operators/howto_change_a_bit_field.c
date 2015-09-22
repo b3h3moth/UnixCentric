6b93a7d16011b2af5f19927d498fe1f3fd477a38
B3h3m0th <behemoth@autistici.org>
2015-09-22 18:29:59 +0200

    parentesi, per la leggibilita'

--- C_lang/bitwise_operators/howto_change_a_bit_field.c
+++ C_lang/bitwise_operators/howto_change_a_bit_field.c
@@ -5,7 +5,7 @@ int main(void) {
     /* Modificare un campo di bit
     Si vogliono modificare i bit della variabile 'num' dal bit 4 al bit 6 */
     unsigned int num = 0;
-    num = num & ~0x0070 | 0x0050; /* L'operatore bitwise AND '&' ha una
+    num = (num & ~0x0070) | 0x0050; /* L'operatore bitwise AND '&' ha una
                                      precedenza maggiore rispetto all'operatore
                                      bitwise OR '|', quindi le parentesi in
                                      questo caso non sono necessarie, tuttavia
