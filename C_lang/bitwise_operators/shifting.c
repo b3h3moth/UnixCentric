3edd85f7214676e78e6e39b4a6e54028f563e10f
B3h3m0th <behemoth@autistici.org>
2015-09-22 18:35:46 +0200

    shifting, word 32 bit

--- C_lang/bitwise_operators/shifting.c
+++ C_lang/bitwise_operators/shifting.c
@@ -3,7 +3,8 @@
 
 int main(void) {
     /* In tutti e 3 i casi ci sara' un messaggio di warning del compilatore,
-    inoltre ciascun risultato sara' 0x00. */
+    inoltre ciascun risultato sara' 0x00. Il motivo e' la lunghezza della word
+    a 32 bit. */
     int      num1 = 0xFEDCBA98  << 32; // 32 bit
     int      num2 = 0xFEDCBA98  >> 36; // 32 bit
     unsigned num3 = 0xFEDCBA98  >> 40; // 32 bit
