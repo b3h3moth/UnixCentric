a99ad42882b25325a2b6a9bc698054ecf5b5a9d5
B3h3m0th <behemoth@autistici.org>
2015-09-22 12:07:20 +0200

    info su chiasmata di funzione con array, prototipo, dichiarazione

--- C_lang/functions/02_array_as_parameter.c
+++ C_lang/functions/02_array_as_parameter.c
@@ -3,13 +3,16 @@
 
 #define SIZE    10
 
-// Prototipo
+// Prototipo, l'array sia nel prototipo sia nella dichiarazione della funzione
+// e' inserito con l'operatore di subscript [], la lunghezza dello stesso 
+// invece non e' invece obbligatoria
 int sum(int data[], int length);
 
 int main(void) {
     int my_data[SIZE] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
 
     // Chiamata della funzione, con l'array - monodimensionale in questo caso.
+    // L'operatore di subscript non e' necessario.
     int res = sum(my_data, SIZE);
 
     printf("Sum is: %d\n", res);
