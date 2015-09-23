221fd9ae8995623e30d696e309af1d94d1c38923
B3h3m0th <behemoth@autistici.org>
2015-09-23 10:41:14 +0200

    ROWS & COLS, lunghezza di righe e colonne dell'array multidim.

--- C_lang/functions/03_array_multidim_as_parameter.c
+++ C_lang/functions/03_array_multidim_as_parameter.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Lunghezza di righe e colonne dell'array multidimensionale
+#define TOT_ROWS 2
+#define TOT_COLS 3
+
 int main(void) {
     return(EXIT_SUCCESS);
 }
