46c6587bffaa8d2fabf339f929d639c3a7a848c7
B3h3m0th <behemoth@autistici.org>
2015-09-25 18:59:35 +0200

    casting da short a unsigned short, rappresentazione bit

--- C_lang/integer_representations/conversions_signed_unsigned.c
+++ C_lang/integer_representations/conversions_signed_unsigned.c
@@ -9,7 +9,19 @@ int main(void) {
     /* 'short int' oppure semplicemente 'short' e' un tipo di dato da 2 byte, 
     ovvero 16 bit. */
     short val = -12345;
+
+    /* Casting di un numero negativo di tipo 'short' in un 'unsigned short',
+    entrambi di 2 byte ma con valori minimi e massimi differenti */
     unsigned short uval = (unsigned short) val;
+
+    /* Il risultato sara':
+
+    val = -12345 (binario: 1110001111110011)
+    uval = 53191 (binario: 1110001111110011)
+    
+    Il casting da short [int] ad 'unsigned short' cambia il valore numerico 
+    lasciando immutata la raoppresentazione in complemento a due dei bit.
+    */
 
     printf("val=%d\nuval=%d\n", val, uval);
 
