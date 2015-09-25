a51b68021cd33e15b919a2c6d52ac749bc6de175
B3h3m0th <behemoth@autistici.org>
2015-09-25 18:45:28 +0200

    sostituito short int, con l'equivalente short

--- C_lang/integer_representations/conversions_signed_unsigned.c
+++ C_lang/integer_representations/conversions_signed_unsigned.c
@@ -3,8 +3,15 @@
 
 int main(void) {
     /* Convertire un numero negativo in un 'unsigned' potrebbe dare come
-    risultato 0, convertire invece un 'unsigned' troppo grande per essere
-    rappresentato in complemento a due potrebbe dare il massimo valore di quel
-    tipo di dato */
+    risultato 0, per quanto riguarda le conversioni ci si deve sempre basare
+    sulla rappresentazione dei singoli bit. */
+
+    /* 'short int' oppure semplicemente 'short' e' un tipo di dato da 2 byte, 
+    ovvero 16 bit. */
+    short val = -12345;
+    unsigned short uval = (unsigned short) val;
+
+    printf("val=%d\nuval=%d\n", val, uval);
+
     return(EXIT_SUCCESS);
 }
