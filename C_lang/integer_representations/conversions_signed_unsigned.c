6303a320d12aaa6013885ff7c7c2368e5fe30045
B3h3m0th <behemoth@autistici.org>
2015-09-25 18:37:11 +0200

    info su numeri negativi, unsigned e complemento a due

--- C_lang/integer_representations/conversions_signed_unsigned.c
+++ C_lang/integer_representations/conversions_signed_unsigned.c
@@ -2,5 +2,9 @@
 #include <stdlib.h>
 
 int main(void) {
+    /* Convertire un numero negativo in un 'unsigned' potrebbe dare come
+    risultato 0, convertire invece un 'unsigned' troppo grande per essere
+    rappresentato in complemento a due potrebbe dare il massimo valore di quel
+    tipo di dato */
     return(EXIT_SUCCESS);
 }
