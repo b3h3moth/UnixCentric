b455f6b304567e505774291fa68c91702baee81e
B3h3m0th <behemoth@autistici.org>
2015-09-29 10:07:58 +0200

    two's complement machine, signed vs unsigned

--- C_lang/integer_representations/signed_unsigned.c
+++ C_lang/integer_representations/signed_unsigned.c
@@ -2,5 +2,13 @@
 #include <stdlib.h>
 
 int main(void) {
+    /* Generalmente un 'int', su una macchina two's complement, e' dichiarato
+    signed di default */
+    int val1 = -1; // signed di default
+    unsigned val2 = -1;
+
+    printf("  signed val1: %d %x %u\n", val1, val1, val1);
+    printf("unsigned val2: %d %x %u\n", val2, val2, val2);
+
     return(EXIT_SUCCESS);
 }
