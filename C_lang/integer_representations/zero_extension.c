a8d8772a0948155ad94534327de784fb2a6fda6c
B3h3m0th <behemoth@autistici.org>
2015-09-30 19:49:44 +0200

    byte representation, unsigned short and int

--- C_lang/integer_representations/zero_extension.c
+++ C_lang/integer_representations/zero_extension.c
@@ -5,7 +5,14 @@
 int main(void) {
     /* La conversione dal tipo 'unsigned' ad un tipo di maggior grandezza
     riempira' con diversi zero i bit iniziali (zero extension) */
-    unsigned short val = 12345;
-    int cval = val;
+    unsigned short us_val1 = 12345;
+    int i_val1 = us_val1;
+
+    printf("(unsigned short=%d)\t", us_val1);
+    show_bytes((byte_pointer) &us_val1, sizeof(unsigned short));
+
+    printf("(int=%d)\t", i_val1);
+    show_bytes((byte_pointer) &i_val1, sizeof(int));
+
     return(EXIT_SUCCESS);
 }
