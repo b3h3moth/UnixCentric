82b3ea40d15460ea7f9f173ad3b61ec47da1453e
B3h3m0th <behemoth@autistici.org>
2015-09-30 18:48:53 +0200

    zero extension, from unsigned to int

---
+++ C_lang/integer_representations/zero_extension.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    /* La conversione dal tipo 'unsigned' ad un tipo di maggior grandezza
+    riempira' con diversi zero i bit iniziali (zero extension) */
+    unsigned short val = 12345;
+    int cval = val;
+
+
+    return(EXIT_SUCCESS);
+}
