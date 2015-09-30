9b7fb517f28e76028bd95433cde4c53fe14fab11
B3h3m0th <behemoth@autistici.org>
2015-09-30 20:35:15 +0200

    fixed data types on comments

--- C_lang/integer_representations/zero_extension.c
+++ C_lang/integer_representations/zero_extension.c
@@ -6,9 +6,9 @@ int main(void) {
     /* La conversione dal tipo 'unsigned' ad un tipo di maggior grandezza
     riempira' con diversi zero i bit iniziali (zero extension) */
 
-    unsigned short val1 = 12345;    // short 2 byte
-    unsigned val2 = val1;           // int 4 byte
-    unsigned long long val3 = val1; // long long 8 byte
+    unsigned short val1 = 12345;    // unsigned short 2 byte
+    unsigned val2 = val1;           // unsigned 4 byte
+    unsigned long long val3 = val1; // unsigned long long 8 byte
 
     printf("(val1=%d)\t", val1);
     show_bytes((byte_pointer) &val1, sizeof(unsigned short));
