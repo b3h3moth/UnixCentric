88b15128e587236c1adf63acb646d382247db532
B3h3m0th <behemoth@autistici.org>
2015-09-30 10:37:01 +0200

    modalita' per assegnare array a un puntatore

--- C_lang/pointers/pointers_and_array.c
+++ C_lang/pointers/pointers_and_array.c
@@ -2,5 +2,11 @@
 #include <stdlib.h>
 
 int main(void) {
+    int data[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    
+    // Due modalita' equivalenti per assegnare un array a un puntatore
+    int *ptr_arr1 = data;    // 'data' contiene l'indirizzo del primo elemento
+    int *ptr_arr2 = &data[0] // Si rende esplicito il concetto sopra espresso
+
     return(EXIT_SUCCESS);
 }
