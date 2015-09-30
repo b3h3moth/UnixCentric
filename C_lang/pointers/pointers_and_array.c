9ae8cf170f7399c942e72d892831c259eb95d7ad
B3h3m0th <behemoth@autistici.org>
2015-09-30 10:40:21 +0200

    scostamento dell'array e stampa

--- C_lang/pointers/pointers_and_array.c
+++ C_lang/pointers/pointers_and_array.c
@@ -5,8 +5,11 @@ int main(void) {
     int data[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
     
     // Due modalita' equivalenti per assegnare un array a un puntatore
-    int *ptr_arr1 = data;    // 'data' contiene l'indirizzo del primo elemento
-    int *ptr_arr2 = &data[0] // Si rende esplicito il concetto sopra espresso
+    int *ptr_arr1 = data;     // 'data' contiene l'indirizzo del primo elemento
+    int *ptr_arr2 = &data[0]; // Si rende esplicito il concetto sopra espresso
+
+    ptr_arr1 += 4;  // punta al quinto elemento dell'array
+    printf("%d\n", *ptr_arr1);  // Stampa il valore del quinto elemento
 
     return(EXIT_SUCCESS);
 }
