2d137a18e940c81fd513f5350c65e2450534ba13
B3h3m0th <behemoth@autistici.org>
2015-09-30 20:23:10 +0200

    unsigned short, unsigned, unsigned long long, conversions

--- C_lang/integer_representations/zero_extension.c
+++ C_lang/integer_representations/zero_extension.c
@@ -5,18 +5,19 @@
 int main(void) {
     /* La conversione dal tipo 'unsigned' ad un tipo di maggior grandezza
     riempira' con diversi zero i bit iniziali (zero extension) */
-    short val1 = 12345;     // short 2 byte
-    int val2 = val1;        // int 4 byte
-    long long val3 = val1;  // long long 8 byte
+
+    unsigned short val1 = 12345;    // short 2 byte
+    unsigned val2 = val1;           // int 4 byte
+    unsigned long long val3 = val1; // long long 8 byte
 
     printf("(val1=%d)\t", val1);
     show_bytes((byte_pointer) &val1, sizeof(unsigned short));
 
     printf("(val2=%d)\t", val2);
-    show_bytes((byte_pointer) &val2, sizeof(int));
+    show_bytes((byte_pointer) &val2, sizeof(unsigned));
 
     printf("(val3=%lld)\t", val3);
-    show_bytes((byte_pointer) &val3, sizeof(long long));
+    show_bytes((byte_pointer) &val3, sizeof(unsigned long long));
 
     return(EXIT_SUCCESS);
 }
