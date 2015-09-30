0989e9f497aa74f7706b46ec071d2626f9436698
B3h3m0th <behemoth@autistici.org>
2015-09-30 19:33:49 +0200

    from usigned short to int

--- C_lang/integer_representations/zero_extension.c
+++ C_lang/integer_representations/zero_extension.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "show_bytes.h"
 
 int main(void) {
     /* La conversione dal tipo 'unsigned' ad un tipo di maggior grandezza
     riempira' con diversi zero i bit iniziali (zero extension) */
     unsigned short val = 12345;
     int cval = val;
-
-
     return(EXIT_SUCCESS);
 }
