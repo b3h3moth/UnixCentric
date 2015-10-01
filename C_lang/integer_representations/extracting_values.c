9b6801674854a8402f6110171fb965a66bfb8549
B3h3m0th <behemoth@autistici.org>
2015-10-01 19:45:43 +0200

    prototipi e definizioni di foo() bar()

--- C_lang/integer_representations/extracting_values.c
+++ C_lang/integer_representations/extracting_values.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int foo(unsigned word);
+int bar(unsigned word);
+
 int main(void) {
     return(EXIT_SUCCESS);
+}
+
+int foo(unsigned word) {
+    return (int) ((word << 24) >> 24);
+}
+
+int bar(unsigned word) {
+    return ((int) word << 24) >> 24;
 }
