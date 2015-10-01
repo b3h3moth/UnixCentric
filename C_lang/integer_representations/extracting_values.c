d6e086a56d2b372bc0a9cd23381d5980824f9b65
B3h3m0th <behemoth@autistici.org>
2015-10-01 20:17:08 +0200

    shifting arithmetic, logical

--- C_lang/integer_representations/extracting_values.c
+++ C_lang/integer_representations/extracting_values.c
@@ -5,6 +5,17 @@ int foo(unsigned word);
 int bar(unsigned word);
 
 int main(void) {
+    unsigned val1 = 0x00000076;
+    unsigned val2 = 0x87654321;
+    unsigned val3 = 0x000000c9;
+    unsigned val4 = 0xEDCBA987;
+
+    printf("%10s %12s\n", "foo()", "bar()");
+    printf("%#.8x - %#.8x\n", foo(val1), bar(val1));
+    printf("%#.8x - %#.8x\n", foo(val2), bar(val2));
+    printf("%#.8x - %#.8x\n", foo(val3), bar(val3));
+    printf("%#.8x - %#.8x\n", foo(val4), bar(val4));
+
     return(EXIT_SUCCESS);
 }
 
