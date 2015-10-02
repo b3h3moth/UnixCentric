a32570073daf1fce4fd347be8804dadf469c4d77
B3h3m0th <behemoth@autistici.org>
2015-10-02 19:27:04 +0200

    comment about truncating numbers

--- C_lang/integer_representations/truncating_numbers.c
+++ C_lang/integer_representations/truncating_numbers.c
@@ -2,5 +2,8 @@
 #include <stdlib.h>
 
 int main(void) {
+    /* Talvolta e' necessario convertire un tipo di dato in un altro tipo di
+    minor grandezza, in questo caso vi e' la possibilita' che taluni bit 
+    possano essere troncati */
     return(EXIT_SUCCESS);
 }
