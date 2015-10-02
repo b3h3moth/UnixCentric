6ac1c72f174089f3c1206590736e7d76806aba1e
B3h3m0th <behemoth@autistici.org>
2015-10-02 12:19:49 +0200

    replaced tricks with tips and tricks

--- C_lang/integer_representations/tips_and_tricks.c
+++ C_lang/integer_representations/tips_and_tricks.c
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 
 int main(void) {
-    /* Con una word di 32 bit e complemento a due, il tipo di dato int ha un
-    valore massimo positivo uguale a 2147483647, se volessi il minimo valore
+    /* Con una word di 32 bit e complemento a due, il tipo di dato 'int' ha un
+    valore massimo positivo uguale a 2147483647, e se volessi il minimo valore
     negativo rappresentabile? */
     int max_int = 2147483647;
     int min_int = max_int + 1;
    
     /* I numeri rappresentabili su un calcolatore sono finiti, per cui
     sommando uno al valore massimo e' come se si chiudesse il cerchio dei 
-    postivi per aprire nuovamente i negativi, ma il concetto fondamentale e'
-    che il calcolatore lavora coi numeri binari, pertanto:
+    postivi per aprire nuovamente i quello dei negativi, ma il concetto 
+    fondamentale e'che il calcolatore lavora coi numeri binari, pertanto:
 
     0111 1111 1111 1111 1111 1111 1111 1111 +        2147483647 +
                                           1 =                 1 =
