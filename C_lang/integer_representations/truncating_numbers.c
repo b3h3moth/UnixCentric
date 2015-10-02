72a11813339c866732e14a3ea06bf7afd4cfc903
B3h3m0th <behemoth@autistici.org>
2015-10-02 19:37:44 +0200

    printf() sui valori

--- C_lang/integer_representations/truncating_numbers.c
+++ C_lang/integer_representations/truncating_numbers.c
@@ -6,9 +6,11 @@ int main(void) {
     minor grandezza, in questo caso vi e' la possibilita' che taluni bit 
     possano essere troncati */
 
-    // Casting da 'int' 'short'
-    int val1 = 12345;           // int 4 byte (32 bit)
+    // Casting da 'int' a 'short'
+    int val1 = 53191;           // int 4 byte (32 bit)
     short val2 = (short) val1;  // short 2 byte (16 bit)
 
+    printf("val1 %#10x %7d\nval2 %#10x %7d\n", val1, val1, val2, val2);
+    
     return(EXIT_SUCCESS);
 }
