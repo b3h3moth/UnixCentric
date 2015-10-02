60fabf7f38230fb9234b95972d3ed72187279f41
B3h3m0th <behemoth@autistici.org>
2015-10-02 20:04:19 +0200

    ulteriori esempi, truncating numbers

--- C_lang/integer_representations/truncating_numbers.c
+++ C_lang/integer_representations/truncating_numbers.c
@@ -7,13 +7,21 @@ int main(void) {
     possano essere troncati */
 
     // Casting da 'int' a 'short'
-    int val1 = 53191;           // int 4 byte (32 bit)
-    short val2 = (short) val1;  // short 2 byte (16 bit)
-    unsigned val3 = 65535;
-    unsigned short val4 = (unsigned short) val3;
+    int val1 = 53191;                               // 4 byte (32 bit)
+    short val2 = (short) val1;                      // 2 byte (16 bit)
+    // Casting da 'unsigned' ad 'unsigned short'
+    unsigned val3 = 65535;                          // 4 byte (32 bit)
+    unsigned short val4 = (unsigned short) val3;    // 2 byte (16 bit)
+    // Casting da 'int' a 'short'
+    int val5 = -212748364;
+    short val6 = (short) val5;
+    int val7 = 212748364;
+    short val8 = (short) val7;
 
-    printf("from %#10x %7d\n  to %#10x %7d\n", val1, val1, val2, val2);
-    printf("from %#10x %7d\n  to %#10x %7d\n", val3, val3, val4, val4);
+    printf("from %#12x %12d\n  to %#12x %12d\n", val1, val1, val2, val2);
+    printf("from %#12x %12d\n  to %#12x %12d\n", val3, val3, val4, val4);
+    printf("from %#12x %12d\n  to %#12x %12d\n", val5, val5, val6, val6);
+    printf("from %#12x %12d\n  to %#12x %12d\n", val7, val7, val8, val8);
     
     return(EXIT_SUCCESS);
 }
