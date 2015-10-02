0759130ac55caf80cfbccc8b15a399d8b743e0f6
B3h3m0th <behemoth@autistici.org>
2015-10-02 19:52:46 +0200

    from int to short, from unsigned to unsigned short

--- C_lang/integer_representations/truncating_numbers.c
+++ C_lang/integer_representations/truncating_numbers.c
@@ -9,8 +9,11 @@ int main(void) {
     // Casting da 'int' a 'short'
     int val1 = 53191;           // int 4 byte (32 bit)
     short val2 = (short) val1;  // short 2 byte (16 bit)
+    unsigned val3 = 65535;
+    unsigned short val4 = (unsigned short) val3;
 
-    printf("val1 %#10x %7d\nval2 %#10x %7d\n", val1, val1, val2, val2);
+    printf("from %#10x %7d\n  to %#10x %7d\n", val1, val1, val2, val2);
+    printf("from %#10x %7d\n  to %#10x %7d\n", val3, val3, val4, val4);
     
     return(EXIT_SUCCESS);
 }
