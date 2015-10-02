c44772b5900b8c3b54fe5550e587d25ea7c6f4b2
B3h3m0th <behemoth@autistici.org>
2015-10-02 19:29:34 +0200

    casting from int to short, short casting

--- C_lang/integer_representations/truncating_numbers.c
+++ C_lang/integer_representations/truncating_numbers.c
@@ -2,8 +2,13 @@
 #include <stdlib.h>
 
 int main(void) {
-    /* Talvolta e' necessario convertire un tipo di dato in un altro tipo di
+    /* Talvolta e' necessario convertire un tipo di dato in un altro di
     minor grandezza, in questo caso vi e' la possibilita' che taluni bit 
     possano essere troncati */
+
+    // Casting da 'int' 'short'
+    int val1 = 12345;           // int 4 byte (32 bit)
+    short val2 = (short) val1;  // short 2 byte (16 bit)
+
     return(EXIT_SUCCESS);
 }
