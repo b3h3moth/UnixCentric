6f49efbab3f0f1e0443a397f3363d4b82d2ed05b
B3h3m0th <behemoth@autistici.org>
2015-10-02 12:01:00 +0200

    numeri finiti rappresentabili

--- C_lang/integer_representations/tricks.c
+++ C_lang/integer_representations/tricks.c
@@ -2,5 +2,16 @@
 #include <stdlib.h>
 
 int main(void) {
+    /* Con una word di 32 bit e complemento a due, il tipo di dato int ha un
+    valore massimo positivo uguale a 2147483647, se volessi il minimo valore
+    negativo rappresentabile? */
+    int max_int = 2147483647;
+    int min_int = max_int + 1;
+   
+    /* I numeri rappresentabili su un calcolatore sono finiti, per cui
+    sommando uno al valore massimo e' come se si tornasse al punto di partenza
+    */
+    printf("max_int = %d\tmin_int = %d\n", max_int, min_int);
+
     return(EXIT_SUCCESS);
 }
