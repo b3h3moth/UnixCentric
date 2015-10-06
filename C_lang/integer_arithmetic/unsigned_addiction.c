93ce058b3ff729e3499f109806e53c3d34a26287
B3h3m0th <behemoth@autistici.org>
2015-10-06 21:35:44 +0200

    check if arguments can be added without overflow

--- C_lang/integer_arithmetic/unsigned_addiction.c
+++ C_lang/integer_arithmetic/unsigned_addiction.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int check_overflow(unsigned x, unsigned y);
+
 int main(void) {
+    unsigned val1 = 5;
+    unsigned val2 = 6;
+
+    int res = check_overflow(val1, val2);
+    printf("res: %d\n", res);
+
     return(EXIT_SUCCESS);
+}
+
+// Returna 1 se gli argomenti possono essere sommati senza overlow
+int check_overflow(unsigned x, unsigned y) {
+    unsigned sum = x + y;
+
+    return (sum >= x);
 }
