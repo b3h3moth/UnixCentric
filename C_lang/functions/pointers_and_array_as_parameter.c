4244bd362e061bd3fbf44faab6e03408c8217fd6
B3h3m0th <behemoth@autistici.org>
2015-10-06 11:14:39 +0200

    prototipo funzione ed equivalenti allo stesso

--- C_lang/functions/pointers_and_array_as_parameter.c
+++ C_lang/functions/pointers_and_array_as_parameter.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define SIZE 5
+
 /* Quando si dichiara una funzione con un parametro formale di tipo array, ad
 esempio 'function(int data[])', il parametro formale data[] viene letto dal
 compilatore 'int *data', ovvero come se fosse un puntatore. Tale comportamento
 avviene solo nel contesto della dichiarazione di un parametro formale. */
+
+int sum(int data[], int len);
+
+/* Prototipi equivalenti: 
+                            int sum(int data*, int len);
+                            int sum(int [], int len);
+                            int sum(int *, int len);
+*/
+
 
 int main(void) {
     return(EXIT_SUCCESS);
