94a71c89cf66cbb433eec3fa5107960173926ddd
B3h3m0th <behemoth@autistici.org>
2015-10-06 11:11:16 +0200

    parametri formali e puntatori

---
+++ C_lang/functions/pointers_and_array_as_parameter.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Quando si dichiara una funzione con un parametro formale di tipo array, ad
+esempio 'function(int data[])', il parametro formale data[] viene letto dal
+compilatore 'int *data', ovvero come se fosse un puntatore. Tale comportamento
+avviene solo nel contesto della dichiarazione di un parametro formale. */
+
+int main(void) {
+    return(EXIT_SUCCESS);
+}
