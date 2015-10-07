c3144b29481c10040cefc5487303c640e4b5bed4
B3h3m0th <behemoth@autistici.org>
2015-10-07 19:56:35 +0200

    puntatore all'indirizzo [0][0]

--- C_lang/pointers/pointers_and_arraymultidim.c
+++ C_lang/pointers/pointers_and_arraymultidim.c
@@ -8,23 +8,26 @@ int main(void) {
     };
 
     // Entrambi ritornano l'indirizzo dell'elemento 0
-    printf("       data: %p\n", (void *)data);
-    printf("   &data[0]: %p\n", (void *)&data[0]);
+    printf("        data: %p\n", (void *)data);
+    printf("    &data[0]: %p\n", (void *)&data[0]);
 
     // Ritorna l'indirizzo dell'elemento 0
-    printf("    data[0]: %p\n", (void *)data[0]);
+    printf("     data[0]: %p\n", (void *)data[0]);
 
     // Ritorna l'indirizzo dell'elemento [0][0]
-    printf("&data[0][0]: %p\n", (void *)&data[0][0]);
+    printf(" &data[0][0]: %p\n", (void *)&data[0][0]);
 
     // Ritorna l'indirizzo iniziale dell'array
-    printf("      &data: %p\n", (void *)&data);
+    printf("       &data: %p\n", (void *)&data);
 
     // Entrambi puntano all'indirizzo 0
-    printf("      *data: %p\n", (void *)*data);
-    printf("  *&data[0]: %p\n", (void *)*&data[0]);
+    printf("       *data: %p\n", (void *)*data);
+    printf("   *&data[0]: %p\n", (void *)*&data[0]);
 
     // Ritorna il valore del primo elemento, [0][0]
-    printf("   *data[0]: %d\n", *data[0]);
+    printf("    *data[0]: %d\n", *data[0]);
+
+    // Ritorna il valore del primo elemento, [0][0]
+    printf("*&data[0][0]: %d\n", *&data[0][0]);
     return(EXIT_SUCCESS);
 }
