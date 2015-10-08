1df0500367e50ee0bdbb7b8bbc819542ffe9d01e
B3h3m0th <behemoth@autistici.org>
2015-10-08 18:48:30 +0200

    Print 0 if MSB (Most Significant Bit) is 0, 1 otherwise

--- C_lang/bitwise_operators/from_decimal_to_binary.c
+++ C_lang/bitwise_operators/from_decimal_to_binary.c
@@ -7,13 +7,14 @@ int main(void) {
 
     int size = sizeof(val);
     int bit_val = size * CHAR_BIT;
-    int mask = 1 << (bit_val - 1); // Work on Most Significan Bit
+    int mask = 1 << (bit_val - 1); // Work on Most Significant Bit
 
     printf("%d byte - %d bit\n", size, bit_val);
 
     for (int i=1; i<=bit_val; ++i) {
 
-        ( ((val & mask) == 0) ? printf("0") : printf("1") );
+        // Print 0 if MSB (Most Significant Bit) is 0, 1 otherwise
+        (val & mask) == 0 ? printf("0") : printf("1");
 
         val <<= 1;
 
