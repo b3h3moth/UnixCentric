a385b1e5dd690413120b2426f175b28ab4dcdc68
B3h3m0th <behemoth@autistici.org>
2015-10-08 16:06:17 +0200

    maschera per determinare il LSB (least significant bit)

--- C_lang/bitwise_operators/mask.c
+++ C_lang/bitwise_operators/mask.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define LEN 10
+
 int main(void) {
     /* Una maschera e' una costante o una variabile, e' utilizzata per estrarre
     i bit desiderati da una variabile o da un'espressione */
+
+    int mask = 1; // 00000000 00000000 00000000 00000001
+
+    // Si utilizza la maschera per determinare il bit meno significativo
+    for (int i=0; i<LEN; i++)
+        printf("%d & mask(%d) = %d\n", i, mask, i & mask);
+
     return(EXIT_SUCCESS);
 }
