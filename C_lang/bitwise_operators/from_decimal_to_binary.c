ff934c2aa521e21ad8b7ab02fa8d5c2d96e18d4e
B3h3m0th <behemoth@autistici.org>
2015-10-08 18:44:14 +0200

    from decimal to binary, with mask, working on MSB

--- C_lang/bitwise_operators/from_decimal_to_binary.c
+++ C_lang/bitwise_operators/from_decimal_to_binary.c
@@ -7,17 +7,18 @@ int main(void) {
 
     int size = sizeof(val);
     int bit_val = size * CHAR_BIT;
+    int mask = 1 << (bit_val - 1); // Work on Most Significan Bit
 
     printf("%d byte - %d bit\n", size, bit_val);
 
-    for (int i=0; i<bit_val; i++) {
-        if (val & (1 << i))
-            printf("1");
-        else
-            printf("0");
+    for (int i=1; i<=bit_val; ++i) {
 
-        // Stampa a gruppi di 8 bit
-        if (i % CHAR_BIT == 0 && i != 0)
+        ( ((val & mask) == 0) ? printf("0") : printf("1") );
+
+        val <<= 1;
+
+        // Print 8 bit then add a space
+        if ((i % 8 == 0) && (i != 0))
             printf(" ");
     }
 
