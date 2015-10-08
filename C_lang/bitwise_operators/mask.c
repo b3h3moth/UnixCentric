6f82f6e9e864a673697bb83022b795e086a36ffc
B3h3m0th <behemoth@autistici.org>
2015-10-08 15:59:51 +0200

    il concetto di maschera (mask)

--- C_lang/bitwise_operators/mask.c
+++ C_lang/bitwise_operators/mask.c
@@ -2,5 +2,7 @@
 #include <stdlib.h>
 
 int main(void) {
+    /* Una maschera e' una costante o una variabile, e' utilizzata per estrarre
+    i bit desiderati da una variabile o da un'espressione */
     return(EXIT_SUCCESS);
 }
