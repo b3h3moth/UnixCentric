1cfdbac199134b43b405fbdbefbe48b77733e42e
B3h3m0th <behemoth@autistici.org>
2015-10-09 19:16:49 +0200

    Check for various possible errors, errno, perror()

--- C_lang/bitwise_operators/from_decimal_to_binary.c
+++ C_lang/bitwise_operators/from_decimal_to_binary.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h> // to get CHAR_BIT, or 8
+#include <limits.h>
+#include <errno.h>
 
 int main(int argc, char *argv[]) {
     
@@ -8,8 +9,18 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Usage: %s <decimal_number>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
-
+    
+    errno = 0;
+    
+    // To convert the initial part of the string/parameter to integer
     int val = strtol(argv[1], NULL, 10);
+
+    // Check for various possible errors
+    if ((errno == ERANGE && (val == LONG_MAX || val == LONG_MIN)) \
+                         || (errno != 0 && val == 0)) {
+        perror("strtol()");
+        exit(EXIT_FAILURE);
+    }
 
     int size = sizeof(val);
     int bit_val = size * CHAR_BIT;
