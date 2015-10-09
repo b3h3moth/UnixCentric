c8c3eb7dfd70535cd94e6040a05e19a14c5df43e
B3h3m0th <behemoth@autistici.org>
2015-10-09 23:27:44 +0200

    definizione di pack_char()

--- C_lang/bitwise_operators/pack_char.c
+++ C_lang/bitwise_operators/pack_char.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+
+/* Lo scopo del programma e' di inserire 4 caratteri da un byte ciascuno in un
+intero da 32 bit, ovvero 4 byte */
+
+int pack_char(char cha, char chb, char chc, char chd);
 
 int main(void) {
     return(EXIT_SUCCESS);
+}
+
+int pack_char(char cha, char chb, char chc, char chd) {
+    int integer_val = cha;
+
+    integer_val =<< CHAR_BIT | chb;
+    integer_val =<< CHAR_BIT | chc;
+    integer_val =<< CHAR_BIT | chd;
+
+    return integer_val;
 }
