78124966767fb77c439e1e321f766a2179a6d380
B3h3m0th <behemoth@autistici.org>
2015-10-10 18:03:06 +0200

    added show_short/double/long() function definitions

--- C_lang/representing_information/addressing_and_byte_ordering/show_bytes.c
+++ C_lang/representing_information/addressing_and_byte_ordering/show_bytes.c
@@ -3,9 +3,8 @@
 
 // Returns each byte in hexadecimal format
 void show_bytes(byte_pointer start, int len) {
-    int i;
 
-    for (i=0; i<len; i++) {
+    for (int i=0; i<len; i++) {
         printf("%.2x ", start[i]);
     }
 
@@ -13,16 +12,31 @@ void show_bytes(byte_pointer start, int len) {
 }
 
 // Show 'int' data type
-void show_int(int x) {
-    show_bytes((byte_pointer) &x, sizeof(int));
+void show_int(int data) {
+    show_bytes((byte_pointer) &data, sizeof(int));
 }
 
 // Show 'float' data type
-void show_float(float x) {
-    show_bytes((byte_pointer) &x, sizeof(float));
+void show_float(float data) {
+    show_bytes((byte_pointer) &data, sizeof(float));
 }
 
 // Show 'void' data type
-void show_pointer(void *x) {
-    show_bytes((byte_pointer) &x, sizeof(void *));
+void show_pointer(void *data) {
+    show_bytes((byte_pointer) &data, sizeof(void *));
+}
+
+// Show 'short' data type
+void show_short(short data) {
+    show_bytes((byte_pointer) &data, sizeof(short));
+}
+
+// Show 'double' data type
+void show_double(double data) {
+    show_bytes((byte_pointer) &data, sizeof(double));
+}
+
+// Show 'long' data type
+void show_long(long data) {
+    show_bytes((byte_pointer) &data, sizeof(long));
 }
