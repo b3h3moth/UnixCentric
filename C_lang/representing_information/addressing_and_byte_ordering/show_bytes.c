963dcf275b172495e00598af358c0fde807b009c
B3h3m0th <behemoth@autistici.org>
2015-10-10 18:59:56 +0200

    added decimal number before hex representation

--- C_lang/representing_information/addressing_and_byte_ordering/show_bytes.c
+++ C_lang/representing_information/addressing_and_byte_ordering/show_bytes.c
@@ -13,30 +13,36 @@ void show_bytes(byte_pointer start, int len) {
 
 // Show 'int' data type
 void show_int(int data) {
+    printf("[%d:    int] - ", data);
     show_bytes((byte_pointer) &data, sizeof(int));
 }
 
 // Show 'float' data type
 void show_float(float data) {
+    printf("[%.f:  float] - ", data);
     show_bytes((byte_pointer) &data, sizeof(float));
 }
 
 // Show 'void' data type
 void show_pointer(void *data) {
+    printf("[%d: void *] - ", (int)data);
     show_bytes((byte_pointer) &data, sizeof(void *));
 }
 
 // Show 'short' data type
 void show_short(short data) {
+    printf("[%d:  short] - ", data);
     show_bytes((byte_pointer) &data, sizeof(short));
 }
 
 // Show 'double' data type
 void show_double(double data) {
+    printf("[%g: double] - ", data);
     show_bytes((byte_pointer) &data, sizeof(double));
 }
 
 // Show 'long' data type
 void show_long(long data) {
+    printf("[%ld:   long] - ", data);
     show_bytes((byte_pointer) &data, sizeof(long));
 }
