87703c9d2f3e1291151bccdcbbec38edc1235fea
B3h3m0th <behemoth@autistici.org>
2015-10-10 19:24:04 +0200

    endian() function added

--- C_lang/representing_information/addressing_and_byte_ordering/endian.c
+++ C_lang/representing_information/addressing_and_byte_ordering/endian.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+int endian(void);
+
 int main(void) {
     return(EXIT_SUCCESS);
 }
+
+/* Se il puntatore dereferenziato puntera' ad 1 la macchina avra' una
+architettura - ordinamento dei byte nello specifico -, di tipo little-endian, 
+big-endian altrimenti */
+int endian(void);
