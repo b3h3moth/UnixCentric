14ac3581c5e31948bdf31072a1639572a5dff1c8
B3h3m0th <behemoth@autistici.org>
2015-10-10 19:26:58 +0200

    definition of endian()

--- C_lang/representing_information/addressing_and_byte_ordering/endian.c
+++ C_lang/representing_information/addressing_and_byte_ordering/endian.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+// Prototipo funzione
 int endian(void);
 
 int main(void) {
@@ -11,4 +11,8 @@ int main(void) {
 /* Se il puntatore dereferenziato puntera' ad 1 la macchina avra' una
 architettura - ordinamento dei byte nello specifico -, di tipo little-endian, 
 big-endian altrimenti */
-int endian(void);
+int endian(void) {
+    int number_one = 1;
+    char *ptr = (char *)&number_one;
+    return *ptr;
+}
