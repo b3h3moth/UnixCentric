d342c870737f4d1cb7c3e70cb0d5905efb31c8de
B3h3m0th <behemoth@autistici.org>
2015-10-11 16:21:12 +0200

    commented each line about data types

--- C_lang/representing_information/addressing_and_byte_ordering/show_bytes.c
+++ C_lang/representing_information/addressing_and_byte_ordering/show_bytes.c
@@ -3,46 +3,44 @@
 
 // Returns each byte in hexadecimal format
 void show_bytes(byte_pointer start, int len) {
-
-    for (int i=0; i<len; i++) {
+    for (int i=0; i<len; i++)
         printf("%.2x ", start[i]);
-    }
 
     printf("(%d byte)\n", len);
 }
 
 // Show 'int' data type
 void show_int(int data) {
-    printf("[%d:    int] - ", data);
+    // printf("[%d:    int] - ", data);
     show_bytes((byte_pointer) &data, sizeof(int));
 }
 
 // Show 'float' data type
 void show_float(float data) {
-    printf("[%.f:  float] - ", data);
+    // printf("[%.f:  float] - ", data);
     show_bytes((byte_pointer) &data, sizeof(float));
 }
 
 // Show 'void' data type
 void show_pointer(void *data) {
-    printf("[%d: void *] - ", (int)data);
+    // printf("[%d: void *] - ", (int)data);
     show_bytes((byte_pointer) &data, sizeof(void *));
 }
 
 // Show 'short' data type
 void show_short(short data) {
-    printf("[%d:  short] - ", data);
+    // printf("[%d:  short] - ", data);
     show_bytes((byte_pointer) &data, sizeof(short));
 }
 
 // Show 'double' data type
 void show_double(double data) {
-    printf("[%g: double] - ", data);
+    // printf("[%g: double] - ", data);
     show_bytes((byte_pointer) &data, sizeof(double));
 }
 
 // Show 'long' data type
 void show_long(long data) {
-    printf("[%ld:   long] - ", data);
+    // printf("[%ld:   long] - ", data);
     show_bytes((byte_pointer) &data, sizeof(long));
 }
