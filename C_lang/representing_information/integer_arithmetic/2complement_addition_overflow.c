b87506862fe8bf4bc918a0c71f4777608ede9b04
B3h3m0th <behemoth@autistici.org>
2015-10-14 22:54:19 +0200

    check_overflow() function, two's complement addition

--- C_lang/representing_information/integer_arithmetic/2complement_addition_overflow.c
+++ C_lang/representing_information/integer_arithmetic/2complement_addition_overflow.c
@@ -6,10 +6,35 @@
 tra due valori in complemento a due, e quindi di segnalare un eventuale
 overflow, sia positivo sia negativo */
 
-int main(void) {
-    printf("MAX_INT: %11d\n", MAX_INT);
-    printf("MIN_INT: %11d\n", MIN_INT);
+int check_overflow(int x, int y);
 
-    int val1 = 0;
+int main(void) {
+    int val1 = 2147483647;
+    int val2 = 1;
+    int val3 = 2;
+
+    printf("MAX_INT: %11d\n", INT_MAX);
+    printf("MIN_INT: %11d\n", INT_MIN);
+
+    if (check_overflow(val1, val2))
+        printf("%d + %d = %d\n", val1, val2, 1);
+
     return(EXIT_SUCCESS);
+}
+
+int check_overflow(int x, int y) {
+    int sum = x + y;
+
+    int negative_overflow = x < 0 && y < 0 && sum >= 0;
+    int positive_overflow = x >= 0 && y >= 0 && sum < 0;
+
+    return !negative_overflow && !positive_overflow;
+    /*
+    if (INT_MAX <= x + y)
+        return 0;
+    else if ( x + y < INT_MIN)
+        return 0;
+    else 
+        return 1;
+    */
 }
