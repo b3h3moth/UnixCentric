01d7bde0b6a82df1bf11698a22d65b029b8f2b01
B3h3m0th <behemoth@autistici.org>
2015-10-14 18:44:17 +0200

    the program's purpose

--- C_lang/representing_information/integer_arithmetic/2complement_addition_overflow.c
+++ C_lang/representing_information/integer_arithmetic/2complement_addition_overflow.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Lo scopo del programma e' di verificare la correttezza della somma algebrica
+tra due valori in complemento a due, e quindi di segnalare un eventuale
+overflow, sia positivo sia negativo */
 int main(void) {
     return(EXIT_SUCCESS);
 }
