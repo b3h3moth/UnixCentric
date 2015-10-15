1d96b97052f1b21eaf4706136429e45121639cf7
B3h3m0th <behemoth@autistici.org>
2015-10-15 17:05:19 +0200

    sum() function

--- C_lang/representing_information/integer_arithmetic/2complement_addition_overflow.c
+++ C_lang/representing_information/integer_arithmetic/2complement_addition_overflow.c
@@ -18,10 +18,10 @@ int main(void) {
     printf("MIN_INT: %11d\n", INT_MIN);
 
     int res1 = check_overflow(val1, val2);
-    printf("%d + %d = %d (res: %d\n", val1, val2, val1+val2, res1);
+    printf("%d + %d = %d (returns: %d)\n", val1, val2, val1+val2, res1);
 
     int res2 = check_overflow(val1, val3);
-    printf("%d + %d = %d (res: %d - overflow)\n", val1, val3, val1+val3, res2);
+    printf("%d + %d = %d (resturns: %d - overflow)\n", val1, val3, val1+val3, res2);
 
     return(EXIT_SUCCESS);
 }
@@ -40,4 +40,5 @@ int check_overflow(int x, int y) {
 }
 
 int sum(int x, int y) {
+    return (!check_overflow(x, y));
 }
