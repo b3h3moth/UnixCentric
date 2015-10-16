08ae71745e93d1f77358662059f4bb87df689be7
B3h3m0th <behemoth@autistici.org>
2015-10-17 01:49:26 +0200

    compare hexadecimal and binary numbers, both int 32 bit (4 byte)

--- C_lang/stuff/binary_constant.c
+++ C_lang/stuff/binary_constant.c
@@ -7,11 +7,16 @@ Una costante binaria puo' essere scritta utilizzando il prefisso
 '0b' oppure '0B', non e' tuttavia parte di alcuno standard */
 
 int main(void) {
-    int x = 0B00000001; // 1
-    int y = 0b00000010; // 2
-    int z = x + y;
+    int val1 = 0B00000001; // 1
+    int val2 = 0b00000010; // 2
+    int total = val1 + val2;
+    int val3 = 0xF;
+    int val4 = 0b00001111;
 
-    printf("%d + %d = %d\n", x, y , z);
+    printf("%d + %d = %d\n", val1, val2, total);
+
+    if (val3 == val4)
+        printf("They are both %d\n", val4);
 
     return(EXIT_SUCCESS);
 }
