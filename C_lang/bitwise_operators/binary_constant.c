29b0defb7813adebcf185b6085cfcaeaed353f1f
B3h3m0th <behemoth@autistici.org>
2015-10-16 20:05:26 +0200

    warning: binary integer literals are a GNU extension

--- C_lang/bitwise_operators/binary_constant.c
+++ C_lang/bitwise_operators/binary_constant.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Una costante binaria puo' essere scritta in GCC utilizzando il prefisso 
+'0b' oppure '0B' */
+
 int main(void) {
+    int x = 0B00000001; // 1
+    int y = 0b00000010; // 2
+    int z = x + y;
+
+    printf("%d + %d = %d\n", x, y , z);
+
     return(EXIT_SUCCESS);
 }
