b69e16424dec62018757c14e2b5de16bc1c87ac1
B3h3m0th <behemoth@autistici.org>
2015-10-17 20:12:30 +0200

    moved integer 'fd' (filehandle) at the top of the code

--- C_lang/representing_information/integer_representations/hexdump.c
+++ C_lang/representing_information/integer_representations/hexdump.c
@@ -11,15 +11,14 @@ int main (int argc, char* argv[]) {
     unsigned char buffer[MAX_BUF];
     size_t offset = 0;
     size_t bytes_read;
-    int i;
+    int i, fd;
     
     if (argc == 1) {
         fprintf(stderr,"Usage: %s <filename>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
-        
+
     // Open the file for reading
-    int fd;
     if ((fd = open (argv[1], O_RDONLY)) == -1) {
         fprintf(stderr, "Err. open: %s\n", strerror(errno));
         exit(EXIT_FAILURE);
