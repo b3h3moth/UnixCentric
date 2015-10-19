f2bbfedef52e98328bb8e1110d8a91a9ab2fe706
B3h3m0th <behemoth@autistici.org>
2015-10-19 20:08:23 +0200

    checking integer overflow, multiplication is correct

--- C_lang/representing_information/integer_arithmetic/multiplication_overflow.c
+++ C_lang/representing_information/integer_arithmetic/multiplication_overflow.c
@@ -4,11 +4,21 @@
 int check_overflow(int x, int y);
 
 int main(void) {
+    int val1 = 900000000;
+    int val2 = 2;
+
+    int result = check_overflow(val1, val2);
+
+    if (result)
+        printf("%d * %d = %d\n", val1, val2, val1*val2);
+    else
+        printf("Err. %d - overflow\n", result);
+
     return(EXIT_SUCCESS);
 }
 
 int check_overflow(int x, int y) {
-    int result = x * y;
+    int res = x * y;
 
-    return !x || result/x == y;
+    return !x || res/x == y;
 }
