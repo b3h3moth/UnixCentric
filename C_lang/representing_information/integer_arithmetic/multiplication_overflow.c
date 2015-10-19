048ad6f2f4bf7f7bf84310224782c0fdad7782b5
B3h3m0th <behemoth@autistici.org>
2015-10-19 20:11:28 +0200

    replaced val2 with val3

--- C_lang/representing_information/integer_arithmetic/multiplication_overflow.c
+++ C_lang/representing_information/integer_arithmetic/multiplication_overflow.c
@@ -6,18 +6,20 @@ int check_overflow(int x, int y);
 int main(void) {
     int val1 = 900000000;
     int val2 = 2;
+    int val3 = 3;
 
     int result1 = check_overflow(val1, val2);
+    int result2 = check_overflow(val1, val3);
 
     if (result1)
         printf("%d * %d = %d\n", val1, val2, val1*val2);
     else
         printf("Err. %d - overflow\n", result1);
 
-    if (result1)
-        printf("%d * %d = %d\n", val1, val2, val1*val2);
+    if (result2)
+        printf("%d * %d = %d\n", val1, val3, val1*val3);
     else
-        printf("Err. %d - overflow\n", result1);
+        printf("Err. %d - overflow\n", result2);
 
     return(EXIT_SUCCESS);
 }
