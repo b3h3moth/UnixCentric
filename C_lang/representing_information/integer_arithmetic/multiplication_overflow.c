e3aaa52c3e5d967dfcb8b6aa7cb227a5cdaacb3b
B3h3m0th <behemoth@autistici.org>
2015-10-19 20:00:39 +0200

    function prototype and defifinition, check_overflow()

--- C_lang/representing_information/integer_arithmetic/multiplication_overflow.c
+++ C_lang/representing_information/integer_arithmetic/multiplication_overflow.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int check_overflow(int x, int y);
+
 int main(void) {
     return(EXIT_SUCCESS);
+}
+
+int check_overflow(int x, int y) {
+    int result = x * y;
+
+    return !x || result/x == y;
 }
