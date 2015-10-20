e72eb9d1e94830cfc14fdaf69713a59e76504a2e
B3h3m0th <behemoth@autistici.org>
2015-10-20 12:40:54 +0200

    multiplication (unsigned, two's complement) equivalent to addition and shifting

--- C_lang/representing_information/integer_arithmetic/multiplication_vs_shifting.c
+++ C_lang/representing_information/integer_arithmetic/multiplication_vs_shifting.c
@@ -6,8 +6,14 @@ moltiplicazione potrebbe essere sostituita da shifting e addizione, ma solo
 se si tratta di potenze del 2 */
 
 int main(void) {
-    int val1 = 10;
-    int val2 = 20;
+    int val1 = 4;
+    int val2 = 8;
+
+    int res1 = val1 * val2;
+    int res2 = (2 << 3) + (2 << 3);
+
+    printf("%d * %d = %d\n", val1, val2, res1);
+    printf("%d + %d = %d\n", (2 << 3), (2 << 3), res2);
 
     return(EXIT_SUCCESS);
 }
