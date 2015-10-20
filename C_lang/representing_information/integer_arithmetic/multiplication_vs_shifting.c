880b3c5a25b1ed9eb1b15fa5e2d050e9760c4dbf
B3h3m0th <behemoth@autistici.org>
2015-10-20 12:29:40 +0200

    program's purpose

--- C_lang/representing_information/integer_arithmetic/multiplication_vs_shifting.c
+++ C_lang/representing_information/integer_arithmetic/multiplication_vs_shifting.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Sia per interi 'unsigned' sia per interi in "two's complement" la 
+moltiplicazione potrebbe essere sostituita da shifting e addizione, ma solo
+se si tratta di potenze del 2 */
+
 int main(void) {
+    int val1 = 10;
+    int val2 = 20;
+
     return(EXIT_SUCCESS);
 }
