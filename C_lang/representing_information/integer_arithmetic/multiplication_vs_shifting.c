53960c499c0edb9962e44f075b5c4fa4dac47fa9
B3h3m0th <behemoth@autistici.org>
2015-10-20 19:11:33 +0200

    improved code legibility

--- C_lang/representing_information/integer_arithmetic/multiplication_vs_shifting.c
+++ C_lang/representing_information/integer_arithmetic/multiplication_vs_shifting.c
@@ -6,31 +6,25 @@ moltiplicazione potrebbe essere sostituita da shifting e addizione,
 o sottrazione */
 
 int main(void) {
-    int val1 = 4;
-    int val2 = 8;
-
-    int res1 = val1 * val2;
-    int res2 = (2 << 3) + (2 << 3); // 32
-    int res3 = (2 << 4);            // 32
-
-    printf("%d * %d = %d\n", val1, val2, res1);
-    printf("%d + %d = %d - %d\n", (2 << 3), (2 << 3), res2, res3);
 
     // The total is 15 (5*3)
-    int val3 = 5;
-    int res4 = (val3 << 1) + val3;
-    printf("%d\n", res4);
+    int vala = 5;
+    int resx = (vala << 1) + vala;
+    printf("%d\n", resx);
 
     // The total is 9 (3*3)
-    int val4 = 3;
-    int res5 = (val4 << 1) + val4;
-    printf("%d\n", res5);
+    int valb = 3;
+    int resy = (valb << 1) + valb;
+    printf("%d\n", resy);
 
     // The total is 6 (1*6)
-    int val5 = 1;
-    int res6 = (x<<2) + (x<<1);
-    printf("%d\n", res6);
+    int valc = 1;
+    int resw = (valc << 2) + (valc << 1);
+    printf("%d\n", resw);
 
+    // The total is 31 (1*31)
+    int resz = (valc << 5) - valc;
+    printf("%d\n", resz);
 
     return(EXIT_SUCCESS);
 }
