a3e36d2943e841c0c53056a12e2dd1b163e61f00
B3h3m0th <behemoth@autistici.org>
2015-10-21 16:47:02 +0200

    signed short multiplication_8bit prototype

--- C_lang/representing_information/integer_arithmetic/multiplication_booth_algorithm.c
+++ C_lang/representing_information/integer_arithmetic/multiplication_booth_algorithm.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+signed short multiplication_8bit(signed char x, signed char y);
+
 int main(void) {
     return(EXIT_SUCCESS);
 }
+
+/* Booth algorithm per la moltiplicazione numeri 
+in two's complement con word-size di 8-bit */
+signed short multiplication_8bit(signed char x, signed char y);
