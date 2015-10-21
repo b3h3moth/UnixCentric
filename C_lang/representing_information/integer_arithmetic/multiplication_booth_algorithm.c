66ce5cbef080b7f746177bf9aedf1695a71f74e3
B3h3m0th <behemoth@autistici.org>
2015-10-21 16:54:20 +0200

    Booth Algorithm definition

--- C_lang/representing_information/integer_arithmetic/multiplication_booth_algorithm.c
+++ C_lang/representing_information/integer_arithmetic/multiplication_booth_algorithm.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-signed short multiplication_8bit(signed char x, signed char y);
+#include <limits.h>
+signed short multiplication_8bit(signed char m, signed char r);
 
 int main(void) {
     return(EXIT_SUCCESS);
@@ -9,4 +9,26 @@ int main(void) {
 
 /* Booth algorithm per la moltiplicazione numeri 
 in two's complement con word-size di 8-bit */
-signed short multiplication_8bit(signed char x, signed char y);
+signed short multiplication_8bit(signed char m, signed char r) {
+    signed int A, S, P;
+    unsigned char i;
+
+    A = m << 9;
+    S = (-m) << 9;
+    P (r & 0xFF) << 1;
+
+    for (i=0; i<CHAR_BIT; i++) {
+        switch ( P & 3) {
+            case 1:     // 01
+                p += A;
+                break;
+            case 2:     // 10
+                p += S;
+                break;
+            default:    // 11 oppure 00
+                break;
+        }
+        p >>= 1;
+    }
+    return P >> 1;
+}
