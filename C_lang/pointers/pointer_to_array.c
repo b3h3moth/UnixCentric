76e17ac9eb98475fcced471590923ccf6576860a
B3h3m0th <behemoth@autistici.org>
2015-10-22 22:08:37 +0200

    address of and indirection

--- C_lang/pointers/pointer_to_array.c
+++ C_lang/pointers/pointer_to_array.c
@@ -8,12 +8,12 @@ int main(void) {
     char *ptr = football_team[0];
     char **ptr_to_ptr = &ptr;
 
-    printf("Memory address of:\n");
+    printf("       address of:\n");
     printf("              ptr: %p\n", ptr);
     printf(" football_team[0]: %p\n", (void *)football_team[0]);
     printf("       ptr_to_ptr: %p\n", (void *)*ptr_to_ptr);
 
-    printf("      Dereference:\n");
+    printf("      Indirection:\n");
     printf("              ptr: %s\n", ptr);
     printf("       ptr_to_ptr: %s\n", *ptr_to_ptr);
 
