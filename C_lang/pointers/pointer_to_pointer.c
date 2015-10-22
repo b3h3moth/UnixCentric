d9149d248e30d2a411703a605c21a790210e6070
B3h3m0th <behemoth@autistici.org>
2015-10-22 19:18:41 +0200

    fixed output, added (void *) to print memory addresses

--- C_lang/pointers/pointer_to_pointer.c
+++ C_lang/pointers/pointer_to_pointer.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 
 int main(void) {
-    int number = 100;
+    int number = 31337;
     int *ptr = &number;
     int **ptr_to_ptr = &ptr;
 
-    printf("address of number: %p\n", &number);
-    printf("address of number: %p\n", ptr);
-    printf("address of number: %p\n", *ptr_to_ptr);
+    printf("      address of number: %p\n", (void *)&number);
+    printf("         *ptr points to: %p\n", (void *)ptr);
+    printf(" **ptr_to_ptr points to: %p\n", (void *)*ptr_to_ptr);
 
     return(EXIT_SUCCESS);
 }
