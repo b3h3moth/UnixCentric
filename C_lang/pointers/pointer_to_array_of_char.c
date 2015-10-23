520d77d8c386ec2034108bc21ebfa754491589b9
B3h3m0th <behemoth@autistici.org>
2015-10-23 14:10:41 +0200

    get offset for the secondo element of the pointer to array

--- C_lang/pointers/pointer_to_array_of_char.c
+++ C_lang/pointers/pointer_to_array_of_char.c
@@ -8,6 +8,9 @@ int main(void) {
     char *ptr = football_team[0];
     char **ptr_to_ptr = &ptr;
 
+    // Si calcola l'offset per il secondo elemento del puntatore ad array
+    short int offset_second_elem = football_team[1] - football_team[0];
+
     printf("       address of:\n");
     printf("              ptr: %p\n", ptr);
     printf(" football_team[0]: %p\n", (void *)football_team[0]);
@@ -16,6 +19,8 @@ int main(void) {
     printf("      Indirection:\n");
     printf("              ptr: %s\n", ptr);
     printf("       ptr_to_ptr: %s\n", *ptr_to_ptr);
+
+    printf(" football_team[1]: %s\n", (ptr + offset_second_elem) );
 
     return(EXIT_SUCCESS);
 }
