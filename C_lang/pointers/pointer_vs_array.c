a2c044e7f97fc8ff7cf8fd4051abe0515b5a0b16
B3h3m0th <behemoth@autistici.org>
2015-10-23 22:25:11 +0200

    how to get value of pointer to array

--- C_lang/pointers/pointer_vs_array.c
+++ C_lang/pointers/pointer_vs_array.c
@@ -1,25 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define SIZE 5
+#define SIZE 3
 
 int main(void) {
-    float data[SIZE] = {1.1, 2.2, 3.3, 4.4, 5.5}, *ptr;
+    float data[SIZE] = {1.1, 2.2, 3.3}, *ptr;
     
     ptr = &data[0]; 
     // Istruzione equivalente: ptr = data;
 
     printf(" address of the first element of the array data: data[0]\n");
-    printf("       ptr: %p\n", (void *) ptr);
-    printf("  &data[0]: %p\n", (void *) &data[0]);
-    printf("      data: %p\n", (void *) data);
+    printf("        ptr: %p\n", (void *) ptr);
+    printf("   &data[0]: %p\n", (void *) &data[0]);
+    printf("       data: %p\n", (void *) data);
 
     printf("\n");
 
     printf("address of the second element of the array data: data[1]\n");
-    printf("  (ptr +1): %p\n", (void *) (ptr +1));
-    printf("  &data[1]: %p\n", (void *) &data[1]);
-    printf(" (data +1): %p\n", (void *) (data +1));
+    printf("   (ptr +1): %p\n", (void *) (ptr + 1));
+    printf("   &data[1]: %p\n", (void *) &data[1]);
+    printf("  (data +1): %p\n", (void *) (data + 1));
+
+    printf("\n");
+
+    printf("value of the third element of the array data: data[2]\n");
+    printf("  *(ptr +1): %f\n", *(ptr + 2));
+    printf("*(&data[1]): %f\n", *(&data[2]));
+    printf("  *(ptr +1): %f\n", *(data + 2));
 
     return(EXIT_SUCCESS);
 }
