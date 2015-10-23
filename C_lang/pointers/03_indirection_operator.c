1575808af6fbf4f9972528a76d3fb22285df208b
B3h3m0th <behemoth@autistici.org>
2015-10-23 18:18:44 +0200

    pointer to other memory addresses or values

--- C_lang/pointers/03_indirection_operator.c
+++ C_lang/pointers/03_indirection_operator.c
@@ -2,25 +2,30 @@
 #include <stdlib.h>
 
 int main(void) {
-    int n = 10;
-    int *ptr = &n;
+    int number = 10;
+    int *ptr = &number;
 
     /* L'operatore di indirezione applicato a un puntatore produce il contenuto
     della locazione di memoria a cui il puntatore punta, che generalmente
     equivale ad accedere al valore della variabile puntata */
 
-    printf("*ptr: %d\n", *ptr);
+    printf("number: %d\n", number);
+    printf("   ptr: %d\n", *ptr);
 
     /* Il valore di ritorno dell'operatore di indirezione puo' essere
     assegnato ad un lvalue. */
 
     int val = *ptr;
-    printf("val: %d\n", val);
+    printf("   val: %d\n", val);
 
     /* Un puntatore nel corso del programma puo' puntare verso altri indirizzi
-    o valori */
+    o valori, in questo caso cambiera' anche il valore della variabile a cui 
+    puntava originariamente */
+
     *ptr = 150;
-    printf("*ptr: %d\n", *ptr);
+    printf("After\n");
+    printf("   ptr: %d\n", *ptr);
+    printf("number: %d\n", number);
 
     return(EXIT_SUCCESS);
 }
