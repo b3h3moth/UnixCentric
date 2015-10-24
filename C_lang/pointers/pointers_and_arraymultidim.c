da6a76f848ecf0ffec784f84c6d75545fb97aed4
B3h3m0th <behemoth@autistici.org>
2015-10-24 19:34:52 +0200

    relation between pointer and array multidimensional

--- C_lang/pointers/pointers_and_arraymultidim.c
+++ C_lang/pointers/pointers_and_arraymultidim.c
@@ -8,31 +8,32 @@ int main(void) {
         {40, 50, 60},
     };
 
-    // Entrambi ritornano l'indirizzo dell'elemento 0
-    printf("        data: %p\n", (void *)data);
-    printf("    &data[0]: %p\n", (void *)&data[0]);
+    /* Stampano l'indirizzo iniziale dell'array multidimensionale o del primo
+    elemento  */
+    printf("          data: %p\n", (void *)data);
+    printf("      &data[0]: %p\n", (void *)&data[0]);
+    printf("       data[0]: %p\n", (void *)data[0]);
+    printf("         &data: %p\n", (void *)&data);
+    printf("         *data: %p\n", (void *)*data);
+    printf("     *&data[0]: %p\n", (void *)*&data[0]);
+    printf("        *&data: %p\n", (void *)*&data);
+    printf("   &data[0][0]: %p\n", (void *)&data[0][0]);
 
-    // Ritorna l'indirizzo dell'elemento 0
-    printf("     data[0]: %p\n", (void *)data[0]);
+    // Stampano il valore del primo elemento del primo sottoarray, [0][0]
+    printf("      *data[0]: %d\n", *data[0]);
+    printf("  *&data[0][0]: %d\n", *&data[0][0]);
 
-    // Ritorna l'indirizzo dell'elemento [0][0]
-    printf(" &data[0][0]: %p\n", (void *)&data[0][0]);
+    // Stampa l'indirizzo del primo elemento del secondo sottoarray
+    printf("     *(data+1): %p\n", (void *)*(data +1));
+    
+    // Stampa il valore del primo elemento del secondo sottoarray [1][0]
+    printf("  *(*(data+1)): %d\n", *(*(data + 1)));
 
-    // Ritorna l'indirizzo iniziale dell'array
-    printf("       &data: %p\n", (void *)&data);
+    // Stampa l'indirizzo del secondo elemento del secondo sottoarray [1][1]
+    printf("   *(data+1)+1: %p\n", (void *)(*(data + 1) + 1));
 
-    // Entrambi puntano all'indirizzo 0
-    printf("       *data: %p\n", (void *)*data);
-    printf("   *&data[0]: %p\n", (void *)*&data[0]);
-
-    // Ritorna il valore del primo elemento, [0][0]
-    printf("    *data[0]: %d\n", *data[0]);
-
-    // Ritorna il valore del primo elemento, [0][0]
-    printf("*&data[0][0]: %d\n", *&data[0][0]);
-
-    // Ritorna l'indirizzo dell'elemento 0
-    printf("      *&data: %p\n", (void *)*&data);
+    // Stampa il valore del secondo elemento del secondo sottoarray [1][1]
+    printf("*(*(data+1)+1): %d\n", *(*(data + 1) + 1) );
     
     return(EXIT_SUCCESS);
 }
