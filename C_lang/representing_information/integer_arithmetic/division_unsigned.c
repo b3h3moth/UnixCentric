86e886306b2e0093776ff252b1af7e4cd362a892
B3h3m0th <behemoth@autistici.org>
2015-10-25 17:51:31 +0100

    if left-shift is two multiplication, then right-shift is two division

--- C_lang/representing_information/integer_arithmetic/division_unsigned.c
+++ C_lang/representing_information/integer_arithmetic/division_unsigned.c
@@ -2,5 +2,10 @@
 #include <stdlib.h>
 
 int main(void) {
+    /* Si sta considerando la divisione di un numero di tio unsigned per una
+    potenza del 2, si intende dimostrare che se il left-shift equivale a
+    moltiplicare per due, allora il right-shift corrisponde alla divisione
+    per due */
+
     return(EXIT_SUCCESS);
 }
