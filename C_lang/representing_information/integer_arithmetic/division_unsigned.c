4b3236331168fe547bcead124ae0af0a4869cad9
B3h3m0th <behemoth@autistici.org>
2015-10-25 18:16:24 +0100

    right-shift division, power of two

--- C_lang/representing_information/integer_arithmetic/division_unsigned.c
+++ C_lang/representing_information/integer_arithmetic/division_unsigned.c
@@ -2,10 +2,20 @@
 #include <stdlib.h>
 
 int main(void) {
-    /* Si sta considerando la divisione di un numero di tio unsigned per una
+    /* Si sta considerando la divisione di un numero di tipo unsigned per una
     potenza del 2, si intende dimostrare che se il left-shift equivale a
     moltiplicare per due, allora il right-shift corrisponde alla divisione
     per due */
+
+    unsigned int num = 32768;
+
+    printf("%d\n", num);
+
+    // right-shift di 1, ovvero divisione per due
+    for (int i=0; i<15; i++) {
+        num = num >> 1;
+        printf("%5d\n", num);
+    }
 
     return(EXIT_SUCCESS);
 }
