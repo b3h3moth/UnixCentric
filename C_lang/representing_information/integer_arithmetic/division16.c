53b4159d76021abd020f6fa187b1495c19cbfccd
B3h3m0th <behemoth@autistici.org>
2015-10-26 22:28:49 +0100

    definition of div16() function

--- C_lang/representing_information/integer_arithmetic/division16.c
+++ C_lang/representing_information/integer_arithmetic/division16.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int div16(int num);
+
 int main(void) {
     return(EXIT_SUCCESS);
+}
+
+int div16(int num) {
+    int bias = (x >> 31) & 0xF;
+    return (x + bias) >> 4;
 }
