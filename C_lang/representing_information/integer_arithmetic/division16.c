2bdde35441d10a51242a81ab90df49f069d5afa3
B3h3m0th <behemoth@autistici.org>
2015-10-26 22:33:22 +0100

    testing division, rounding and bias

--- C_lang/representing_information/integer_arithmetic/division16.c
+++ C_lang/representing_information/integer_arithmetic/division16.c
@@ -4,10 +4,15 @@
 int div16(int num);
 
 int main(void) {
+    const int val = 165;
+
+    printf("%d / %d = %d\n", val, 16, div16(val));
+
     return(EXIT_SUCCESS);
 }
 
+// Ritorna il risultato della divisione num / 16
 int div16(int num) {
-    int bias = (x >> 31) & 0xF;
-    return (x + bias) >> 4;
+    int bias = (num >> 31) & 0xF;
+    return (num + bias) >> 4;
 }
