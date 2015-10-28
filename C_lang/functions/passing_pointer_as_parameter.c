126657b4ca9806e353977e9c7504c2e51ebde846
B3h3m0th <behemoth@autistici.org>
2015-10-28 19:35:05 +0100

    testing pointer and function arguments

--- C_lang/functions/passing_pointer_as_parameter.c
+++ C_lang/functions/passing_pointer_as_parameter.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void sum(int *number, unsigned short esp);
+
 int main(void) {
+    int val = 2;
+    int *ptr = &val;
+
+    /* Il puntatore 'ptr' passato come argomento alla funzione 'sum()'
+    contiene l'indirizzo della variabile 'val', pertanto si modifichera'
+    il valore della variabile stessa.
+    
+    I  puntatori sono lvalue, e come tali sono oggetti di assegnamento */
+    sum(ptr, 3);
+    printf("%d\n", val);
+
+    /* Oppure si sarebbe potuto passare alla funzione l'indirizzo della
+    variabile 'val', ottenendo il medesimo risultato */
+
     return(EXIT_SUCCESS);
+}
+
+void sum(int *number, unsigned short esp) {
+    for (int i=1; i<esp; i++) 
+        *number += *number;
 }
