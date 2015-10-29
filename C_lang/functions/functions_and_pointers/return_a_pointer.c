f95c5eb9962dea0678b851ed73a2bf6fdcf4f8fa
B3h3m0th <behemoth@autistici.org>
2015-10-29 19:30:41 +0100

    function returns a pointer

--- C_lang/functions/functions_and_pointers/return_a_pointer.c
+++ C_lang/functions/functions_and_pointers/return_a_pointer.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Lo scopo del programma e' di ottenere il valore massimo
+
+int *get_max(int *x, int *y);
+
 int main(void) {
+    int num1 = 20;
+    int num2 = 30;
+
+    int *val = get_max(&num1, &num2);
+
+    printf("Val: %d\n", *val);
+
     return(EXIT_SUCCESS);
+}
+
+int *get_max(int *x, int *y) {
+    if (*x > *y)
+        return x;
+    else 
+        return y;
 }
