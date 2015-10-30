bf8e90bc51ef2a1c9e566de244a7bd32ae6d714f
B3h3m0th <behemoth@autistici.org>
2015-10-30 17:18:59 +0100

    browse array elements with a pointer to address memory

--- C_lang/pointers/pointers_and_array.c
+++ C_lang/pointers/pointers_and_array.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define SIZE 5
+
 int main(void) {
-    float data[] = {1.1, 2.2, 3.3}, *ptr;
+    float data[SIZE] = {1.1, 2.2, 3.3, 4.4, 5.5}, *ptr;
     
     ptr = &data[0]; 
     // Istruzione equivalente: ptr = data;
@@ -25,6 +27,21 @@ int main(void) {
     printf("  *(ptr +1): %f\n", *(ptr + 2));
     printf("*(&data[1]): %f\n", *(&data[2]));
     printf("  *(ptr +1): %f\n", *(data + 2));
+
+
+    /* Sebbene l'elemento SIZE non esista - gli elementi del vettore sono
+    SIZE -1 - e' perfettamente lecito e anche sicuro utilizzare
+    p < &data[SIZE] */
+    
+    float *p;
+    printf("\n");
+
+    for (p = &data[0]; p < &data[SIZE]; p++)
+        printf("%.1f, ", *p);
+
+    
+    /* Operatore di indirezione (Indirection operator *) e 
+    Operatore di incremento  (Increment operator ++) con i puntatori */
 
     return(EXIT_SUCCESS);
 }
