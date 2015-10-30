8c7b90728fda039f8c2e8cc799a4ec211e45b351
B3h3m0th <behemoth@autistici.org>
2015-10-30 16:17:46 +0100

    how to achieve a compound literal

--- C_lang/array/compound_literal.c
+++ C_lang/array/compound_literal.c
@@ -2,7 +2,31 @@
 #include <stdlib.h>
 
 // Compound literals (Letterali composti)
-
+int sum(const int vec[], int size);
 int main(void) {
+    /* Un letterale composto viene creato alla bisogna, specificando soltanto
+    gli elementi che dovra' contenere.
+    
+    Si realizza con:
+    - Tipo di dato tra tonde seguito o meno dal numero di elementi del vettore;
+    - All'interno di graffe, gli elementi. 
+    
+    Si puo' utilizzare 'const' per avere un array in sola lettura*/
+    
+    int tot   = sum( (int []){1,2,3,4,5}, 5);
+    int total = sum( (const int [5]){1,2,3,4,5}, 5);
+
+    printf("  tot: %d\n", tot);
+    printf("total: %d\n", total);
+
     return(EXIT_SUCCESS);
+}
+
+int sum(const int vec[], int size) {
+    int tmp = 0;
+
+    for (int i=0; i<size; i++)
+        tmp += vec[i];
+
+    return tmp;
 }
