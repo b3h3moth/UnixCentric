21859593d0e9aec8f4136202bd955014a9fb84d4
B3h3m0th <behemoth@autistici.org>
2015-10-30 15:47:38 +0100

    added comment about pinter subctration features

--- C_lang/pointers/arithmetic_3_subtraction_ptr.c
+++ C_lang/pointers/arithmetic_3_subtraction_ptr.c
@@ -9,18 +9,22 @@
 
 int main(void) {
     int vec[SIZE] = {98, 16, 24, 10, 87, 28, 93, 11, 54, 92};
-    // Si fa puntare il puntatore all'ultimo elemento del vettore
+
+    // Il puntatore punta all'ultimo elemento del vettore
     int *ptr0 = &vec[SIZE-1];
-    // Si fa puntare il puntatore all'elemento 6 del vettore
+    // Il puntatore punta all'elemento 6 del vettore
     int *ptr1 = &vec[5];
-    // Si fa puntare il puntatore all'elemento 2 del vettore
-    int *ptr2 = &vec[1];
+    // Il puntatore punta all'elemento 3 del vettore
+    int *ptr2 = &vec[2];
 
     /* Il tipo di dato da utilizzare per la differenza tra puntatori si
     esprime mediante il tipo 'ptrdiff_t' definito in <stddef.h>;
-    lo specificatore di formato associabile e' %td */
-    ptrdiff_t res1 = ptr0 - ptr1;
-    ptrdiff_t res2 = ptr1 - ptr2;
+    lo specificatore di formato associabile e' %td.
+   
+    Il risultato consiste nella distanza tra i due puntatori, misurata in
+    elementi del vettore */
+    ptrdiff_t res1 = ptr0 - ptr1;   // 9-5=4
+    ptrdiff_t res2 = ptr1 - ptr2;   // 5-2=3
 
     printf("             Array: ");
     for (int i=0; i<sizeof(vec)/sizeof(vec[0]); i++)
