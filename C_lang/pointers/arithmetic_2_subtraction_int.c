4ba49e5b4e34e6450962e79f61078c23e450c701
B3h3m0th <behemoth@autistici.org>
2015-10-30 14:50:53 +0100

    arithmetic subtraction with pointers

---
+++ C_lang/pointers/arithmetic_2_subtraction_int.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Array length
+#define SIZE 10
+
+int main(int argc, char *argv[]) {
+    // Sottrarre un intero a un puntatore
+    int vec[SIZE] = {98, 16, 24, 10, 87, 28, 93, 11, 54, 92};
+    int *ptr = &vec[SIZE-1];
+
+    printf("            Array: ");
+    for (int i=0; i<sizeof(vec)/sizeof(vec[0]); i++)
+        printf("%d, ", vec[i]); 
+
+    printf("\nAddress of vector: %d %p \n", *ptr, (void *)ptr);
+
+    ptr--;                  /* 4 byte avanti */
+    printf("ptr - %d (%2d byte): %d %p\n", 1, 4, *ptr, (void *)ptr);
+    
+    ptr -= 1;               /* 4 byte avanti */
+    printf("ptr - %d (%2d byte): %d %p\n", 1, 4, *ptr, (void *)ptr);
+
+    printf("ptr - %d (%2d byte): %d %p\n", 4, 16, *(ptr-4), (void *)(ptr-4));
+
+    return(EXIT_SUCCESS);
+}
