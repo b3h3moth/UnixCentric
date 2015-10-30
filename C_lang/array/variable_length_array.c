0310b8c978df8358f6070d910cb5a768e4002e18
B3h3m0th <behemoth@autistici.org>
2015-10-30 18:53:02 +0100

    variable length array (VLA)

--- C_lang/array/variable_length_array.c
+++ C_lang/array/variable_length_array.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Array di lunghezza variabile (Variable Length Array)
+
 int main(void) {
+    int size;
+
+    printf("How many number of elements to you want? ");
+    scanf("%d", &size);
+
+    int vec[size];  // Dal C99 in poi
+
+    printf("Initialize an array of %d elements:\n", size);
+    for(int i=0; i<size; i++) {
+        printf("\tvector[%d]: ", i);
+        scanf("%d", &vec[i]);
+    }
+
+    printf("Print each element:\n");
+    for(int j=0; j<size; j++)
+        printf("\tvec[%d] = %d\n", j, vec[j]);
+
     return(EXIT_SUCCESS);
 }
