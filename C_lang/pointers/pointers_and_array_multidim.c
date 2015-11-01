0a3f2666f43586b87ee165f9b82212d7cc60fdf8
B3h3m0th <behemoth@autistici.org>
2015-11-01 19:09:25 +0100

    replaced array size (rows|cols) with define preprocessor

--- C_lang/pointers/pointers_and_array_multidim.c
+++ C_lang/pointers/pointers_and_array_multidim.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define ROWS    2
+#define COLS    3
+
 int main(void) {
     
-    int data[][3] = {   // Equivalente a data[2][3]
-        {10, 20, 30},
-        {40, 50, 60},
+    int data[ROWS][COLS] = {
+        {10, 20, 30},   // Row 1
+        {40, 50, 60},   // Row 2
     };
 
     /* Stampano l'indirizzo iniziale dell'array multidimensionale o del primo
@@ -34,6 +37,11 @@ int main(void) {
 
     // Stampa il valore del secondo elemento del secondo sottoarray [1][1]
     printf("*(*(data+1)+1): %d\n", *(*(data + 1) + 1) );
-    
+   
+    int *ptr;
+
+    for (ptr = &data[0][0]; ptr <= &data[ROWS-1][COLS-1]; ptr++)
+        printf("%d, ", *ptr);
+
     return(EXIT_SUCCESS);
 }
