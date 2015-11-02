ce8e9c64c26e8c7ee1763440d9aebc3f5f82e48e
B3h3m0th <behemoth@autistici.org>
2015-11-02 16:22:19 +0100

    C Standard Library on strings

--- C_lang/working_with_strings/standard_library.c
+++ C_lang/working_with_strings/standard_library.c
@@ -4,33 +4,25 @@
 
 #define STR_LEN 32
 
-int main(int argc, char *argv[], char *envp[])
-{
-   /*
-    * Nel C le stringhe vengono trattate come vettori e quindi subiscono le
-    * medesime restrizioni di questi ultimi, per cui non possono essere ne'
-    * copiate ne' confrontate mediante operatori
-    */
+int main(void) {
+    /* Nel C le stringhe vengono trattate come vettori e quindi subiscono le
+    medesime restrizioni di questi ultimi, per cui non possono essere ne'
+    copiate ne' confrontate mediante operatori */
 
-   /* COPIARE UNA STRINGA 
-    * strcpy
-    */
+    // strcpy() copiare una stringa
    char str1[] = "Hacking is not a crime";
    char str2[STR_LEN];
    strcpy(str2, str1);
-   printf("str1 %s - strlen(str1): %d\nstr2 %s - strlen(str2): %d\n", str1, strlen(str1), str2, strlen(str2) );
+   printf("str1 %s - strlen(str1): %d\nstr2 %s - strlen(str2): %d\n", \
+           str1, strlen(str1), str2, strlen(str2) );
 
-   /* 
-    * strncpy
-    */
+    // strncpy() copiare una stringa
    char str3[STR_LEN];
    strncpy(str3, str2, sizeof(str3) -1);
    str3[sizeof(str3)-1] = '\0';
-   printf("str3 %s - strlen(str3) %d\n", str3, strlen(str3));
+   printf("str3 %s - strlen(str3): %d\n", str3, strlen(str3));
 
-   /* CONCATENAZIONE
-    * strcat
-    */
+   // strcat() concatenazione di stringhe
    char strb[] = "is not ";
    char strc[] = "a crime";
    char str5[] ="hacking ";
@@ -38,29 +30,19 @@ int main(int argc, char *argv[], char *envp[])
    strcat(str5, strc);
    printf("str5 %s\n", str5);
 
-   /*
-    * strncat
-    */
+   // strncat() concatenazione di stringhe
    strncat(strb, strc, strlen(strc));
    printf("strb %s\n", strb);
 
-   /* COMPARAZIONE
-    * strcmp
-    * strcmp(s1, s2)
-    * restituisce un valore minore, uguale o maggiore di 0, a seconda che s1 sia
-    * appunto minore, uguale o maggiore di s2
-    */
-
+   /* strcmp(str1, str2) comparazione di stringhe
+   La funzione strcmp() estituisce un valore minore, uguale o maggiore di 0, 
+   a seconda che str1 sia per l'appunto minore, uguale o maggiore di str2 */
    if ( strcmp(strb,strc) > 0)
       printf(" %d < %d\n", strlen(strc), strlen(strb));
    if ( strcmp(strc,strb) < 0)
       printf(" %d > %d\n", strlen(strb), strlen(strc));
    if ( strcmp(strb,strb) == 0)
       printf(" %d == %d\n", strlen(strb), strlen(strb));
-
-
-
-
 
    return(EXIT_SUCCESS);
 }
