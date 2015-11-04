c7ce3c5687f90806f41e0510bc4429fa2b8751b2
B3h3m0th <behemoth@autistici.org>
2015-11-04 17:43:55 +0100

    encrypt and decrypt function is now xor_char_by_char()

--- cryptography/bitwise_crypto/xor_with_key.c
+++ cryptography/bitwise_crypto/xor_with_key.c
@@ -1,28 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 #define STR_SIZE    100
 
-/* Lo scopo del programma e' di cryptare una stringa utilizzando una chiave
-da 8 bit. 
+/* Lo scopo del programma e' di crittare una stringa utilizzando una chiave
+da 8 bit. */
 
-Nota: una chiave da 8 bit comprende 2^8 -1 chiavi diverse, ovvero 255.
-*/
-
-int encrypt(char *str, unsigned char key);
+void xor_char_by_char(char *str, unsigned char key);
 
 int main(void) {
     unsigned short key;         // Chiave di crittazione
     char    string[STR_SIZE];   // Stringa da crittare
 
-    printf("write string to encrypt: ");
-    fgets(line, sizeof(line), stdin);
+    printf("write a string to encrypt: ");
+    fgets(string, sizeof(string), stdin);
 
     printf("write key: ");
     scanf("%hu", &key);
     
+    // Una chiave da 8 bit comprende '2^8' chiavi diverse, ovvero 256
+    key = (key % 256);
+
+    xor_char_by_char(string, key);
+    printf(" encrypted string: %s", string);
+
+    xor_char_by_char(string, key);
+    printf("dencrypted string: %s", string);
+    
     return(EXIT_SUCCESS);
 }
 
-int encrypt(char *str, unsigned char key) {
+void xor_char_by_char(char *str, unsigned char key) {
+    for (int i=0; i<strlen(str); i++)
+        if (isprint(str[i])) {
+            str[i] ^= key;
+        }
+
 }
