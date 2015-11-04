d5be01bb7990f5cfa732a9db0acaefb4c8ffeff5
B3h3m0th <behemoth@autistici.org>
2015-11-04 22:30:27 +0100

    removed control on key

--- cryptography/bitwise_crypto/xor_crypto_2.c
+++ cryptography/bitwise_crypto/xor_crypto_2.c
@@ -14,28 +14,27 @@ int main(void) {
     unsigned short key;         // Chiave di crittazione
     char    string[STR_SIZE];   // Stringa da crittare
 
-    printf("write a string to encrypt: ");
+    printf("write a text to encrypt: ");
     fgets(string, sizeof(string), stdin);
 
-    printf("write key: ");
+    printf("write key (integer 1,...,256): ");
     scanf("%hu", &key);
     
     // Una chiave da 8 bit comprende '2^8 -1' chiavi diverse, ovvero 255
     key = (key % 256);
 
     xor_char_by_char(string, key);
-    printf(" encrypted string: %s\n", string);
+    printf(" encrypted text: %s", string);
 
     xor_char_by_char(string, key);
-    printf("dencrypted string: %s\n", string);
+    printf("dencrypted text: %s", string);
     
     return(EXIT_SUCCESS);
 }
 
 void xor_char_by_char(char *str, unsigned char key) {
     for (int i=0; i<strlen(str); i++)
-        if (isprint(str[i])) {
             str[i] ^= key;
-        }
 
+    printf("\n");
 }
