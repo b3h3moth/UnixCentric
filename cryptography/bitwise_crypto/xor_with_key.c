28ae6037e5c3f3f27328e666b8c3774dc06be4e0
B3h3m0th <behemoth@autistici.org>
2015-11-04 14:26:09 +0100

    key and string to encrypt

--- cryptography/bitwise_crypto/xor_with_key.c
+++ cryptography/bitwise_crypto/xor_with_key.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define STR_SIZE    100
+
 /* Lo scopo del programma e' di cryptare una stringa utilizzando una chiave
 da 8 bit. 
 
-Nota: una chiave da 8 bit comprende 2^8 -1 permutazioni, ovvero 255 */
+Nota: una chiave da 8 bit comprende 2^8 -1 chiavi diverse, ovvero 255.
+*/
+
+int encrypt(char *str, unsigned char key);
+
 int main(void) {
+    unsigned short key;         // Chiave di crittazione
+    char    string[STR_SIZE];   // Stringa da crittare
+
+    printf("write string to encrypt: ");
+    fgets(line, sizeof(line), stdin);
+
+    printf("write key: ");
+    scanf("%hu", &key);
+    
     return(EXIT_SUCCESS);
+}
+
+int encrypt(char *str, unsigned char key) {
 }
