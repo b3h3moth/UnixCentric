67a97feea61b4fda17c4269af480a92af46dbd8c
B3h3m0th <behemoth@autistici.org>
2015-11-04 12:43:22 +0100

    The program's purpose

--- cryptography/bitwise_crypto/xor_with_key.c
+++ cryptography/bitwise_crypto/xor_with_key.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Lo scopo del programma e' di cryptare una stringa utilizzando una chiave
+da 8 bit. 
+
+Nota: una chiave da 8 bit comprende 2^8 -1 permutazioni, ovvero 255 */
 int main(void) {
     return(EXIT_SUCCESS);
 }
