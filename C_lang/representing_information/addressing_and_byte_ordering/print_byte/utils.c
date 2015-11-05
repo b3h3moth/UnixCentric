361d531823603169422bf40b5e8add7a5d82a045
B3h3m0th <behemoth@autistici.org>
2015-11-05 18:34:54 +0100

    #define(ed) conditional preprocessor instructions to check byte-ordering

--- C_lang/representing_information/addressing_and_byte_ordering/print_byte/utils.c
+++ C_lang/representing_information/addressing_and_byte_ordering/print_byte/utils.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <limits.h>
+
+/* Per stampare correttamente i dati binari ed esadecimali e' necessario
+verificare il byte-ordering, ovvero se l'architettura e' di tipo little-endian,
+big-endian oppure ne' l'una ne' l'altra */
+#if BYTE_ORDER == LITTLE_ENDIAN
+
+#define FIRST_CHAR(ptr, size)   (ptr + size - 1)
+#define NEXT_CHAR(ptr)        --(ptr)
+
+#elif BYTE_ORDER == BIG_ENDIAN
+
+#define FIRST_CHAR(ptr, size)   (ptr)
+#define NEXT_CHAR(ptr)        ++(ptr)
+
+#endif
 
 int main(void) {
     return(EXIT_SUCCESS);
