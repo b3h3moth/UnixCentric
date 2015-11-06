9c138796f7ebc708bfd27d740c628836b9bd3ae9
B3h3m0th <behemoth@autistici.org>
2015-11-06 12:47:07 +0100

    fixed token-pasting

--- C_lang/preprocessor/stringization_and_token_pasting.c
+++ C_lang/preprocessor/stringization_and_token_pasting.c
@@ -2,15 +2,20 @@
 #include <stdlib.h>
 
 #define PRINT_CHAR(c)   printf(#c " = '%c'(%d)\n", c, c)
+#define ID_(x)          i##x
 
 /* Le definizioni di macro possono contenere due operatori speciali:
 - '#'  converte un argomento in una stringa, da cui 'stringization';
 - '##' compatta/incolla assieme due token, da cui 'token pasting';
 */
 int main(void) {
-    char ch = 'z';
 
-    PRINT_CHAR(ch); // Output: ch = 'z'(122)
+    char ID_(1)='a', ID_(2)='b', ID_(3)='c';
 
+    PRINT_CHAR(ID_(1)); // printf("ID_(1)" " = '%c'(%d)\n", i1, i1);
+    PRINT_CHAR(ID_(2)); // printf("ID_(2)" " = '%c'(%d)\n", i2, i2);
+    PRINT_CHAR(ID_(3)); // printf("ID_(3)" " = '%c'(%d)\n", i3, i3);
+
+    // L'operatore detto token pasting da il meglio con l'utilizzo di funzioni
     return(EXIT_SUCCESS);
 }
