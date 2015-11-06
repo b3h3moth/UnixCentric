a37400c771dbd5941d8f6c235fa290a34e0d772b
B3h3m0th <behemoth@autistici.org>
2015-11-06 11:08:30 +0100

    print_byte() and print_bit() testing functions

--- C_lang/representing_information/addressing_and_byte_ordering/print_byte/main.c
+++ C_lang/representing_information/addressing_and_byte_ordering/print_byte/main.c
@@ -5,8 +5,12 @@
 
 int main(void) {
     char c = 'A';
+    int num = 123456;
 
     print_bit( (unsigned char *)&c, sizeof(char));
+    print_byte( (unsigned char *)&c, sizeof(char));
 
+    print_bit( (unsigned char *)&num, sizeof(int));
+    print_byte( (unsigned char *)&num, sizeof(int));
     return(EXIT_SUCCESS);
 }
