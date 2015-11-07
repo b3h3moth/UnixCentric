f11d218b941d3a6239ca87c58f24ee91827a415a
B3h3m0th <behemoth@autistici.org>
2015-11-07 11:19:19 +0100

    read public.key and print with cairo

--- Library/cairo/png.c
+++ Library/cairo/png.c
@@ -1,28 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <cairo/cairo.h>
+
+#define SIZE 2048
 
 int main(void) {
     cairo_surface_t *surface;
     cairo_t *cc;
     FILE *fd;
+    char public_key[SIZE];
+    size_t read;
 
     fd = fopen("public.key", "r");
+    while ((read = fread(public_key, 1, SIZE, fd)) != 0) {
+        fwrite(public_key, 1, read, stdout);
+    }
 
     // Create surface (400x600 px) and cairo context
     surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, 400, 600);
     cc = cairo_create(surface);
 
-    // Draw surface with a white background
+    // Draw in white
     cairo_set_source_rgb(cc, 255, 255, 255);
 
-    // Set font-type and font-size
+    // Set font-type
     cairo_select_font_face(cc, "Sans", CAIRO_FONT_SLANT_NORMAL, \
                                        CAIRO_FONT_WEIGHT_NORMAL);
+    // Set font-size
+    cairo_set_font_size(cc, 20);
 
     // Move to 10.0, 10.0 position and drawing text
     cairo_move_to(cc, 10.0, 10.0);
-    cairo_show_text(cc, "Fingerprint:");
+    cairo_show_text(cc, "hello");
 
     // Create png image
     cairo_surface_write_to_png(surface, "fingerpring.png");
