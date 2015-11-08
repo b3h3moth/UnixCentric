97c5fc3c1e1c92f06901eb53fb32974385ef1b25
B3h3m0th <behemoth@autistici.org>
2015-11-08 20:46:28 +0100

    cairo library, png image, fixed font-size and source

--- Library/cairo/png.c
+++ Library/cairo/png.c
@@ -5,7 +5,7 @@
 
 #define WIDTH   300
 #define HEIGHT  200
-#define F_SIZE  14
+#define F_SIZE  18
 
 int main(void) {
     cairo_surface_t *surface;
@@ -17,7 +17,7 @@ int main(void) {
     cr = cairo_create(surface);
 
     // Settaggio del source al color bianco
-    cairo_set_source_rgb(cr, 1, 1, 1);
+    cairo_set_source_rgb(cr, 0.0, 0.70, 0.0);
 
     // Setta il font-type
     cairo_select_font_face(cr, "Monospace", CAIRO_FONT_SLANT_NORMAL, \
@@ -26,14 +26,14 @@ int main(void) {
     cairo_set_font_size(cr, F_SIZE);
     
     // Si sposta alla posizione specificata per scrivere il testo
-    cairo_move_to(cr, 20.0, 15.0);
+    cairo_move_to(cr, 20.0, 20.0);
     cairo_show_text(cr, "PNG image");
 
-    cairo_move_to(cr, 20.0, 30.0);
+    cairo_move_to(cr, 20.0, 100.0);
     cairo_set_source_rgb(cr, 1, 1, 1);
     cairo_show_text(cr, "created with");
 
-    cairo_move_to(cr, 20.0, 45.0);
+    cairo_move_to(cr, 20.0, 180.0);
     cairo_set_source_rgb(cr, 0.7, 0.2, 0.0);
     cairo_show_text(cr, "Cairo 2D graphic library");
 
