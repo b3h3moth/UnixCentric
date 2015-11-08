70db101aca031c37081aeb525bb816bccc8efb8b
B3h3m0th <behemoth@autistici.org>
2015-11-08 18:49:52 +0100

    cairo, replaced sans font type with monospace

--- Library/cairo/png.c
+++ Library/cairo/png.c
@@ -14,17 +14,17 @@ int main(void) {
     surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, 300, 300);
     cc = cairo_create(surface);
 
-    // Black surface
+    // Draw in black
     cairo_set_source_rgb(cc, 0, 0, 0);
 
     // Set font-type
-    cairo_select_font_face(cc, "Sans", CAIRO_FONT_SLANT_NORMAL, \
-                                       CAIRO_FONT_WEIGHT_NORMAL);
+    cairo_select_font_face(cc, "Monospace", CAIRO_FONT_SLANT_NORMAL, \
+                                       CAIRO_FONT_WEIGHT_BOLD);
     // Set font-size
     cairo_set_font_size(cc, 15);
     
     // Move to 10.0, 10.0 position and drawing text
-    cairo_move_to(cc, 1.0, 50.0);
+    cairo_move_to(cc, 15.0, 15.0);
     cairo_show_text(cc, "Cairo 2D library");
 
     // Create png image
