882e2ecfaf6cc403560e86dec239e3199c312af8
B3h3m0th <behemoth@autistici.org>
2015-11-08 20:42:04 +0100

    cairo library, testing colours

--- Library/cairo/png.c
+++ Library/cairo/png.c
@@ -5,38 +5,44 @@
 
 #define WIDTH   300
 #define HEIGHT  200
-#define POS_X   15.0
-#define POS_Y   15.0
 #define F_SIZE  14
 
 int main(void) {
     cairo_surface_t *surface;
-    cairo_t *cc;
+    cairo_t *cr;
     char image_name[] = "image.png";
 
     // Crea una surface di WIDTH x HEIGHT px e il cairo context
     surface = cairo_image_surface_create(CAIRO_FORMAT_RGB24, WIDTH, HEIGHT);
-    cc = cairo_create(surface);
+    cr = cairo_create(surface);
 
     // Settaggio del source al color bianco
-    cairo_set_source_rgb(cc, 1, 1, 1);
+    cairo_set_source_rgb(cr, 1, 1, 1);
 
     // Setta il font-type
-    cairo_select_font_face(cc, "Monospace", CAIRO_FONT_SLANT_NORMAL, \
+    cairo_select_font_face(cr, "Monospace", CAIRO_FONT_SLANT_NORMAL, \
                                        CAIRO_FONT_WEIGHT_BOLD);
     // Setta il font-size a F_SIZE
-    cairo_set_font_size(cc, F_SIZE);
+    cairo_set_font_size(cr, F_SIZE);
     
-    // Si sposta alla posizione POS_X, POS_Y per scrivere il testo
-    cairo_move_to(cc, POS_X, POS_Y);
-    cairo_show_text(cc, "Cairo 2D library");
+    // Si sposta alla posizione specificata per scrivere il testo
+    cairo_move_to(cr, 20.0, 15.0);
+    cairo_show_text(cr, "PNG image");
+
+    cairo_move_to(cr, 20.0, 30.0);
+    cairo_set_source_rgb(cr, 1, 1, 1);
+    cairo_show_text(cr, "created with");
+
+    cairo_move_to(cr, 20.0, 45.0);
+    cairo_set_source_rgb(cr, 0.7, 0.2, 0.0);
+    cairo_show_text(cr, "Cairo 2D graphic library");
 
     // Crea l'immagine PNG
     cairo_surface_write_to_png(surface, image_name);
     printf("Image created: %s\n", image_name);
 
     // Pulizia degli oggetti creati, rimozione di context e di surface.
-    cairo_destroy(cc);
+    cairo_destroy(cr);
     cairo_surface_destroy(surface);
 
     return(EXIT_SUCCESS);
