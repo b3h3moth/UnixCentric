2af11cba5a215f8cbe08805a466b5f5878ab1c9e
B3h3m0th <behemoth@autistici.org>
2015-11-08 23:14:20 +0100

    cairo skeleton

--- Library/cairo/cairo_basics.c
+++ Library/cairo/cairo_basics.c
@@ -2,16 +2,17 @@
 #include <stdlib.h>
 #include <cairo/cairo.h>
 
-#define WIDTH   800
-#define HEIGHT  600
+#define WIDTH   150
+#define HEIGHT  150
 
 int main(void) {
-    // Nuovo oggetto con una surface di 800x600 px, RGB32 con Alpha channel
+    // Context con una surface di 800x600 px e RGB con Alpha channel
     cairo_t         *cr_object;
     cairo_surface_t *surface;
     surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, WIDTH, HEIGHT);
     cr_object = cairo_create(surface);
 
+    // ...
 
     // Rimozione dell'oggetto creato
     cairo_destroy(cr_object);
