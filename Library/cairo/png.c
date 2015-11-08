451fc562667a7605140fae8a23f6c9fff454b445
B3h3m0th <behemoth@autistici.org>
2015-11-08 17:08:54 +0100

    cairo library; fixed image weigth and image name

--- Library/cairo/png.c
+++ Library/cairo/png.c
@@ -8,34 +8,28 @@
 int main(void) {
     cairo_surface_t *surface;
     cairo_t *cc;
-    FILE *fd;
-    char public_key[SIZE];
-    size_t read;
-
-    fd = fopen("public.key", "r");
-    while ((read = fread(public_key, 1, SIZE, fd)) != 0) {
-        fwrite(public_key, 1, read, stdout);
-    }
+    char image_name[] = "image.png";
 
     // Create surface (600x800 px) and cairo context
-    surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, 600, 800);
+    surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, 300, 300);
     cc = cairo_create(surface);
 
-    // Draw in white
-    cairo_set_source_rgb(cc, 255, 255, 255);
+    // Black surface
+    cairo_set_source_rgb(cc, 0, 0, 0);
 
     // Set font-type
     cairo_select_font_face(cc, "Sans", CAIRO_FONT_SLANT_NORMAL, \
                                        CAIRO_FONT_WEIGHT_NORMAL);
     // Set font-size
     cairo_set_font_size(cc, 15);
-
+    
     // Move to 10.0, 10.0 position and drawing text
-    cairo_move_to(cc, 1.0, 1.0);
-    cairo_show_text(cc, "Hacking on Planet Earth");
+    cairo_move_to(cc, 1.0, 50.0);
+    cairo_show_text(cc, "Cairo 2D library");
 
     // Create png image
-    cairo_surface_write_to_png(surface, "fingerpring.png");
+    cairo_surface_write_to_png(surface, image_name);
+    printf("Image created: %s\n", image_name);
 
     // Clean and destroy resources
     cairo_destroy(cc);
