6443ce49ccc95e53fe5ce9442b55cf84bfc05fa7
B3h3m0th <behemoth@autistici.org>
2015-11-09 13:08:00 +0100

    cairo context and cairo surface data types

--- Library/cairo/cairo_basics.c
+++ Library/cairo/cairo_basics.c
@@ -6,9 +6,18 @@
 #define HEIGHT  150
 
 int main(void) {
-    /* Quando si lavora con Cairo, per prima cosa e' necessario creare un
-    Cairo context, che contiene tutti i parametri inerenti la grafica, dallo
-    spessore della linea al colore, alla superficie su cui disegnare , etc., */
+    /* Quando si lavora con Cairo, per prima cosa e' necessario creare il
+    Cairo context (un oggetto Cairo), che contiene tutti i parametri di stato 
+    inerenti il disegno, dallo spessore della linea al colore, alla superficie
+    su cui disegnare, etc..., 
+    
+    Il context e' sempre collegato ad una surface, e' di tipo 'cairo_t'
+    La surface invece e' di tipo 'cairo_surface_t'
+    
+    Ci sono diversi tipi di surface, una per ogni formato supportato da Cairo,
+    nel caso specifico si e' optato per una surface da 150 x 150 px, con 
+    32 bit/px, colore RGB e Alpha */
+
     cairo_t         *cr;
     cairo_surface_t *surface;
     surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, WIDTH, HEIGHT);
