b6bd80d9f137e113ecf6f5c944b93df18da184af
B3h3m0th <behemoth@autistici.org>
2015-11-11 23:13:33 +0100

    create cells with for loop

--- Library/cairo/lines/boardchess.c
+++ Library/cairo/lines/boardchess.c
@@ -13,6 +13,7 @@ int main(void) {
     surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, WIDTH, HEIGHT);
     cr = cairo_create(surface);
 
+   
     // Spessore delle linee
     cairo_set_line_width(cr, 280.0);
 
@@ -36,12 +37,29 @@ int main(void) {
     cairo_line_to(cr, 280, 0);
     cairo_stroke(cr);
 
+    /*
     // Linee orizzontali nere
     cairo_set_source_rgba(cr, 0.0, 0.0, 0.0, 1.0);
     cairo_move_to(cr, 20, 300);
     cairo_line_to(cr, 340, 300);
     cairo_stroke(cr);
-
+*/
+    cairo_set_source_rgba(cr, 1.0, 1.8, 1.0, 1.0);
+    cairo_set_line_width(cr, 0.0);
+    static int j=20; 
+    for (int i=0; i<4; i++) {
+        cairo_rectangle(cr, j, 40, 40, 40);
+        cairo_fill(cr);
+        j += 80;
+    }
+    
+    cairo_set_source_rgba(cr, 0.0, 0.0, 0.0, 1.0);
+    static int x=60; 
+    for (int i=0; i<4; i++) {
+        cairo_rectangle(cr, x, 40, 40, 40);
+        cairo_fill(cr);
+        x += 80;
+    }
     // Salvataggio del file, PNG
     cairo_surface_write_to_png(surface, "image.png");
 
