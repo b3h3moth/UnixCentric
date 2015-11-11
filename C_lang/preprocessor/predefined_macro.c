9bd6a3e80ef326076288819ee60550e2a42e3551
B3h3m0th <behemoth@autistici.org>
2015-11-11 18:07:37 +0100

    macro preprocessor

--- C_lang/preprocessor/predefined_macro.c
+++ C_lang/preprocessor/predefined_macro.c
@@ -6,5 +6,8 @@ int main(void) {
    printf("Line (%d) file (%s)\n", __LINE__, __FILE__);
    printf("Standard C version: %ld\n", __STDC_VERSION__);
 
+   /* Queste macro sono probabilmente le piu' conosciute, ma ce ne sono tante
+   altre aggiunte  sia nello standard c99 sia nello standard c11 */
+  
    return(EXIT_SUCCESS);
 }
