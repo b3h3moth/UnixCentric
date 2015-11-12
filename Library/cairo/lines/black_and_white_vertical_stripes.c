eb3d37d7a61a380b9598f62ba1e55b9e55d2db24
B3h3m0th <behemoth@autistici.org>
2015-11-12 15:22:24 +0100

    black and white vertical stripes

---
+++ Library/cairo/lines/black_and_white_vertical_stripes.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cairo/cairo.h>
+
+#define WIDTH   320
+#define HEIGHT  320
+
+// Il disegno di una linea verticale al centro dell'immagine
+
+int main(void) {
+    cairo_t         *cr;
+    cairo_surface_t *surface;
+    surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, WIDTH, HEIGHT);
+    cr = cairo_create(surface);
+
+    cairo_set_line_width(cr, 40.0);
+
+    // Linea bianca
+    cairo_set_source_rgba(cr, 1.0, 1.0, 1.0, 1.0);
+    cairo_move_to(cr, 20, 360);
+    cairo_line_to(cr, 20, 0);
+    cairo_move_to(cr, 100, 320);
+    cairo_line_to(cr, 100, 0);
+    cairo_move_to(cr, 180, 320);
+    cairo_line_to(cr, 180, 0);
+    cairo_move_to(cr, 260, 320);
+    cairo_line_to(cr, 260, 0);
+    cairo_stroke(cr);
+
+    // Linea nera
+    cairo_set_source_rgba(cr, 0.0, 0.0, 0.0, 1.0);
+    cairo_move_to(cr, 60, 320);
+    cairo_line_to(cr, 60, 0);
+    cairo_move_to(cr, 140, 320);
+    cairo_line_to(cr, 140, 0);
+    cairo_move_to(cr, 220, 320);
+    cairo_line_to(cr, 220, 0);
+    cairo_move_to(cr, 300, 320);
+    cairo_line_to(cr, 300, 0);
+    cairo_stroke(cr);
+
+    // Salvataggio del file, PNG
+    cairo_surface_write_to_png(surface, "image.png");
+
+    // Rimozione del context, ovvero dell'oggetto Cairo
+    cairo_destroy(cr);
+    cairo_surface_destroy(surface);
+
+    return(EXIT_SUCCESS);
+}
