988cb0643f57dd4eb80c64cdf5e0e1ef4389a980
B3h3m0th <behemoth@autistici.org>
2015-11-12 15:26:09 +0100

    fixed comments

--- Library/cairo/lines/black_and_white_vertical_stripes.c
+++ Library/cairo/lines/black_and_white_vertical_stripes.c
@@ -5,7 +5,7 @@
 #define WIDTH   320
 #define HEIGHT  320
 
-// Il disegno di una linea verticale al centro dell'immagine
+// Il disegno di 8 strisce verticali bianche e nere alternate
 
 int main(void) {
     cairo_t         *cr;
@@ -13,9 +13,10 @@ int main(void) {
     surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, WIDTH, HEIGHT);
     cr = cairo_create(surface);
 
+    // Spessore delle linee
     cairo_set_line_width(cr, 40.0);
 
-    // Linea bianca
+    // Linee bianche
     cairo_set_source_rgba(cr, 1.0, 1.0, 1.0, 1.0);
     cairo_move_to(cr, 20, 360);
     cairo_line_to(cr, 20, 0);
@@ -27,7 +28,7 @@ int main(void) {
     cairo_line_to(cr, 260, 0);
     cairo_stroke(cr);
 
-    // Linea nera
+    // Linee nere
     cairo_set_source_rgba(cr, 0.0, 0.0, 0.0, 1.0);
     cairo_move_to(cr, 60, 320);
     cairo_line_to(cr, 60, 0);
