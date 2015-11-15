26e3db1f69dce61a8830b8a93372a80e489a5a9c
B3h3m0th <behemoth@autistici.org>
2015-11-15 19:40:29 +0100

    read image with MagickWand

--- Library/ImageMagick/magickWand/resize_image.c
+++ Library/ImageMagick/magickWand/resize_image.c
@@ -3,5 +3,14 @@
 #include <ImageMagick-6/wand/MagickWand.h>
 
 int main(void) {
+    int width, height;
+    MagickWand *mw; = NULL;
+
+    MagickWandGenesis();
+    mw = NewMagickWand();
+
+    // Read the image
+    MagickReadImage(mw, "image.jpg");
+    
     return(EXIT_SUCCESS);
 }
