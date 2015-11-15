6579d8f5569e60b53334af1e0edddb7fa2de40cf
B3h3m0th <behemoth@autistici.org>
2015-11-15 20:14:13 +0100

    set compression quality

--- Library/ImageMagick/magickWand/resize_image.c
+++ Library/ImageMagick/magickWand/resize_image.c
@@ -4,13 +4,30 @@
 
 int main(void) {
     int width, height;
-    MagickWand *mw; = NULL;
+    MagickWand *mw = NULL;
 
     MagickWandGenesis();
     mw = NewMagickWand();
 
     // Read the image
     MagickReadImage(mw, "image.jpg");
-    
+   
+    // Get original image width/height
+    width = MagickGetImageWidth(mw);
+    height = MagickGetImageWidth(mw);
+
+    if ((width /= 2) < 1)
+        width = 1;
+
+    if ((height /= 2) < 1)
+        height = 1;
+
+    // Resize the image
+    MagickResizeImage(mw, width, height, LanczosFilter, 1);
+
+    // Set the compression quality
+    MagickSetImageCompressionQuality(mw, 95);
+
+
     return(EXIT_SUCCESS);
 }
