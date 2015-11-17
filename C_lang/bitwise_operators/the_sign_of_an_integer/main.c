4ca5e4b423acb314c5cf63e7697044b6059a43d4
B3h3m0th <behemoth@autistici.org>
2015-11-17 18:20:06 +0100

    check the sign of an integer with bit manipulation

--- C_lang/bitwise_operators/the_sign_of_an_integer/main.c
+++ C_lang/bitwise_operators/the_sign_of_an_integer/main.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
-/* The purpose of this program is to find the sign of variable 'value', the
-result goes into result 'variable'. It will be positive (0) or negative (-1) */
+/* The purpose of this program is to find the sign of a number, the result 
+goes into 'result' variable.
+Positive is +1, Negative is -1 */
 
 int main(void) {
-    int value = 10;
-    int result = -(value < 0);
+    int value = -378;
+    int result = +1 | (value >> (sizeof(int) * CHAR_BIT - 1));
 
-
-
+    printf("%d\n", result);
 
     return(EXIT_SUCCESS);
 }
