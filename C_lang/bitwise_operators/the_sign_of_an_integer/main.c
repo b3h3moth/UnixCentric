a145dc15b86246cbc010f95a4a05682f248a712f
B3h3m0th <behemoth@autistici.org>
2015-11-17 17:26:08 +0100

    The program's purpose

--- C_lang/bitwise_operators/the_sign_of_an_integer/main.c
+++ C_lang/bitwise_operators/the_sign_of_an_integer/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* The purpose of this program is to find the sign of variable 'value', the
+result goes into result 'variable'. It will be positive (0) or negative (-1) */
+
 int main(void) {
+    int value = 10;
+    int result = -(value < 0);
+
+
+
+
     return(EXIT_SUCCESS);
 }
