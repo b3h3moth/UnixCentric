36221dd136636781ab4e1d6c908e07a7f7b95d8d
B3h3m0th <behemoth@autistici.org>
2015-11-19 20:03:38 +0100

    The program's purpose

--- C_lang/structures_and_unions/unions.c
+++ C_lang/structures_and_unions/unions.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Il tipo di dato union e' perfetto per la creazione di strutture dati
+composite. Lo scopo del programma infatti e' di realizzare un vettore i cui
+membri saranno interi di diverso peso. */
+
+union%
 int main(void) {
     return(EXIT_SUCCESS);
 }
