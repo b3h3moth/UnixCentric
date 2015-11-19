01a9dc917fbf0ab73f1b814d328eb065b7e91247
B3h3m0th <behemoth@autistici.org>
2015-11-19 18:54:24 +0100

    replaced include name with rgb_color.h

--- C_lang/structures/01_declare_and_initialize.c
+++ C_lang/structures/01_declare_and_initialize.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "struct_01.h" /* Si include la definizione della struttura */
+#include "rgb_color.h" /* Si include la definizione della struttura */
 
 int main(void) {
 
