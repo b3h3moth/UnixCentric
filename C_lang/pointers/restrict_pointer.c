0d85d773754761e504a7aa394002acafc7400a4e
B3h3m0th <behemoth@autistici.org>
2015-11-20 20:23:14 +0100

    improved restrict comments

--- C_lang/pointers/restrict_pointer.c
+++ C_lang/pointers/restrict_pointer.c
@@ -8,22 +8,36 @@ Il qualificatore restrict riguarda solo i puntatori, da cui restricted pointer.
 Se un puntatore 'ptr' dovesse puntare a un oggetto, e se tale oggetto fosse
 stato modificato in seguito, allora l'oggetto non sara' accessibile in nessun
 altro modo se non mediante il puntatore 'ptr'. Tuttavia l'oggetto sara'
-accessibile in alcuni quasi, quali:
+accessibile in taluni casi, quali:
 - con un ulteriore puntatore al medesimo oggetto;
-- ptr che punti a una variabile con nome.
-*/
+- con ptr che punti a una variabile con nome.
+
+restrict deve essere posto prima dell'identificatore del puntatore, poiche' si
+occupa di qualificare il puntatore stesso e non l'oggetto cui ptr punta */
+
+int f(int * restrict p1, int * restrict *p2);
 
 int main(void) {
-    int val = 10;
+    int * restrict px;
+    int * restrict py;
 
-    /* restrict deve essere posto prima dell'identificatore del puntatore,
-    poiche' si occupa di qualificare il puntatore stesso e non l'oggetto cui
-    ptr punta */
-    int * restrict ptr1 = &val;
-    int *ptr2 = &val;
+    px = malloc(sizeof(int));
+    *px = 10;
+    py = px;
 
-    *ptr1 = 20;
-    *ptr2 = 30;
+    printf("%d\n", *px);
+
+    // In questo caso si puo' verificare un comportamento indefinito
+    *py = 20;
+
+    printf("%d\n", *py);
 
     return(EXIT_SUCCESS);
+}
+
+int f(int * restrict p1, int * restrict *p2) {
+    *p1 = 100;
+    *p2 = 200;
+
+    return *p1 + *p2;
 }
