734d420d0f8532b129c1d10b1abd29449ac80a70
B3h3m0th <behemoth@autistici.org>
2015-11-21 18:16:44 +0100

    sizeof structure

---
+++ C_lang/structures_and_unions/sizeof_strucrtures.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct st {
+    char    c1;
+    double  d;
+    char    c2;
+} newst = {'a', 2.0, 'b'};
+
+
+int main(void) {
+    return(EXIT_SUCCESS);
+}
