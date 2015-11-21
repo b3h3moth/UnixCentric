9dd47e9f87b50db60206331b20b334e257508139
B3h3m0th <behemoth@autistici.org>
2015-11-21 14:25:41 +0100

    union syntax

--- C_lang/structures_and_unions/unions.c
+++ C_lang/structures_and_unions/unions.c
@@ -3,9 +3,14 @@
 
 #define MAX_SIZE    3
 
-/* Il tipo di dato union e' perfetto per la creazione di strutture dati
-composite. Lo scopo del programma infatti e' di realizzare un vettore i cui
-membri saranno interi di diverso peso. */
+/* Una 'union' condivide la sintassi delle 'struct', la differenza sostanziale
+risiede nel fatto che, a differenza delle 'struct', i membri delle 'union' 
+condividono la memoria, il compilatore infatti allora memoria sufficiente per 
+contenere il membro col maggior peso. 
+
+Nel caso specifico vi e' uno 'short' da 2 byte, un 'int' da 4 byte e un 'long'
+da 4 byte, sara' pertanto allocata una porzione di memoria di 4 byte.
+*/
 
 typedef union {
     short srt_type;
@@ -13,8 +18,12 @@ typedef union {
     long l_type;
 } IntegerFamily;
 
+/* Il tipo di dato union e' perfetto per la creazione di strutture dati
+composite. Lo scopo del programma infatti e' di realizzare un vettore i cui
+membri saranno interi di diverso peso. */
+
 int main(void) {
-    // Un vettore conterra' di diversi tipi di dato
+    // Un vettore conterra' diversi tipi di dato
     IntegerFamily vec[MAX_SIZE];
 
     vec[0].srt_type = 30000;
