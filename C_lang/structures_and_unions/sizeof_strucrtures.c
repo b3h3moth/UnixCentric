a7cc0b74476abe63cf86453cdfde429b7e3a53ff
B3h3m0th <behemoth@autistici.org>
2015-11-21 18:35:43 +0100

    how compiler allocates memory

--- C_lang/structures_and_unions/sizeof_strucrtures.c
+++ C_lang/structures_and_unions/sizeof_strucrtures.c
@@ -2,12 +2,29 @@
 #include <stdlib.h>
 
 struct st {
-    char    c1;
-    double  d;
-    char    c2;
-} newst = {'a', 2.0, 'b'};
+    char    a;
+    double  b;
+    char    c;
+} newst = {'x', 2.0, 'y'};
 
+/* 10 byte e' la somma del peso di ciascun membro, il compilatore tuttavia
+allochera' per l'intera struttura 24 byte, ben 14 byte in piu'. Perche'?
+
+Il compilatore alloca la memoria necessaria a contenere ciascun membro, ma in 
+caso di non allineamento fara' riferimento al tipo piu' grande dei membri, in
+questo caso un double da 8 byte. 
+
+Pertanto la memoria allocata nel caso specifico e':
+8 byte per un char   +
+8 byte per il double +
+8 byte per un char   =
+24 byte              
+
+Se al posto del double ci fosse stato un int, il compilatore avrebbe allocato
+12 byte, se ci fosse stato uno short i byte sarebbero stati 6 e cosi via. */
 
 int main(void) {
+    printf("sizeof structure: %d\n", sizeof(newst));
+
     return(EXIT_SUCCESS);
 }
