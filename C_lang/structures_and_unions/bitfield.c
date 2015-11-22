7a31f1a169b5707e12fb448126188c39947cd389
B3h3m0th <behemoth@autistici.org>
2015-11-22 15:09:50 +0100

    bitfield restrictions

--- C_lang/structures_and_unions/bitfield.c
+++ C_lang/structures_and_unions/bitfield.c
@@ -8,7 +8,10 @@ di bit (bitfield), inoltre:
 - a seconda della macchina il compilatore assegna i bit da destra a sinistra
   (Little-endian) o da sinistra a destra (Big-endian);
 
-
+Ci sono anche delle restrizioni:
+- non sono consentiti campi di bit di array;
+- l'operatore indirizzo & (address of) non e' consentito sui campi di bit, per
+  cui un puntatore non puo' indirizzare uno di essi.
 */
 
 struct date {
