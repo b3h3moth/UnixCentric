b742463ba3296d504da51fb555a19c0733b88cd5
B3h3m0th <behemoth@autistici.org>
2015-11-22 15:07:10 +0100

    how to use bitfield with structures

--- C_lang/structures_and_unions/bitfield.c
+++ C_lang/structures_and_unions/bitfield.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* I campi di bit deve essere dichiarati 'int' oppure 'unsigned int'
+/* Un membro di una struct o di una union puo' essere dichiarato come un campo
+di bit (bitfield), inoltre:
+- i tipi di dato associabili sono 'int' oppure 'unsigned int';
+- il peso massimo consentito equivale al numero di bit della word;
+- a seconda della macchina il compilatore assegna i bit da destra a sinistra
+  (Little-endian) o da sinistra a destra (Big-endian);
+
+
 */
-str
+
+struct date {
+    unsigned int 
 int main(void) {
     return(EXIT_SUCCESS);
 }
