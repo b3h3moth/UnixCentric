97aec4147f29a02603ba90e014e9d914fbc5bc15
B3h3m0th <behemoth@autistici.org>
2015-11-24 19:41:35 +0100

    the program's purpose; IEEE 754 Floating Point single precision representation

--- C_lang/representing_information/floating_point/converting_from_int_to_fp.c
+++ C_lang/representing_information/floating_point/converting_from_int_to_fp.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* The program's purpose is to convert an integer number into a floating point
+single precision (IEEE 754) number */
+
 int main(void) {
     return(EXIT_SUCCESS);
 }
