61f1a82ef37b6ab45ab5a454198f460ff1de77a4
B3h3m0th <behemoth@autistici.org>
2015-11-25 19:07:03 +0100

    tips and tricks with integer promotion with signed and unsigned char

--- C_lang/representing_information/integer/integer_representations/tips_and_tricks/integer_promotion.c
+++ C_lang/representing_information/integer/integer_representations/tips_and_tricks/integer_promotion.c
@@ -2,5 +2,13 @@
 #include <stdlib.h>
 
 int main(void) {
+    unsigned char uch = 0xFF;
+    char ch = 0xFF;
+
+    // Which is the result? Are you sure?
+    int val = (uch == ch);
+
+    printf("Result is: %d\n", val);
+
     return(EXIT_SUCCESS);
 }
