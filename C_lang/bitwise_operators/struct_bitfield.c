8b56547e48d3daf3a99ce5b4a0b847951f628616
B3h3m0th <behemoth@autistici.org>
2015-11-25 10:27:18 +0100

    new struct data type, it splits a 32 bit word into 4 byte

--- C_lang/bitwise_operators/struct_bitfield.c
+++ C_lang/bitwise_operators/struct_bitfield.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Nuovo tipo struct che definisce i 4 byte di una word da 32 bit
+typedef struct {
+    unsigned byte0: 8;
+    unsigned byte1: 8;
+    unsigned byte2: 8;
+    unsigned byte3: 8;
+} word_bytes;
+
 int main(void) {
     return(EXIT_SUCCESS);
 }
