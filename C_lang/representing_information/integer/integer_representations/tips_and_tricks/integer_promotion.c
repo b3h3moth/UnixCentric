97b48f210f183e6c93c43b009804df1447d8ef39
B3h3m0th <behemoth@autistici.org>
2015-11-26 01:46:17 +0100

    on integer promotion

--- C_lang/representing_information/integer/integer_representations/tips_and_tricks/integer_promotion.c
+++ C_lang/representing_information/integer/integer_representations/tips_and_tricks/integer_promotion.c
@@ -10,15 +10,17 @@ int main(void) {
 
     printf("Result is: %d\n", val);
 
-    /* Nella rappresentazione in complemento a due il range di numeri 
-    rapppresentabili dell'unsigned char va da 0 a 255, mentre per il 
-    signed char va da -128 a + 127, per cui il risultato della conversione
-    ad int e':
+    /* Nella rappresentazione in complemento a due del tipo di dato 
+    'unsigned char' e' strutturato mediante un intervallo di valori compreso 
+    tra 0 a 255, mentre per il tipo di dato 'signed char' l'intervallo e' 
+    compreso tra -128 e 127. 
+    
+    Il risultato della promozione di entrambi i tipi su citati in 'int' e'
+    chiaramente falso '0', poiche':
 
     uch = 0xFF = 255 = 0x000000FF
      ch = 0xFF =  -1 = 0xFFFFFFFF
-
-    Il risultato naturalmente e' false, 1. */
+    */
 
     return(EXIT_SUCCESS);
 }
