610ce15962b6736c986b0ba93ab2630967f5cabc
B3h3m0th <behemoth@autistici.org>
2015-11-27 11:31:30 +0100

    bitwise tips and tricks, get the mininum from two integers

--- C_lang/bitwise_operators/extract_the_minimum/main.c
+++ C_lang/bitwise_operators/extract_the_minimum/main.c
@@ -2,5 +2,12 @@
 #include <stdlib.h>
 
 int main(void) {
+    int val1 = 567;
+    int val2 = 987;
+
+    int result = val2 ^ ((val1 ^ val2) & -(val1 < val2));
+
+    printf("The minimum of %d and %d is: %d\n", val1, val2, result);
+
     return(EXIT_SUCCESS);
 }
