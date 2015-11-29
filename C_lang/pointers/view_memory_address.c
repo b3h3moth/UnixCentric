558535637b4339e73404bd289517070a48ba1099
B3h3m0th <behemoth@autistici.org>
2015-11-29 20:22:49 +0100

    print address of main() function

--- C_lang/pointers/view_memory_address.c
+++ C_lang/pointers/view_memory_address.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef unsigned char BYTE;
+
 int main(void) {
+    unsigned int addr;
+    int i, n;
+    BYTE *ptr;
+
+    printf("main() function, address: %#x\n", (unsigned int main());
     return(EXIT_SUCCESS);
 }
