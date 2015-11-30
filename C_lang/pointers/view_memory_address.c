1a43113f9be509c7ce56b72aa80b22fc6cbac36c
B3h3m0th <behemoth@autistici.org>
2015-11-30 16:07:41 +0100

    get memory address, done

--- C_lang/pointers/view_memory_address.c
+++ C_lang/pointers/view_memory_address.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 typedef unsigned char BYTE;
 
@@ -9,8 +10,8 @@ int main(void) {
     BYTE *ptr;
     char str_addr[] = "Address of";
 
-    printf("%9s %5s %#x\n", str_addr, "main", (unsigned int) main);
-    printf("%9s %5s %#x\n", str_addr, "addr", (unsigned int) &addr);
+    printf("%9s %6s %#x\n", str_addr, "main()", (unsigned int) main);
+    printf("%9s %6s %#x\n", str_addr, "addr", (unsigned int) &addr);
 
     // Immissione di un indirizzo di memoria in formato esadecimale
     printf("\nHexadecimal address: ");
@@ -26,6 +27,22 @@ int main(void) {
 
     for (; nbyte > 0; nbyte -= 10) {
         printf("%#8x ", (unsigned int) ptr);
+
+        for (i=0; i<10 && i<nbyte; i++)
+            printf("%.2x ", *(ptr +i));
+
+        for (; i<10; i++)
+            printf(" ");
+        printf(" ");
+
+        for (i=0; i<10 && i<nbyte; i++) {
+            BYTE ch = *(ptr +i);
+            if (!isprint(ch))
+                ch = '.';
+            printf("%c", ch);
+        }
+        printf("\n");
+        ptr += 10;
     }
 
     return(EXIT_SUCCESS);
