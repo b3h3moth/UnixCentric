1c56f1e0efe4ad4d67c280f6d5eaf204dbc89f82
B3h3m0th <behemoth@autistici.org>
2015-11-30 20:39:54 +0100

    list of available printers on the system, done

--- Library/cups/list_available_printers.c
+++ Library/cups/list_available_printers.c
@@ -3,5 +3,16 @@
 #include <cups/cups.h>
 
 int main(void) {
+    int i;
+    cups_dest_t     *dests, *dest;
+    int num_dests = cupsGetDests(&dests);
+
+    for (i = num_dests, dest = dests; i>0; i--, dest++) {
+        if (dest->instance)
+            printf("%s/%s\n", dest->name, dest->instance);
+        else
+            fputs(dest->name, stdout);
+    }
+
     return(EXIT_SUCCESS);
 }
