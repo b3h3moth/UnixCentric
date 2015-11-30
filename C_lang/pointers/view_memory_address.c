07fc5279713f92e9fe14cfdb884d3e8820023720
B3h3m0th <behemoth@autistici.org>
2015-11-30 14:34:48 +0100

    removed ADDRESS macro for testing purposes, fixed scanf() datatype

--- C_lang/pointers/view_memory_address.c
+++ C_lang/pointers/view_memory_address.c
@@ -12,15 +12,21 @@ int main(void) {
     printf("%9s %5s %#x\n", str_addr, "main", (unsigned int) main);
     printf("%9s %5s %#x\n", str_addr, "addr", (unsigned int) &addr);
 
+    // Immissione di un indirizzo di memoria in formato esadecimale
     printf("\nHexadecimal address: ");
     scanf("%x", &addr);
-#ifdef ADDRESS
-    printf("%#x\n", addr);
-#endif
+    
+    // Il numero di byte dell'indirizzo da visualizzare
     printf("How many bytes: ");
-    scanf("%x", &nbyte);
+    scanf("%d", &nbyte);
 
-    printf("%10s %20s %20s", "Address", "Byte", "Characters");
+    printf("%10s %20s %20s\n", "Address", "Byte", "Characters");
+
+    ptr = (BYTE *) addr;
+
+    for (; nbyte > 0; nbyte -= 10) {
+        printf("%#8x ", (unsigned int) ptr);
+    }
 
     return(EXIT_SUCCESS);
 }
