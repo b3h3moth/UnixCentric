32a1a1f2f0f4c0d026aaf1d01aa276ae30c7a2f9
B3h3m0th <behemoth@autistici.org>
2015-11-30 14:23:58 +0100

    layout of the output is done

--- C_lang/pointers/view_memory_address.c
+++ C_lang/pointers/view_memory_address.c
@@ -5,12 +5,22 @@ typedef unsigned char BYTE;
 
 int main(void) {
     unsigned int addr;
-    int i, n;
+    int i, nbyte;
     BYTE *ptr;
     char str_addr[] = "Address of";
 
     printf("%9s %5s %#x\n", str_addr, "main", (unsigned int) main);
     printf("%9s %5s %#x\n", str_addr, "addr", (unsigned int) &addr);
-    
+
+    printf("\nHexadecimal address: ");
+    scanf("%x", &addr);
+#ifdef ADDRESS
+    printf("%#x\n", addr);
+#endif
+    printf("How many bytes: ");
+    scanf("%x", &nbyte);
+
+    printf("%10s %20s %20s", "Address", "Byte", "Characters");
+
     return(EXIT_SUCCESS);
 }
