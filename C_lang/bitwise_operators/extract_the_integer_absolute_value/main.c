64a8e2aae92a5a2a3d09ee5326e2ba14dddc76c9
B3h3m0th <behemoth@autistici.org>
2015-12-01 20:00:02 +0100

    added computation within absolute_value() function

--- C_lang/bitwise_operators/extract_the_integer_absolute_value/main.c
+++ C_lang/bitwise_operators/extract_the_integer_absolute_value/main.c
@@ -2,19 +2,26 @@
 #include <stdlib.h>
 #include <limits.h>
 
+void absolute_value(int val);
+
 // The program's purpose is to extract the absolute value of an integer
 
 int main(void) {
-    int value = -10;
+    int values[] = {0x0, -1, 0x00000001, 0xFFFFFF00, 0xFFFFFEFF, 10};
 
-    /* Extract the LSB (Least Significant Bit)
-    on 4 byte (32 bit) int
-    mask = value >> 31 */
-    int const mask = value >> (sizeof(int) * CHAR_BIT - 1);
-
-    unsigned int result = (value + mask) ^ mask;
-
-    printf("absolute (abs) value of %d is %u\n", value, result);
+    for (int i=0; i<sizeof(values)/sizeof(values[0]); i++)
+        absolute_value(values[i]);
 
     return(EXIT_SUCCESS);
+}
+
+/* The function absolute_value() extracts the LSB (Least Significant Bit) from
+4 byte (32 bit) integer */
+void absolute_value(int val) {
+    /* It's equivalent to:
+    mask = value >> 31 */
+    int const mask = val >> (sizeof(int) * CHAR_BIT - 1);
+    unsigned int result = (val + mask) ^ mask;
+
+    printf("absolute (abs) value of %d is %u\n", val, result);
 }
