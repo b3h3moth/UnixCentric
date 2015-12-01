bd3be6ddf6475626dbd2cc15170f7cbf165293af
B3h3m0th <behemoth@autistici.org>
2015-12-01 19:46:54 +0100

    extracting integer absolute value

--- C_lang/bitwise_operators/extract_the_integer_absolute_value/main.c
+++ C_lang/bitwise_operators/extract_the_integer_absolute_value/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
-// The program's purpose is to extract the integer absolute value
+// The program's purpose is to extract the absolute value of an integer
 
 int main(void) {
+    int value = -10;
+
+    /* Extract the LSB (Least Significant Bit)
+    on 4 byte (32 bit) int
+    mask = value >> 31 */
+    int const mask = value >> (sizeof(int) * CHAR_BIT - 1);
+
+    unsigned int result = (value + mask) ^ mask;
+
+    printf("absolute (abs) value of %d is %u\n", value, result);
+
     return(EXIT_SUCCESS);
 }
