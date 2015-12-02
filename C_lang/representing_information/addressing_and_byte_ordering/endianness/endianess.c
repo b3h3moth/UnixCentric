172486b520790403ab994de6390c35ac6e61b8ae
B3h3m0th <behemoth@autistici.org>
2015-12-02 17:01:13 +0100

    testing on PPC Big-endian 32 bit machine

--- C_lang/representing_information/addressing_and_byte_ordering/endianness/endianess.c
+++ C_lang/representing_information/addressing_and_byte_ordering/endianness/endianess.c
@@ -10,14 +10,17 @@ struct {
     char str2[MAX_STR];
 } data;
 
-/* Lo scopo del programma e' di scrivere i membri della struttura 'data' in un
-file, un vettore char da 4 byte, un int da 4 byte e un altro vettore char da 4
-byte, per un totale di 12 byte. Dopo aver lanciato l'eseguibile, sul file 
-risultante dovra' essere eseguito un hexdump, in modo tale da verificare 
-l'allinemento dei byte.
+/* Lo scopo del programma e' di scrivere i valori dei membri della struttura 
+'data' all'interno di un file, che sono:
+- un vettore char da 4 byte;
+- un int da 4 byte;
+- un altro vettore char da 4 byte
+per un totale di 12 byte. D
 
-$ ./a.
-*/
+Dpo aver lanciato l'eseguibile, sul file risultante dovra' essere eseguito un
+hexdump, in modo tale da verificare l'allinemento dei byte, l'output infatti
+sara' differente a seconda che si utilizzi una macchina Big-endian oppure una
+Little-endian. */
 
 int main(void) {
     FILE *fp;
@@ -25,11 +28,11 @@ int main(void) {
     unsigned int data_size = sizeof(data);
 
     // Copia la stringa "xxx" nel membro data.str1
-    strcpy(data.str1, "xxx");
+    strcpy(data.str1, "abc");
     // Inizializza il membro data.value con un valore esadecimale
-    data.value = 0xAABBCCDD;
+    data.value = 0x12ABCDEF;
     // Copia la stringa "yy" nel membro data.str2
-    strcpy(data.str2, "yyy");
+    strcpy(data.str2, "xyz");
 
     // Scrittura della struttura 'data' nel file
     if ((fp = fopen(filename, "wb"))) {
@@ -43,3 +46,16 @@ int main(void) {
 
     return(EXIT_SUCCESS);
 }
+
+/* Su un PPC Big-Endian 32bit il risultato di un hexdump sarebbe:
+
+$ hd data.txt
+00000000  61 62 63 00 12 ab cd ef  78 79 7a 00              |xxx.....yyy.|
+0000000c
+
+Seppur molto breve, l'hexdump del file data.txt fornisce diverse informazioni,
+anzitutto che si sta utilizzando una macchina con un byte-ordering Big-endian,
+poiche' ciascun byte e' scritto da sinistra a destra, ovvero il MSB (Most
+Significant Byte) corrisponde al 61 (a), mentre LSB (Least Significant Byte)
+corrispinde al byte 7a (z).
+*/
