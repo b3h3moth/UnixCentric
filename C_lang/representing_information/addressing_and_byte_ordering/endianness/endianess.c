d4c2a0e372faaca6cc70af43a73a6d06bc9f862b
B3h3m0th <behemoth@autistici.org>
2015-12-02 16:19:20 +0100

    store data.txt with char and int data types

--- C_lang/representing_information/addressing_and_byte_ordering/endianness/endianess.c
+++ C_lang/representing_information/addressing_and_byte_ordering/endianness/endianess.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 
 int main(void) {
+    FILE *fp;
+
+    struct {
+        char one[4];
+        int two;
+        char three[4];
+    } data;
+
+    strcpy(data.one, "foo");
+    data.two = 0x01234567;
+    strcpy(data.three, "bar");
+
+    if ((fp = fopen("data.txt", "wb"))) {
+        fwrite(&data, sizeof(data), 1, fp);
+        fclose(fp);
+    }
+
     return(EXIT_SUCCESS);
 }
