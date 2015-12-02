6a9dc7f86fcbd5e0abf0765ece9b21d74bdd9168
B3h3m0th <behemoth@autistici.org>
2015-12-02 14:24:09 +0100

    replaced endian() with little_endian()

--- C_lang/representing_information/addressing_and_byte_ordering/endianness/endian.c
+++ C_lang/representing_information/addressing_and_byte_ordering/endianness/endian.c
@@ -3,10 +3,10 @@
 #include <endian.h>
 
 // Prototipo funzione
-int endian(void);
+int little_endian(void);
 
 int main(void) {
-    if (endian())
+    if (little_endian())
         printf("little-endian: %d\n", __LITTLE_ENDIAN);
     else
         printf("big-endian: %d\n", __BIG_ENDIAN);
@@ -17,8 +17,14 @@ int main(void) {
 /* Se il puntatore dereferenziato puntera' ad 1 la macchina avra' una
 architettura - ordinamento dei byte nello specifico -, di tipo little-endian, 
 big-endian altrimenti */
-int endian(void) {
+int little_endian(void) {
     int number_one = 1;
     char *ptr = (char *)&number_one;
     return *ptr;
 }
+
+/*
+0x00     0x08     0x10     0x18
+00000000 00000000 00000000 00000001
+ptr -> 0x18 = 1
+*/
