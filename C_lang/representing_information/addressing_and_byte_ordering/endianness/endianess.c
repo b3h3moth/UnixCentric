0918a8770b46f07666d0545fe5ea9b6faadb45ca
B3h3m0th <behemoth@autistici.org>
2015-12-02 16:26:26 +0100

    The program's purpose

--- C_lang/representing_information/addressing_and_byte_ordering/endianness/endianess.c
+++ C_lang/representing_information/addressing_and_byte_ordering/endianness/endianess.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+    
+struct {
+    char one[4];
+    int two;
+    char three[4];
+} data;
 
+/* Lo scopo del programma e' di scrivere i membri della struttura 'data' in un
+file, un char da 4 byte, un int da 4 byte e un altro char da 4 byte, per un
+totale di 12 byte. Dopo aver lanciato l'eseguibile, sul file risultante dovra'
+essere eseguito un hexdump, in modo tale da verificare l'allinemento dei byte.
+*/
 
 int main(void) {
     FILE *fp;
 
-    struct {
-        char one[4];
-        int two;
-        char three[4];
-    } data;
-
     strcpy(data.one, "foo");
-    data.two = 0x01234567;
+    data.two = 0xAABBCCDD;
     strcpy(data.three, "bar");
 
     if ((fp = fopen("data.txt", "wb"))) {
