8c11a2a333df1d57a7ea56768a3aa6c73ba5bc29
B3h3m0th <behemoth@autistici.org>
2015-12-04 18:41:57 +0100

    unsigned  char minimum value doesnt' have a predefined macro

--- C_lang/representing_information/integer/integer_representations/range_data_types.c
+++ C_lang/representing_information/integer/integer_representations/range_data_types.c
@@ -13,11 +13,13 @@ int main(void) {
     fputs("It prints the typical range for C integer data types.\n"
           "min/max macro are defined inside <limits.h> header.\n\n", stdout);
 
-    printf("%19s (%d byte:%d bit) CHAR_MIN:'%d' CHAR_MAX:'%d'\n", 
-            data_types[0], sizeof(char), CHAR_BIT, CHAR_MIN, CHAR_MAX);
+    printf("%19s (%d byte:%d bit) %10s:'%d' %10s:'%d'\n", 
+            data_types[0], sizeof(char), CHAR_BIT, "CHAR_MIN", CHAR_MIN, 
+            "CHAR_MAX", CHAR_MAX);
 
-    printf("%19s -%2d byte - MIN=0 | MAX=%d\n", 
-            data_types[1], sizeof(unsigned char), UCHAR_MAX);
+    printf("%19s (%d byte:%d bit) %10s:'%d' %10s:'%d'\n", 
+            data_types[1], sizeof(unsigned char), CHAR_BIT, "", 0,
+            "UCHAR_MAX", UCHAR_MAX);
     
     printf("%19s -%2d byte - MIN=%d | MAX=%d\n", 
             data_types[2], sizeof(signed char), SCHAR_MIN, SCHAR_MAX);
