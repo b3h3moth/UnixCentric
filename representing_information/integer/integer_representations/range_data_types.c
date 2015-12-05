20e310b047f01fa28a940cd90df0f6b3ec10f95f
B3h3m0th <behemoth@autistici.org>
2015-12-05 18:47:36 +0100

    replaced printf() precision from 22 o 21

--- representing_information/integer/integer_representations/range_data_types.c
+++ representing_information/integer/integer_representations/range_data_types.c
@@ -14,47 +14,47 @@ int main(void) {
           "min/max macro are defined inside <limits.h> header.\n\n", stdout);
 
     // char
-    printf("%22d %-10s | %10s %-22d\n", 
+    printf("%21d %-9s | %10s %-22d\n", 
             CHAR_MIN, "CHAR_MIN", "CHAR_MAX", CHAR_MAX);
 
     // unsigned char
-    printf("%22d %-10s | %10s %-22d\n", 
+    printf("%21d %-9s | %10s %-22d\n", 
             0, "min", "UCHAR_MAX", UCHAR_MAX);
     
     // signed char
-    printf("%22d %-10s | %10s %-22d\n", 
+    printf("%21d %-9s | %10s %-22d\n", 
             SCHAR_MIN, "SCHAR_MIN", "SCHAR_MAX", SCHAR_MAX);
 
     // (signed) short
-    printf("%22d %-10s | %10s %-22d\n", 
+    printf("%21d %-9s | %10s %-22d\n", 
             SHRT_MIN, "SHRT_MIN", "SHRT_MAX", SHRT_MAX);
 
     // unsigned short
-    printf("%22d %-10s | %10s %-22d\n", 
+    printf("%21d %-9s | %10s %-22d\n", 
             0, "min", "USHRT_MAX", USHRT_MAX);
 
     // (signed) int
-    printf("%22d %-10s | %10s %-22d\n", 
+    printf("%21d %-9s | %10s %-22d\n", 
             INT_MIN, "INT_MIN", "INT_MAX", INT_MAX);
 
     // unsigned int
-    printf("%22d %-10s | %10s %-22u\n", 
+    printf("%21d %-9s | %10s %-22u\n", 
             0, "min", "UINT_MAX", UINT_MAX);
 
     // (signed) long
-    printf("%22ld %-10s | %10s %-22ld\n", 
+    printf("%21ld %-9s | %10s %-22ld\n", 
             LONG_MIN, "LONG_MIN", "LONG_MAX", LONG_MAX);
 
     // unsigned long
-    printf("%22d %-10s | %10s %-22lu\n", 
+    printf("%21d %-9s | %10s %-22lu\n", 
             0, "min", "ULONG_MAX", ULONG_MAX);
     
     // (signed) long long
-    printf("%22lld %-10s | %10s %-22lld\n", 
+    printf("%21lld %-9s | %10s %-22lld\n", 
             LLONG_MIN, "LLONG_MIN", "LLONG_MAX", LLONG_MAX);
 
     // unsigned long long
-    printf("%22d %-10s | %10s %-22llu\n", 
+    printf("%21d %-9s | %10s %-22llu\n", 
             0, "miN", "ULLONG_MAX", ULLONG_MAX);
 
     return(EXIT_SUCCESS);
