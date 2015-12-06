28ca9df7a198b7adcbe18a96d07449eb92fdb181
B3h3m0th <behemoth@autistici.org>
2015-12-06 23:01:03 +0100

    assert() result

--- bitwise_working/tips_and_tricks/any_bit_equals_one.c
+++ bitwise_working/tips_and_tricks/any_bit_equals_one.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 int main(void) {
-    int val = -1;
-    int mask = -1; // 11111111 11111111 11111111 11111111
+    int val = ~0;  // 11111111 11111111 11111111 11111111
+    int mask = ~0; // 11111111 11111111 11111111 11111111
     
     // if any bit of 'val' is 1, returns one
     int result = !(val ^ mask);
+
+    assert(result == 1);
     
     printf("result: %d\n", result);
 
