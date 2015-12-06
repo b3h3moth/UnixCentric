0fd6c94d53cc47132405eeca05722fcb0b637f36
B3h3m0th <behemoth@autistici.org>
2015-12-06 20:54:39 +0100

    if any bit of LSB (Least Significant Byte) is 1, returns 1

---
+++ bitwise_working/tips_and_tricks/any_bit_LSB_equals_one.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    int val = 0xA1B1C1FF; // 10101010 01010101 10101010 11111111
+    int mask = 0xFF;      // 00000000 00000000 00000000 11111111
+    
+    // if any bit in the LSB (Least Significant Byte) of 'val' is 1, returns 1
+    int result = !((val<<24) ^ (mask<<24));
+    
+    printf("result: %d\n", result);
+
+    return(EXIT_SUCCESS);
+}
