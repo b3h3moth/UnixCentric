064dd262d843cc0f88385aa013a605ecb46dd410
B3h3m0th <behemoth@autistici.org>
2015-12-07 22:34:05 +0100

    an array of unsigned int integer created

--- bitwise_working/check_integer_power_of_two/main.c
+++ bitwise_working/check_integer_power_of_two/main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// The program's purpose is to check if the integer 'value' is a power of two
+// The program's purpose is to check if an integer is a power of two
 
 int main(void) {
-    unsigned int val = 1024;
+    unsigned int *values[] = {2, 26, 64, 78, 88, 100, 31, 34, 1024, 16};
     _Bool result;
 
+    for
     if ((result = (val & (val - 1))) == 0)
         printf("%d is a power of two. Result: %d\n", val, result);
 
