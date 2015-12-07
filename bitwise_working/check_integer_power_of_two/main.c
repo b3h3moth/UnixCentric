7aa0866b23dd670f68d35db565375e76166ce0ea
B3h3m0th <behemoth@autistici.org>
2015-12-07 22:32:17 +0100

    the result goes into 'result' variable

--- bitwise_working/check_integer_power_of_two/main.c
+++ bitwise_working/check_integer_power_of_two/main.c
@@ -4,5 +4,12 @@
 // The program's purpose is to check if the integer 'value' is a power of two
 
 int main(void) {
+    unsigned int val = 1024;
+    _Bool result;
+
+    if ((result = (val & (val - 1))) == 0)
+        printf("%d is a power of two. Result: %d\n", val, result);
+
+    
     return(EXIT_SUCCESS);
 }
