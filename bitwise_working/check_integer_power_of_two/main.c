71b5a3f08195c9305a34bd0207e762d5b3198b5c
B3h3m0th <behemoth@autistici.org>
2015-12-07 22:42:19 +0100

    added negative else branch

--- bitwise_working/check_integer_power_of_two/main.c
+++ bitwise_working/check_integer_power_of_two/main.c
@@ -7,10 +7,12 @@ int main(void) {
     unsigned int values[] = {2, 26, 64, 78, 88, 100, 31, 34, 1024, 16};
     _Bool result;
 
-    for (int i=0; i<sizeof(values)/sizeof(values[0]); i++) {
+    for (int i=0; i<sizeof(values)/sizeof(values[0]); i++)
         if ((result = (values[i] & (values[i] - 1))) == 0)
             printf("%d is a power of two.\n", values[i]);
-    }
+        else
+            printf("%d is not a power of two.\n", values[i]);
+
     
     return(EXIT_SUCCESS);
 }
