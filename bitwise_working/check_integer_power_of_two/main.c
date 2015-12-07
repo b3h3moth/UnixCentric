568aa82a95950516bc64619a1084b2fd4e59d65f
B3h3m0th <behemoth@autistici.org>
2015-12-07 22:41:09 +0100

    (val & (val -1)) check power of two

--- bitwise_working/check_integer_power_of_two/main.c
+++ bitwise_working/check_integer_power_of_two/main.c
@@ -4,13 +4,13 @@
 // The program's purpose is to check if an integer is a power of two
 
 int main(void) {
-    unsigned int *values[] = {2, 26, 64, 78, 88, 100, 31, 34, 1024, 16};
+    unsigned int values[] = {2, 26, 64, 78, 88, 100, 31, 34, 1024, 16};
     _Bool result;
 
-    for
-    if ((result = (val & (val - 1))) == 0)
-        printf("%d is a power of two. Result: %d\n", val, result);
-
+    for (int i=0; i<sizeof(values)/sizeof(values[0]); i++) {
+        if ((result = (values[i] & (values[i] - 1))) == 0)
+            printf("%d is a power of two.\n", values[i]);
+    }
     
     return(EXIT_SUCCESS);
 }
