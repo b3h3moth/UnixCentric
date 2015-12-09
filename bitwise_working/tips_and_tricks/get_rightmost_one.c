19ea5b59472b6b2b496ddc7dd499652e3380d818
B3h3m0th <behemoth@autistici.org>
2015-12-09 17:35:18 +0100

    get_rightmost_one_bi() prototipe and definition

--- bitwise_working/tips_and_tricks/get_rightmost_one.c
+++ bitwise_working/tips_and_tricks/get_rightmost_one.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int get_rightmost_one_bit(int value);
+
 int main(void) {
+
     return(EXIT_SUCCESS);
+}
+
+/* Thi get_rightmost_one_bit() function returns the rightmost 1 bit within
+a integer 'int' value */
+int get_rightmost_one_bit(int value) {
+    return (value & -value);
 }
