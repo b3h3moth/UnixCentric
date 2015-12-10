70313f6327cecb4df07faf73b962684ba4afa4b0
B3h3m0th <behemoth@autistici.org>
2015-12-10 15:40:28 +0100

    how to get UINT_MAX from unsigned long '-1'

--- representing_information/integer/integer_representations/tips_and_tricks/get_maxint_from_unsignedlong.c
+++ representing_information/integer/integer_representations/tips_and_tricks/get_maxint_from_unsignedlong.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+
 
 int main(void) {
+    unsigned long ulval = -1;
+
+    if (ulval == UINT_MAX)
+        printf("%lu = %u\n", ulval, UINT_MAX);
+    else
+        fputs("Err.\n", stdout);
+
     return(EXIT_SUCCESS);
 }
