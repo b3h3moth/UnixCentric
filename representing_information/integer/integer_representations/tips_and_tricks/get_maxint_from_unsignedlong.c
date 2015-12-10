f8b2054d9065ee2b96701013e8a61c8bcb2e1429
B3h3m0th <behemoth@autistici.org>
2015-12-10 15:47:46 +0100

    comment on unsigned long -1 and unsigned int

--- representing_information/integer/integer_representations/tips_and_tricks/get_maxint_from_unsignedlong.c
+++ representing_information/integer/integer_representations/tips_and_tricks/get_maxint_from_unsignedlong.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <limits.h>
 
+/* Assegnando ad una variabile di tipo 'unsigned long' il valore '-1', ciascun
+bit sara' impostato a 1, per cui essendo 'unsigned long' di 4 byte come 'int'
+si otterra' il valore massimo 'UINT_MAX' 'unsigned int'. Dovrebbe funzionare 
+su quasi tutte le architetture. */
 
 int main(void) {
     unsigned long ulval = -1;
