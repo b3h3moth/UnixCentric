ef674b5f8d25a27aa91343e2b5e589e3b39b0c88
B3h3m0th <behemoth@autistici.org>
2015-12-12 02:28:45 +0100

    memory allocation to new buffer

--- IO-Unbuffered/04_lseek-holes-3.c
+++ IO-Unbuffered/04_lseek-holes-3.c
@@ -7,27 +7,37 @@
 #define FILE_PERMS (S_IRUSR | S_IWUSR |S_IRGRP | S_IWGRP |S_IROTH | S_IWOTH)
 
 int main(int argc, char *argv[]) {
-    int fd, p;      /* File descriptor, parametro */
+    int fd, p;      // File descriptor, parametro
     size_t len;
+    ssize_t num_read, num_written;
+    char *buf;
+
 
     if (argc < 3 || strncmp(argv[1], "--help", 6)  == 0) {
         fprintf(stderr, "Usage: %s <filename> [r|R|w!s]\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    /* Apri un file in lettura/scrittura, se non esiste e' necessario crearlo,
-    dovra' essere settato con i permessi impostati a rw-rw-rw */
+    // Apertura del file su cui lavorare
     if ((fd = open(argv[1], O_RDWR | O_CREAT, FILE_PERMS)) == -1) {
         fprintf(stderr, "Err. open(%s): %s\n", argv[1], strerror(errno));
         exit(EXIT_FAILURE);
     }
 
-    for (p=2; p<argc; p++) {
+    for (p = 2; p < argc; p++) {
         switch(argv[p][0]) {
-            /* Visualizza il file-offset corrente in byte, in testo */
+            // Visualizza il file-offset corrente in byte, in testo
             case 'r': 
-            /* Visualizza il file-offset corrente in byte, in esadecimale */
+            // Visualizza il file-offset corrente in byte, in esadecimale
             case 'R':
+                // lunghezza del buffer
+                len = atoi(argv[1], NULL, 10);
+                
+                // Allocazione della memoria per il buffer
+                if ((buf = malloc(len)) == NULL) {
+                    fprintf(stderr, "Err. malloc()\n");
+                    exit(EXIT_FAILURE);
+                }
 
 
     return(EXIT_SUCCESS);
