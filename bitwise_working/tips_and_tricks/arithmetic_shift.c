fd744e5da0d3e95996863342ca3930e70531c3f2
B3h3m0th <behemoth@autistici.org>
2015-12-12 19:46:05 +0100

    testing arithmetic shifts

--- bitwise_working/tips_and_tricks/arithmetic_shift.c
+++ bitwise_working/tips_and_tricks/arithmetic_shift.c
@@ -4,9 +4,19 @@
 int arithmetic_shifts();
 
 int main(void) {
+    if ( arithmetic_shifts() )
+        fputs("Machine with arithmetic shifts", stdout);
+    else
+        fputs("Machine with logical shifts", stdout);
+
     return(EXIT_SUCCESS);
 }
 
 int arithmetic_shifts() {
     // Imposta di ciascuno dei 32 bit a 1;
     int val = ~0;
+
+    /* Se dopo lo shift di un bit a destra, 'val' sara' ancora uguale ad 1,
+    allora sara' stato un shift aritmetico */
+    return (val >> 1) == val;
+}
