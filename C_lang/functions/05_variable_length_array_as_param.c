89a642795f3a79b9aaaa38b0230dc3927e5ce353
B3h3m0th <behemoth@autistici.org>
2015-12-13 17:38:40 +0100

    VLA as parameter of the function

---
+++ C_lang/functions/05_variable_length_array_as_param.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Con i vettori a lunghezza variabile (Variable-length array) si puo' 
+rendere esplicito il valore che rappresenta l'ampiezza del vettore.
+
+L'ampiezza deve essere dichiarata per prima 'length', e successivamente il 
+vettore associato alla propria ampiezza 'data[length]' */
+int sum(int length, int data[length]);
+
+int main(void) {
+
+    return(EXIT_SUCCESS);
+}
+
+// Somma ciascun elemento dell'array
+int sum(int data[], int length) {
+    int result = data[0];
+
+    for (int i=0; i<length; i++) 
+        result += data[i];
+
+    return result;
+}
