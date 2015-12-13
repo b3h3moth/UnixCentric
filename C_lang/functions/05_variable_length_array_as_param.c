6926c4fc85748377746fc8c8e54a930778982089
B3h3m0th <behemoth@autistici.org>
2015-12-13 18:13:04 +0100

    definition, declaration and invocation of VLA as a parameter

--- C_lang/functions/05_variable_length_array_as_param.c
+++ C_lang/functions/05_variable_length_array_as_param.c
@@ -13,18 +13,32 @@ int sum(int length, int data[]);
 int sum(int, int [*]);
 int sum(int, int []);
 */
-int sum(int length, int data[length]);
+int sum(int len, int data[*]);
 
 int main(void) {
+    int size;
 
+    printf("Give di size of the array: ");
+    scanf("%d", &size);
+
+    // Array a lunghezza varibile
+    int vec[size];
+
+    // Inizializza ciascun elemento del vettore con l'indice corrispondente
+    for (int i=0; i<size; i++) 
+        vec[i] = i;
+
+    // L'invocazione della funzione segue le normali regole
+    printf("The sum of each array element is: %d\n", sum(size, vec));
+   
     return(EXIT_SUCCESS);
 }
 
 // Somma ciascun elemento dell'array
-int sum(int data[], int length) {
+int sum(int len, int data[len]) {
     int result = data[0];
 
-    for (int i=0; i<length; i++) 
+    for (int i=0; i<len; i++) 
         result += data[i];
 
     return result;
