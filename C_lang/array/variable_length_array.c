36bd411e7823eb76da9fb1fed03c56354b1abc89
B3h3m0th <behemoth@autistici.org>
2015-12-13 17:18:26 +0100

    Variable-length array (VLA) new c99 feature

--- C_lang/array/variable_length_array.c
+++ C_lang/array/variable_length_array.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Array di lunghezza variabile (Variable Length Array)
+/* I vettori a lunghezza variabile (Variable-length arrays) sono stati 
+introdotti nello standard c99, essi consentono di specificare la lunghezza di
+un vettore mediante una espressione non costante */
 
 int main(void) {
     int size;
 
-    printf("How many number of elements to you want? ");
+    printf("Give di size of the array: ");
     scanf("%d", &size);
 
-    int vec[size];  // Dal C99 in poi
+    /* La lunghezza del vettore e' impostata da un'espressione non costante,
+    che nel caso specifico e' 'size' */
+    int vec[size];  
 
     printf("Initialize an array of %d elements:\n", size);
     for(int i=0; i<size; i++) {
