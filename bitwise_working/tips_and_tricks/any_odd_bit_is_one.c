d508e32a387ed4772b583f8070a8981d96ca29ed
B3h3m0th <behemoth@autistici.org>
2015-12-15 20:07:31 +0100

    The program's purpose

--- bitwise_working/tips_and_tricks/any_odd_bit_is_one.c
+++ bitwise_working/tips_and_tricks/any_odd_bit_is_one.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* The function any_odd_is_one() returns 1 when any odd bit of an integer 
+variable equals 1, 0 otherwise */
+
 int main(void) {
     return(EXIT_SUCCESS);
 }
