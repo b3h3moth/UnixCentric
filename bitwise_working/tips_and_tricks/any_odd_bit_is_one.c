4ad65f13c95334b025586285ee4268d75e090bee
B3h3m0th <behemoth@autistici.org>
2015-12-15 20:21:24 +0100

    ineteger values passing to function into an array

--- bitwise_working/tips_and_tricks/any_odd_bit_is_one.c
+++ bitwise_working/tips_and_tricks/any_odd_bit_is_one.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int any_odd_is_one(unsigned val);
 
 int main(void) {
+    int num[] = { 0xA, 0xF, 0x5, 0x8, 0xFF, 0x9a };
+
+
+    if ( any_odd_is_one(num) )
+        printf("Any odd bit of %#x is one\n", num);
+
     return(EXIT_SUCCESS);
 }
 
@@ -11,5 +18,5 @@ int main(void) {
 variable equals 1, 0 otherwise */
 int any_odd_is_one(unsigned val) {
     // 0xaaaaaaaa = 10101010101010101010101010101010
-    return !!(val & 0xaaaaaaaa); //
+    return (val & 0xaaaaaaaa); //
 }
