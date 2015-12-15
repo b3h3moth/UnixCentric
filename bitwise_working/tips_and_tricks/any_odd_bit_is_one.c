eef13e3eb2e8d75d6166083cf820fa28abf93cfa
B3h3m0th <behemoth@autistici.org>
2015-12-15 20:13:20 +0100

    any_odd_is_one() w=32

--- bitwise_working/tips_and_tricks/any_odd_bit_is_one.c
+++ bitwise_working/tips_and_tricks/any_odd_bit_is_one.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* The function any_odd_is_one() returns 1 when any odd bit of an integer 
-variable equals 1, 0 otherwise */
+int any_odd_is_one(unsigned val);
 
 int main(void) {
     return(EXIT_SUCCESS);
+}
+
+/* The function any_odd_is_one() returns 1 when any odd bit of an integer 
+variable equals 1, 0 otherwise */
+int any_odd_is_one(unsigned val) {
+    // 0xaaaaaaaa = 10101010101010101010101010101010
+    return !!(val & 0xaaaaaaaa); //
 }
