24582994b080f7b7ad51cfd1b064140f22ce7d17
B3h3m0th <behemoth@autistici.org>
2015-12-17 19:37:54 +0100

    each byte will be printed on the stdout separately

--- the_language/structures_and_unions/unions/get_individual_bytes.c
+++ the_language/structures_and_unions/unions/get_individual_bytes.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Si intende stampare sullo stdout ciascun byte di un 'unsigned', tale
+rappresentazione sara' possibile grazie alla 'union' che consentira' di
+visualizzare un byte per volta e quindi di accertare anche l'ordinamento
+dei byte stessi della macchina. */
+
 typedef union unsigned_inspect unsigned_inspect;
 
 union unsigned_inspect {
@@ -8,8 +13,14 @@ union unsigned_inspect {
     unsigned char bytes[sizeof(unsigned)];
 };
 
-unsigned_inspect twofold = { .val = 0xAABBCCDD, };
+unsigned_inspect datatest = { .val = 0x12ABCDEF, };
 
 int main(void) {
+    printf("Byte representation of unsigned value: '%#x'\n", datatest.val);
+
+    // Each byte will be printed on the stdout separately
+    for (size_t i = 0; i<sizeof(datatest.bytes); ++i)
+        printf("byte[%zu]: %#x\n", i, datatest.bytes[i]);
+
     return(EXIT_SUCCESS);
 }
