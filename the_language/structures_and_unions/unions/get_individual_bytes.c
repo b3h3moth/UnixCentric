090c249d65fa2430695ee28ee32a5c2a5da998b4
B3h3m0th <behemoth@autistici.org>
2015-12-17 18:04:01 +0100

    unsigned_inspect new data type and union

--- the_language/structures_and_unions/unions/get_individual_bytes.c
+++ the_language/structures_and_unions/unions/get_individual_bytes.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef union unsigned_inspect unsigned_inspect;
+
+union unsigned_inspect {
+    unsigned val;
+    unsigned char bytes[sizeof(unsigned)];
+};
+
+unsigned_inspect twofold = { .val = 0xAABBCCDD, };
+
 int main(void) {
     return(EXIT_SUCCESS);
 }
