65504b865e69dd8418f488909e33a59e7529e891
B3h3m0th <behemoth@autistici.org>
2015-12-21 20:13:02 +0100

    removed errno varible

--- Error-Handling/get_errno_macro.c
+++ Error-Handling/get_errno_macro.c
@@ -2,23 +2,19 @@
 #include <stdlib.h>
 #include <errno.h>
 
-/*
- L'argomento fornito in input fornira' informazioni sulla macro associata alla
- variabile 'errno'.
-*/
+/* L'argomento fornito in input fornira' informazioni sulla macro associata
+alla variabile 'errno'. */
 
 int main(int argc, char *argv[]) {
-
-   if (argc < 2) 
-      fprintf(stderr, "Uso: %s <errno_number>\n", argv[0]);
-   else {
-      errno = atoi(argv[1]);
-      perror(argv[1]);
-      /* 
-       stesso risultato con:
-       perror(""); 
-      */
-   }
-   
-   return(EXIT_SUCCESS);
+    
+    if (argc < 2) 
+        fprintf(stderr, "Uso: %s <errno_number>\n", argv[0]);
+    else {
+        //errno = atoi(argv[1]);
+        perror(argv[1]);
+    }
+    
+    // medesimo risultato con perror("");
+    
+    return(EXIT_SUCCESS);
 }
