5487ae4d714415a8b33e15879624f399f48ee9db
B3h3m0th <behemoth@autistici.org>
2015-12-22 10:29:46 +0100

    fixed function to set the memory free

--- the_language/dynamic_memory_allocation/malloc_2.c
+++ the_language/dynamic_memory_allocation/malloc_2.c
@@ -4,20 +4,16 @@
 #include <errno.h>
 
 /* L'allocazione dinamica della memoria è usata di continuo con le stringhe, in
-tal caso peraltro il lavoro sara' facilitato, poichè si dovrà utilizzare un char
-che corrisponde esattamente a 1 byte, per cui, una stringa di N caratteri sara'
-sizeof(char) * N; da non dimenticare di aggiungere un byte per il carattere di 
-fine stringa \0. */
+tal caso peraltro il lavoro sara' facilitato, poichè si dovrà utilizzare un
+char che corrisponde esattamente a 1 byte, per cui, una stringa di N caratteri 
+sara' sizeof(char) * N; da non dimenticare di aggiungere un byte per il 
+carattere di fine stringa \0. */
 
-
-/* La funzione concat() concatena due stringhe */
 char *concat(const char *str1, const char *str2);
-
-/* Libera la memoria occupata dalle stringhe concatenate */
 void freeMemory(char *block);
 
 int main(void) {
-   char *full_string = concat("Learning"," C-ANSI (C99)");
+   char *full_string = concat("Learning"," c-ansi (c11)");
 
    printf("%s\n", full_string);
    
@@ -26,6 +22,7 @@ int main(void) {
    return(EXIT_SUCCESS);
 }
 
+/* La funzione concat() concatena due stringhe */
 char *concat(const char *str1, const char *str2)
 {
    char *result;
@@ -43,9 +40,10 @@ char *concat(const char *str1, const char *str2)
    return result;
 }
 
+/* Libera la memoria occupata dalle stringhe concatenate */
 void freeMemory(char *block)
 {
-   printf("-- FREE --\n");
+   printf("-- set the memory free\n");
    free(block);
    block = NULL;
 }
