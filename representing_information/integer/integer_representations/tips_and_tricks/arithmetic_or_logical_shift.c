9725f6c799b8dd6ffd10317cf437881970177288
B3h3m0th <behemoth@autistici.org>
2015-12-23 20:27:30 +0100

    the program's purpose, undefined behavior on unsigned integers

---
+++ representing_information/integer/integer_representations/tips_and_tricks/arithmetic_or_logical_shift.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Si evince come un right shift su un operando unsigned possa portare ad un
+undefined behavior */
+
+int main(void) {
+
+    return(EXIT_SUCCESS);
+}
