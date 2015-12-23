c28a75a958602d922ba568c2a6be58ad3123b9e3
B3h3m0th <behemoth@autistici.org>
2015-12-23 16:34:28 +0100

    calloc(), checking error control

--- the_language/dynamic_memory_allocation/calloc_1.c
+++ the_language/dynamic_memory_allocation/calloc_1.c
@@ -18,18 +18,23 @@ per gli array, da notare tuttavia che richiede piu' tempo di una malloc().
 */
 
 int main(void) {
-    /* Si provvede ora ad allocare 40 byte di memoria */
     int i;
     const int tot_elem = 10;
-    int *ptr = calloc(tot_elem, sizeof(int));
+    int *ptr;
 
-    /* Sono stati allocati 40 byte (10 * 4 byte), il risultato e' un array dove
-    ciascun eleemento e' settato a 0; lo stesso risultato si sarebbe potuto
-    ottenere con malloc() e memset():
+    /* Si allocano 10 elementi da 4 byte ciascuno, per un totale di 40 byte, 
+    ogni elemento dell'array e' settato a 0. */
+    if ((ptr = calloc(tot_elem, sizeof(int))) == NULL) {
+        fprintf(stderr, "Err.(line:%d) calloc()\n", __LINE__ - 1);
+        exit(EXIT_FAILURE);
+    }
+    
+    /* Lo stesso risultato si sarebbe potuto ottenere con malloc() e memset():
     
     int *ptr = malloc(tot_elem * sizeof(int));  
     memset(ptr, 0, tot_elem * sizeof(int));     
     */
+
     for (i=0; i<tot_elem; i++)
         printf("%p = %d\n", (void *)&ptr[i], ptr[i]);
 
