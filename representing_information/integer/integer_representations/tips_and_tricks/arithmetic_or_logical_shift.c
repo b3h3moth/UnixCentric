15f334703e0b91355d447d25786af65c7318868c
B3h3m0th <behemoth@autistici.org>
2015-12-24 13:43:04 +0100

    testing arithmetic and logical shift on unsigned and signed int operand

--- representing_information/integer/integer_representations/tips_and_tricks/arithmetic_or_logical_shift.c
+++ representing_information/integer/integer_representations/tips_and_tricks/arithmetic_or_logical_shift.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Si evince come un right shift su un operando unsigned possa portare ad un
-undefined behavior */
+/* Si evince come eseguendo un right shift su un operando unsigned possa 
+portare ad un undefined behavior */
 
 int main(void) {
+    char buf[sizeof(3+1)];
+    unsigned int value = 0x80000000;
+    int result = snprintf(buf, sizeof(buf), "%u", value >> 24);
+
+    printf("%d %#x\n", value, value);
+    printf("%d %#x\n", result, result);
+    printf("%s\n", buf);
+
 
     return(EXIT_SUCCESS);
 }
