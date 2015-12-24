79b161934d320be070303b4af8cece85e19ed6ac
B3h3m0th <behemoth@autistici.org>
2015-12-24 15:38:14 +0100

    right shift on signed/unsigned

--- representing_information/integer/integer_representations/tips_and_tricks/arithmetic_or_logical_shift.c
+++ representing_information/integer/integer_representations/tips_and_tricks/arithmetic_or_logical_shift.c
@@ -5,14 +5,23 @@
 portare ad un undefined behavior */
 
 int main(void) {
+    // signed integer
     char buf[sizeof(3+1)];
-    unsigned int value = 0x80000000;
+    int value = 0x80000000;
     int result = snprintf(buf, sizeof(buf), "%u", value >> 24);
 
-    printf("%d %#x\n", value, value);
-    printf("%d %#x\n", result, result);
-    printf("%s\n", buf);
+    printf("S: %d %#x\n", value, value);
+    printf("S: %d %#x\n", result, result);
+    printf("S: %s\n", buf);
 
+    // unsigned integer
+    char buf2[sizeof(3+1)];
+    unsigned int value2 = 0x80000000;
+    int result2 = snprintf(buf2, sizeof(buf2), "%u", value2 >> 24);
+
+    printf("U: %d %#x\n", value2, value2);
+    printf("U: %d %#x\n", result2, result2);
+    printf("U: %s\n", buf2);
 
     return(EXIT_SUCCESS);
 }
