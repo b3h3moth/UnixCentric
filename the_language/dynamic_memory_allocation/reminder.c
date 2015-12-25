3469d4694c1ea1a7cd7d415ed867c2086b53bd8e
B3h3m0th <behemoth@autistici.org>
2015-12-25 20:30:21 +0100

    maximum number of reminders and string, #define

--- the_language/dynamic_memory_allocation/reminder.c
+++ the_language/dynamic_memory_allocation/reminder.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_REMIND  50  // numero massimo di promemoria
+#define MSG_LEN     60  // lunghezza massima dei messaggi
+
+
 int main(void) {
     return(EXIT_SUCCESS);
 }
