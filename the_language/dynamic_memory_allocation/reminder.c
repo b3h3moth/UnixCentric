00abb01960ec33ecdaf43c316ec3abc96c55ce6d
B3h3m0th <behemoth@autistici.org>
2015-12-25 20:33:51 +0100

    read_line() function added

--- the_language/dynamic_memory_allocation/reminder.c
+++ the_language/dynamic_memory_allocation/reminder.c
@@ -4,7 +4,21 @@
 #define MAX_REMIND  50  // numero massimo di promemoria
 #define MSG_LEN     60  // lunghezza massima dei messaggi
 
+int read_line(char str[], int n);
 
 int main(void) {
     return(EXIT_SUCCESS);
 }
+
+int read_line(char str[], int n) {
+    int ch, i = 0;
+
+    while ((ch = getchar()) != '\n') {
+        if (i < n)
+            str[i++] = ch;
+        str[i] = '\0';
+    }
+
+    return i;
+}
+
