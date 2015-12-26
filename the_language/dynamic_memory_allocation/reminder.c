d0fbf8706cef3d6685b9be23bc40ca71b2fc5e58
B3h3m0th <behemoth@autistici.org>
2015-12-26 17:48:42 +0100

    fixed and replaced a lot of names, code legibility

--- the_language/dynamic_memory_allocation/reminder.c
+++ the_language/dynamic_memory_allocation/reminder.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_NOTE  100  // numero massimo di promemoria
-#define MSG_LEN   255  // lunghezza massima dei messaggi
+#define MAX_NOTE  50    // numero massimo di promemoria
+#define MSG_LEN   255   // lunghezza massima dei messaggi
 
 void read_line(char str[], int n);
 
@@ -11,34 +11,35 @@ int main(void) {
     // Vettore per contenere i promemoria
     char *notes[MAX_NOTE];
     char day_str[3], msg_str[MSG_LEN+1];
-    int day, i, j, num_remind = 0;
+    int day, i, j, num_note = 0;
 
     for (;;) {
-        if (num_remind == MAX_NOTE) {
+        if (num_note == MAX_NOTE) {
             printf("--- No space left ---\n");
             break;
         }
 
-        fputs("enter day and reminder: ", stdout);
+        fputs("enter day and noteer: ", stdout);
         scanf("%2d", &day);
         
         if (day == 0)
             break;
 
-        // Copia 'day' in 'day_str'
+        // Copia la data 'day' in 'day_str'
         sprintf(day_str, "%2d", day);
 
+        // La nota da archiviare
         read_line(msg_str, MSG_LEN);
 
-        for (i=0; i<num_remind; i++)
+        for (i=0; i<num_note; i++)
             if (strcmp(day_str, notes[i]) < 0)
                 break;
 
-        for (j=num_remind; j>i; j--)
+        for (j=num_note; j>i; j--)
             notes[i] = notes[j-1];
 
-        /* Alloca l'esatto numero di caratteri per il reminder
-        Ovvero 3 (data e spazi) piu'messaggio */
+        /* Alloca l'esatto numero di caratteri per la nota
+        Ovvero 3 (data e spazio) piu'messaggio */
         notes[i] = malloc(3 + strlen(msg_str) +1);
 
         if (notes[i] == NULL) {
@@ -49,17 +50,18 @@ int main(void) {
         strcpy(notes[i], day_str);
         strcat(notes[i], msg_str);
 
-        num_remind++;
+        num_note++;
     }
 
-    fputs("\nDay reminder\n", stdout);
+    fputs("\nDay noteer\n", stdout);
 
-    for (i=0; i<num_remind; i++)
+    for (i=0; i<num_note; i++)
         printf(" %s\n", notes[i]);
 
     return(EXIT_SUCCESS);
 }
 
+// Legge il messaggio da archiviare
 void read_line(char str[], int n) {
     int ch, i = 0;
 
@@ -68,4 +70,3 @@ void read_line(char str[], int n) {
             str[i++] = ch;
         str[i] = '\0';
 }
-
