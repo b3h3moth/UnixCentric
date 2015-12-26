b0b668602921f53e5321a0826731947b5142eed5
B3h3m0th <behemoth@autistici.org>
2015-12-26 12:26:38 +0100

    replaced REMIND with NOTE

--- the_language/dynamic_memory_allocation/reminder.c
+++ the_language/dynamic_memory_allocation/reminder.c
@@ -2,19 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_REMIND  100  // numero massimo di promemoria
+#define MAX_NOTE  100  // numero massimo di promemoria
 #define MSG_LEN     255  // lunghezza massima dei messaggi
 
 void read_line(char str[], int n);
 
 int main(void) {
     // Vettore per contenere i promemoria
-    char *reminders[MAX_REMIND];
+    char *notes[MAX_NOTE];
     char day_str[3], msg_str[MSG_LEN+1];
     int day, i, j, num_remind = 0;
 
     for (;;) {
-        if (num_remind == MAX_REMIND) {
+        if (num_remind == MAX_NOTE) {
             printf("--- No space left ---\n");
             break;
         }
@@ -31,23 +31,23 @@ int main(void) {
         read_line(msg_str, MSG_LEN);
 
         for (i=0; i<num_remind; i++)
-            if (strcmp(day_str, reminders[i]) < 0)
+            if (strcmp(day_str, notes[i]) < 0)
                 break;
 
         for (j=num_remind; j>i; j--)
-            reminders[i] = reminders[j-1];
+            notes[i] = notes[j-1];
 
         /* Alloca l'esatto numero di caratteri per il reminder
         Ovvero 3 (data e spazi) piu'messaggio */
-        reminders[i] = malloc(3 + strlen(msg_str) +1);
+        notes[i] = malloc(3 + strlen(msg_str) +1);
 
-        if (reminders[i] == NULL) {
+        if (notes[i] == NULL) {
             printf("--- No space left ---\n");
             break;
         }
 
-        strcpy(reminders[i], day_str);
-        strcat(reminders[i], msg_str);
+        strcpy(notes[i], day_str);
+        strcat(notes[i], msg_str);
 
         num_remind++;
     }
@@ -55,7 +55,7 @@ int main(void) {
     fputs("\nDay reminder\n", stdout);
 
     for (i=0; i<num_remind; i++)
-        printf(" %s\n", reminders[i]);
+        printf(" %s\n", notes[i]);
 
     return(EXIT_SUCCESS);
 }
