8e63c3277dde6671c52391a6792a73c8268b5f98
B3h3m0th <behemoth@autistici.org>
2016-01-02 17:42:37 +0100

    added _Generic syntax

--- the_language/expressions/generic_selections/generic.c
+++ the_language/expressions/generic_selections/generic.c
@@ -2,8 +2,31 @@
 #include <stdlib.h>
 
 /* Il c11 fornisce una sorta di overloading mediante l'utilizzo della keyword
-_Generic. */
+_Generic. 
+
+Syntax:
+-------------------------------------------------------------------------------
+_Generic ( controlling-expression, association-list )
+
+association-list:
+    type-name: assignment-expression                                  
+    default  : assignment-expression
+-------------------------------------------------------------------------------
+L' espressione di controllo (controlling-expression) non e' valutata, piuttosto
+il suo tipo sara' comparato con i nomi dei tipi inseriti nella lista delle 
+associazioni (association-list).
+
+Se il tipo dell'espressione di controllo e' compatibile con uno dei nomi di
+tipo della lista delle associazioni allora il compilatore selezionera' 
+l'espressione relativa. In caso contrario si utilizzera' l'associazione di
+default.
+
+_Generic (2.0, int: "int", double: "double", default: "nothing to do")
+
+In pratica una "generic selection", valuta un valore in base al tipo di
+input ricevuto. */
 
 int main(void) {
+    
     return(EXIT_SUCCESS);
 }
