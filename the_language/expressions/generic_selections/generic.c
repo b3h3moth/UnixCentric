8cb5c28495a4f10d71d7d02b09d23a37761f77fc
B3h3m0th <behemoth@autistici.org>
2016-01-02 16:54:15 +0100

    limited  overloading with _Generic keyword

--- the_language/expressions/generic_selections/generic.c
+++ the_language/expressions/generic_selections/generic.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Il c11 fornisce una sorta di overloading mediante l'utilizzo della keyword
+_Generic. */
+
 int main(void) {
     return(EXIT_SUCCESS);
 }
