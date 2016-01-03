86219cc8d9a030a3dbbedc4975ed35d5663b1d40
B3h3m0th <behemoth@autistici.org>
2016-01-03 22:10:11 +0100

    added errno, string and stdlib headers

--- Files-and-Directories/filesize.c
+++ Files-and-Directories/filesize.c
@@ -1,14 +1,15 @@
-/* filesize.c - prints size of passwd file */
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 #include <sys/stat.h>
 
-int main()
-{
-	struct stat infobuf;              /* place to store info */
-
-	if ( stat( "/etc/passwd", &infobuf) == -1 )  /* get info */
-		perror("/etc/passwd");
-	else
-		printf(" The size of /etc/passwd is %d\n", infobuf.st_size );
+int main(){
+    struct stat infobuf; 
+    
+    if (stat("/etc/passwd", &infobuf) == -1 ) {
+        fprintf(stderr, "Err. stat(): %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    } else
+        printf("The size of /etc/passwd is %ld\n", infobuf.st_size );
 }
