e7753f96edbd795ef1a57600face82b869e6322a
B3h3m0th <behemoth@autistici.org>
2016-01-04 20:10:30 +0100

    removed useless comments

--- Terminal_IO/password_input.c
+++ Terminal_IO/password_input.c
@@ -2,44 +2,44 @@
 #include <stdlib.h>
 #include <termios.h>
 
+// Prototipe
+int get_password(char buf[], int maxlen, int star);
+
 /* The program's purpose is to read a password in input, each character is
 replaced by a '*'. 
 */
 
 int main(void) {
-    char	b[100];
-	int	n;
+    char    b[100];
+	int	    n;
 
 	n = get_password(b, 100, '*');
 	printf("you typed in \"%s\"\n", b);
+
+    return(EXIT_SUCCESS);
 }
 
-int
-get_password(char buf[], int maxlen, int star)
-{
+int get_password(char buf[], int maxlen, int star) {
 	struct termios ttystate, orig;
 	int	c;
-	int	pos = 0;		/* buffer position */
+	int	pos = 0;
 
-	tcgetattr(0, &ttystate);		/* get current settings	*/
-	orig = ttystate;			/* store orig settings	*/
-	ttystate.c_lflag   &= ~ICANON;		/* no canonical proc.	*/
-	ttystate.c_lflag   &= ~ECHO;		/* no echoing please	*/
-	ttystate.c_cc[VMIN] = 1;		/* read chars 1-by-1	*/
-	tcsetattr(0,TCSANOW, &ttystate);	/* set new ones now	*/
+	tcgetattr(0, &ttystate);
+	orig = ttystate;	
+	ttystate.c_lflag   &= ~ICANON;
+	ttystate.c_lflag   &= ~ECHO;
+	ttystate.c_cc[VMIN] = 1;	
+	tcsetattr(0,TCSANOW, &ttystate);
 
-	while( ( c = getchar() ) != '\n' && c != '\r' )
-	{
-		if ( pos < maxlen-1 ){
+	while ((c = getchar()) != '\n' && c != '\r') {
+		if (pos < maxlen-1) {
 			buf[pos++] = c ;
 			putchar(star);
 		}
 	}
-	/*
-	 * saw end of line here
-	 */
+
 	buf[pos] = '\0';
 	tcsetattr(0, TCSANOW, &orig );
-	return pos;
+	
+    return pos;
 }
-
