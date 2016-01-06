a6704cc983e8736d9474401d89e3dadfe284cc02
B3h3m0th <behemoth@autistici.org>
2016-01-06 22:32:03 +0100

    adde stdlib.h, EXIT_SUCCESS inside return

--- libraries_APIs/pam/pam.c
+++ libraries_APIs/pam/pam.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <security/pam_appl.h>
 #include <security/pam_misc.h>
 
 int main(void) {
-  pam_handle_t* pamh;
-  struct pam_conv pamc;
-
-  /* Set up the PAM conversation.  */
-  pamc.conv = &misc_conv;
-  pamc.appdata_ptr = NULL;
-  /* Start a new authentication session.  */
-  pam_start ("su", getenv ("USER"), &pamc, &pamh);
-  /* Authenticate the user.  */
-  if (pam_authenticate (pamh, 0) != PAM_SUCCESS)
-    fprintf (stderr, "Authentication failed!\n");
-  else
-    fprintf (stderr, "Authentication OK.\n");
-  /* All done.  */
-  pam_end (pamh, 0);
-  return 0;
+    pam_handle_t* pamh;
+    struct pam_conv pamc;
+    
+    // Setup PAM conversation
+    pamc.conv = &misc_conv;
+    pamc.appdata_ptr = NULL;
+  
+    // New authentication session
+    pam_start ("su", getenv ("USER"), &pamc, &pamh);
+  
+    // User authentication
+    if (pam_authenticate (pamh, 0) != PAM_SUCCESS)
+        fprintf (stderr, "Authentication failed!\n");
+    else
+        fprintf (stderr, "Authentication OK.\n");
+  
+    /* All done.  */
+    pam_end (pamh, 0);
+  
+    return(EXIT_SUCCESS);
 }
