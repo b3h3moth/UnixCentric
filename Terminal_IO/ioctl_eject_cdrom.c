aa8dfbbf216224f8f81b93266d8d92f419d9fd5a
B3h3m0th <behemoth@autistici.org>
2016-01-07 11:18:21 +0100

    open() file descriptor

--- Terminal_IO/ioctl_eject_cdrom.c
+++ Terminal_IO/ioctl_eject_cdrom.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <linux/cdrom.h>
 
 int main(int argc, char *argv[]) {
+    int fd = open(argv[1], O_RDONLY);
+
     return(EXIT_SUCCESS);
 }
