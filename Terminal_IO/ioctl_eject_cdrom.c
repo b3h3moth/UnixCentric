ec52ff702e695b8a50aee4539a41461e0687bbcd
B3h3m0th <behemoth@autistici.org>
2016-01-07 11:21:35 +0100

    using file descriptor and CDROMEJECT

--- Terminal_IO/ioctl_eject_cdrom.c
+++ Terminal_IO/ioctl_eject_cdrom.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include <linux/cdrom.h>
+#include <sys/ioctl.h>
 
 int main(int argc, char *argv[]) {
     int fd = open(argv[1], O_RDONLY);
 
+    // Eject CDROM
+    ioctl(fd, CDROMEJECT);
+
+    close(fd);
     return(EXIT_SUCCESS);
 }
