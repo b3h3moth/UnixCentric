a41313c5edff5a005c58e16c3dec8fc60924601f
B3h3m0th <behemoth@autistici.org>
2016-01-10 19:12:08 +0100

    check getrlimit() error on getting CPU resource limit

--- Process-Environment/CPU_limit.c
+++ Process-Environment/CPU_limit.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 #include <sys/resource.h>
 
 int main(void) {
+    struct rlimit res_limit;
+
+    // Get the current limits
+    if (getrlimit(RLIMIT_CPU, &res_limit) != 0) {
+        fprintf(stderr, "Err. getrlimit(): %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
     return(EXIT_SUCCESS);
 }
