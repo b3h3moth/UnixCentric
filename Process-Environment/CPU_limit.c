9361e5c6b4fbf113d90ac9eb89aea165859a0d63
B3h3m0th <behemoth@autistici.org>
2016-01-10 19:38:11 +0100

    get CPU resource limits

--- Process-Environment/CPU_limit.c
+++ Process-Environment/CPU_limit.c
@@ -2,19 +2,43 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/resource.h>
 
 int main(void) {
-    struct rlimit res_limit;
+    struct rlimit res_lim;
+    pid_t pid;
 
     // Get the current CPU limits
-    if (getrlimit(RLIMIT_CPU, &res_limit) != 0) {
+    if (getrlimit(RLIMIT_CPU, &res_lim) != 0) {
         fprintf(stderr, "Err. getrlimit(): %s\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
 
-    // Set a CPU limit
-    res_limit.rlim_cur = 1;
+    pid = fork();
+
+    if (pid < 0) {
+        fprintf(stderr, "Err.(%d) - fork - %s\n", errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    } else if (pid == 0) {
+        
+        if (prlimit(pid, RLIMIT_CPU, NULL, &res_lim) == -1) {
+            fprintf(stderr, "Err. prlimit(): %s\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+        
+        printf("Limits: soft=%d; hard=%d\n", res_lim.rlim_cur, 
+                res_lim.rlim_max);
+    } else {
+
+        if (prlimit(pid, RLIMIT_CPU, NULL, &res_lim) == -1) {
+            fprintf(stderr, "Err. prlimit(): %s\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+
+        printf("Limits: soft=%d; hard=%d\n", res_lim.rlim_cur, 
+                res_lim.rlim_max);
+    }
 
     return(EXIT_SUCCESS);
 }
