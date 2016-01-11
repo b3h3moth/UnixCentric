18082bb65f5b55ec72b48bf55cdf69abded22f05
B3h3m0th <behemoth@autistici.org>
2016-01-11 19:11:20 +0100

    testing strdup(), added string.h header

--- the_language/working_with_strings/strdup.c
+++ the_language/working_with_strings/strdup.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* 
 Prototipe: char *strdup(const char *str);
@@ -8,9 +9,16 @@ La funzione strdup() e similari non fanno parte dello standard c ansi, tuttavia
 nei sistemi Unix e' ampiamente utilizzata.
 
 Lo scopo della funzione e' di duplicare la stringa 'str', restituendo un
-puntatore ad essa.
+puntatore ad essa in caso di successo.
 */
 
 int main(void) {
+    char str[] = "Get up stand up, stand up for your rights";
+    char *dup_str;
+
+    dup_str = strdup(str);
+
+    printf("%s\n", dup_str);
+
     return(EXIT_SUCCESS);
 }
