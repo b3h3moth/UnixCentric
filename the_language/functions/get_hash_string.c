b62d63e1dcb880d0c634a125d6397988ff4c01be
B3h3m0th <behemoth@autistici.org>
2016-01-22 10:15:54 +0100

    replaced a simple string with a vector of strings

--- the_language/functions/get_hash_string.c
+++ the_language/functions/get_hash_string.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define VEC_SIZE(x) (sizeof(x)/sizeof(x[0]))
+
 unsigned int hash(char *str);
 
 int main(void) {
-    unsigned int val;
-    char *string = "secrets";
-    val = hash(string);
-    printf("hash of %s is: %d\n", string, val);
+    char *strings[] = { "take", "a", "walk", "on", "the", "wild", "side" };
+    
+    for (int i=0; i<VEC_SIZE(strings); i++)
+        printf("hash of \'%4s\' is: %u\n", strings[i], hash(strings[i]));
 
     return(EXIT_SUCCESS);
 }
 
 unsigned int hash(char *str) {
     unsigned char *p;
-    int h;
+    int h = 0;
 
     for (p = (unsigned char *)str; *p != '\0'; p++)
         h = 31 * h + *p;
 
-    return h % 4093;
+    return h % 4092;
 }
 
