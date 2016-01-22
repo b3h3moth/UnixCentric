2cf88a21e9d316ea0b19e570fbd8c8e4c81c9d25
B3h3m0th <behemoth@autistici.org>
2016-01-22 10:02:08 +0100

    how to get the hash of an string

--- the_language/functions/get_hash_string.c
+++ the_language/functions/get_hash_string.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+unsigned int hash(char *str);
+
 int main(void) {
+    unsigned int val;
+    char *string = "secrets";
+    val = hash(string);
+    printf("hash of %s is: %d\n", string, val);
+
     return(EXIT_SUCCESS);
 }
+
+unsigned int hash(char *str) {
+    unsigned char *p;
+    int h;
+
+    for (p = (unsigned char *)str; *p != '\0'; p++)
+        h = 31 * h + *p;
+
+    return h % 4093;
+}
+
