b26664d691a6f59dca05cc64a2d08b70633b449f
B3h3m0th <behemoth@autistici.org>
2016-01-22 20:15:07 +0100

    how malloc() works, idiom

--- the_language/dynamic_memory_allocation/malloc_4_idiom.c
+++ the_language/dynamic_memory_allocation/malloc_4_idiom.c
@@ -2,5 +2,17 @@
 #include <stdlib.h>
 
 int main(void) {
+    char str[] = "Take a walk on the wild side";
+    char *p;
+
+    /* Così facendo si allochera' la memoria necessaria a contenere l'oggetto
+    a cui 'p' puntera' */
+    p = malloc(sizeof(*p));
+
+    // Ora 'p' punta a 'str', pertanto saranno allocati sizeof(str) byte
+    p = str;
+
+    printf("%s\n", p);
+
     return(EXIT_SUCCESS);
 }
