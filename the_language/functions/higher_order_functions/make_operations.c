737758266644465d8dc057fcc9bba3186c45a1dc
B3h3m0th <behemoth@autistici.org>
2016-01-26 11:34:43 +0100

    added the foour operations

--- the_language/functions/higher_order_functions/make_operations.c
+++ the_language/functions/higher_order_functions/make_operations.c
@@ -5,6 +5,9 @@
 int make_operations(int val1, int val2, int (*func)(int, int));
 // A supporto della funzione make_operations().
 int addition(int val1, int val2);
+int subtraction(int val1, int val2);
+int multiplication(int val1, int val2);
+int division(int val1, int val2);
 
 /* L'intero programma ruoto attorno alla funzione generica make_operations(),
 che e' in grado di eseguire diverse operazioni tra due valori mediante la
@@ -12,4 +15,21 @@ funzione int (*func)(int, int) passata come argomento. */
 
 int main(void) {
     return(EXIT_SUCCESS);
+}
+
+
+int addition(int val1, int val2) {
+    return (val1 + val2);
+}
+
+int subtraction(int val1, int val2);
+    return (val1 - val2);
+}
+
+int multiplication(int val1, int val2);
+    return (val1 * val2);
+}
+
+int division(int val1, int val2);
+    return (val1 / val2);
 }
