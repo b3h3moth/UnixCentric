c889d30468d568c1c37722b73b09e6021964b1de
B3h3m0th <behemoth@autistici.org>
2016-01-26 18:21:56 +0100

    callback functions to manage a stream

--- the_language/functions/higher_order_functions/callback_functions.c
+++ the_language/functions/higher_order_functions/callback_functions.c
@@ -2,6 +2,35 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main(void) {
+#define ELEM_SIZE   10
+
+// Prototipi
+void open_file(FILE *stream, char *filename, int(*fail)(int, char*));
+int fail(int err_num, char *arg);
+void access_granted(char *filename);
+
+int main(int argc, char *argv[]) {
+    FILE *fi;
+    
+    open_file(fi, argv[1], &fail);
+
     return(EXIT_SUCCESS);
+}
+
+void open_file(FILE *stream, char *filename, int(*fail)(int, char*)) {
+    stream = fopen(filename, "r");
+
+    if (stream == NULL)
+        (*fail)(-1, "Opening %s file");
+    else
+        (*access_granted)(filename);
+}
+
+int fail(int err_num, char *arg) {
+    fprintf(stdout, "Err. %d - %s\n", err_num, arg);
+    return(err_num);
+}
+
+void access_granted(char *filename) {
+    fprintf(stdout, "The file \'%s\' is readable\n", filename);
 }
