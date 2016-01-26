dd32075d7cd64e4daca13525d220c234990097f2
B3h3m0th <behemoth@autistici.org>
2016-01-26 11:31:20 +0100

    make each operation through higher order function

---
+++ the_language/functions/higher_order_functions/make_operations.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Prototipi
+int make_operations(int val1, int val2, int (*func)(int, int));
+// A supporto della funzione make_operations().
+int addition(int val1, int val2);
+
+/* L'intero programma ruoto attorno alla funzione generica make_operations(),
+che e' in grado di eseguire diverse operazioni tra due valori mediante la
+funzione int (*func)(int, int) passata come argomento. */
+
+int main(void) {
+    return(EXIT_SUCCESS);
+}
