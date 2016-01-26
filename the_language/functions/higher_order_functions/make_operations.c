67a2eae48d641f64f5cc7e9be4a4e4725d86c34f
B3h3m0th <behemoth@autistici.org>
2016-01-26 12:01:24 +0100

    testing higher order function make_operations()

--- the_language/functions/higher_order_functions/make_operations.c
+++ the_language/functions/higher_order_functions/make_operations.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define VECSIZE(x) ((int)sizeof(x) / sizeof(x[0]))
+
 // Prototipi
 int make_operations(int val1, int val2, int (*func)(int, int));
 // A supporto della funzione make_operations().
@@ -17,6 +19,14 @@ int main(void) {
 
     int (*operations_list[])(int, int) = { &addition, &subtraction,
         &multiplication, &division };
+   
+    char *operations_name[] = { "addition", "subtraction", "multiplication",
+        "division" };
+
+    for (int i=0; i<VECSIZE(operations_list); i++) {
+        printf("(%14s %d %d) = %d\n", operations_name[i], i, i, 
+                make_operations(i, i, operations_list[i]));
+    }
 
     return(EXIT_SUCCESS);
 }
