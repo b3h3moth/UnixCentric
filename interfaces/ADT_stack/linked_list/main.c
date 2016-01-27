9937c6a8a75a15d6a97e6874bbb146a50963c487
B3h3m0th <behemoth@autistici.org>
2016-01-27 18:29:05 +0100

    abstract data type, stack linked list testing functions

--- interfaces/ADT_stack/linked_list/main.c
+++ interfaces/ADT_stack/linked_list/main.c
@@ -3,21 +3,19 @@
 #include "stack.h"
 
 int main(void) {
+    Stack st1 = create();
+
     for (int i=0; i<10; i++)
-        push(i);
+        push(st1, i);
     
-    // Rimuove 9
-    pop();
-    // Rimuove 8
-    pop();
-    // Rimuove 7
-    pop();
+    printf("pop: %d from stack\n", pop(st1));
+    printf("pop: %d from stack\n", pop(st1));
+    printf("pop: %d from stack\n", pop(st1));
+    printf("pop: %d from stack\n", pop(st1));
 
-    print_stack_elements();
+    print_stack(st1);
 
-    make_empty();
+    make_empty(st1);
 
-    print_stack_elements();
-    
     return(EXIT_SUCCESS);
 }
