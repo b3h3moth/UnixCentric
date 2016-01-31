29d959424b5073bbd340cc954f71f826618f9dbc
B3h3m0th <behemoth@autistici.org>
2016-01-31 02:42:42 +0100

    check_error() doesn't work at the moment, reset all

--- the_language/functions/higher_order_functions/manage_different_data_types.c
+++ the_language/functions/higher_order_functions/manage_different_data_types.c
@@ -2,33 +2,43 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <inttypes.h>
-#include <string.h>
 
-void check_error(char *data, char *arg, char *format, long min, long max, void(*verify)(char *, char *, char *, int, int));
-void verify(char *data, char *arg, char *format);
+void func(char *data, char *arg, void(*check_error)(char *, char *));
+void check_error(char *data, char *fmt);
 
 int main(void) {
-
+    func("1532", "%d", &check_error);
     return(EXIT_SUCCESS);
 }
 
-void check_error(char *data, char *arg, char *format, long min, long max, void(*verify)(char *, char *, char *, int, int)) {
-        verify(data, arg, format, min, max);
+void func(char *data, char *arg, void(*check_error)(char *, char *)) {
+    check_error(data, arg);
 }
 
-void verify(char *data, char *arg, char *format) {
-    char *fmt = (void *)format;
+void check_error(char *data,char *arg) {
+    char *fmt = (void *)arg;
+
+    printf(fmt, strtol(data, NULL, 10));
+}
+
+/*
+void check_error(const char *data_type, char *value, int min, int max) {
+    printf("%lu\n", (long int)strtoimax(value, NULL, 10));
+
+    if (strtoimax(value, NULL, 10) == 0) {
+        fprintf(stderr, "0 is not a valid value, please\n");
+    } 
     
-    if ((atoll(value)) > max) {
+    if (strtoimax(value, NULL, 10) > max) {
         fprintf(stderr, "[Error]: Integer Overflow on type '%s'.\n"
                 "Available numeric range: from '%d' to '%d'\n", 
                 data_type, min, max);
-        exit(EXIT_FAILURE);
-    } else if ((atoll(value)) < min) {
+    } 
+    
+    if (strtoimax(value, NULL, 10) < min) {
         fprintf(stderr, "[Error]: Integer Underflow on type '%s'.\n"
-                "Available numeric range: from '%d' to '%d'\n", 
+                "Available numeric range: from '%d' to '%u'\n", 
                 data_type, min, max);
-        exit(EXIT_FAILURE);
     }
 }
-
+*/
