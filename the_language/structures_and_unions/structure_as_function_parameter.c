68ce42d551d102851b1b8cf681198a9af4769ce3
B3h3m0th <behemoth@autistici.org>
2016-01-31 16:31:32 +0100

    struct point added

--- the_language/structures_and_unions/structure_as_function_parameter.c
+++ the_language/structures_and_unions/structure_as_function_parameter.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct point {
+    int x;
+    int y;
+    int sum;
+};
+
+struct point init(int x, int y)
+
 int main(void) {
     return(EXIT_SUCCESS);
 }
