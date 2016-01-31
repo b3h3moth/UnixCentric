a7f48a0ead5ee2ccb632a78d490a3dea561d4e7c
B3h3m0th <behemoth@autistici.org>
2016-01-31 16:55:53 +0100

    designated initializers within init() function

--- the_language/structures_and_unions/structure_as_function_parameter.c
+++ the_language/structures_and_unions/structure_as_function_parameter.c
@@ -13,15 +13,37 @@ struct point sum(struct point sp1, struct point sp2);
 void random(struct point *sptr);
 
 int main(void) {
+    struct point my_sp;
+    my_sp = init(10, 20);
+    printf("%d %d\n",my_sp.x, my_sp.y);
     return(EXIT_SUCCESS);
 }
 
 // Inizializza e restituisce la struttura point
-struct point init(int x, int y) {
+struct point init(int val1, int val2) {
+    
+    /* Un modo potrebbe essere di creare una struttura, inizializzarne i
+    membri e infine restuirla al chiamante:
+    
+    struct point temp;
+    temp.x = val1;
+    temp.y = val2;
+    
+    return temp; 
+
+    Oppure si fa ritornare direttamente una struttura anonima i cui membri 
+    saranno inizializzati mediante, per l'appunto, gli inizializzatori 
+    designati (designated initializers) */
+
+    return (struct point) {.x = val1, .y = val2};
+}
+
 
 /* Somma i membri delle strutture sp1 e sp2, e restituisce il risultato sotto
 forma di struttura di tipo point */
 struct point sum(struct point sp1, struct point sp2) {
+}
 
 // Assegna ai membri della struttura puntata da *sptr valori random
 void random(struct point *sptr) {
+}
