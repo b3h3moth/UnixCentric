f7256b1fada90be2966b001d7c19adc381288674
B3h3m0th <behemoth@autistici.org>
2016-01-31 16:41:06 +0100

    init() sum() random() function prototypes

--- the_language/structures_and_unions/structure_as_function_parameter.c
+++ the_language/structures_and_unions/structure_as_function_parameter.c
@@ -7,8 +7,21 @@ struct point {
     int sum;
 };
 
-struct point init(int x, int y)
+// Function prototypes
+struct point init(int x, int y);
+struct point sum(struct point sp1, struct point sp2);
+void random(struct point *sptr);
 
 int main(void) {
     return(EXIT_SUCCESS);
 }
+
+// Inizializza e restituisce la struttura point
+struct point init(int x, int y) {
+
+/* Somma i membri delle strutture sp1 e sp2, e restituisce il risultato sotto
+forma di struttura di tipo point */
+struct point sum(struct point sp1, struct point sp2) {
+
+// Assegna ai membri della struttura puntata da *sptr valori random
+void random(struct point *sptr) {
