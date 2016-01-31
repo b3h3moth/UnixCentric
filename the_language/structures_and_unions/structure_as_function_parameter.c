96e992d0607769e07f05e9164d9b9895ea37d003
B3h3m0th <behemoth@autistici.org>
2016-01-31 17:12:54 +0100

    sum() function added, returns the sum of structures sp1 and sp2

--- the_language/structures_and_unions/structure_as_function_parameter.c
+++ the_language/structures_and_unions/structure_as_function_parameter.c
@@ -50,6 +50,13 @@ struct point init(int val1, int val2) {
 /* Somma i membri delle strutture sp1 e sp2, e restituisce il risultato sotto
 forma di struttura di tipo point */
 struct point sum(struct point sp1, struct point sp2) {
+    struct point temp;
+    
+    temp.x = sp1.x + sp2.x;
+    temp.y = sp1.y + sp2.y;
+    temp.sum = 0;
+
+    return temp;
 }
 
 // Assegna ai membri della struttura puntata da *sptr valori random
