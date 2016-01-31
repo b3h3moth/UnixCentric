1d92d524e8d65391694a84517e205f5cd4124a98
B3h3m0th <behemoth@autistici.org>
2016-01-31 17:30:54 +0100

    two techniques to get the same result

--- the_language/structures_and_unions/structure_as_function_parameter.c
+++ the_language/structures_and_unions/structure_as_function_parameter.c
@@ -55,6 +55,10 @@ struct point init(int val1, int val2) {
 /* Somma i membri delle strutture sp1 e sp2, e restituisce il risultato sotto
 forma di struttura di tipo point */
 struct point sum(struct point sp1, struct point sp2) {
+
+    /* Come nel caso della funzione init() la somma di due strutture e la
+    relativa restituzione puo' essere eseguita mediante:
+
     struct point temp;
     
     temp.x = sp1.x + sp2.x;
@@ -62,6 +66,11 @@ struct point sum(struct point sp1, struct point sp2) {
     temp.sum = 0;
 
     return temp;
+
+    Oppure utilizzando nuovamente gli inizializzatori designati con una
+    struttura anonima: */
+
+    return (struct point) { .x = sp1.x + sp2.x, .y = sp1.y + sp2.y};
 }
 
 // Assegna ai membri della struttura puntata da *sptr valori random
