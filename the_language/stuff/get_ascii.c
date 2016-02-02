cbc9135aabe307832e7cd75838cffdc82f5f054b
B3h3m0th <behemoth@autistici.org>
2016-02-02 09:32:45 +0100

    get_ascii() definition

--- the_language/stuff/get_ascii.c
+++ the_language/stuff/get_ascii.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define BUF_SIZE 1024
+#define MIN_SIZE 10
+
 void get_ascii(char *name, FILE *f_in);
 
 int main(void) {
@@ -8,3 +11,22 @@ int main(void) {
 }
 
 void get_ascii(char *name, FILE *f_in) {
+    int c, i;
+    char buf[BUF_SIZE];
+
+    do {
+        for (i=0; (c = fgetc(f_in)) != EOF) {
+            if (!isprint(c))
+                break;
+
+            buf[i++] = c;
+
+            if (i >= BUF_SIZE)
+                break;
+        }
+
+        // Stampa solo se e' sufficientemente lungo
+        if (i >= MIN_SIZE)
+            printf("%s:%.*s\n", name, i, buf);
+    } while (c != EOF);
+}
