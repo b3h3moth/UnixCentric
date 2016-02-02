98ef7e29ece13d6e9a74a6ad8b39c126c2305792
B3h3m0th <behemoth@autistici.org>
2016-02-02 10:39:14 +0100

    format string fixed

--- the_language/stuff/printf_width_precision.c
+++ the_language/stuff/printf_width_precision.c
@@ -11,8 +11,12 @@ int main(int argc, char *argv[]) {
     int size = (sizeof(unix_skills) / sizeof(unix_skills[0]));
 
     for (int i=0; i<size-1; i++)
-        // Indica la la lunghezza del dato
+        // Indica la la lunghezza del dato da stampare in output
         printf("%.*s\n", strlen(unix_skills[i]), unix_skills[i]);
+
+    for (int i=0; i<size-1; i++)
+        // Cosi' facendo stampo solo i primi due caratteri di ciascuna stringa
+        printf("%.*s\n", 2, unix_skills[i]);
 
     return(EXIT_SUCCESS);
 }
