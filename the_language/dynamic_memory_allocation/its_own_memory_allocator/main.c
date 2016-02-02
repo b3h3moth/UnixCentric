f180225be4985604e93550f538966910b31c0d6c
B3h3m0th <behemoth@autistici.org>
2016-02-02 18:34:05 +0100

    memory allocation with malloca() within main()

--- the_language/dynamic_memory_allocation/its_own_memory_allocator/main.c
+++ the_language/dynamic_memory_allocation/its_own_memory_allocator/main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "my_malloca.h"
 
 int main(void) {
+    char *title = "The Unix Programming Environment";
+    unsigned int size = sizeof(title);
+    char *ptr;
+
+    ptr = malloca(size);
+
+    strcpy(ptr, title);
+
+    printf(ptr);
+
     return(EXIT_SUCCESS);
 }
