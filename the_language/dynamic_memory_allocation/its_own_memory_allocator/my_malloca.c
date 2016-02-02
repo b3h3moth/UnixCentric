c7de2c901f53806b2e6bdb6bf80773e8b623585f
B3h3m0th <behemoth@autistici.org>
2016-02-02 18:19:28 +0100

    my memory allocation (a) definition is done

--- the_language/dynamic_memory_allocation/its_own_memory_allocator/my_malloca.c
+++ the_language/dynamic_memory_allocation/its_own_memory_allocator/my_malloca.c
@@ -1,13 +1,29 @@
 #ifndef MY_ALLOCA
 #define MY_ALLOCA
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h> // sbrk()
+#include <errno.h>
+#include <string.h>
 
 /* my malloc (a)
 utilizza la syscall sbrk() per la manipolazione dello heap; sbrk(0) ritorna un
 puntatore a 'break', ovvero in cima allo heap, sbrk(n) incrementa lo heap di
-'n' byte e ritorna un puntatore al nuovo indirizzo. */
+'n' byte e ritorna un puntatore al nuovo indirizzo. In caso di fallimento
+riorna (void *)-1, con il settaggio appropriato della variabile 'errno'. */
 
 void *malloca(size_t size) {
+    void *ptr = sbrk(0);
+    void *increment = sbrk(size);
 
+    if (increment == (void *) - 1) {
+        fprintf(stderr, "Err.(%d) sbrk(): %s\n", errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    } else {
+        assert (ptr == increment);
+        return ptr;
+    }
+}
 
+#endif
