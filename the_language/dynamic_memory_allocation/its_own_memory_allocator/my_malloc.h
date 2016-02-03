e85741a8e8d0afe2d749ca86b2a837f77944ccef
B3h3m0th <behemoth@autistici.org>
2016-02-03 10:25:01 +0100

    top of the linked list, data structure to manage memory allocation

--- the_language/dynamic_memory_allocation/its_own_memory_allocator/my_malloc.h
+++ the_language/dynamic_memory_allocation/its_own_memory_allocator/my_malloc.h
@@ -2,10 +2,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define META_BLOCK    sizeof(struct mem_block)
+
 typedef struct mem_block *t_mem_block;
 
 struct mem_block {
     size_t      size;
-    int         free;
     t_mem_block next;
+    int         free;
 };
+
+/* L'allocazione della memoria sara' effettuata mediante una linked-list,
+base punta alla testa della lista */
+void *base = NULL;
