976449f2b42550459aa54885540dc5c417e21c5c
B3h3m0th <behemoth@autistici.org>
2016-02-04 10:38:22 +0100

    memory allocator initialization, init_alloc() added

--- the_language/dynamic_memory_allocation/its_own_memory_allocator/my_mallocb.c
+++ the_language/dynamic_memory_allocation/its_own_memory_allocator/my_mallocb.c
@@ -1,10 +1,21 @@
+#include <unistd.h>
 #include "my_mallocb.h"
 
 // L'allocatore e' inizializzato
-int init = 0;
+int init_allocator = 0;
 
 // Puntatore all'inizio dell'area di memoria da gestire
 void *beginning_memory;
 
-// Individua l'ultimo indirizzo valido
+// Individua l'ultimo indirizzo valido, riconducibile a 'break'
 void *last_address;
+
+// Inizializza l'allocatore di memoria
+void init_alloc(void) {
+    // Salva l'ultimo indirizzo valido puntato da 'break'
+    last_address = sbrk(0);
+
+    beginning_memory = last_address;
+
+    init_allocator = 1;
+}
--- the_language/dynamic_memory_allocation/its_own_memory_allocator/my_mallocb.h
+++ the_language/dynamic_memory_allocation/its_own_memory_allocator/my_mallocb.h
@@ -1,6 +1,7 @@
 #ifndef MY_ALLOCB
 #define MY_ALLOCB
 
-
+// Prototipi
+void init_alloc(void);
 
 #endif
