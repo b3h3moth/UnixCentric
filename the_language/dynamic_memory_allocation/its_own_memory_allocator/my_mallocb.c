aa707b92e3e466a35a28de2cc5e1a92c02f52b7a
B3h3m0th <behemoth@autistici.org>
2016-02-04 10:13:39 +0100

    allocator global variables

--- the_language/dynamic_memory_allocation/its_own_memory_allocator/my_mallocb.c
+++ the_language/dynamic_memory_allocation/its_own_memory_allocator/my_mallocb.c
@@ -1,5 +1,10 @@
 #include "my_mallocb.h"
 
+// L'allocatore e' inizializzato
 int init = 0;
+
+// Puntatore all'inizio dell'area di memoria da gestire
 void *beginning_memory;
+
+// Individua l'ultimo indirizzo valido
 void *last_address;
