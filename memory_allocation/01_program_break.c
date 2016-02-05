4ae3db11de49f07988b72f004f6da9d7fa4583f2
B3h3m0th <behemoth@autistici.org>
2016-02-05 18:36:04 +0100

    sbrk() how it works

--- memory_allocation/01_program_break.c
+++ memory_allocation/01_program_break.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 /* Sui sistemi UNIX il modo piu' semplice per allocare memoria dinamicamente
 e' mediante le syscalls brk() e sbrk() che consentono la gestione e il relativo
-innalzamento del 'program break'. */
+innalzamento del 'program break'.
+
+HEADER    : <unistd.h>
+PROTOTYPE : void *sbrk (intptr_t increment);
+
+La funzione sbrk() consente di incrementare e/o decrementare il valore corrente
+del 'program break' mediante un offset intero specificato da 'increment'; 
+va da se che incrementare sta per allocare memoria, decrementare invece sta per
+deallocare. Ritorna il valore del 'program break' in caso di esito positivo,
+-1 nel caso fallisse. La chiamata sbrk(0) restituisce la locazione corrente 
+del 'program break'.
+
+*/
+
 
 int main(void) {
     return(EXIT_SUCCESS);
