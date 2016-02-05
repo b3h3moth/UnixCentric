765535d024b289d6e8a519cfef81cbd9b9ed8914
B3h3m0th <behemoth@autistici.org>
2016-02-05 18:18:16 +0100

    how manage program break on UNIX system

--- memory_allocation/01_program_break.c
+++ memory_allocation/01_program_break.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Sui sistemi UNIX il modo piu' semplice per allocare memoria dinamicamente
+e' mediante le syscalls brk() e sbrk() che consentono la gestione e il relativo
+innalzamento del 'program break'. */
+
 int main(void) {
     return(EXIT_SUCCESS);
 }
