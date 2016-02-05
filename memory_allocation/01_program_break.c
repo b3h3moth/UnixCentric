7e4300965fc837e2a8d32dcd9f8dfd66408484da
B3h3m0th <behemoth@autistici.org>
2016-02-05 20:26:28 +0100

    replaced -1 with (void*)-1, if sbrk() fails

--- memory_allocation/01_program_break.c
+++ memory_allocation/01_program_break.c
@@ -15,7 +15,7 @@ La funzione sbrk() consente di incrementare e/o decrementare il valore corrente
 del 'program break' mediante un offset intero specificato da 'increment'; 
 va da se che incrementare sta per allocare memoria, decrementare invece sta per
 deallocare. Ritorna il valore del 'program break' in caso di esito positivo,
--1 nel caso fallisse. La chiamata sbrk(0) restituisce la locazione corrente 
+(void*)-1 nel caso fallisse. La chiamata sbrk(0) restituisce la locazione corrente 
 del 'program break'.
 */
 
