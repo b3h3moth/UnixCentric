ea9113cb7da3bbb9585d6cc40e7b8fdc95263c26
B3h3m0th <behemoth@autistici.org>
2016-02-05 18:47:45 +0100

    get current program break address

--- memory_allocation/01_program_break.c
+++ memory_allocation/01_program_break.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
+#include <string.h>
 
 /* Sui sistemi UNIX il modo piu' semplice per allocare memoria dinamicamente
 e' mediante le syscalls brk() e sbrk() che consentono la gestione e il relativo
@@ -17,10 +19,19 @@ deallocare. Ritorna il valore del 'program break' in caso di esito positivo,
 del 'program break'.
 */
 
-
-/* Lo scopo del programma e' di invocare sbrk() modificando il valore del
-'program break', nel caso specifico sara' incrementato */
+/* Lo scopo del programma e' di incrementare il 'program break' di 10 byte,
+di salvare il suo valore prima e dopo la chiamata alla system call e infine
+stampare in output i relativi valori. */
 
 int main(void) {
+    const intptr_t size_incr = 10;
+    void *pb_old_addr, *pb_current_addr;
+
+    // Si ottiene il valore corrente del 'program 'break'
+    if ( (pb_old_addr = sbrk(0)) == (void *) - 1) {
+        fprintf(stderr, "Err.(%d) sbrk(): %s\n", errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
     return(EXIT_SUCCESS);
 }
