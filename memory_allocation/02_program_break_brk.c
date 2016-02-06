8c652a8a368b98bea6af2a630c18dc4bd6c5a35f
B3h3m0th <behemoth@autistici.org>
2016-02-06 18:17:09 +0100

    malloc() and sbrk() are implemented on top of brk()

--- memory_allocation/02_program_break_brk.c
+++ memory_allocation/02_program_break_brk.c
@@ -9,8 +9,9 @@ PROTOTYPE : int brk(void *address);
 
 brk() imposta il 'program break' all'indirizzo specificato da 'address', in 
 altre parole setta la fine del segmento data (BSS) all'indirizzo 'address'. 
-Ritorna 0 in caso di successo, -1 in caso di errore. */
+Ritorna 0 in caso di successo, -1 in caso di errore. 
 
+Sia malloc() sia sbrk() sono implementate sopra brk(). */
 
 int main(void) {
     return(EXIT_SUCCESS);
