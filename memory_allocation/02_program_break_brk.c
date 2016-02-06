099fde12312a17022412340145b10285e0fa76ff
B3h3m0th <behemoth@autistici.org>
2016-02-06 18:24:56 +0100

    checking for argument

--- memory_allocation/02_program_break_brk.c
+++ memory_allocation/02_program_break_brk.c
@@ -13,6 +13,21 @@ Ritorna 0 in caso di successo, -1 in caso di errore.
 
 Sia malloc() sia sbrk() sono implementate sopra brk(). */
 
-int main(void) {
+/* Lo scopo del programma e' di salvare il 'program break' corrente, 
+incrementare la memoria mediante un valore ricevuto in input, stampare il nuovo
+'program break'. */
+
+int main(int argc, char *argv[]) {
+    void *pb_old_addr, *pb_cur_addr;
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <size>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    
+    intptr_t size_incr = strtol(argv[1], NULL, 10);
+
+
+
     return(EXIT_SUCCESS);
 }
