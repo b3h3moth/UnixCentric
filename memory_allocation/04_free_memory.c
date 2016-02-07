ba8a389d6edde74402a275c91cbae264a78f3de4
B3h3m0th <behemoth@autistici.org>
2016-02-07 20:02:17 +0100

    fixed printf() output and improved comments

--- memory_allocation/04_free_memory.c
+++ memory_allocation/04_free_memory.c
@@ -9,29 +9,49 @@
 deallocarli */
 
 int main(int argc, char *argv[]) {
-    void *addr;
+    void *addr_a, *addr_b, *addr_c;
     intptr_t size_incr = 32;
 
     // Salva il valore corrente del 'program break'
-    if ((addr = sbrk(0)) == (void *)-1) {
+    if ((addr_a = sbrk(0)) == (void *)-1) {
         fprintf(stderr, "Err.(%d) sbrk(0): %s\n", errno, strerror(errno));
         exit(EXIT_FAILURE);
     }
 
-    printf("Current program break address: %p\n", addr);
+    printf("%p - Current program break address (starting program) \n", \
+            addr_a);
 
     // Incrementa il 'program break' di 'size_incr' byte
-    if (brk(((uint8_t*)addr + size_incr)) == -1 ) {
+    if (brk(((uint8_t*)addr_a + size_incr)) == -1 ) {
         fprintf(stderr, "Err.(%d) brk(): %s\n", errno, strerror(errno));
         exit(EXIT_FAILURE);
     }
 
+    // Salva nuovamente il valore corrente del 'program break'
+    if ((addr_b = sbrk(0)) == (void *)-1) {
+        fprintf(stderr, "Err.(%d) sbrk(0): %s\n", errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
 
-    // Libera la memoria precedentemente allocata
-    if (brk(addr) == -1) {
+    printf("%p - Current program break address (after %d byte allocation) \n", \
+            addr_b, size_incr);
+
+    /* Libera la memoria precedentemente allocata, fornendo a brk() l'indirizzo
+    del 'program break' iniziale */
+    if (brk(addr_a) == -1) {
         fprintf(stderr, "Err.(%d) brk(): %s\n", errno, strerror(errno));
         exit(EXIT_FAILURE);
     }
+
+    /* Dopo aver aver liberato la memoria salva ancora una volta il valore
+    corrente del 'program break' */
+    if ((addr_c = sbrk(0)) == (void *)-1) {
+        fprintf(stderr, "Err.(%d) sbrk(0): %s\n", errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    printf("%p - Current program break address (after %d byte deallocation)\n",\
+            addr_b, size_incr);
 
     return(EXIT_SUCCESS);
 }
