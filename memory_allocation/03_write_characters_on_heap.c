753bbd4670203996c7622b2942c0741cecc653de
B3h3m0th <behemoth@autistici.org>
2016-02-07 18:54:16 +0100

    write characers within heap

---
+++ memory_allocation/03_write_characters_on_heap.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <stdint.h>
+
+/* Lo scopo del programma e' di scrivere la stringa "Hello World" a partire
+dall'indirizzo iniziale della heap, ovvero dal program break. */
+
+int main(int argc, char *argv[]) {
+    void *pb_old_addr, *pb_cur_addr;
+    intptr_t size_incr = 11;
+
+    // Salva il valore corrente del 'program break'
+    if ((pb_old_addr = sbrk(0)) == (void *)-1) {
+        fprintf(stderr, "Err.(%d) old sbrk(0): %s\n", errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    // Incrementa il 'program break' di 'size_incr' byte
+    if (brk(((uint8_t*)pb_old_addr + size_incr)) == -1 ) {
+        fprintf(stderr, "Err.(%d) brk(): %s\n", errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    // Salva nuovamente il valore corrente del 'program break'
+    if ((pb_cur_addr = sbrk(0)) == (void *)-1) {
+        fprintf(stderr, "Err.(%d) cur sbrk(0): %s\n", errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    *((char *)pb_old_addr) = 'H';
+    *((char *)pb_old_addr + 1) = 'e';
+    *((char *)pb_old_addr + 2) = 'l';
+    *((char *)pb_old_addr + 3) = 'l';
+    *((char *)pb_old_addr + 4) = 'o';
+    *((char *)pb_old_addr + 5) = ' ';
+    *((char *)pb_old_addr + 6) = 'W';
+    *((char *)pb_old_addr + 7) = 'o';
+    *((char *)pb_old_addr + 8) = 'r';
+    *((char *)pb_old_addr + 9) = 'l';
+    *((char *)pb_old_addr + 10) = 'd';
+
+    return(EXIT_SUCCESS);
+}
