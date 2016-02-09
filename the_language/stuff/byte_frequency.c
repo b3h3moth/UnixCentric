469f5097978c2b3e295b0e34d9df4617749434c1
B3h3m0th <behemoth@autistici.org>
2016-02-09 12:21:30 +0100

    get byte frequency

---
+++ the_language/stuff/byte_frequency.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+unsigned long count[UCHAR_MAX+1];
+
+// Lo scopo del programma e' di visualizzare la frequenza dei byte
+
+int main(void) {
+    return(EXIT_SUCCESS);
+}
