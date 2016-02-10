600eabdb3e1dd8bad10dd5d331a895e8cd3745b2
B3h3m0th <behemoth@autistici.org>
2016-02-10 22:23:35 +0100

    offset of void *ptr

--- the_language/structures_and_unions/offsetof.c
+++ the_language/structures_and_unions/offsetof.c
@@ -22,6 +22,7 @@ struct data {
         int uc;
         int ud;
     } type;
+    void *ptr;
 };
 
 typedef struct data data;
@@ -29,23 +30,25 @@ typedef struct data data;
 int main(void) {
     /* Il primo membro della struttura, poiche' corrisponde all'indirizzo della
     struttura stessa, dovrebbe avere offset pari a 0 */
-    printf("%2d is the offset 1st member (char c)\n", offsetof(data, c));
-    printf("%2d is the offset 2nd member (short s)\n", offsetof(data, s));
-    printf("%2d is the offset 3rd member (int vec[4])\n", offsetof(data, vec));
-    printf("%2d is the offset 4th member (float f)\n", offsetof(data, f));
-    printf("%2d is the offset 5th member (double d)\n", offsetof(data, d));
+    printf("%2d offset 1st member (char c)\n", offsetof(data, c));
+    printf("%2d offset 2nd member (short s)\n", offsetof(data, s));
+    printf("%2d offset 3rd member (int vec[4])\n", offsetof(data, vec));
+    printf("%2d offset 4th member (float f)\n", offsetof(data, f));
+    printf("%2d offset 5th member (double d)\n", offsetof(data, d));
 
     // Si puo' calcolare l'offset anche degli elementi di un vettore
-    printf("%2d is the offset of (int vec[0])\n", offsetof(data, vec[0]));
-    printf("%2d is the offset of (int vec[1])\n", offsetof(data, vec[1]));
-    printf("%2d is the offset of (int vec[2])\n", offsetof(data, vec[2]));
-    printf("%2d is the offset of (int vec[3])\n", offsetof(data, vec[3]));
+    printf("%2d offset (int vec[0])\n", offsetof(data, vec[0]));
+    printf("%2d offset (int vec[1])\n", offsetof(data, vec[1]));
+    printf("%2d offset (int vec[2])\n", offsetof(data, vec[2]));
+    printf("%2d offset (int vec[3])\n", offsetof(data, vec[3]));
 
-    printf("%2d is the offset of (union int ua)\n", offsetof(data, type.ua));
-    printf("%2d is the offset of (union int ub)\n", offsetof(data, type.ub));
-    printf("%2d is the offset of (union int uc)\n", offsetof(data, type.uc));
-    printf("%2d is the offset of (union int ud)\n", offsetof(data, type.ud));
+    // Qualsiasi offset inerente la union sara' sempre il medesimo
+    printf("%2d offset 6th (union int ua)\n", offsetof(data, type.ua));
+    printf("%2d offset 6th (union int ub)\n", offsetof(data, type.ub));
+    printf("%2d offset 6th (union int uc)\n", offsetof(data, type.uc));
+    printf("%2d offset 6th (union int ud)\n", offsetof(data, type.ud));
 
+    printf("%2d offset 7th member (void *ptr)\n", offsetof(data, ptr));
 
     return(EXIT_SUCCESS);
 }
