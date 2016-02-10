1609530bb79a978db0bd1dd7d032ed6aa9a1b5bc
B3h3m0th <behemoth@autistici.org>
2016-02-10 19:23:26 +0100

    get offset of the fourth structures's member

--- the_language/structures_and_unions/offsetof.c
+++ the_language/structures_and_unions/offsetof.c
@@ -23,9 +23,10 @@ typedef struct data data;
 int main(void) {
     /* Il primo membro della struttura, poiche' corrisponde all'indirizzo della
     struttura stessa, dovrebbe avere offset pari a 0 */
-    printf("%d offset first member (char)\n", offsetof(data, c));
-    printf("%d offset second member (short)\n", offsetof(data, s));
+    printf("%d offset first member (char c)\n", offsetof(data, c));
+    printf("%d offset second member (short s)\n", offsetof(data, s));
     printf("%d offset third member (int vec[4])\n", offsetof(data, vec));
+    printf("%d offset fourth member (float d)\n", offsetof(data, f));
 
     return(EXIT_SUCCESS);
 }
