b29112e03a3c3204dea1ac7f9e06ca0ad9585e58
B3h3m0th <behemoth@autistici.org>
2016-02-10 18:58:47 +0100

    offsetof, how macro works

--- the_language/structures_and_unions/offsetof.c
+++ the_language/structures_and_unions/offsetof.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
+
+/* 
+HEADER: <stddef.h>
+MACRO : size_t offsetof(type, member);
+
+La macro offsetof richiede due argomenti, il tipo di una struttura 'type' e un
+mebro della struttura 'member', lo scopo e' di calcolare il numero di byte
+tra l'inizio della struttura e il membro designato. */
 
 int main(void) {
     return(EXIT_SUCCESS);
