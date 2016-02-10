ec9c18257a73e7557d3bd30608c0372eab9397de
B3h3m0th <behemoth@autistici.org>
2016-02-10 19:00:55 +0100

    struct data, get offset from each member

--- the_language/structures_and_unions/offsetof.c
+++ the_language/structures_and_unions/offsetof.c
@@ -7,8 +7,16 @@ HEADER: <stddef.h>
 MACRO : size_t offsetof(type, member);
 
 La macro offsetof richiede due argomenti, il tipo di una struttura 'type' e un
-mebro della struttura 'member', lo scopo e' di calcolare il numero di byte
-tra l'inizio della struttura e il membro designato. */
+mebro della struttura 'member', lo scopo e' di calcolare il numero di byte - 
+l'offset ovviamente - tra l'inizio della struttura e il membro designato. */
+
+struct data {
+    char c;
+    short s;
+    int vec[4];
+    float f;
+    double d;
+};
 
 int main(void) {
     return(EXIT_SUCCESS);
