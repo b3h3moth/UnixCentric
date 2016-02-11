6e4b20f04bc8f892f35096deb809dfb3b763955f
B3h3m0th <behemoth@autistici.org>
2016-02-11 11:32:07 +0100

    insert() function added

--- the_language/structures_and_unions/full_programs/inventory.c
+++ the_language/structures_and_unions/full_programs/inventory.c
@@ -3,7 +3,7 @@
 //#include "readline.h"
 
 #define MAX_LEN 25
-#define MAX_HW 100  // Compomenti Hardware
+#define MAX_COMPONENTS 100  // Componenti Hardware
 
 struct hw {
     int number;
@@ -13,7 +13,7 @@ struct hw {
 
 typedef struct hw Hardware; 
 
-Hardware inventory[MAX_HW];
+Hardware inventory[MAX_COMPONENTS];
 
 // Il numero dei componenti hardware memorizzati
 int num_hw_components = 0;
@@ -29,7 +29,7 @@ int main(void) {
 
     for (;;) {
         printf("[i]nsert [u]pdate [s]earch] [p]rint [q]uit: ");
-        scanf("%c", &code);
+        scanf(" %c", &code);
 
         /* Salta a fine riga, consentendo una interazione col menu'
         pulita e ben formattata */
@@ -49,8 +49,33 @@ int main(void) {
     return(EXIT_SUCCESS);
 }
 
+/* Inserisce un componente nel database; nel caso fosse gia' presente nel db 
+opppure il db stesso fosse pieno restituisce un messaggio di errore */
 int insert(void) {
-    fputs("insert\n", stdout);
+    int component_num;
+
+    if (num_hw_components == MAX_COMPONENTS) {
+        printf("DB is full\n");
+        return;
+    }
+
+    printf("Enter component number: ");
+    scanf("%d", &component_num);
+
+    // Verifica se un componente gia' esiste
+    if (find_component(component_num) >= 0) {
+        printf("Component already exists.\n");
+        return;
+    }
+    inventory[component_num].number = component_num;
+
+    printf("Enter component name: ");
+    //read_line(inventory[component_num].name, MAX_LEN);
+
+    printf("Enter quantity: ");
+    scanf("%d", &inventory[component_num].on_hand);
+
+    component_num++;
 }
 void update(void) {
     fputs("update\n", stdout);
@@ -69,5 +94,5 @@ int find_component(int num) {
         if (inventory[i].number == num)
             return i;
 
-    return(EXIT_FAILURE);
+    return (-1);
 }
