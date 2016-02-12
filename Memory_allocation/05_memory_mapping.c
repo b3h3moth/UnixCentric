7fced09681d304422f4665ea624872e21f1e2637
B3h3m0th <behemoth@autistici.org>
2016-02-12 19:32:01 +0100

    address and length of mmap()

--- Memory_allocation/05_memory_mapping.c
+++ Memory_allocation/05_memory_mapping.c
@@ -7,6 +7,11 @@ della memoria stessa utilizzando le syscall mmap() e munmap().
 HEADER   : <sys/mman.h>
 PROTOTYPE: void *mmap(void *addr, size_t length, int prot, int flags, 
                       int fd, off_t offset);
+
+mmap() crea una nuova mappa di memoria nello spazio virtuale degli indirizzi
+del processo chiamante. L'indirizzo di partenza e' indicato da 'addr', mentre
+la lunghezza della mappa e' indicata da 'length'.
+
 */
 
 int main(void) {
