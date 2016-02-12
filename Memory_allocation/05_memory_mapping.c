3c53443349aaa8ee4396b20802e7708ec0b31ab4
B3h3m0th <behemoth@autistici.org>
2016-02-12 22:50:24 +0100

    memory page value is 4096 KB

--- Memory_allocation/05_memory_mapping.c
+++ Memory_allocation/05_memory_mapping.c
@@ -12,12 +12,12 @@ mmap() crea una nuova mappa di memoria nello spazio virtuale degli indirizzi
 del processo chiamante. L'indirizzo di partenza e' indicato da 'addr', mentre
 la lunghezza della mappa e' indicata da 'length'.
 
-Se 'addr' e' NULL sara' il kernel a scegliere l'indirizzo dal quale creare la
+Se 'addr' vale NULL sara' il kernel a scegliere l'indirizzo dal quale creare la
 mappa, che corrisponde peraltro alla modalita' piu' portabile per la creazione
 di una nuova mapppa; se invece 'addr' non e' impostato a NULL allora il 
 kernel prendera' in considerazione tale indirizzo per posizionare la mappa; il
-kernel linux iniziera' la mappatura dalla pagina immediatamente successiva - 
-una pagina e' di 4096 Kb.
+kernel linux iniziera' la mappatura dalla pagina "confinante" immediatamente
+successiva - da ricordare che una pagina e' di 4096 Kb.
 
 'prot' indica la protezione di memoria della mappa; 'flags' determina se la
 mappa dev'essere condivisa o privata; 'fd' riguarda la porzione di un eventuale
