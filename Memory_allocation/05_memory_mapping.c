2f9be84496ce28c0d10fc3fa4fd57c524525ad2d
B3h3m0th <behemoth@autistici.org>
2016-02-12 19:25:29 +0100

    header and prototype of mmap()

--- Memory_allocation/05_memory_mapping.c
+++ Memory_allocation/05_memory_mapping.c
@@ -2,7 +2,12 @@
 #include <stdlib.h>
 
 /* L'allocazione della memoria puo' essere gestita anche mediante una mappatura
-della memoria con le syscall mmap() e munmap(). */
+della memoria stessa utilizzando le syscall mmap() e munmap(). 
+
+HEADER   : <sys/mman.h>
+PROTOTYPE: void *mmap(void *addr, size_t length, int prot, int flags, 
+                      int fd, off_t offset);
+*/
 
 int main(void) {
     return(EXIT_SUCCESS);
