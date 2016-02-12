1f4368ab7c2abd6709700ac327616926a5fcd2c7
B3h3m0th <behemoth@autistici.org>
2016-02-12 19:19:58 +0100

    memory mapping with mmap() and munmap()

--- Memory_allocation/05_mmap.c
+++ Memory_allocation/05_mmap.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* L'allocazione della memoria puo' essere gestita anche mediante una mappatura
+della memoria con le syscall mmap() e munmap(). */
+
 int main(void) {
     return(EXIT_SUCCESS);
 }
