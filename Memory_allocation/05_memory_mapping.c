288548b7a87cb4ee00fab325c7cbbc5584f95314
B3h3m0th <behemoth@autistici.org>
2016-02-13 18:29:38 +0100

    memory mapping and anon mapping

--- Memory_allocation/05_memory_mapping.c
+++ Memory_allocation/05_memory_mapping.c
@@ -7,6 +7,10 @@
 /* L'allocazione della memoria puo' essere gestita anche mediante una mappatura
 della memoria stessa utilizzando le syscall mmap() e munmap(). 
 
+Una mappa o mappatura puo' essere sostanzialmente di due tipi: 
+1) File mapping.
+2) Anonymous mapping.
+
 HEADER   : <sys/mman.h>
 PROTOTYPE: void *mmap(void *addr, size_t length, int prot, int flags, 
                       int fd, off_t offset);
@@ -19,8 +23,8 @@ Se 'addr' vale NULL sara' il kernel a scegliere l'indirizzo dal quale creare la
 mappa, che corrisponde peraltro alla modalita' piu' portabile per la creazione
 di una nuova mapppa; se invece 'addr' non e' impostato a NULL allora il 
 kernel prendera' in considerazione tale indirizzo per posizionare la mappa; il
-kernel linux iniziera' la mappatura dalla pagina "confinante" immediatamente
-successiva - da ricordare che una pagina e' di 4096 Kb.
+kernel linux iniziera' la mappatura dalla pagina "confinante"  - da ricordare 
+che una pagina e' di 4096 Kb.
 
 'prot' indica la protezione di memoria della mappa; 'flags' determina il tipo
 di mappa, ovvero se e' visibile agli altri processi mappati nella stessa 
