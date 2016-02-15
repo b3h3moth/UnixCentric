84989f078e87f61a89b93be40eb7bf7bdf89e664
B3h3m0th <behemoth@autistici.org>
2016-02-15 11:47:46 +0100

    linux kernel and file mapping

--- IO_Advanced/mapping_files_into_memory/01_mapping_file.c
+++ IO_Advanced/mapping_files_into_memory/01_mapping_file.c
@@ -22,7 +22,11 @@ definire l'indirizzo dal quale iniziare la mappatura. La dimensione del file da
 mappare e' definita da 'length'. La protezione del range di memoria e'
 stabilito da 'prot', i cui valori riguardano lettura 'PROT_READ', scrittura 
 'PROT_WRITE', esecuzione 'PROT_EXEC' e accesso non consentito 'PROT_NONE'. Il
-tipo di memoria impostato mediante la variabile intera 'flags'.
+tipo di memoria e' impostato mediante la variabile intera 'flags'.
+
+Nota: Il kernel linux splitta il file in pagine (tipicamente da 4096 Kb)
+      copiandole nelle pagine della memoria virtuale, disponibili pertanto
+      nello spazio degli indirizzi del processo.
 
 La funzione mmap() restituisce un puntatore alla regione di memoria associata
 in caso di successo, -1 altrimenti.
