94e5512c5a1788363589a91af1ec74772517edf8
B3h3m0th <behemoth@autistici.org>
2016-02-18 01:42:31 +0100

    removed uint32_t data

--- the_language/structures_and_unions/write_structures_to_file.c
+++ the_language/structures_and_unions/write_structures_to_file.c
@@ -7,7 +7,6 @@
 #define MAX_STR 32
 
 struct rec {
-    uint32_t id;
     char alias[MAX_STR];
     char email[MAX_STR];
 };
@@ -20,24 +19,25 @@ int main(void) {
     Record *address_book;
     FILE *fout;
     char *my_alias = "b3h3m0th\0";
-    char *my_mail = "b3h3m0th@email.org\0";
+    char *my_mail = "mymail@email.org\0";
 
     // It allocates the proper amount of memory
     address_book = malloc(sizeof(Record));
 
     // Structure initialization
-    address_book->id = 1978;
     strncpy(address_book->alias, my_alias, strlen(my_alias));
     strncpy(address_book->email, my_mail, strlen(my_mail));
 
-    fout = fopen("fout.txt", "wb");
+    fout = fopen("fout.txt", "w+");
 
     if (fout == NULL) {
         fprintf(stderr, "Err.(%d) fopen() - %s\n", errno, strerror(errno));
         exit(EXIT_FAILURE);
     }
-    
+
+    // write the structure to file
     fwrite(address_book, sizeof(Record), 1, fout);
+
     fclose(fout);
 
     return(EXIT_SUCCESS);
