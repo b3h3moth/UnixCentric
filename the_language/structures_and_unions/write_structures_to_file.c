930ea303ed2ef297d47ee989efb2ec6008624fb3
B3h3m0th <behemoth@autistici.org>
2016-02-18 01:02:53 +0100

    struct rec and its typedef added

--- the_language/structures_and_unions/write_structures_to_file.c
+++ the_language/structures_and_unions/write_structures_to_file.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+
+#define MAX_STR 64
+
+struct rec {
+    uint32_t id;
+    char alias[MAX_STR];
+    char email[MAX_STR];
+};
+
+typedef struct rec Record;
 
 int main(void) {
     return(EXIT_SUCCESS);
