4a8029c0fef52481e3374876646dd887962e848a
B3h3m0th <behemoth@autistici.org>
2016-02-19 00:26:01 +0100

    testing 'int' data type

--- the_language/functions/higher_order_functions/manage_different_data_types.c
+++ the_language/functions/higher_order_functions/manage_different_data_types.c
@@ -5,7 +5,7 @@
 
 enum types {CHAR, UCHAR, SHORT, USHORT, INT, UINT, LONG, ULONG};
 
-union data_val {
+union data_tag {
     char valchar;
     short valshort;
     int valint;
@@ -13,27 +13,30 @@ union data_val {
     long valong;
 };
 
+typedef union data_tag Data;
+
 // Function Prototypes
-void check_error(int type, char *val, void(*print)(char *, union data_val *));
-void print(char *fmt, union data_val *d);
+void check_error(int type, char *val, void(*print)(char *, Data *));
+void print(char *fmt, Data *d);
 
 int main(int argc, char *argv[]) {
+    check_error(4, "10", &print);
 
     return(EXIT_SUCCESS);
 }
 
 // testing, it doesn't work well!
-void check_error(int type, char *val, void(*print)(char *, union data_val *)) {
-    dava_val value;
+void check_error(int type, char *val, void(*print)(char *, Data *)) {
+    Data value;
 
-    if (type == CHAR) {
-        value.valchar = atoi(val);
-        print("%c\n", value.valchar);
+    if (type == INT) {
+        value.valint = atol(val);
+        print("%d\n", &value);
     }
 }
 
-void print(char *fmt, union data_val *d) {
+void print(char *fmt, Data *d) {
     char *format = (void *)fmt;
 
-    printf(format, d->valchar);
+    printf(format, d->valint);
 }
