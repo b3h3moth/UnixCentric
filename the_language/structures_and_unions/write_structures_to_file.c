1b6b3e9bd95191095352b4184b9f2962b711aefc
B3h3m0th <behemoth@autistici.org>
2016-02-18 01:13:49 +0100

    allocate proper amount of memory with malloc()

--- the_language/structures_and_unions/write_structures_to_file.c
+++ the_language/structures_and_unions/write_structures_to_file.c
@@ -15,5 +15,11 @@ typedef struct rec Record;
 /* The program's purpose is to write a full structure to a file */
 
 int main(void) {
+    Record *address_book;
+
+    // It allocates the proper amount of memory
+    address_book = malloc(sizeof(Record));
+    address_book->id = 1;
+
     return(EXIT_SUCCESS);
 }
