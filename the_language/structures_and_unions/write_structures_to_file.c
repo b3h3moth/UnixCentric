fc0329a44c1df0d5f0f856251b3e49a0f4263918
B3h3m0th <behemoth@autistici.org>
2016-02-18 01:33:55 +0100

    open and write structure to the file

--- the_language/structures_and_unions/write_structures_to_file.c
+++ the_language/structures_and_unions/write_structures_to_file.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#include <errno.h>
 
-#define MAX_STR 64
+#define MAX_STR 32
 
 struct rec {
     uint32_t id;
@@ -17,16 +18,27 @@ typedef struct rec Record;
 
 int main(void) {
     Record *address_book;
-    char *my_alias = "b3h3m0th";
-    char *my_mail = "b3h3m0th@email.org";
+    FILE *fout;
+    char *my_alias = "b3h3m0th\0";
+    char *my_mail = "b3h3m0th@email.org\0";
 
     // It allocates the proper amount of memory
     address_book = malloc(sizeof(Record));
 
     // Structure initialization
-    address_book->id = 1;
+    address_book->id = 1978;
     strncpy(address_book->alias, my_alias, strlen(my_alias));
-    strncpy(address_book->alias, my_mail, strlen(my_mail));
+    strncpy(address_book->email, my_mail, strlen(my_mail));
+
+    fout = fopen("fout.txt", "wb");
+
+    if (fout == NULL) {
+        fprintf(stderr, "Err.(%d) fopen() - %s\n", errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    
+    fwrite(address_book, sizeof(Record), 1, fout);
+    fclose(fout);
 
     return(EXIT_SUCCESS);
 }
