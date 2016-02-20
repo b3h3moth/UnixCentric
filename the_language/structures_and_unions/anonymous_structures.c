693c913f0aea83a0ba6fbcc60121cf6ed8de96d7
B3h3m0th <behemoth@autistici.org>
2016-02-20 19:39:19 +0100

    c11 nonamed structure

--- the_language/structures_and_unions/anonymous_structures.c
+++ the_language/structures_and_unions/anonymous_structures.c
@@ -3,7 +3,11 @@
 
 /* Lo standard C11 ha inrodotto le 'anonymous structures and unions', mediante
 le quali e' lecito definire strutture e unioni prive sia di tag sia di 
-variabili, dotate del solo specificatore 'struct' o 'union' */
+variabili, dotate del solo specificatore 'struct' o 'union'.
+
+c11 standard definition:
+"A structure or union is defined without any named members".
+*/
 
 int main(void) {
 
