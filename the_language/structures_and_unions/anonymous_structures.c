a7e4559d2afa0fff004a7b1167e74dd8d68670bb
B3h3m0th <behemoth@autistici.org>
2016-02-20 17:32:36 +0100

    What is anonymouse structure

--- the_language/structures_and_unions/anonymous_structures.c
+++ the_language/structures_and_unions/anonymous_structures.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Lo standard C11 ha inrodotto le 'anonymous structure', mediante le quali e'
+lecito definire strutture prive sia di tag sia di variabile struttura, dotate 
+del solo specificatore 'struct' */
+
 int main(void) {
     return(EXIT_SUCCESS);
 }
