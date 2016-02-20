cbe481602b80bc3c1bc519eaeb8e184f01fb8b31
B3h3m0th <behemoth@autistici.org>
2016-02-20 19:25:06 +0100

    c11 anonymous struct/union

--- the_language/structures_and_unions/anonymous_structures.c
+++ the_language/structures_and_unions/anonymous_structures.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Lo standard C11 ha inrodotto le 'anonymous structure', mediante le quali e'
-lecito definire strutture prive sia di tag sia di variabile struttura, dotate 
-del solo specificatore 'struct' */
+/* Lo standard C11 ha inrodotto le 'anonymous structures and unions', mediante
+le quali e' lecito definire strutture e unioni prive sia di tag sia di 
+variabili, dotate del solo specificatore 'struct' o 'union' */
 
 int main(void) {
+
     return(EXIT_SUCCESS);
 }
