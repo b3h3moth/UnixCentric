767d99657c7ff32b5210baec4d4482b385e387aa
B3h3m0th <behemoth@autistici.org>
2016-02-21 19:52:14 +0100

    write freopen() fixed

--- the_language/input_output/freopen.c
+++ the_language/input_output/freopen.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+// The program's purpose is to redirect standard I/O stream to file
 
 int main(void) {
+    time_c sec;
+    char fname[] = "data.txt";
+
+    fputs("Before freopen()", stdout);
+
+    if (freopen(fname, "w", stdout) == NULL) {
+        fprintf(stderr, "Unable to redirect stdout stream.\n");
+    } else 
+        printf("I'm writing inside a file\n");
+    
     return(EXIT_SUCCESS);
 }
