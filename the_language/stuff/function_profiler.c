893689aebfb5940c51c58205b1c97b9e962851bc
B3h3m0th <behemoth@autistici.org>
2016-02-22 10:39:07 +0100

    replaced functions with block of instructions

--- the_language/stuff/function_profiler.c
+++ the_language/stuff/function_profiler.c
@@ -2,13 +2,20 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define MAX_LOOP    10000000
+// Lo scopo del programma e' di calcolare il tempo di esecuzione di un ciclo
+
 int main(void) {
     clock_t before;
-    double elapsed;
+    double after;
 
     before = clock();
-    system("ls -l");
-    elapsed = clock()  - before;
-    printf("function used %.3f seconds\n", elapsed/CLOCKS_PER_SEC);
+    for (int i=0; i<MAX_LOOP; i++)
+        ;
+    after = clock()  - before;
+    
+    printf("for loop of \'%d\' elements used \'%f\' seconds.\n", \
+            MAX_LOOP, after/CLOCKS_PER_SEC);
+
     return(EXIT_SUCCESS);
 }
