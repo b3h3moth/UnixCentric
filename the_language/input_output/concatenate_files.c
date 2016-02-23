a308e7f78d64224c9065d2241a05357b012ef948
B3h3m0th <behemoth@autistici.org>
2016-02-23 11:59:04 +0100

    cat simulation

--- the_language/input_output/concatenate_files.c
+++ the_language/input_output/concatenate_files.c
@@ -5,8 +5,22 @@
 
 enum { BUF_MAX = 64 };
 
-int main(void) {
+int main(int argc, char *argv[]) {
     // Inizializza a 0 buf (si sarebbe potuto utilizzare memset())
     char buf[BUF_MAX] = {0};
+    FILE *fin;
+
+    for (int i=1; i<argc; i++) {
+        if ((fin = fopen(argv[i], "r"))) {
+            while (fgets(buf, BUF_MAX, fin)) {
+                fputs(buf, stdout);
+            }
+            fclose(fin);
+        } else {
+            fprintf(stderr, "Err. fopen(); %s\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+    }
+
     return(EXIT_SUCCESS);
 }
