0230cffbbe1a09eef9703dbf3f776ac088502779
B3h3m0th <behemoth@autistici.org>
2016-02-23 11:34:26 +0100

    sevbuf() semantics added

--- the_language/input_output/setvbuf.c
+++ the_language/input_output/setvbuf.c
@@ -9,21 +9,32 @@ static const char fname[] = "/etc/passwd";
 
 int main(void) {
     char buf[BUF_SIZE];
+    int ch;
     FILE *fp;
 
-    if ((fp = fopen(fname, "r"))) {
-        printf("%s is open.\n", fname);
-
-        /* Impostazione del buffer a 'BUF_SIZE'
-        _IOLBF; il buffer dev'essere svuotato  ad ogni 'new line' */
-        if (setvbuf(fp, buf, _IOLBF, BUF_SIZE) != 0) {
-            fprintf(stderr, "Err. setvbuf() %s\n", strerror(errno));
-            exit(EXIT_FAILURE);
-        }
-    } else {
+    if ((fp = fopen(fname, "r")) == NULL ) {
         fprintf(stderr, "Err. fopen() %s\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
+        
+    printf("%s is open.\n", fname);
+    
+    /* Impostazione del buffer a 'BUF_SIZE', si e' optato per un tipo di
+    buffer 'line buffered' ovvero che dev'essere svuotato ad ogni 'new line'
+    per l'appunto. Le opzioni sono :
+    _IOLBF; line buffered
+    _IOFBF; fully
+    _IONBF; unbuffered
+    */
+    if (setvbuf(fp, buf, _IOLBF, BUF_SIZE) != 0) {
+        fprintf(stderr, "Err. setvbuf() %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    while ((ch = fgetc(fp)) != EOF)
+        fputc(ch, stdout);
+
+    fclose(fp);
 
     return(EXIT_SUCCESS);
 }
