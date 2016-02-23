a95f6e7dbd561a545fe639d5e8af225f584fa783
B3h3m0th <behemoth@autistici.org>
2016-02-23 16:50:48 +0100

    fopen, argc error checking

--- the_language/input_output/count_characters.c
+++ the_language/input_output/count_characters.c
@@ -2,11 +2,29 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include <stdint.h>
 
-/* Lo scopo del programma ricevere un file in input selezionato dall'utente e
-di contare ciascun carattere del file stesso */
+/* Lo scopo del programma e' di ricevere un file in input come argomento e
+di contare ciascun carattere del file stesso, ponendo un limite al numero 
+massimo di caratteri da salvare. */
 
 int main(int argc, char argv[]) {
     int ch;
+    File *fp;
+    uint32_t count = 0;
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <filename>.\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    if ((fp = fopen(argv[1], "r")) == NULL) {
+        fprintf(stderr, "Err. fopen(), %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    // Legge 'char by char' salvando di volta in volta il toale dei caratteri
+
+
     return(EXIT_SUCCESS);
 }
