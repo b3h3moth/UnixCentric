d1ddca9a5b07cbf56db39ee6a8e741258c9b72ca
B3h3m0th <behemoth@autistici.org>
2016-02-23 16:42:30 +0100

    The program's purpose

--- the_language/input_output/count_characters.c
+++ the_language/input_output/count_characters.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 
-int main(void) {
+/* Lo scopo del programma ricevere un file in input selezionato dall'utente e
+di contare ciascun carattere del file stesso */
+
+int main(int argc, char argv[]) {
+    int ch;
     return(EXIT_SUCCESS);
 }
