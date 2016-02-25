0f2a856bbf4581fb913977bd9bff22d74a396842
B3h3m0th <behemoth@autistici.org>
2016-02-25 21:53:14 +0100

    calling malloc() and strcpy() from set_hostname_info()

--- the_language/input_output/ascii_file_write_with_fscanf.c
+++ the_language/input_output/ascii_file_write_with_fscanf.c
@@ -8,7 +8,7 @@
 per evitare messaggi di warning dal compilatore sarebbe meglio compilare con
 l'opzione _D_BSD_SOURCE */
 
-void set_hostname_info(char *arg, char *vec);
+void set_hostname_info(char *arg, char **vec);
 
 int main(void) {
     struct utsname buf;
@@ -19,27 +19,14 @@ int main(void) {
         exit(EXIT_FAILURE);
     }
 
-    set_hostname_info(buf.sysname, hostinfo[0]);
-/*
-    hostinfo[0] = malloc(sizeof(strlen(buf.sysname)));
-    hostinfo[1] = malloc(sizeof(strlen(buf.release)));
-    hostinfo[2] = malloc(sizeof(strlen(buf.version)));
-    strcpy(hostinfo[0], buf.sysname);
-    strcpy(hostinfo[1], buf.release);
-    strcpy(hostinfo[2], buf.version);
-    
-    printf(hostinfo[0]);
-    printf(hostinfo[1]);
-    printf(hostinfo[2]);
-*/
-    printf(hostinfo[0]);
+    set_hostname_info(buf.sysname, &hostinfo[0]);
+    set_hostname_info(buf.release, &hostinfo[1]);
+    set_hostname_info(buf.version, &hostinfo[2]);
 
     return(EXIT_SUCCESS);
 }
 
-void set_hostname_info(char *arg, char *vec) {
-    vec = malloc(sizeof(strlen(arg)));
-    strcpy(vec, arg);
-    printf(vec);
-    printf(arg);
+void set_hostname_info(char *arg, char **vec) {
+    *vec = malloc(sizeof(strlen(arg)));
+    strcpy(*vec, arg);
 }
