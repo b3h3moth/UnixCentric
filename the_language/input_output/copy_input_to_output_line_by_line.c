7b832b5d6507efff372bae4df0865eb6436a5d33
B3h3m0th <behemoth@autistici.org>
2016-02-25 12:34:06 +0100

    The program's purpose

--- the_language/input_output/copy_input_to_output_line_by_line.c
+++ the_language/input_output/copy_input_to_output_line_by_line.c
@@ -5,7 +5,18 @@ enum { MAX_LINE = 64, };
 
 void copy_line_by_line(FILE *fin, FILE *fout);
 
+/* Lo scopo del programma e' di creare una funzione compatta che cosenta una
+veloce redirezione dello standard input verso lo standard output; nel caso
+specifico il programma, dopo essere stato compilato, puo' essere lanciato in
+questo modo:
+
+$ copylinebyline < input_file.txt
+*/
+
 int main(void) {
+
+    copy_line_by_line(stdin, stdout);
+
     return(EXIT_SUCCESS);
 }
 
