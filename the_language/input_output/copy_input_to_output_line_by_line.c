e56f637784dee1933e7254420b9f21888c99f233
B3h3m0th <behemoth@autistici.org>
2016-02-25 12:27:51 +0100

    line_by_line() function definition

--- the_language/input_output/copy_input_to_output_line_by_line.c
+++ the_language/input_output/copy_input_to_output_line_by_line.c
@@ -9,3 +9,9 @@ int main(void) {
     return(EXIT_SUCCESS);
 }
 
+void copy_line_by_line(FILE *fin, FILE *fout) {
+    char buf[MAX_LINE];
+
+    while (fgets(buf, MAX_LINE, fin) != NULL)
+        fputs(buf, fout);
+}
