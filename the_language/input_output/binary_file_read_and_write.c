ddddedc1916cd1d67c2210a15e3d37bcc0ecfe79
B3h3m0th <behemoth@autistici.org>
2016-02-26 20:07:50 +0100

    how binary file works

--- the_language/input_output/binary_file_read_and_write.c
+++ the_language/input_output/binary_file_read_and_write.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Lo scopo del programma e' di scrivere i dati contenuti inuna struttura 
+all'interno di in un file binario. */
+
 int main(void) {
     return(EXIT_SUCCESS);
 }
