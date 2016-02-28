b0c7006e76159611120a5b6c405988ac69d3deb1
B3h3m0th <behemoth@autistici.org>
2016-02-28 18:16:27 +0100

    the program's purpose

--- the_language/array/find_duplicate_members.c
+++ the_language/array/find_duplicate_members.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Lo scopo del programma e' di scrivere una funzione che consenta di trovare
+elementi duplicati in un array */
+
 int main(void) {
     return(EXIT_SUCCESS);
 }
