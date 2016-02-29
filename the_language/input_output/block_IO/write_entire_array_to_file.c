0e5611a0add460f5fd00d58c93f42e80e168f6ad
B3h3m0th <behemoth@autistici.org>
2016-02-29 20:11:52 +0100

    How to write an entire array to file, Block I/O

--- the_language/input_output/block_IO/write_entire_array_to_file.c
+++ the_language/input_output/block_IO/write_entire_array_to_file.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Lo scopo del programma e' di scrivere un intero array all'all'inerno di un
+file mediante una sola chiamata, utilizzando la funzione fwrite(). */
+
 int main(void) {
     return(EXIT_SUCCESS);
 }
