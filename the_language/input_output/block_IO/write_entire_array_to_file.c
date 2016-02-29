827d28b6845e8245709bde5aa4f7e335cfac9cfd
B3h3m0th <behemoth@autistici.org>
2016-02-29 20:33:47 +0100

    write entire array with fwrite() to file

--- the_language/input_output/block_IO/write_entire_array_to_file.c
+++ the_language/input_output/block_IO/write_entire_array_to_file.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* Lo scopo del programma e' di scrivere un intero array all'all'inerno di un
 file mediante una sola chiamata, utilizzando la funzione fwrite(). */
 
 int main(void) {
+    FILE *fp;
+    const char *unix_os[] = { 
+        "Debian GNU/Linux", "OpenBSD", "FreeBSD", 
+        "HP-UX", "Solaris", "Mac OS X", "NetBSD", 
+        "OpenSolaris", "AIX", "Minix"
+    };
+
+    fp = fopen("file.txt", "wb");
+
+    fwrite(unix_os, strlen(unix_os[0]), strlen(*unix_os)/strlen(unix_os[0]), fp);
+
+    fclose(fp);
+
+
     return(EXIT_SUCCESS);
 }
