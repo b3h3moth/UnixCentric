da20f6137211549febf778f4df04f7362bc5b1cd
B3h3m0th <behemoth@autistici.org>
2016-02-29 22:42:55 +0100

    write vector of integers with fwrite()

--- the_language/input_output/block_IO/write_entire_array_to_file.c
+++ the_language/input_output/block_IO/write_entire_array_to_file.c
@@ -7,15 +7,18 @@ file mediante una sola chiamata, utilizzando la funzione fwrite(). */
 
 int main(void) {
     FILE *fp;
+    /*
     const char *unix_os[] = { 
         "Debian GNU/Linux", "OpenBSD", "FreeBSD", 
         "HP-UX", "Solaris", "Mac OS X", "NetBSD", 
         "OpenSolaris", "AIX", "Minix"
     };
+    */
+    int vec[] = { 1,2,3,4,5,6,7,8,9,10};
 
-    fp = fopen("file.txt", "wb");
+    fp = fopen("file.txt", "w");
 
-    fwrite(unix_os, strlen(unix_os[0]), strlen(*unix_os)/strlen(unix_os[0]), fp);
+    fwrite(&vec, sizeof(vec[0]), sizeof(vec)/sizeof(vec[0]), fp);
 
     fclose(fp);
 
