ed86ce031220c87d5ea6a5395aaf095555a967e7
B3h3m0th <behemoth@autistici.org>
2016-03-01 18:32:06 +0100

    write data to binary file

--- the_language/input_output/block_IO/write_entire_array_to_file.c
+++ the_language/input_output/block_IO/write_entire_array_to_file.c
@@ -12,7 +12,7 @@ mediante la funzione fwrite() */
 int main(void) {
     FILE *fp;
     char *binfile = "data.bin";
-    int data[MAX_LEN];
+    int data[MAX_LEN], n;
 
     /*
     const char *unix_os[] = { 
@@ -22,7 +22,7 @@ int main(void) {
     };
     */
 
-    fp = fopen("file.txt", "wb");
+    fp = fopen(binfile, "wb");
 
     if (fp == NULL) {
         perror("fopen");
@@ -30,12 +30,11 @@ int main(void) {
     }
 
     for (int i=0; i<MAX_LEN; i++)
-        *(data + i) = random();
+        *(data + i) = rand();
 
-    fwrite(&vec, sizeof(vec[0]), sizeof(vec)/sizeof(vec[0]), fp);
+    n = fwrite(data, sizeof(data[0]), sizeof(data)/sizeof(data[0]), fp);
 
     fclose(fp);
-
 
     return(EXIT_SUCCESS);
 }
