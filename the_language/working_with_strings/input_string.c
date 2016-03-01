83e95b67ea19373281e51a32614fad9e3a757110
B3h3m0th <behemoth@autistici.org>
2016-03-02 00:09:28 +0100

    replaced line[100] with buf[MAX_BUF]

--- the_language/working_with_strings/input_string.c
+++ the_language/working_with_strings/input_string.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_BUF 50
+
 int main(void) {
-    char ch, line[100];
+    char ch, buf[MAX_BUF];
     unsigned i = 0;
     
     printf("Type a string: ");
     while ((ch = getchar()) != '\n') {
-        if (i < 99)
-            line[i++] = ch;
+        if (i < MAX_BUF-1)
+            buf[i++] = ch;
         else {
             printf("Sorry, the string is too large.\n");
             exit(EXIT_FAILURE);
         }
     }
 
-    printf("%s\n", line);
+    printf("%s\n", buf);
 
     return(EXIT_SUCCESS);
 }
