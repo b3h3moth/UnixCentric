91441fbf1d0ab04a2d54b99b642ec5edf7098d98
B3h3m0th <behemoth@autistici.org>
2016-03-02 00:03:46 +0100

    fixed string output error

--- the_language/working_with_strings/input_string.c
+++ the_language/working_with_strings/input_string.c
@@ -2,16 +2,20 @@
 #include <stdlib.h>
 
 int main(void) {
-    char ch, line [100];
+    char ch, line[100];
+    unsigned i = 0;
     
     printf("Type a string: ");
     while ((ch = getchar()) != '\n') {
         if (i < 99)
-            line[i++] = c;
+            line[i++] = ch;
         else {
-            printf("maximum string\n");
+            printf("Sorry, the string is too large.\n");
             exit(EXIT_FAILURE);
         }
     }
+
+    printf("%s\n", line);
+
     return(EXIT_SUCCESS);
 }
