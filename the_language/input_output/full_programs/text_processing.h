800ea2d30999643d888206249921475d7d6cdec1
B3h3m0th <behemoth@autistici.org>
2016-03-02 19:13:23 +0100

    text processing header added

---
+++ the_language/input_output/full_programs/text_processing.h
@@ -0,0 +1,7 @@
+#ifndef TEXT_PROCESSING_H
+#define TEXT_PROCESSING_H
+
+int print_numbers(FILE *sream, char const form[static 1]\
+        char const sep[static 1], size_t len, size_t nums[len]);
+
+#endif
