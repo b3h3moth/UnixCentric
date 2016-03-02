9fb8ab3ed1990080f06bb04686ab362cfd50faa8
B3h3m0th <behemoth@autistici.org>
2016-03-02 20:09:27 +0100

    first version of vis

--- the_language/input_output/characters_IO/vis.c
+++ the_language/input_output/characters_IO/vis.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 int main(void) {
+    int ch;
+
+    while ((ch = getchar()) != EOF) {
+        if ((isprint(ch) || ch == '\n' || ch == '\t' || ch == ' '))
+            printf("%03o ", ch);
+        else
+            printf("\\%03o", ch);
+    }
+
     return(EXIT_SUCCESS);
 }
