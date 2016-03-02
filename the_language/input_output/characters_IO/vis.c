688f62a743288a60ff6bb910eef2f25852c955e4
B3h3m0th <behemoth@autistici.org>
2016-03-02 20:13:34 +0100

    isascii() added

--- the_language/input_output/characters_IO/vis.c
+++ the_language/input_output/characters_IO/vis.c
@@ -6,8 +6,8 @@ int main(void) {
     int ch;
 
     while ((ch = getchar()) != EOF) {
-        if ((isprint(ch) || ch == '\n' || ch == '\t' || ch == ' '))
-            printf("%03o ", ch);
+        if (isascii(ch) && (isprint(ch) || ch == '\n' || ch == '\t' || ch == ' '))
+            fputc(ch, stdout);
         else
             printf("\\%03o", ch);
     }
