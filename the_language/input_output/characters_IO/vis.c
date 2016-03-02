51d7d55675319636e6c4033bb0b0fcd293b70dc3
B3h3m0th <behemoth@autistici.org>
2016-03-02 20:26:33 +0100

    added show option

--- the_language/input_output/characters_IO/vis.c
+++ the_language/input_output/characters_IO/vis.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
 
 /* Lo scopo del programma e' di leggere uno o piu' file in input e di stampare
 su stdout i caratteri speciali */
 
-int main(void) {
+int main(int argc, char *argv[]) {
     int ch;
+    int show = 0; // mostra caratteri speciali se attivata
+
+    if (argc > 1 && strcmp(argv[1], "-s") == 0)
+        show = 1;
 
     while ((ch = getchar()) != EOF) {
-        if (isascii(ch) && (isprint(ch) || ch == '\n' || ch == '\t' || ch == ' '))
-            fputc(ch, stdout);
-        else
+        if (show)
             printf("\\%03o", ch);
+        else if (isascii(ch) && (isprint(ch) || ch == '\n' || ch == '\t' || ch == ' '))
+            fputc(ch, stdout);
     }
 
     return(EXIT_SUCCESS);
