10430165a9006625b3e6a5ca78b13d2f81a7ce99
B3h3m0th <behemoth@autistici.org>
2016-03-03 13:44:29 +0100

    make characters visible in FILE *fp, becames a function

--- the_language/input_output/characters_IO/wildchar.c
+++ the_language/input_output/characters_IO/wildchar.c
@@ -4,22 +4,32 @@
 #include <ctype.h>
 
 /* Lo scopo del programma e' di leggere uno o piu' file in input e di stampare
-su stdout il/i file compresi alcuni caratteri speciali */
+su stdout i caratteri speciali */
 
 int main(int argc, char *argv[]) {
-    FILE *fp;
     int ch;
-    static int strip = 0; // taglia dalla visione i caratteri speciali
+    int strip = 0; // taglia dalla visione i caratteri speciali
 
-    if (argc > 1 && strcmp(argv[1], "-s") == 0)
-        strip = 1;
-
-    while ((ch = getchar()) != EOF) {
-        if (isascii(ch) && (isprint(ch) || ch == '\n' || ch == '\t' || ch == ' '))
-            putchar(ch);
-        else if (!strip)
-            printf("\\%03o", ch);
+    while (argc > 1 && argv[1][0] == '-') {
+        switch (argv[1][1]) {
+            case 's':
+                strip = 1;
+                break;
+            default:
+                fprintf(stderr, "%s: Unknow argument %s\n", argv[0], argv[1]);
+                exit(EXIT_FAILURE);
+        }
     }
 
     return(EXIT_SUCCESS);
 }
+
+void visible_chars(FILE *fp) {
+    while ((ch = getchar()) != EOF) {
+        if (isascii(ch) && (isprint(ch) || ch == '\n' || ch == '\t' || ch == ' '))
+            fputc(ch, stdout);
+        else if (!strip)
+            printf("\\%03o", ch);
+    }
+}
+
