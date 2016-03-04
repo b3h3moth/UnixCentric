85749656f699ed5672711f3077aa0e555393cb66
B3h3m0th <behemoth@autistici.org>
2016-03-04 18:31:34 +0100

    the program's purpose, print input in chunks

---
+++ the_language/input_output/characters_IO/read_chunks.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Lo scopo del programma e' di leggere l'input una pagina alla volta, la
+dimensione della pagina, in righe naturalmente, e' definita da PAGE_SIZE */
+
+int main(void) {
+    return(EXIT_SUCCESS);
+}
