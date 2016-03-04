60b53f2b0bc424b9a9e09a24a7f93469a4f3be0b
B3h3m0th <behemoth@autistici.org>
2016-03-04 18:32:33 +0100

    PAGE_SIZE added

--- the_language/input_output/characters_IO/input_chunks.c
+++ the_language/input_output/characters_IO/input_chunks.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define PAGE_SIZE 22
+
 /* Lo scopo del programma e' di leggere l'input una pagina alla volta, la
 dimensione della pagina, in righe naturalmente, e' definita da PAGE_SIZE */
 
