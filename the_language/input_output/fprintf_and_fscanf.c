0f64dde9f085fc3498a9d77cbc24c25312b2171a
B3h3m0th <behemoth@autistici.org>
2016-03-06 19:53:29 +0100

    The program's purpose

--- the_language/input_output/fprintf_and_fscanf.c
+++ the_language/input_output/fprintf_and_fscanf.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Lo scopo del programma e' di scrivere degli interi in un file, dopodiche'
+leggere il file scritto e invertire i numeri */
+
 int main(void) {
     return(EXIT_SUCCESS);
 }
