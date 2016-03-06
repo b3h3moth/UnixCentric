26a01d31c5069a27d790195fc1628d94a51f0d1e
B3h3m0th <behemoth@autistici.org>
2016-03-06 20:06:02 +0100

    arguments error checking added

--- the_language/input_output/fprintf_and_fscanf.c
+++ the_language/input_output/fprintf_and_fscanf.c
@@ -12,6 +12,11 @@ int main(int argc, char *argv[]) {
     FILE *fp;
     int data[LEN], val;
 
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
     if ((fp = fopen(argv[1], "w")) == NULL) {
         fprintf(stderr, "Err. fopen(), %s\n", strerror(errno));
         exit(EXIT_FAILURE);
@@ -19,7 +24,7 @@ int main(int argc, char *argv[]) {
 
     for (int i=0; i<LEN; i++) {
         val = rand() % 1000;
-        fprintf(fp, "%d %d\n", i, val);
+        fprintf(fp, "%2d %3d\n", i, val);
     }
     
     return(EXIT_SUCCESS);
