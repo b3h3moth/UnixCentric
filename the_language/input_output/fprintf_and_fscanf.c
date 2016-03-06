358c9d961b195de6ff704108f23368ffd3f0cb6a
B3h3m0th <behemoth@autistici.org>
2016-03-06 20:03:37 +0100

    fixed typographical errors

--- the_language/input_output/fprintf_and_fscanf.c
+++ the_language/input_output/fprintf_and_fscanf.c
@@ -8,7 +8,7 @@
 /* Lo scopo del programma e' di scrivere degli interi in un file, dopodiche'
 leggere il file scritto e invertire i numeri */
 
-int main(void) {
+int main(int argc, char *argv[]) {
     FILE *fp;
     int data[LEN], val;
 
@@ -18,9 +18,9 @@ int main(void) {
     }
 
     for (int i=0; i<LEN; i++) {
-        val = rand % 1000;
+        val = rand() % 1000;
         fprintf(fp, "%d %d\n", i, val);
     }
-
+    
     return(EXIT_SUCCESS);
 }
