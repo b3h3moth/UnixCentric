0f0c1308342bc6cb2154e2fff684947c69a585cc
B3h3m0th <behemoth@autistici.org>
2016-03-06 19:58:44 +0100

    fopen() error checking

--- the_language/input_output/fprintf_and_fscanf.c
+++ the_language/input_output/fprintf_and_fscanf.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+#define LEN 50
 
 /* Lo scopo del programma e' di scrivere degli interi in un file, dopodiche'
 leggere il file scritto e invertire i numeri */
 
 int main(void) {
+    FILE *fp;
+    int data[LEN];
+
+    if ((fp = fopen(argv[1], "w")) == NULL) {
+        fprintf(stderr, "Err. fopen(), %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
     return(EXIT_SUCCESS);
 }
