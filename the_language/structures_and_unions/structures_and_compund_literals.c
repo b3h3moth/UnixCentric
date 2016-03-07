019b6ca95e4534f4c6e3f159728cbf90247cd3e8
B3h3m0th <behemoth@autistici.org>
2016-03-07 20:05:57 +0100

    compund literal as a constant

--- the_language/structures_and_unions/structures_and_compund_literals.c
+++ the_language/structures_and_unions/structures_and_compund_literals.c
@@ -9,9 +9,16 @@ struct db {
 
 int main(void) {
     // Inizializzazione di una struttura con un compound literals
-    struct db data = (struct db){1, 2016, 30};
+    struct db data = (struct db){1, 2000, 30};
 
-    printf("id: %d\nyear: %d\nage: %d\n", data.id, data.year, data.age);
+    printf("id: %d\tyear: %d\tage: %d\n", data.id, data.year, data.age);
 
+    // E' possibile anche utilizzare un compund literal per come costante
+    data.id = (const int){2};
+    data.year = (const int){2016};
+    data.age = (const int){20};
+
+    printf("id: %d\tyear: %d\tage: %d\n", data.id, data.year, data.age);
+    
     return(EXIT_SUCCESS);
 }
