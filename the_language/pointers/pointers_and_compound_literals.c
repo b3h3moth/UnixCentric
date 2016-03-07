925d6990c4b7886aef4e6f0c5b699ac8ee00ffbf
B3h3m0th <behemoth@autistici.org>
2016-03-07 19:39:42 +0100

    how to initialize a pointer to the first element of the compund literal) array

--- the_language/pointers/pointers_and_compound_literals.c
+++ the_language/pointers/pointers_and_compound_literals.c
@@ -2,5 +2,10 @@
 #include <stdlib.h>
 
 int main(void) {
+    // Inizializzazione del puntatore 'ptr' al primo elemento dell'array
+    int *ptr = (int []){10, 20, 30, 40, 50};
+
+    printf("%d\n", *(ptr + 4));
+
     return(EXIT_SUCCESS);
 }
