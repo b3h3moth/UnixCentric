04da66b11095216cb4f47a1af934a8559c3658ee
B3h3m0th <behemoth@autistici.org>
2016-03-07 20:02:11 +0100

    initialize a structure with a compound literal, then print the results

--- the_language/structures_and_unions/structures_and_compund_literals.c
+++ the_language/structures_and_unions/structures_and_compund_literals.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct db {
+    int id;
+    int year;
+    int age;
+};
+
 int main(void) {
+    // Inizializzazione di una struttura con un compound literals
+    struct db data = (struct db){1, 2016, 30};
+
+    printf("id: %d\nyear: %d\nage: %d\n", data.id, data.year, data.age);
+
     return(EXIT_SUCCESS);
 }
