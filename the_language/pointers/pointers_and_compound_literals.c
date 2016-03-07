7afea7ea12890271d32f536a2df99683c64da44f
B3h3m0th <behemoth@autistici.org>
2016-03-07 19:48:06 +0100

    print each array elements

--- the_language/pointers/pointers_and_compound_literals.c
+++ the_language/pointers/pointers_and_compound_literals.c
@@ -3,9 +3,12 @@
 
 int main(void) {
     // Inizializzazione del puntatore 'ptr' al primo elemento dell'array
-    int *ptr = (int []){10, 20, 30, 40, 50};
+    int *ptr = (int []){10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
 
-    printf("%d\n", *(ptr + 4));
+    while(*ptr > 0) {
+        printf("%3d\n", *ptr);
+        ptr++;
+    }
 
     return(EXIT_SUCCESS);
 }
