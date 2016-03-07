3be99df17a1e3e91dc95605123d6f90f9e678a3a
B3h3m0th <behemoth@autistici.org>
2016-03-07 20:12:10 +0100

    initialize a pointer with a compund literal sructure

--- the_language/pointers/pointers_and_compound_literals.c
+++ the_language/pointers/pointers_and_compound_literals.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct db {
+    int id;
+    int year;
+};
+
 int main(void) {
     // Inizializzazione del puntatore 'ptr' al primo elemento dell'array
     int *ptr = (int []){10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
@@ -9,6 +14,11 @@ int main(void) {
         printf("%3d\n", *ptr);
         ptr++;
     }
+
+    // Un compund literal per l'inizializzazione di un puntatore a struttura
+    struct db *data = &(struct db){1, 2016};
+
+    printf("id: %d\tyear: %d\n", data->id, data->year);
 
     return(EXIT_SUCCESS);
 }
