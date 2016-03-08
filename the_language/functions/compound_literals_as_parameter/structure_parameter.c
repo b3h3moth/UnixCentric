e7ba448ff20d02c0a74d1a53b639d0c84d810f20
B3h3m0th <behemoth@autistici.org>
2016-03-08 17:58:17 +0100

    added name into struct db

--- the_language/functions/compound_literals_as_parameter/structure_parameter.c
+++ the_language/functions/compound_literals_as_parameter/structure_parameter.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_LEN 20
+
 struct db {
     int id;
     char gender;
+    char name[MAX_LEN];
 };
 
 struct db init(struct db val);
 void print_data(struct db val);
 
 int main(void) {
-    struct db data = init((struct db){1, 'm'});
+    struct db data = init((struct db){0,'m', "behemoth"});
     print_data(data);
 
     return(EXIT_SUCCESS);
@@ -18,9 +21,11 @@ int main(void) {
 
 struct db init(struct db val) {
     struct db temp = val;
+    temp.id = 1;
+
     return temp;
 }
 
 void print_data(struct db val) {
-    printf(" id: %d\ngender: %c", val.id, val.gender);
+    printf(" id: %d\ngender: %c\tname: %s", val.id, val.gender, val.name);
 }
