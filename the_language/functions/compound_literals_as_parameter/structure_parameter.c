ace777d32a41140f9707bfeae892462bac406ab4
B3h3m0th <behemoth@autistici.org>
2016-03-08 13:51:07 +0100

    invoke structure parameter as a compound literal

--- the_language/functions/compound_literals_as_parameter/structure_parameter.c
+++ the_language/functions/compound_literals_as_parameter/structure_parameter.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct db {
+    int id;
+    char gender;
+};
+
+struct db init(struct db val);
+void print_data(struct db val);
+
 int main(void) {
+    struct db data = init((struct db){1, 'm'});
+    print_data(data);
+
     return(EXIT_SUCCESS);
+}
+
+struct db init(struct db val) {
+    struct db temp = val;
+    return temp;
+}
+
+void print_data(struct db val) {
+    printf(" id: %d\ngender: %c", val.id, val.gender);
 }
