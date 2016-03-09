b983eb491c9ce4c3f8e15c5b3f5859ed96ae39ab
B3h3m0th <behemoth@autistici.org>
2016-03-09 22:24:50 +0100

    new lyric to vector of strings added

--- the_language/working_with_strings/search_substring.c
+++ the_language/working_with_strings/search_substring.c
@@ -5,7 +5,7 @@
 #define MAX_LEN 40
 
 char lyrics[][MAX_LEN] = { 
-        "take a walk on the wild side",
+        "no, we don't need no more trouble",
         "getup stand up standuo for your rights",
         "could you be loved",
         "The zion train is coming our way"
@@ -17,23 +17,28 @@ strstr() */
 void find_str(char str[]);
 
 int main(void) {
+    int len=sizeof(lyrics)/sizeof(lyrics[0]);
+    for (int i=0; i<sizeof(lyrics)/sizeof(lyrics[0]); i++)
+    printf("%d\n", len);
     char buf[MAX_LEN];
 
     printf("Search: ");
     fgets(buf, MAX_LEN, stdin);
+
     for (int i=0; i<strlen(buf); i++) {
         if (buf[i] == '\n') {
             buf[i] = '\0';
             break;
         }
     }
+    
     find_str(buf);
 
     return(EXIT_SUCCESS);
 }
 
 void find_str(char str[]) {
-    for (int i=0; i<4; i++)
+    for (int i=0; i<sizeof(lyrics)/sizeof(lyrics[0]); i++)
         if (strstr(lyrics[i], str)) {
             printf("Lyric %d: %s\n", i, lyrics[i]);
         }
