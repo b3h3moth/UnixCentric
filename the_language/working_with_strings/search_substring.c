94ef24057443bd4510ceabaef23e2ac77bc840ee
B3h3m0th <behemoth@autistici.org>
2016-03-09 21:55:46 +0100

    declared a vector of strings and its MAX size

--- the_language/working_with_strings/search_substring.c
+++ the_language/working_with_strings/search_substring.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_LEN 40
+
 /* Lo scopo del programma e' di verificare il funzionamento della funzione
 strstr() */
 
 int main(void) {
+    char lyrics[][MAX_LEN]
     return(EXIT_SUCCESS);
 }
