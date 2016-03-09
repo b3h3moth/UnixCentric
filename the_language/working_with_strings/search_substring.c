c59ed9ba2ce84ebc7c7370f0f4893da166a5b2c2
B3h3m0th <behemoth@autistici.org>
2016-03-09 22:04:41 +0100

    fixed lyrics

--- the_language/working_with_strings/search_substring.c
+++ the_language/working_with_strings/search_substring.c
@@ -3,24 +3,29 @@
 
 #define MAX_LEN 40
 
+char lyrics[][MAX_LEN] = { 
+        "take a walk on the wild side",
+        "getup stand up standuo for your rights",
+        "could you be loved",
+        "The zion train is coming our way"
+};
+
 /* Lo scopo del programma e' di verificare il funzionamento della funzione
 strstr() */
 
 void find_str(char str[]);
 
 int main(void) {
-    char lyrics[][MAX_LEN] = { 
-        "take a walk on the wild side",
-        "getup stand up standuo for your rights",
-        "could you be loved",
-        "The zion train is coming our way"
-    };
+    char buf[MAX_LEN];
+
+    printf("Search: ");
+    fgets(
 
     return(EXIT_SUCCESS);
 }
 
 void find_str(char str[]) {
     for (int i=0; i<4; i++)
-        if (strstr(str[i], find_str()))
+        if (strstr(str[i], ))
             printf("Lyric %d: %s\n", i, str[i]);
 }
