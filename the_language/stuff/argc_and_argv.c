e99e75c8202cddeb244f0e1cf877cc7417ed7ed2
B3h3m0th <behemoth@autistici.org>
2016-03-10 20:32:57 +0100

    how command line arguments can work

--- the_language/stuff/argc_and_argv.c
+++ the_language/stuff/argc_and_argv.c
@@ -5,6 +5,9 @@
 senza l'ausilio di funzioni quali possono essere getopt() o getopt_long() */
 
 int main(int argc, char *argv[]) {
+
+    /* Cicla tra gli argomenti verificando che il primo carattere di ciascun
+    argomento sia '-' */
     for (int i=1; i<argc; i++) {
         if (argv[i][0] != '-')
             break;
