e07db61b25e018eea8e855df4c43bbbfaccd1f0d
B3h3m0th <behemoth@autistici.org>
2016-03-12 19:45:38 +0100

    fopen() error checking control added

--- the_language/input_output/add_words_to_file.c
+++ the_language/input_output/add_words_to_file.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 
 #define MAX_LEN 31
 
 int main(void) {
     FILE *fp;
     char words[MAX_LEN];
+    char *filename = "data.txt";
 
+    if ((fp = fopen(filename, "a+")) == NULL) {
+        fprintf(sdterr, "Err. fopen(); %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
     return(EXIT_SUCCESS);
 }
