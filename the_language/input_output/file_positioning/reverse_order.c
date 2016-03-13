6efab7f7a281bdba8702a4cb3b32e8cdc380836a
B3h3m0th <behemoth@autistici.org>
2016-03-13 17:56:30 +0100

    fopen() error checking when open an input file added

--- the_language/input_output/file_positioning/reverse_order.c
+++ the_language/input_output/file_positioning/reverse_order.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 
 #define MAX_LEN 80
 
 main(int argc, char *argv[]) {
-    
+
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
+
+    if ((fp = fopen(argv[1], "rb")) == NULL) {
+        fprintf(stderr, "Err. fopen(), %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
     
     return(EXIT_SUCCESS);
 }
