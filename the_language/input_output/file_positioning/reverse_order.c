6014f6e6c58ca7871f7bf8dcfd4c3fef9e2c9b05
B3h3m0th <behemoth@autistici.org>
2016-03-13 17:48:22 +0100

    command line usage

--- the_language/input_output/file_positioning/reverse_order.c
+++ the_language/input_output/file_positioning/reverse_order.c
@@ -4,6 +4,11 @@
 #define MAX_LEN 80
 
 main(int argc, char *argv[]) {
-    char
+    
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    
     return(EXIT_SUCCESS);
 }
