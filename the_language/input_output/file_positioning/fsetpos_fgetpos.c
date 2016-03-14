805bc5ffd3121af5a08eae49bc08dfba1dca863c
B3h3m0th <behemoth@autistici.org>
2016-03-14 18:25:16 +0100

    removed unused routines

--- the_language/input_output/file_positioning/fsetpos_fgetpos.c
+++ the_language/input_output/file_positioning/fsetpos_fgetpos.c
@@ -11,7 +11,7 @@ offset. Si utilizzeranno le funzioni portabili fsetpos() e fgetpos(). */
 int main(int argc, char *argv[]) {
     FILE *fp;
     fpos_t pos;
-    char buf[MAX_BUF];
+    int ch;
 
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
@@ -23,18 +23,6 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    // Lettura di MAX_BUF byte dal file e salvati in buf
-    if (!feof(fp)) {
-        fread(buf, sizeof(char), MAX_BUF, fp);
-        buf[MAX_BUF-1] = '\0';
-
-        if (fgetpos(fp, &pos) != 0) {
-            fprintf(stderr, "Err. fgetpos(), %s\n", strerror(errno));
-            exit(EXIT_FAILURE);
-        }
-    }
-    
-    printf("%s\n", buf);
 
     fclose(fp);
 
