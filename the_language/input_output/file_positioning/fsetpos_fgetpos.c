deb69e1fe0d1578b4a051e6a38febd4c3cdea654
B3h3m0th <behemoth@autistici.org>
2016-03-14 19:05:20 +0100

    the program's purpose is to create a simil SEEK_END, SEEK_END through fgetpos() and fsetpos()

--- the_language/input_output/file_positioning/fsetpos_fgetpos.c
+++ the_language/input_output/file_positioning/fsetpos_fgetpos.c
@@ -3,15 +3,12 @@
 #include <errno.h>
 #include <string.h>
 
-#define MAX_BUF 72
-
-/* Lo scopo del programma e' di aprire un file e leggere alcuni byte da diversi
-offset. Si utilizzeranno le funzioni portabili fsetpos() e fgetpos(). */
+/* Lo scopo del programma e' di simulare mediante fgetpos() e fsetpos() il
+concetto di SEEK_SET e SEEK_END associato a fseek(). */
 
 int main(int argc, char *argv[]) {
     FILE *fp;
-    fpos_t pos;
-    int ch;
+    fpos_t pos_beg, pos_end;
 
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
@@ -22,7 +19,28 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Err. fopen(), %s\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
+    
+    if (fgetpos(fp, &pos_beg) != 0) {
+        fprintf(stderr, "Err. fgetpos(), %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
 
+    // Lettura di MAX_BUF byte dal file e salvati in buf
+    while (feof(fp)) {
+        if (fgetpos(fp, &pos_end) != 0) {
+            fprintf(stderr, "Err. fgetpos(), %s\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    fsetpos(fp, &pos_end);
+
+    int ch;
+    while ((ch = fgetc(fp))) {
+        if (feof(fp))
+            break;
+        fputc(ch, stdout);
+    }
 
     fclose(fp);
 
