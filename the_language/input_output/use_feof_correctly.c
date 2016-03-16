626e6ce030f49cde082fba7934e293225a9552d6
B3h3m0th <behemoth@autistici.org>
2016-03-16 15:21:41 +0100

    how feof() works

--- the_language/input_output/use_feof_correctly.c
+++ the_language/input_output/use_feof_correctly.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
     FILE *fp;
 
     fp = fopen(argv[1], "r");
     if (fp != NULL) {
-        fprintf(stderr, "Err. fopen()\n", strerror(errno));
+        fprintf(stderr, "Err. fopen(), %s\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
+
+    /* La funzione feof() ritorna un valore diverso da zero se e' stata tentata
+    una lettura dello stream oltre le fine del file. La feof() non controlla
+    esplicitamente il raggiungimento della fine del file, ma piuttosto essa 
+    si limita a ritornare il valore di una variabile conservata nella libreria
+    I/O indicante che e' stata gia' tentata una lettura oltre la fine del file.
+    */
     
     fclose(fp);
     return(EXIT_SUCCESS);
