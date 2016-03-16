38ccf1eff61ef8be73e43ff4f597c35f98f3822e
B3h3m0th <behemoth@autistici.org>
2016-03-16 15:13:37 +0100

    fopen error checking

--- the_language/input_output/use_feof_correctly.c
+++ the_language/input_output/use_feof_correctly.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
+int main(int argc, char *argv[]) {
+    FILE *fp;
+
+    fp = fopen(argv[1], "r");
+    if (fp != NULL) {
+        fprintf(stderr, "Err. fopen()\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    
+    fclose(fp);
     return(EXIT_SUCCESS);
 }
