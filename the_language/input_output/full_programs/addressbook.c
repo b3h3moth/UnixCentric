219fea9c86732682a38fa0a08b61cd2c220759d0
B3h3m0th <behemoth@autistici.org>
2016-03-17 22:26:37 +0100

    set_option() definition and switch skeleton added

--- the_language/input_output/full_programs/addressbook.c
+++ the_language/input_output/full_programs/addressbook.c
@@ -22,16 +22,35 @@ int main(void) {
     int option;
     char *faddrbook = "addressbook.bin";
 
-    if ((fp = fopen(faddrbook, "rb+")) == NULL) {
+    if ((fp = fopen(faddrbook, "ab+")) == NULL) {
         fprintf(stderr, "Err. opening file. fopen(), %s\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
 
     while ( (option = set_option()) != MENU_OPTIONS) {
+        switch (option) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            default:
+                fputs("Unknow option\n", stdout);
+                break;
     }
     return(EXIT_SUCCESS);
 }
 
 int set_option(void) {
-    return 1;
+    int value;
+    fputs("[1] save data for printing\n"
+          "[2] update an account\n"
+          "[3] add a new account\n"
+          "[4] delete an account\n"
+          "[5] quit\n : ",  stdout);
+
+    return (scanf("%1d", &value));
 }
