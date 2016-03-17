e3fe92b66c8c1270f18e7261fbe67cd111ded680
B3h3m0th <behemoth@autistici.org>
2016-03-17 21:59:12 +0100

    client_data structures added

--- the_language/input_output/full_programs/addressbook.c
+++ the_language/input_output/full_programs/addressbook.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_LEN 20
+
+struct client_data {
+    int id;
+    char last_name[MAX_LEN];
+    char first_name[MAX_LEN];
+
 int main(void) {
     return(EXIT_SUCCESS);
 }
