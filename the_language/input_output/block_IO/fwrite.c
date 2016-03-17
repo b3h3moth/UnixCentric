d98c6ca42d5713f6c26435549076e7bd39243c26
B3h3m0th <behemoth@autistici.org>
2016-03-17 18:36:54 +0100

    write a four byte integer to stream

--- the_language/input_output/block_IO/fwrite.c
+++ the_language/input_output/block_IO/fwrite.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 
 int main(void) {
-    int val = 10;
+    int val = 255;
+
     FILE *fp;
 
     if ((fp = fopen("data.bin", "ab+")) == NULL) {
-        fprintf(stderr, "Err. open stream with fopen()\n", strerror(errno));
+        fprintf(stderr, "Err. open stream with fopen(), %s\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
 
-    // Scrittuta di un intero in un file
+    // Scrive quattro byte dell'intero 'val' nel file
+    fwrite(&val, sizeof(int), 1, fp);
 
+    fclose(fp);
     return(EXIT_SUCCESS);
 }
