b127e76956991ec47dd2ba099ecfeff6e2cefe75
B3h3m0th <behemoth@autistici.org>
2016-03-17 22:03:30 +0100

    fopen() error checking

--- the_language/input_output/full_programs/addressbook.c
+++ the_language/input_output/full_programs/addressbook.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 
 #define MAX_LEN 20
 
@@ -7,7 +9,19 @@ struct client_data {
     int id;
     char last_name[MAX_LEN];
     char first_name[MAX_LEN];
+    double balance;
+};
+
+// Prototipi
 
 int main(void) {
+    FILE *fp;
+    int choice;
+    char *faddrbook = "addressbook.bin";
+
+    if ((fp = fopen(faddrbook, "rb+")) == NULL) {
+        fprintf(stderr, "Err. opening file. fopen(), %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
     return(EXIT_SUCCESS);
 }
