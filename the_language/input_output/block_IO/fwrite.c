bce3cce7d960d50565553cab44abb6e722599ecc
B3h3m0th <behemoth@autistici.org>
2016-03-17 18:29:44 +0100

    fopen() error checking, open a stream

--- the_language/input_output/block_IO/fwrite.c
+++ the_language/input_output/block_IO/fwrite.c
@@ -2,5 +2,15 @@
 #include <stdlib.h>
 
 int main(void) {
+    int val = 10;
+    FILE *fp;
+
+    if ((fp = fopen("data.bin", "ab+")) == NULL) {
+        fprintf(stderr, "Err. open stream with fopen()\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    // Scrittuta di un intero in un file
+
     return(EXIT_SUCCESS);
 }
