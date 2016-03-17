b22d2e9d7ca9626c9aef42ead13610ad50eb4e28
B3h3m0th <behemoth@autistici.org>
2016-03-17 22:12:11 +0100

    set_option() declaration, starting while loop to setup options

--- the_language/input_output/full_programs/addressbook.c
+++ the_language/input_output/full_programs/addressbook.c
@@ -5,6 +5,8 @@
 
 #define MAX_LEN 20
 
+enum { MENU_OPTIONS=5 };
+
 struct client_data {
     int id;
     char last_name[MAX_LEN];
@@ -13,15 +15,23 @@ struct client_data {
 };
 
 // Prototipi
+int set_option(void);
 
 int main(void) {
     FILE *fp;
-    int choice;
+    int option;
     char *faddrbook = "addressbook.bin";
 
     if ((fp = fopen(faddrbook, "rb+")) == NULL) {
         fprintf(stderr, "Err. opening file. fopen(), %s\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
+
+    while ( (option = set_option()) != MENU_OPTIONS) {
+    }
     return(EXIT_SUCCESS);
+}
+
+int set_option(void) {
+    return 1;
 }
