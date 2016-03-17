94510401d80cabef15058ee46a0c956e35094b23
B3h3m0th <behemoth@autistici.org>
2016-03-17 19:12:50 +0100

    write vec_size array to stream pointed by fp

--- the_language/input_output/block_IO/fread_fwrite.c
+++ the_language/input_output/block_IO/fread_fwrite.c
@@ -4,11 +4,12 @@
 #include <errno.h>
 
 int main(void) {
-    int val = 255;
-    int vec[] = {2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192};
-    int valb;
-
     FILE *fp;
+    int val = 255;
+    int vec[] = {2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096};
+    int valb;
+    int vec_size = sizeof(vec) / sizeof(vec[0]);
+
 
     if ((fp = fopen("data.bin", "ab+")) == NULL) {
         fprintf(stderr, "Err. open stream with fopen(), %s\n", strerror(errno));
@@ -25,6 +26,9 @@ int main(void) {
     // Legge un intero da dallo stream 'fp' salvandolo in valb
     fread(&valb, sizeof(int), 1, fp);
     printf("val backup is: %d\n", valb);
+
+    // Scrive un vettore di 'vec_size' elementi nello stream 'fp'
+    fwrite(&vec, sizeof(vec)/sizeof(vec[0]), 1, fp);
 
     fclose(fp);
     return(EXIT_SUCCESS);
