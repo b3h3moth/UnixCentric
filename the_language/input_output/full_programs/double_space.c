17af9148d5752a5e377d35f03730aba7a4eda95b
B3h3m0th <behemoth@autistici.org>
2016-03-18 19:06:14 +0100

    How functions work

--- the_language/input_output/full_programs/double_space.c
+++ the_language/input_output/full_programs/double_space.c
@@ -11,3 +11,11 @@ in input, salvandone una copia */
 int main(void) {
     return(EXIT_SUCCESS);
 }
+
+// Realizza la doppia spaziatura nel file 'fin',  salva il tutto in 'fout'
+void dbl_spacing(FILE *fin, FILE *fout) {
+}
+
+// Informazioni circa il programma in uso
+void usage(void) {
+}
