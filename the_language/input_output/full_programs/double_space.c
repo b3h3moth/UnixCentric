325f7f55bfce17ec90220b99c707d94971a3dcd5
B3h3m0th <behemoth@autistici.org>
2016-03-18 19:03:15 +0100

    Function prototypes added, usage() and dbl_spacing()

--- the_language/input_output/full_programs/double_space.c
+++ the_language/input_output/full_programs/double_space.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Function Prototypes
+void dbl_spacing(FILE *fin, FILE *fout);
+void usage(void);
+
 /* Lo scopo del programma e' di raddoppiare la spaziatura in un file ricevuto
 in input, salvandone una copia */
 
