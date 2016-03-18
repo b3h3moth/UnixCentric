8926d40b4f141d99f02692c5934e06404332455b
B3h3m0th <behemoth@autistici.org>
2016-03-18 18:59:35 +0100

    The (double_space) program's purpose

--- the_language/input_output/full_programs/double_space.c
+++ the_language/input_output/full_programs/double_space.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Lo scopo del programma e' di raddoppiare la spaziatura in un file ricevuto
+in input, salvandone una copia */
+
 int main(void) {
     return(EXIT_SUCCESS);
 }
