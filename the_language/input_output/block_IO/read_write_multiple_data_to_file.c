c5029d0b5fff3dc926363b83850c81bfc8611b46
B3h3m0th <behemoth@autistici.org>
2016-03-19 17:07:14 +0100

    fixed files' section

---
+++ the_language/input_output/block_IO/read_write_multiple_data_to_file.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct data {
+    int id;
+    char name[20];
+    char email[30];
+};
+
+int main(void) {
+    FILE *fp;
+    struct data mydata1 = {1, "luca suriano", "luka@autistici.org"};
+    struct data mydata2 = {2, "richard stallman", "rms@stallman.org"};
+    struct data mydata3 = {3, "Larry Wall", "larry@perl.org"};
+    struct data mydata;
+    fp = fopen("xxx.bin", "wb+");
+
+    fwrite(&mydata1, sizeof(struct data), 1, fp);
+    fwrite(&mydata2, sizeof(struct data), 1, fp);
+    fwrite(&mydata3, sizeof(struct data), 1, fp);
+
+    fseek(fp, 0, SEEK_SET);
+
+    while (fread(&mydata, sizeof(struct data), 1, fp))
+        printf("%d %s %s\n", mydata.id, mydata.name, mydata.email);
+
+    fclose(fp);
+    return(EXIT_SUCCESS);
+}
