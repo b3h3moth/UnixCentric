ec8c89a90159d738e0b35bc3f70329a45989a196
B3h3m0th <behemoth@autistici.org>
2016-03-19 18:08:00 +0100

    pointer mydata4 to struct data, initialization and fixing

--- the_language/input_output/block_IO/read_write_multiple_data_to_file.c
+++ the_language/input_output/block_IO/read_write_multiple_data_to_file.c
@@ -15,9 +15,9 @@ dopodiche' leggere i dati dal file e stamparli sullo stdout */
 int main(void) {
     FILE *fp;
     struct data mydata, *mydata4;
-    struct data mydata1 = {1, "luca suriano", "luka@autistici.org"};
-    struct data mydata2 = {2, "richard stallman", "rms@stallman.org"};
-    struct data mydata3 = {3, "Larry Wall", "larry@perl.org"};
+    struct data mydata1 = {1, "Rob Pike", "rob@pike.org"};
+    struct data mydata2 = {2, "Richard Stallman", "richard@stallman.org"};
+    struct data mydata3 = {3, "Larry Wall", "larry@wall.org"};
     
     if ((fp = fopen("data.bin", "wb+")) == NULL) {
         fprintf(stderr, "Err. opening file, fopen(), %s\n", strerror(errno));
@@ -28,14 +28,24 @@ int main(void) {
     fwrite(&mydata2, sizeof(struct data), 1, fp);
     fwrite(&mydata3, sizeof(struct data), 1, fp);
 
-    /* Poiche' mydata4 e' un puntatore a una 'struct data' si deve allocare la
-    memoria corretta affinche' possa conterete i dati */
+    /* Poiche' 'mydata4' e' un puntatore a una 'struct data', prima di poter
+    inizializzarla con i dati ricevuti dal file, si deve allocare la memoria */
     mydata4 = malloc(sizeof(struct data));
+
+    // Si riempie la struttura
+    mydata4->id = 4;
+    strcpy(mydata4->name, "Ken Thopmson");
+    strcpy(mydata4->email, "ken@thopmson.org");
+
+    /* Si scrive sul file, avendo l'accortezza di non utilizzare l'operatore
+    'address of (&)' poiche' si sta lavorando con puntatori */
+    fwrite(mydata4, sizeof(struct data), 1, fp);
 
     /* Per leggere e' necessario posizionare il 'file position indicator' 
     all'inizio del file */
     fseek(fp, 0, SEEK_SET);
 
+    // Lettura di tutto cio' che e' stato scritto nel file
     while (fread(&mydata, sizeof(struct data), 1, fp))
         printf("%d %s %s\n", mydata.id, mydata.name, mydata.email);
 
