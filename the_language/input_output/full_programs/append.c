da98ecff1e0a2949b69c9aed1ad25f2f13376ca2
B3h3m0th <behemoth@autistici.org>
2016-03-20 22:40:18 +0100

    get_string() declaration

--- the_language/input_output/full_programs/append.c
+++ the_language/input_output/full_programs/append.c
@@ -3,10 +3,18 @@
 
 #define LEN 21
 
+// Function prototypes
+char *get_string(char *str, int num);
+
 int main(void) {
     FILE *fin, *fout;
-    int files = 0;
+    int nfiles = 0, ch;
     char in_filename[LEN];
     char out_filename[LEN];
+
+    puts("Name of files to append");
+    
     return(EXIT_SUCCESS);
 }
+
+char *get_string(char *str, int num)
