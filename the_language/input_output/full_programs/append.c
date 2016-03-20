883f0e731b330715c84e847930b445161d465919
B3h3m0th <behemoth@autistici.org>
2016-03-20 22:32:29 +0100

    input and output filenames

--- the_language/input_output/full_programs/append.c
+++ the_language/input_output/full_programs/append.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define LEN 21
+
 int main(void) {
+    FILE *fin, *fout;
+    int files = 0;
+    char in_filename[LEN];
+    char out_filename[LEN];
     return(EXIT_SUCCESS);
 }
