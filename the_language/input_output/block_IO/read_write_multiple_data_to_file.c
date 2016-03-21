1545b641c2c441e4a3d72472a7e7b52180a6c2f3
B3h3m0th <behemoth@autistici.org>
2016-03-21 20:02:21 +0100

    init new structure and print it

--- the_language/input_output/block_IO/read_write_multiple_data_to_file.c
+++ the_language/input_output/block_IO/read_write_multiple_data_to_file.c
@@ -14,10 +14,11 @@ dopodiche' leggere i dati dal file e stamparli sullo stdout */
 
 int main(void) {
     FILE *fp;
-    struct data mydata, *mydata4;
+    struct data mydata, *mydata5;
     struct data mydata1 = {1, "Rob Pike", "rob@pike.org"};
     struct data mydata2 = {2, "Richard Stallman", "richard@stallman.org"};
     struct data mydata3 = {3, "Larry Wall", "larry@wall.org"};
+    struct data mydata4 = {4, "Bill Joy", "bill@joy.org"};
     
     if ((fp = fopen("data.bin", "wb+")) == NULL) {
         fprintf(stderr, "Err. opening file, fopen(), %s\n", strerror(errno));
@@ -27,19 +28,20 @@ int main(void) {
     fwrite(&mydata1, sizeof(struct data), 1, fp);
     fwrite(&mydata2, sizeof(struct data), 1, fp);
     fwrite(&mydata3, sizeof(struct data), 1, fp);
+    fwrite(&mydata4, sizeof(struct data), 1, fp);
 
-    /* Poiche' 'mydata4' e' un puntatore a una 'struct data', prima di poter
+    /* Poiche' 'mydata5' e' un puntatore a una 'struct data', prima di poter
     inizializzarla con i dati ricevuti dal file, si deve allocare la memoria */
-    mydata4 = malloc(sizeof(struct data));
+    mydata5 = malloc(sizeof(struct data));
 
     // Si riempie la struttura
-    mydata4->id = 4;
-    strcpy(mydata4->name, "Ken Thopmson");
-    strcpy(mydata4->email, "ken@thopmson.org");
+    mydata5->id = 4;
+    strcpy(mydata5->name, "Ken Thopmson");
+    strcpy(mydata5->email, "ken@thopmson.org");
 
     /* Si scrive sul file, avendo l'accortezza di non utilizzare l'operatore
     'address of (&)' poiche' si sta lavorando con puntatori */
-    fwrite(mydata4, sizeof(struct data), 1, fp);
+    fwrite(mydata5, sizeof(struct data), 1, fp);
 
     /* Per leggere e' necessario posizionare il 'file position indicator' 
     all'inizio del file */
