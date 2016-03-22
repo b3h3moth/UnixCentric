56fa1a876916925898ef60b99ec3e08be50569e5
B3h3m0th <behemoth@autistici.org>
2016-03-22 20:18:53 +0100

    declaration of structure to storing a record

--- the_language/input_output/full_programs/random_access.c
+++ the_language/input_output/full_programs/random_access.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_LEN 20
+
 // Struttura anonima per salvataggio di dati globali, quali il nome del file
 struct {
     char *filename;
@@ -13,7 +15,14 @@ struct date {
     int year;
 };
 
-struc
+typedef struct date Date;
+
+struct rec {
+    Date *dt;
+    char name[MAX_LEN];
+    char email[MAX_LEN];
+};
+
 int main(void) {
     return(EXIT_SUCCESS);
 }
