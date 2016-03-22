6f7cc137d42d174a461664f09322312fe53e24d9
B3h3m0th <behemoth@autistici.org>
2016-03-22 20:14:17 +0100

    anonymous structure to storing filename and file pointer

--- the_language/input_output/full_programs/random_access.c
+++ the_language/input_output/full_programs/random_access.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Struttura anonima per salvataggio di dati globali, quali il nome del file
+struct {
+    char *filename;
+    FILE *fp;
+} global = { .filename = "data.bin", .fp = NULL };
+
 int main(void) {
     return(EXIT_SUCCESS);
 }
