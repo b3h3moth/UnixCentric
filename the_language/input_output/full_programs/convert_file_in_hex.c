a59ddd7f184661c94e6a38bcb87b547ef0d66fb7
B3h3m0th <behemoth@autistici.org>
2016-03-26 20:05:13 +0100

    remove new-line from filename

--- the_language/input_output/full_programs/convert_file_in_hex.c
+++ the_language/input_output/full_programs/convert_file_in_hex.c
@@ -7,12 +7,20 @@
 int main(void) {
     char *filename[FILENAME_MAX]; // Definito in stdio.h
     FILE *fp;
+    size_t len;
 
     if (argc == 1) {
         // Inserimento del nome del file
         fputs("Enter filename: ", stdout);
         fgets(filename, MAXLEN, stdin);
 
-        //
+        len = strlen(filename);
+
+        // Rimuove la new-line finale
+        if (filename[len-1] == '\n')
+            filename[len-1] = '\0';
+    } else
+        strcpy(filename, argv[1]);
+
     return(EXIT_SUCCESS);
 }
