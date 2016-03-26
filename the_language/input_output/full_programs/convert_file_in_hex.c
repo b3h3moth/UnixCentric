4f7acfc0fa4664a6d8e7b2f3349b4867b59c5fa7
B3h3m0th <behemoth@autistici.org>
2016-03-26 20:18:38 +0100

    open file and setvbuf()

--- the_language/input_output/full_programs/convert_file_in_hex.c
+++ the_language/input_output/full_programs/convert_file_in_hex.c
@@ -4,23 +4,34 @@
 #include <ctype.h>
 #include <errno.h>
 
+#define MAXLEN  256
+
 int main(int argc, char *argv[]) {
     char filename[FILENAME_MAX]; // Definito in stdio.h
     FILE *fp;
     size_t len;
 
-    if (argc == 1) {
-        // Inserimento del nome del file
-        fputs("Enter filename: ", stdout);
-        fgets(filename, MAXLEN, stdin);
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
 
-        len = strlen(filename);
+    // Inserimento del nome del file
+    fputs("Enter filename: ", stdout);
+    fgets(filename, MAXLEN, stdin);
+    
+    len = strlen(filename);
 
-        // Rimuove la new-line finale
-        if (filename[len-1] == '\n')
-            filename[len-1] = '\0';
-    } else
-        strcpy(filename, argv[1]);
+    // Rimuove la new-line finale
+    if (filename[len-1] == '\n')
+        filename[len-1] = '\0';
+
+    if (fopen(filename, "rb") == NULL) {
+        fprintf(stderr, "Err. opening file, fopen(), %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    setvbuf(fp, NULL, _IOFBF, BUFSIZ);
 
     return(EXIT_SUCCESS);
 }
