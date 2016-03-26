9e6b2d0d771d51d264ff4fd51ebb30256f4317b3
B3h3m0th <behemoth@autistici.org>
2016-03-26 20:02:53 +0100

    get the filename

--- the_language/input_output/full_programs/convert_file_in_hex.c
+++ the_language/input_output/full_programs/convert_file_in_hex.c
@@ -5,5 +5,14 @@
 #include <errno.h>
 
 int main(void) {
+    char *filename[FILENAME_MAX]; // Definito in stdio.h
+    FILE *fp;
+
+    if (argc == 1) {
+        // Inserimento del nome del file
+        fputs("Enter filename: ", stdout);
+        fgets(filename, MAXLEN, stdin);
+
+        //
     return(EXIT_SUCCESS);
 }
