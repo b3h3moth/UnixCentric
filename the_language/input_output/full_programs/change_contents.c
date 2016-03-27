647ff006843be89fd1b9497d4ce9457228f7383f
B3h3m0th <behemoth@autistici.org>
2016-03-27 23:12:49 +0200

    new type Record added

--- the_language/input_output/full_programs/change_contents.c
+++ the_language/input_output/full_programs/change_contents.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct {
+    uint32_t id;
+    char name[MAXLEN];
+    char email[MAXLEN];
+} Record;
+
 int main(void) {
     return(EXIT_SUCCESS);
 }
