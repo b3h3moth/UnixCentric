a3410c4e585a2d7385095826bff31cb70c06afc3
B3h3m0th <behemoth@autistici.org>
2016-03-28 17:05:05 +0200

    filename to store data

--- the_language/input_output/full_programs/change_contents.c
+++ the_language/input_output/full_programs/change_contents.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+
+#define MAXLEN  256
 
 typedef struct {
     uint32_t id;
     char name[MAXLEN];
     char email[MAXLEN];
 } Record;
+
+static const char *filename = "data.bin";
 
 int main(void) {
     return(EXIT_SUCCESS);
