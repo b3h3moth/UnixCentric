c765acc8bb3c24fd629f374d099d77b56132a551
B3h3m0th <behemoth@autistici.org>
2016-03-29 19:50:43 +0200

    ptr_func() print addresses

--- the_language/functions/functions_and_pointers/playing_with_array_pointer_args.c
+++ the_language/functions/functions_and_pointers/playing_with_array_pointer_args.c
@@ -5,9 +5,11 @@ char vec[] = "abcdefghijklmnopqrstuvwxyz";
 
 // Function Prototypes
 void arr_func(char ch[10]);
+void ptr_func(char *ptr);
 
 int main(void) {
     arr_func(vec);
+    ptr_func(vec);
 
     return(EXIT_SUCCESS);
 }
@@ -16,4 +18,12 @@ void arr_func(char ch[10]) {
     printf("     &ch: %#x\n", &ch);
     printf("&(ch[0]): %#x\n", &(ch[0]));
     printf("&(ch[1]): %#x\n", &(ch[1]));
+    printf("    ++ch: %#x\n", ++ch);
+}
+
+void ptr_func(char *pr) {
+    printf("     &pr: %#x\n", &pr);
+    printf("&(pr[0]): %#x\n", &(pr[0]));
+    printf("&(pr[1]): %#x\n", &(pr[1]));
+    printf("    ++pr: %#x\n", ++pr);
 }
