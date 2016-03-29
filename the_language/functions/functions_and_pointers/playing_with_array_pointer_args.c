2d3f83b11f0752f2e6961d5bd1e76868e78e476d
B3h3m0th <behemoth@autistici.org>
2016-03-29 19:41:42 +0200

    print arr_func() addresses

---
+++ the_language/functions/functions_and_pointers/playing_with_array_pointer_args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char vec[] = "abcdefghijklm";
+
+// Function Prototypes
+void arr_func(char ch[10]);
+
+int main(void) {
+    arr_func(vec);
+
+    return(EXIT_SUCCESS);
+}
+
+void arr_func(char ch[10]) {
+    printf("     &ch: %#x\n", &ch);
+    printf("&(ch[0]): %#x\n", &(ch[0]));
+    printf("&(ch[1]): %#x\n", &(ch[1]));
+}
