c16d711a528d57c3517ac7a8cb7225c747286cfb
B3h3m0th <behemoth@autistici.org>
2016-03-31 19:57:38 +0200

    how to break arrays of arrays of arrays

--- the_language/array/break_multidim_array_components.c
+++ the_language/array/break_multidim_array_components.c
@@ -2,8 +2,19 @@
 #include <stdlib.h>
 
 /* Lo scopo del programma e' di spezzare un array multidimensionale nelle sue
-componenti individuali mediante puntatori. */
+componenti individuali, in modo tale da rendere evidente la relazione che
+intercorre con i puntatori. */
 
 int main(void) {
+    int data[2][3][5];
+
+    int (*pa)[3][5] = data;
+    int (*pb)[5] = data[0];
+    int  *pc = data[2][3];
+
+    printf("%#x\n", (unsigned int)pa);
+    printf("%#x\n", (unsigned int)pb);
+    printf("%#x\n", (unsigned int)pc-180);
+
     return(EXIT_SUCCESS);
 }
