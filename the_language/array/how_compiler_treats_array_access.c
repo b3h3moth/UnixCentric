0648aced90ef607cb4aa2ab311ff0025658e1d81
B3h3m0th <behemoth@autistici.org>
2016-03-31 18:09:51 +0200

    compilers with array

--- the_language/array/how_compiler_treats_array_access.c
+++ the_language/array/how_compiler_treats_array_access.c
@@ -9,5 +9,10 @@ int main(void) {
     val[i][j] e' sempre riscritto come *(val + i) + j)
     */
 
+    int val[] = {1,2,3,4,5,6,7,8,9,10};
+
+    for (int i=0; i<sizeof(val)/sizeof(val[0]); i++)
+        printf("%d, ", *(val + i));
+
     return(EXIT_SUCCESS);
 }
