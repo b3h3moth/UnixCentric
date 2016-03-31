9f2d6b82b5d058c5a9b6b49e6861889d5702681a
B3h3m0th <behemoth@autistici.org>
2016-03-31 18:07:24 +0200

    How compilers treat array and arrays of arrays

--- the_language/array/how_compiler_treats_array_access.c
+++ the_language/array/how_compiler_treats_array_access.c
@@ -2,5 +2,12 @@
 #include <stdlib.h>
 
 int main(void) {
+    /* I compilatori riscrivono l'accesso a un array mediante puntatori, ma
+    attenzione, puntatori e array non sono la stessa cosa.
+    
+    val[i] e' sempre riscritto come *(val + i)
+    val[i][j] e' sempre riscritto come *(val + i) + j)
+    */
+
     return(EXIT_SUCCESS);
 }
