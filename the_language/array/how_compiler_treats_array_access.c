d139c0a38bef2f8b09fac83be3e517d3461c3eda
B3h3m0th <behemoth@autistici.org>
2016-03-31 18:21:53 +0200

    compilers with arrays of arrays

--- the_language/array/how_compiler_treats_array_access.c
+++ the_language/array/how_compiler_treats_array_access.c
@@ -5,14 +5,22 @@ int main(void) {
     /* I compilatori riscrivono l'accesso a un array mediante puntatori, ma
     attenzione, puntatori e array non sono la stessa cosa.
     
-    val[i] e' sempre riscritto come *(val + i)
-    val[i][j] e' sempre riscritto come *(val + i) + j)
+    arr[i]    e' sempre riscritto come:   *(val + i)
+    arr[i][j] e' sempre riscritto come: *(*(val + i) + j)
     */
 
     int val[] = {1,2,3,4,5,6,7,8,9,10};
 
     for (int i=0; i<sizeof(val)/sizeof(val[0]); i++)
-        printf("%d, ", *(val + i));
+        printf("%2d ", *(val + i));
+
+    puts(" ");
+
+    int matrix[2][5] = {{11,12,13,14,15},{16,17,18,19,20}};
+
+    for (int i=0; i<2; i++) 
+        for (int j=0; j<5; j++)
+            printf("%2d ",  *(*(matrix + i)+j) );
 
     return(EXIT_SUCCESS);
 }
