31acf70d95ff3287038e34a3ee177586f0e6ab1e
B3h3m0th <behemoth@autistici.org>
2016-03-31 20:52:16 +0200

    how to initialize multidimentional array with pointer

--- the_language/array/break_multidim_array_components.c
+++ the_language/array/break_multidim_array_components.c
@@ -7,6 +7,14 @@ intercorre con i puntatori. */
 
 int main(void) {
     int data[2][3][5];
+    int *p;
+    int i=1;
+
+    // Inizializza ciascun elemento dell'array utilizzando la variabile 'i'
+    for (p = &data[0][0][0]; p <= &data[2-1][3-1][5-1]; p++, i++)
+        *p = i;
+    
+    puts("\nArray initialized.\n");
 
     int (*pa)[3][5] = data;
     int (*pb)[5] = data[0];
