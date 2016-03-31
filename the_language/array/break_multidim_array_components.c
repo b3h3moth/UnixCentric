21d15191676170f4c1a57ed8d8ea849c5667441f
B3h3m0th <behemoth@autistici.org>
2016-03-31 18:42:43 +0200

    how program works

--- the_language/array/break_multidim_array_components.c
+++ the_language/array/break_multidim_array_components.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Lo scopo del programma e' di spezzare un array multidimensionale nelle sue
+componenti individuali mediante puntatori. */
+
 int main(void) {
     return(EXIT_SUCCESS);
 }
