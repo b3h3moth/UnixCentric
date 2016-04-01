1b6b8900a8f7baaab5b12a7e8f850f62317f1cae
B3h3m0th <behemoth@autistici.org>
2016-04-01 11:59:13 +0200

    defined four pointers to the first array memory address

--- representing_information/addressing_and_byte_ordering/endianess/check_for_endianess.c
+++ representing_information/addressing_and_byte_ordering/endianess/check_for_endianess.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *bits(char ch);
-
 // Creazione di un array di memoria statica
 char vec[4] = {'a', 'b', 'c', 'd'};
 
+// Function Prototypes
+char *bits(char ch);
+
+
 int main(void) {
+    // Puntatori all'inizio di 'vec'
+    char    *ptochr = vec;
+    short   *ptoshr = (short *)vec;
+    int     *ptoint = (int *)vec;
+    float   *ptoflt = (float *)vec;
+
     return(EXIT_SUCCESS);
 }
