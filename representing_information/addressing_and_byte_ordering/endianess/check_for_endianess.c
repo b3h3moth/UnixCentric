2c9007b7cc0de9ba5878016ae0e0f84e805afb37
B3h3m0th <behemoth@autistici.org>
2016-04-01 11:53:48 +0200

    created a segment of static memory

--- representing_information/addressing_and_byte_ordering/endianess/check_for_endianess.c
+++ representing_information/addressing_and_byte_ordering/endianess/check_for_endianess.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char *bits(char ch);
+
+// Creazione di un array di memoria statica
+char vec[4] = {'a', 'b', 'c', 'd'};
+
 int main(void) {
     return(EXIT_SUCCESS);
 }
