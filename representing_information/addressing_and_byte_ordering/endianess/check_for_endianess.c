b40d81b98f53c1d092927b6b17fb884c1a675f2d
B3h3m0th <behemoth@autistici.org>
2016-04-01 12:06:38 +0200

    bits() function added2

--- representing_information/addressing_and_byte_ordering/endianess/check_for_endianess.c
+++ representing_information/addressing_and_byte_ordering/endianess/check_for_endianess.c
@@ -20,6 +20,26 @@ int main(void) {
 }
 
 
+char *bits(char ch) {
+    static char arr[9];
+    int i = (int) c;
+
+    if (!big_endian())
+        i = i >> 24;
+
+    arr[0] = ((c & 128) == 128) + '0';
+    arr[1] = ((c &  64) ==  64) + '0';
+    arr[2] = ((c &  32) ==  32) + '0';
+    arr[3] = ((c &  16) ==  16) + '0';
+    arr[4] = ((c &   8) ==   8) + '0';
+    arr[5] = ((c &   4) ==   4) + '0';
+    arr[6] = ((c &   2) ==   2) + '0';
+    arr[7] = ((c &   1) ==   1) + '0';
+    arr[8] = '\0';
+
+    return arr;
+}
+
 int big_endian(void) {
     long val = 1;
 
