85585e7433b19085d1a0a00e72d1754bd937cdbe
B3h3m0th <behemoth@autistici.org>
2016-04-03 18:34:34 +0200

    time header added

--- File_and_Directory/01_stat_2.c
+++ File_and_Directory/01_stat_2.c
@@ -2,25 +2,24 @@
 #include <stdlib.h>
 #include <dirent.h>
 #include <sys/stat.h>
+#include <time.h>
 
-/*
- Stampa le dimensioni di pathname
-*/
-int main(int argc, char *argv[])
-{
-   struct stat stbuf;
+/* Stampa le dimensioni di pathname
 
-   if (argc < 2) {
-      fprintf(stderr, "Uso: %s <pathname>\n", argv[0]);
-      exit(EXIT_FAILURE);
-   }
+int main(int argc, char *argv[]) {
+    struct stat stbuf;
+    
+    if (argc < 2) {
+        fprintf(stderr, "Uso: %s <pathname>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
 
-   if (stat(argv[1], &stbuf) == -1) {
-      fprintf(stderr, "Err.: fsize %s\n", argv[1]);
-      exit(EXIT_FAILURE);
-   }
+    if (stat(argv[1], &stbuf) == -1) {
+        fprintf(stderr, "Err.: fsize %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
 
-   printf("%8ld %s\n", stbuf.st_size, argv[1]);
+    printf("%8ld %s\n", stbuf.st_size, argv[1]);
 
-   exit(EXIT_SUCCESS);
+    exit(EXIT_SUCCESS);
 }
