4fe75b7ca66c53f8a9c20b2906d11fd94f99c406
B3h3m0th <behemoth@autistici.org>
2016-04-04 22:21:19 +0200

    replaced g_print() with gen_print()

--- the_language/preprocessor/GenericSelection.c
+++ the_language/preprocessor/GenericSelection.c
@@ -15,22 +15,22 @@ long double: "%Lf")             \
 
 // macro parametrica che stampa un valore di qualsiasi tipo di dato mediante
 // la funzione printf()
-#define g_print(p) printf(gen_fmt(p), p)
+#define gen_print(p) printf(gen_fmt(p), p)
 
 #define NL printf("\n")
 
 int main(void) {
     char c = 'H';   
-    g_print(c); NL;   
+    gen_print(c); NL;   
     
     int i = 100;
-    g_print(i); NL;
+    gen_print(i); NL;
     
     float f = 12.5f;
-    g_print(f); NL;
+    gen_print(f); NL;
     
     double d = 20.1234;
-    g_print(d); NL;    
+    gen_print(d); NL;    
 
     return (EXIT_SUCCESS);
 }
