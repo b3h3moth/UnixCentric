53e321e790de19167cd3ca062a2a99c35d91f193
B3h3m0th <behemoth@autistici.org>
2016-04-04 22:20:38 +0200

    fixed generic selection with macro

--- the_language/preprocessor/GenericSelection.c
+++ the_language/preprocessor/GenericSelection.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// definisce una macro parametrica che si espande in un'espressione generica che a seconda
+// macro parametrica che si espande in un'espressione generica che a seconda
 // del tipo di dato ritorna un determinato specificatore di formato
 #define gen_fmt(f) _Generic((f),\
 char: "%c",                     \
@@ -13,23 +13,23 @@ float:"%f",                     \
 double: "%f",                   \
 long double: "%Lf")             \
 
-// definisce una macro parametrica che consente di stampare con printf 
-// un valore di qualsiasi tipo di dato
+// macro parametrica che stampa un valore di qualsiasi tipo di dato mediante
+// la funzione printf()
 #define g_print(p) printf(gen_fmt(p), p)
 
 #define NL printf("\n")
 
 int main(void) {
-    char c = 'A';   
+    char c = 'H';   
     g_print(c); NL;   
     
-    int i = 10;
+    int i = 100;
     g_print(i); NL;
     
-    float f = 10.3f;
+    float f = 12.5f;
     g_print(f); NL;
     
-    double d = 10.3344;
+    double d = 20.1234;
     g_print(d); NL;    
 
     return (EXIT_SUCCESS);
