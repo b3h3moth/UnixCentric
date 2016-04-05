46dece2140922860afd255b4bf1cc24ebbc06513
B3h3m0th <behemoth@autistici.org>
2016-04-05 12:11:14 +0200

    how search_file works

--- the_language/input_output/full_programs/search_file.c
+++ the_language/input_output/full_programs/search_file.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Lo scopo del programma e' di cercare, mediante la funzione find_str(), 
+tutte le linee del file, e stamparle sullo stdout, in cui ci dovesse essere una
+corrispondenza con la stringa fornita alla funzione stessa */
+
 int main(void) {
     return(EXIT_SUCCESS);
 }
