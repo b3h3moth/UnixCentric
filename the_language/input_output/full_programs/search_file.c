51f51a646546c538c968079f5de63b0b95e4c613
B3h3m0th <behemoth@autistici.org>
2016-04-05 12:12:51 +0200

    find_str() prototype and MAX_LEN setted up to 256

--- the_language/input_output/full_programs/search_file.c
+++ the_language/input_output/full_programs/search_file.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAXLEN  256
+
+int find_str(FILE *fp, const char *str);
+
 /* Lo scopo del programma e' di cercare, mediante la funzione find_str(), 
 tutte le linee del file, e stamparle sullo stdout, in cui ci dovesse essere una
 corrispondenza con la stringa fornita alla funzione stessa */
@@ -8,3 +12,6 @@ corrispondenza con la stringa fornita alla funzione stessa */
 int main(void) {
     return(EXIT_SUCCESS);
 }
+
+int find_str(FILE *fp, const char *str) {
+
