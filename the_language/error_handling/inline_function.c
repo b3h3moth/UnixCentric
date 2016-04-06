297a508e133f76d7eef33d8843a17c75c7c811ea
B3h3m0th <behemoth@autistici.org>
2016-04-06 12:15:14 +0200

    err_msg() function to manage error handling is done

--- the_language/error_handling/inline_function.c
+++ the_language/error_handling/inline_function.c
@@ -4,6 +4,13 @@
 /* Lo scopo del programma e' di utilizzare una funzione inline per la gestione
 degli eventuali errori */
 
+static inline void err_msg(int status, const char *msg);
+
 int main(void) {
     return(EXIT_SUCCESS);
+}
+
+static inline void err_msg(int status, const char *msg) {
+    fputs(msg, stderr);
+    exit(status);
 }
