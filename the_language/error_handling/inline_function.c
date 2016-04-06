c980f457121bab687442a3a4f36e5e916e61a7cb
B3h3m0th <behemoth@autistici.org>
2016-04-06 12:01:37 +0200

    handling errors with static inline functions

---
+++ the_language/error_handling/inline_function.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Lo scopo del programma e' di utilizzare una funzione inline per la gestione
+degli eventuali errori */
+
+int main(void) {
+    return(EXIT_SUCCESS);
+}
