cfa117f2ee14ae58a77be5ef79ae779ca1e31f64
B3h3m0th <behemoth@autistici.org>
2016-04-07 20:00:20 +0200

    added '\0' to the end of the string

--- the_language/pointers/pointers_and_array/array_of_pointers/sort_array_of_pointers_to_strings.c
+++ the_language/pointers/pointers_and_array/array_of_pointers/sort_array_of_pointers_to_strings.c
@@ -13,11 +13,12 @@ int main(void) {
 
     fputs("Enter name (one per line), EOF to quit: ", stdout);
 
-    for (n = 0; fgets(buf[n], MAX_BUF, stdin) && n < MAX_BUF; n++) {
+    for (n = 0; fgets(buf[n], BUF_SIZE, stdin) && n < MAX_BUF; n++) {
+        buf[n][strlen(buf[n])-1] = '\0';
         pbuf[n] = buf[n];
     }
 
-    fputs("Unsorted strings", stdout);
+    fputs("\nUnsorted strings:\n", stdout);
     for (int i=0; i<n; i++)
         printf("%s\n", buf[i]);
 
