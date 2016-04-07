9fdd6a6a5b1840af407c93b9282da591d6a2d7ed
B3h3m0th <behemoth@autistici.org>
2016-04-07 19:30:59 +0200

    defined buffer as multidim. array, MAX_BUF,BUF_SIZE

--- the_language/pointers/pointers_and_array/array_of_pointers/sort_array_of_pointers_to_strings.c
+++ the_language/pointers/pointers_and_array/array_of_pointers/sort_array_of_pointers_to_strings.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+#define MAX_BUF     10  // Numero massimo di stringhe in input
+#define BUF_SIZE    10  // Larghezza massima della sringa
 
 int main(void) {
+    int i, n;
+    char buf[MAX_BUF][BUF_SIZE];
+
     return(EXIT_SUCCESS);
 }
