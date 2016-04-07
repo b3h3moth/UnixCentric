2842ebb75f36bf4fa37f085131c357e5760fe04d
B3h3m0th <behemoth@autistici.org>
2016-04-07 19:52:30 +0200

    print each unsorted string

--- the_language/pointers/pointers_and_array/array_of_pointers/sort_array_of_pointers_to_strings.c
+++ the_language/pointers/pointers_and_array/array_of_pointers/sort_array_of_pointers_to_strings.c
@@ -7,17 +7,19 @@
 #define BUF_SIZE    21  // Larghezza massima della sringa
 
 int main(void) {
-    int i;
+    int n, i;
     char buf[MAX_BUF][BUF_SIZE];
     char *pbuf[MAX_BUF];
 
     fputs("Enter name (one per line), EOF to quit: ", stdout);
 
-    for (int n = 0; fgets(buf[n], MAX_BUF, stdin) && n < MAX_BUF; n++) {
+    for (n = 0; fgets(buf[n], MAX_BUF, stdin) && n < MAX_BUF; n++) {
         pbuf[n] = buf[n];
     }
 
     fputs("Unsorted strings", stdout);
+    for (int i=0; i<n; i++)
+        printf("%s\n", buf[i]);
 
     return(EXIT_SUCCESS);
 }
