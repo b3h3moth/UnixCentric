d180d9e0b3499faab62ba22969e22197342d0391
B3h3m0th <behemoth@autistici.org>
2016-04-10 21:55:41 +0200

    six strings defined

--- the_language/pointers/pointers_and_array/differnce_between_pointers_to_array_and_array_to_pointers.c
+++ the_language/pointers/pointers_and_array/differnce_between_pointers_to_array_and_array_to_pointers.c
@@ -2,5 +2,11 @@
 #include <stdlib.h>
 
 int main(void) {
+    char stra[] = "C Language";
+    char strb[] = "C++ Language";
+    char strc[] = "Perl Language";
+    char strd[] = "Lisp Language";
+    char stre[] = "AWK Language";
+
     return(EXIT_SUCCESS);
 }
