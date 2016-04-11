20dfa74d6de4ba0f7feee59870934c81041e0d29
B3h3m0th <behemoth@autistici.org>
2016-04-11 20:33:15 +0200

    sorting array of strings with selection sort

---
+++ the_language/pointers/pointers_and_array/multimensional_array/sorting_strings.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+enum {WORD=10, SIZE=20};
+
+void selection_sort(char (*arr)[SIZE]);
+
+int main() {
+    char strings[WORD][SIZE]={ 
+        {"Rob Pike"},
+        {"Steve Bourne"},
+        {"Richard Stallman"},
+        {"Theo de Raadt"},
+        {"Dennis Ritchie"},
+        {"Kirk Mckusick"},
+        {"Brian Kernighan"}, 
+        {"Ken Thopmson"},
+        {"Larry Wall"},
+        {"Bill Shannon"}
+    };
+    
+    puts("Unsorter order");
+    puts("--------------");
+   
+    for(int i=0; i<WORD; i++)
+        printf("%s\n", strings[i]);
+
+    puts("\n_sorted order");
+    puts("------------");
+   
+    selection_sort(strings);
+   
+    for(int i=0; i<WORD; i++)
+        printf("%s\n",strings[i]);
+
+    exit(EXIT_SUCCESS);
+}
+
+void selection_sort(char (*arr)[SIZE]) {
+    int n=WORD, minpos;
+    char tmp[SIZE];
+    
+    for (int i=0; i<n-1; i++) {
+        minpos = i;
+        
+        for (int j=i+1; j<n; j++)
+            if ((strcmp(arr[j], arr[minpos])) < 0)
+                minpos = j;
+        
+        if (minpos != i) {
+            strcpy(tmp, arr[i]);
+            strcpy(arr[i], arr[minpos]);
+            strcpy(arr[minpos], tmp);
+        }
+    }
+}
