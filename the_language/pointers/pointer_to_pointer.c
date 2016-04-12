725d13b2d75cb134017563d74a4036c98975fed2
B3h3m0th <behemoth@autistici.org>
2016-04-12 20:38:22 +0200

    fixed all filenames

--- the_language/pointers/pointer_to_pointer.c
+++ the_language/pointers/pointer_to_pointer.c
@@ -11,18 +11,18 @@ int main(void) {
         "extern",
     };
 
-    char *str1 = keywords[0];
-    char *str2 = keywords[1];
-    char *str3 = keywords[2];
-    char *str4 = keywords[3];
-    char *str5 = keywords[4];
-    char *str6 = keywords[5];
+    char *pkwords[6] = { 
+        keywords[0], keywords[1], keywords[2], 
+        keywords[3], keywords[4], keywords[5]
+    };
 
-    char *ptr[] = {str1, str2, str3, str4, str5, str6 };
-    char **ptp = &ptr[0];
 
-    for (int i=0; i<sizeof(ptr)/sizeof(ptr[0]); i++)
-        printf("%s\n", ptp[i]);
+    char *ppk[] = {pkwords[0], pkwords[1], pkwords[2], pkwords[3], pkwords[4], pkwords[5] };
+
+    char **ptppk = ppk;
+
+    for (int i=0; i<sizeof(ppk)/sizeof(ppk[0]); i++)
+        printf("%s\n", ptppk[i]);
 
     return(EXIT_SUCCESS);
 }
