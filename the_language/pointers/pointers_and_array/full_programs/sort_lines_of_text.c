9f76f95ed8030c2e76c7844c49140570a8f8f68e
B3h3m0th <behemoth@autistici.org>
2016-04-13 19:45:09 +0200

    invoke get_line() to store buffer

--- the_language/pointers/pointers_and_array/full_programs/sort_lines_of_text.c
+++ the_language/pointers/pointers_and_array/full_programs/sort_lines_of_text.c
@@ -4,9 +4,14 @@
 
 enum { MAX_LINE = 1000 };
 
+char *pbuf[MAX_LINE];
+
 char get_line(void);
 int compare_strings(const void * str1, const void *str2);
 
 int main(void) {
+    for (int n=0; n < MAX_LINE && (pbuf[n] = get_line()) != NULL; ++n)
+        ;
+
     return(EXIT_SUCCESS);
 }
