4937dc789e89d4a9e92b24baf762b943438e50cf
B3h3m0th <behemoth@autistici.org>
2016-04-13 20:33:54 +0200

    definitions of get_line() and compare_strings()

--- the_language/pointers/pointers_and_array/full_programs/sort_lines_of_text.c
+++ the_language/pointers/pointers_and_array/full_programs/sort_lines_of_text.c
@@ -4,14 +4,38 @@
 
 enum { MAX_LINE = 1000 };
 
-char *pbuf[MAX_LINE];
+char *pline[MAX_LINE];
 
-char get_line(void);
-int compare_strings(const void * str1, const void *str2);
+char *get_line(void);
+int compare_strings(const void *str1, const void *str2);
 
 int main(void) {
-    for (int n=0; n < MAX_LINE && (pbuf[n] = get_line()) != NULL; ++n)
+    int n = 0;
+
+    for ( ; n < MAX_LINE && (pline[n] = get_line()) != NULL; ++n)
         ;
 
     return(EXIT_SUCCESS);
+}
+
+char *get_line(void) {
+    char buf[MAX_LINE];
+    *pline = NULL;
+
+    if (fgets(buf, MAX_LINE, stdin) != NULL) {
+        size_t len = strlen(buf);
+
+        if (buf[len-1] == '\n')
+            buf[len-1] = '\0';
+        else
+            ++len;
+
+        if ((pline = malloc(len)) != NULL)
+            strcpy(pline, buf);
+    }
+    return pline;
+}
+
+int compare_strings(const void *str1, const void *str2) {
+    return strcmp(*(char **)str1, *(char **)str2);
 }
