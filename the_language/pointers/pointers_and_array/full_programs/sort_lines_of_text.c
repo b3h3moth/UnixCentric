dc3cbeb8c7b6090273e105d7895e5a811c7c4664
B3h3m0th <behemoth@autistici.org>
2016-04-13 19:41:14 +0200

    get_line() and compare_strings() function prototypes added

--- the_language/pointers/pointers_and_array/full_programs/sort_lines_of_text.c
+++ the_language/pointers/pointers_and_array/full_programs/sort_lines_of_text.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+char get_line(void);
+int compare_strings(const void * str1, const void *str2);
 
 int main(void) {
     return(EXIT_SUCCESS);
