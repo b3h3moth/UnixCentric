b3fbc06df9209b27b2a0bb72343a9184ac2abde8
B3h3m0th <behemoth@autistici.org>
2016-04-15 11:40:43 +0200

    memset() and malloc() intsed of calloc(), How does it work.

---
+++ the_language/memory_allocation/mem_family_functions/memset_and_malloc_insted_calloc.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Lo scopo del sorgente e' di simulare la funzione calloc() mediante l'ausilio
+delle funzioni calloc() e memset(). Per simulazione s'intende l'ottenimento del
+medesimo risultato finale e non la realizzazione interna del programma.  */
+
+int main(void) {
+    return(EXIT_SUCCESS);
+}
