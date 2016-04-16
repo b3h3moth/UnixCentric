2f0a5e58ae7b191277c97baba3ac1fdc2676334c
B3h3m0th <behemoth@autistici.org>
2016-04-16 19:27:12 +0200

    how string is before call memset()

--- the_language/memory_allocation/mem_family_functions/how_memset_memmove_memcpy_work.c
+++ the_language/memory_allocation/mem_family_functions/how_memset_memmove_memcpy_work.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+enum { MAX_BUF = 50 };
+
 int main(void) {
+    char string1[MAX_BUF] = "Take a walk on the wild side";
+    char string2[MAX_BUF] = "qwertyuiopasdfghjklzxcvbnm";
+    char buf[MAX_BUF];
+
+    puts("- before memset()");
+    printf(string1);
+
+
     return(EXIT_SUCCESS);
 }
