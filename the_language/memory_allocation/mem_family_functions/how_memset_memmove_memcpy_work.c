ca929c9b99ef8e0411315cdc8f0aaf08fb489c20
B3h3m0th <behemoth@autistici.org>
2016-04-17 17:54:05 +0200

    print out memset() results

--- the_language/memory_allocation/mem_family_functions/how_memset_memmove_memcpy_work.c
+++ the_language/memory_allocation/mem_family_functions/how_memset_memmove_memcpy_work.c
@@ -6,12 +6,21 @@ enum { MAX_BUF = 50 };
 
 int main(void) {
     char string1[MAX_BUF] = "Take a walk on the wild side";
-    char string2[MAX_BUF] = "qwertyuiopasdfghjklzxcvbnm";
+    char string2[MAX_BUF] = "Rastaman vibration positive";
     char buf[MAX_BUF];
 
     printf("%7s %s\n", "Before:", string1);
     memset(string1 + 5, 'X', 10);
     printf("%7s %s\n", "After:",string1);
+
+    strcpy(buf, string2);
+    printf("%7s %s\n", "Before:", buf);
+    memset(string1 + 5, 'X', 10);
+    memcpy(buf + 4, buf + 16, 10);
+    printf("%7s %s\n", "After:", buf);
+    memset(string1 + 5, 'X', 10);
+    memcpy(buf + 4, buf + 16, 10);
+    printf("%7s %s\n", "After:", buf);
 
     return(EXIT_SUCCESS);
 }
