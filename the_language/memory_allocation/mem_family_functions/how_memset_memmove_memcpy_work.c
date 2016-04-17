bb4eae480e7558ada252a6e8e4f17040cbe898e7
B3h3m0th <behemoth@autistici.org>
2016-04-17 18:04:06 +0200

    testing both memset() and memcpy()

--- the_language/memory_allocation/mem_family_functions/how_memset_memmove_memcpy_work.c
+++ the_language/memory_allocation/mem_family_functions/how_memset_memmove_memcpy_work.c
@@ -2,24 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 
-enum { MAX_BUF = 50 };
+enum { OFFSET = 3, MAX_BUF = 50 };
 
 int main(void) {
     char string1[MAX_BUF] = "Take a walk on the wild side";
     char string2[MAX_BUF] = "Rastaman vibration positive";
     char buf[MAX_BUF];
 
+    // Testing memset()
     printf("%7s %s\n", "Before:", string1);
-    memset(string1 + 5, 'X', 10);
+    memset(string1 + OFFSET, 'X', 10);
     printf("%7s %s\n", "After:",string1);
 
+    // Testing memcpy()
     strcpy(buf, string2);
     printf("%7s %s\n", "Before:", buf);
-    memset(string1 + 5, 'X', 10);
-    memcpy(buf + 4, buf + 16, 10);
+    memcpy(buf + 2, buf + 12, 10);
     printf("%7s %s\n", "After:", buf);
-    memset(string1 + 5, 'X', 10);
-    memcpy(buf + 4, buf + 16, 10);
+    memcpy(buf + 2, buf + 12, 10);
     printf("%7s %s\n", "After:", buf);
 
     return(EXIT_SUCCESS);
