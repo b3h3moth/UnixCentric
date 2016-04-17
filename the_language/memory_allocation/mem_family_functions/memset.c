c5576ed0b0eb22acea1d8786786f9c68888d1ff4
B3h3m0th <behemoth@autistici.org>
2016-04-17 19:26:49 +0200

    how memset() works

--- the_language/memory_allocation/mem_family_functions/memset.c
+++ the_language/memory_allocation/mem_family_functions/memset.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+void stampa(char *s) {
 
 int main(void) {
-    return(EXIT_SUCCESS);
+   char stringa[] = "UNIX Programming with POSIX";
+
+   /* La memset puo' essere utilizzata, ad esempio, per azzerare un array, 
+   tuttavia c'e' una sottile considerazione da porre in essere: 
+   se si utilizza il numero 0 la stringa sara' effettivamente azzerata, se 
+   invece si utilizza il carattere 0 l'array sara' riempito di tanti zeri. */
+   int n_zero = 0;
+   char c_zero = '0';
+   int size = strlen(stringa);
+
+   /* Azzero l'array */
+   memset(stringa, c_zero, size);
+   stampa(stringa);
+
+   /* Inserisco per ogni byte uno zero */
+   memset(stringa, n_zero, size);
+   stampa(stringa);
+
+   return(EXIT_SUCCESS);
+}
+
+void stampa(char *s) {
+   printf("output: %s\n", s);
 }
