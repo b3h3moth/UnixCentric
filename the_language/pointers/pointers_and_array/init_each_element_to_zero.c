ac9010da763666b2f29790826b74af0cb5fd3990
B3h3m0th <behemoth@autistici.org>
2016-04-18 17:56:37 +0200

    for loop to init each array element

--- the_language/pointers/pointers_and_array/init_each_element_to_zero.c
+++ the_language/pointers/pointers_and_array/init_each_element_to_zero.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+enum { SIZE = 10 };
+
 int main(void) {
+    double arr[SIZE];
+    double *ap;
+
+    for (ap = &arr[0]; ap < &arr[SIZE]; ap++) {
+        *ap = 1;
+        printf("%g\n", *ap);
+    }
+
+
     return(EXIT_SUCCESS);
 }
