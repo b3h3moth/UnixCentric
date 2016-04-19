10c02a73f2565792a804ba4800fc3d33bcc6084c
B3h3m0th <behemoth@autistici.org>
2016-04-19 17:10:33 +0200

    find_occurence() prototype added

--- the_language/pointers/full_programs/find_first_occurence.c
+++ the_language/pointers/full_programs/find_first_occurence.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char *find_occurence(char const *source, char const *str);
+
 int main(void) {
     return(EXIT_SUCCESS);
 }
+
+char *find_occurence(char const *source, char const *str) {
