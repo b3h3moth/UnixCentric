2f803d9a7c4daf30ea1870c26d61f3e8283b4747
B3h3m0th <behemoth@autistici.org>
2016-04-19 17:23:07 +0200

    find_occurence() definition added

--- the_language/pointers/full_programs/find_first_occurence.c
+++ the_language/pointers/full_programs/find_first_occurence.c
@@ -12,3 +12,18 @@ int main(void) {
 di ciascun carattere della stringa 'str' nella stringa 'source'. 
 Restituisce un puntatore al primo caratere trovato, altrimenti NULL. */
 char *find_occurence(char const *source, char const *str) {
+    char *tmp;
+
+    if (source != NULL && str != NULL) {
+        // Verifica 'source' un carattere alla volta
+        for ( ; *source != '\0'; source++) {
+            // Confronta ciascun carattere di 'source' con ciascun carattere 
+            // di 'src'
+            for (tmp = str; *tmp != '\0'; tmp++)
+                if (*source == *tmp)
+                    return source;
+        }
+    }
+
+    return NULL;
+}
