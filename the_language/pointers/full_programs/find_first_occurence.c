3b2e24be98e78d6689f19f04d2304d87a411b738
B3h3m0th <behemoth@autistici.org>
2016-04-19 17:50:16 +0200

    testing find_occurence()

--- the_language/pointers/full_programs/find_first_occurence.c
+++ the_language/pointers/full_programs/find_first_occurence.c
@@ -5,6 +5,12 @@
 char *find_occurence(char const *source, char const *str);
 
 int main(void) {
+    char *buf = "take a walk on the wild side, lou reed";
+    char *str = "hi";
+
+    char *temp = find_occurence(buf, str);
+    printf("%s\n", temp);
+
     return(EXIT_SUCCESS);
 }
 
@@ -14,16 +20,11 @@ Restituisce un puntatore al primo caratere trovato, altrimenti NULL. */
 char *find_occurence(char const *source, char const *str) {
     char *tmp;
 
-    if (source != NULL && str != NULL) {
-        // Verifica 'source' un carattere alla volta
-        for ( ; *source != '\0'; source++) {
-            // Confronta ciascun carattere di 'source' con ciascun carattere 
-            // di 'src'
+    if (source != NULL && str != NULL)
+        for ( ; *source != '\0'; source++)
             for (tmp = str; *tmp != '\0'; tmp++)
                 if (*source == *tmp)
                     return source;
-        }
-    }
 
     return NULL;
 }
