0d0f6825b786976a2977ad02624b185832407305
B3h3m0th <behemoth@autistici.org>
2016-04-19 17:14:11 +0200

    how find_occurence() works

--- the_language/pointers/full_programs/find_first_occurence.c
+++ the_language/pointers/full_programs/find_first_occurence.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Function  prototype
 char *find_occurence(char const *source, char const *str);
 
 int main(void) {
     return(EXIT_SUCCESS);
 }
 
+/* Lo scopo della funzione find_occurence() e' di trovare la prima occorrenza
+di ciascun carattere della stringa 'str' nella stringa 'source'. 
+Restituisce un puntatore al primo caratere trovato, altrimenti NULL. */
 char *find_occurence(char const *source, char const *str) {
