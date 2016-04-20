6ad14c267702b6063d4d4774ea0357d5ca64a33d
B3h3m0th <behemoth@autistici.org>
2016-04-20 12:11:52 +0200

    removed unused array of pointers

--- the_language/pointers/pointer_to_pointer.c
+++ the_language/pointers/pointer_to_pointer.c
@@ -8,21 +8,17 @@ int main(void) {
         "Open Solaris",
         "AIX",
         "GNU/Linux Os",
-        "",
+        "HP/UX",
     };
 
-    char *pkwords[6] = { 
-        unixos[0], unixos[1], unixos[2], 
-        unixos[3], unixos[4], unixos[5]
+    char *arr_to_ptrs[6] = { 
+        unixos[0], unixos[1], unixos[2], unixos[3], unixos[4], unixos[5]
     };
 
+    char **pptr = arr_to_ptrs;
 
-    char *ppk[] = {pkwords[0], pkwords[1], pkwords[2], pkwords[3], pkwords[4], pkwords[5] };
-
-    char **ptppk = ppk;
-
-    for (int i=0; i<sizeof(ppk)/sizeof(ppk[0]); i++)
-        printf("%s\n", ptppk[i]);
+    for (int i=0; i<sizeof(arr_to_ptrs)/sizeof(arr_to_ptrs[0]); i++)
+        printf("%s\n", pptr[i]);
 
     return(EXIT_SUCCESS);
 }
