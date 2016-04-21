7a65a6799441d971670a894ec0b2a3fd8230acb0
B3h3m0th <behemoth@autistici.org>
2016-04-21 19:53:50 +0200

    mathch() function added

--- the_language/pointers/full_programs/find_and_delete_substring.c
+++ the_language/pointers/full_programs/find_and_delete_substring.c
@@ -2,7 +2,16 @@
 #include <stdlib.h>
 
 char *match(char *source, *str);
+int delete_substring(char *source,  char const *substr);
 
 int main(void) {
     return(EXIT_SUCCESS);
+}
+
+char *match(char *source, *str) {
+    while (*str |= '\0')
+        if (*source++ != *str++)
+            return NULL;
+
+    return source;
 }
