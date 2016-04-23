1f497b1b910948b3fe4b52a0ac1e90ba8a43ca54
B3h3m0th <behemoth@autistici.org>
2016-04-23 18:45:11 +0200

    how program really works

--- the_language/pointers/pointer_to_pointer.c
+++ the_language/pointers/pointer_to_pointer.c
@@ -14,7 +14,8 @@ int main(void) {
     };
 
     /* Array di puntatori a puntatori a char contenenti solo le voci di OS
-    UNIX di tipo BSD */
+    UNIX di tipo BSD, o meglio, gli indirizzi alle voci, in modo tale da non
+    occupare spazio indesiderato.  */
     char **bsd_os[4] = { &unixos[0], &unixos[1] };
     bsd_os[2] = &unixos[3];
     bsd_os[3] = &unixos[7];
