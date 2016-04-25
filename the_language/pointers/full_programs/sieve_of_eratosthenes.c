7a2ed00576b7af83ce401eddb2780b5723ac968c
B3h3m0th <behemoth@autistici.org>
2016-04-25 18:59:25 +0200

    The program's purpose

--- the_language/pointers/full_programs/sieve_of_eratosthenes.c
+++ the_language/pointers/full_programs/sieve_of_eratosthenes.c
@@ -2,6 +2,16 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+#define SIZE 100
+
+/* Lo scopo del programma e' la realizzazione dell'algoritmo detto il crivello
+di Eratostene con numeri primi. */
+
 int main(void) {
+    char sieve[SIZE];
+    char *sp;
+    int num;
+
+    // setta il crivello di 
     return(EXIT_SUCCESS);
 }
