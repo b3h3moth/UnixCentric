ae908a3a1058aa1414ec3457bfcfcc5382d20986
B3h3m0th <behemoth@autistici.org>
2016-04-25 19:04:49 +0200

    set each sieve's element to 1

--- the_language/pointers/full_programs/sieve_of_eratosthenes.c
+++ the_language/pointers/full_programs/sieve_of_eratosthenes.c
@@ -5,13 +5,15 @@
 #define SIZE 100
 
 /* Lo scopo del programma e' la realizzazione dell'algoritmo detto il crivello
-di Eratostene con numeri primi. */
+di Eratostene per il calcolo di numeri primi. */
 
 int main(void) {
     char sieve[SIZE];
     char *sp;
     int num;
 
-    // setta il crivello di 
+    // setta ciascun elemento del crivello a 1
+    for (sp = sieve; sp < SIZE; sp++) 
+        *sp = 1;
     return(EXIT_SUCCESS);
 }
