f268f2c80a989153d0cbb1f2551e1b9f9a14b9a7
B3h3m0th <behemoth@autistici.org>
2016-04-29 20:28:55 +0200

    delcaring a pointer to the first element of the array

--- the_language/pointers/pointers_and_array/subscripts_and_indirection.c
+++ the_language/pointers/pointers_and_array/subscripts_and_indirection.c
@@ -4,6 +4,14 @@
 #define SIZE 10
 
 int main(void) {
-    char array[SIZE] = {0,1,2,3,4,5,6,7,8,9};
+    int arr[SIZE] = {0,1,2,3,4,5,6,7,8,9};
+    int *ap;
+
+    // 'arr' e' un puntatore costante al primo elemento dell'array stesso.
+    
+    // Il puntatore ap punta al primo elemento dell'array 'arr'
+    ap = arr;
+
+    // Se 'arr' e' un puntatore e' possibile
     return(EXIT_SUCCESS);
 }
