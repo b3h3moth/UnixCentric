03939b043bded73fd5b9890318a1f7cdaf271046
B3h3m0th <behemoth@autistici.org>
2016-05-01 20:48:34 +0200

    data structure array declared as extern

--- interfaces/ADT_black_boxes/addresslist.c
+++ interfaces/ADT_black_boxes/addresslist.c
@@ -1,10 +1,6 @@
 #include <string.h>
 #include "addresslist.h"
 
-static char name[MAX_ADDR][NAME_LEN];
-static char address[MAX_ADDR][ADDR_LEN];
-static char mail[MAX_ADDR][MAIL_LEN];
-
 static int find_entry(char const *str) {
     int entry;
 
--- interfaces/ADT_black_boxes/addresslist.h
+++ interfaces/ADT_black_boxes/addresslist.h
@@ -4,7 +4,11 @@
 #define NAME_LEN    20      // Longest name
 #define ADDR_LEN    80      // Longest address
 #define MAIL_LEN    30      // Longest e-mail
-#define MAX_ADDR    10    // Max addresses allowed
+#define MAX_ADDR    5    // Max addresses allowed
+
+extern char name[MAX_ADDR][NAME_LEN];
+extern char address[MAX_ADDR][ADDR_LEN];
+extern char mail[MAX_ADDR][MAIL_LEN];
 
 char const *lookup_addr(char const *name);
 char const *lookup_mail(char const *name);
