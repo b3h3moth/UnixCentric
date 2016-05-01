281cf8a917569b7aa2a62e1eb36c4663d98ed893
B3h3m0th <behemoth@autistici.org>
2016-05-01 20:48:55 +0200

    redefined main() function within main.c file

---
+++ interfaces/ADT_black_boxes/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "addresslist.h"
+
+char name[MAX_ADDR][NAME_LEN] = {
+    "luca",
+    "maria",
+    "giovanna",
+    "carla",
+    "francesa"
+};
+char address[MAX_ADDR][ADDR_LEN];
+char mail[MAX_ADDR][MAIL_LEN];
+
+int main(void) {
+    return(EXIT_SUCCESS);
+}
