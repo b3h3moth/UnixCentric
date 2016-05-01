020aadb489ea74d66c7b4be938076cdac1224e53
B3h3m0th <behemoth@autistici.org>
2016-05-01 20:24:16 +0200

    for now main() removed

--- interfaces/ADT_black_boxes/addresslist.h
+++ interfaces/ADT_black_boxes/addresslist.h
@@ -4,7 +4,7 @@
 #define NAME_LEN    20      // Longest name
 #define ADDR_LEN    80      // Longest address
 #define MAIL_LEN    30      // Longest e-mail
-#define MAX_ADDR    500    // Max addresses allowed
+#define MAX_ADDR    10    // Max addresses allowed
 
 char const *lookup_addr(char const *name);
 char const *lookup_mail(char const *name);
--- interfaces/ADT_black_boxes/main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "addresslist.h"
-
-int main(void) {
-    return(EXIT_SUCCESS);
-}
