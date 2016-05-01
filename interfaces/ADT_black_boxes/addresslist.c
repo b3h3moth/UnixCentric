bbc683990d30342cb171306a658e9237866e9a57
B3h3m0th <behemoth@autistici.org>
2016-05-01 19:59:03 +0200

    static multidim. array for each name, address, mail

--- interfaces/ADT_black_boxes/addresslist.c
+++ interfaces/ADT_black_boxes/addresslist.c
@@ -1,3 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "addresslist.h"
+
+static char name[MAX_ADDR][NAME_LEN];
+static char address[MAX_ADDR][ADDR_LEN];
+static char mail[MAX_ADDR][MAIL_LEN];
