7b4ccb14a083147c411ee72f710cc333022e64e4
B3h3m0th <behemoth@autistici.org>
2016-05-01 20:02:43 +0200

    find_entry() static function implementation added

--- interfaces/ADT_black_boxes/addresslist.c
+++ interfaces/ADT_black_boxes/addresslist.c
@@ -5,3 +5,13 @@
 static char name[MAX_ADDR][NAME_LEN];
 static char address[MAX_ADDR][ADDR_LEN];
 static char mail[MAX_ADDR][MAIL_LEN];
+
+static int find_entry(char const *str) {
+    int entry;
+
+    for (entry=0; entry<MAX_ADDR; entry++)
+        if (strcmp(str, name[entry]) == 0)
+            return entry;
+
+    return(EXIT_FAILURE);
+}
