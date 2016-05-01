3fbdcb347461e82b46391d67ad40f8b27fdfd140
B3h3m0th <behemoth@autistici.org>
2016-05-01 19:55:32 +0200

    replaced phone with e-mail

--- interfaces/ADT_black_boxes/addresslist.h
+++ interfaces/ADT_black_boxes/addresslist.h
@@ -3,10 +3,10 @@
 
 #define NAME_LEN    20      // Longest name
 #define ADDR_LEN    80      // Longest address
-#define PHONE_LEN   11      // Longest phone
+#define MAIL_LEN    30      // Longest phone
 #define MAX_ADDR    1000    // Max addresses allowed
 
 char const *lookup_addr(char const *name);
-char const *lookup_phone(char const *phone);
+char const *lookup_phone(char const *);
 
 #endif
