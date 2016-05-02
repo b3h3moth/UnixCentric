0b88710e83a3acb254ce4d18b6e4cd684b5f3772
B3h3m0th <behemoth@autistici.org>
2016-05-02 20:35:09 +0200

    increased matrix rows x cols, fixed for loop and pointer to array declaratin

--- the_language/pointers/pointers_and_array/multimensional_array/get_only_row_elements.c
+++ the_language/pointers/pointers_and_array/multimensional_array/get_only_row_elements.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Lo scopo del programma e' di stampare a video solo il primo elemento di
+ciascuna riga della matrice, utilizzando solo i puntatori */
+
 int main(void) {
-    int matrix[2][2] = { 
-        {10,20},
-        {30,40}
+    int matrix[4][4] = { 
+        {10,  20,  30,   40},
+        {50,  60,  70,   80},
+        {90,  100, 110, 120},
+        {130, 140, 150, 160}
     };
 
     // Dichiarazione di un puntatore ad array
-    int (*mp)[2];
+    int (*mp)[4];
 
-    for (mp = matrix; mp<&matrix[2]; mp++)
-        printf("%d\n", (*mp)[0]);
+    for (mp = matrix; mp<&matrix[4]; mp++)
+        printf("%d\n", *(*mp) );
 
     return(EXIT_SUCCESS);
 }
