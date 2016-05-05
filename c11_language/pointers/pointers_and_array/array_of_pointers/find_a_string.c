456eacba254ea75a67ee2151cde0e9b997e22832
B3h3m0th <behemoth@autistici.org>
2016-05-05 18:18:29 +0200

    lookup_os() default returns value  fixed to -1, testing function from main()

--- c11_language/pointers/pointers_and_array/array_of_pointers/find_a_string.c
+++ c11_language/pointers/pointers_and_array/array_of_pointers/find_a_string.c
@@ -15,7 +15,12 @@ int main(void) {
         NULL
     };
 
-    lookup_os("OpenBSD", unixos, 6);
+    int res = lookup_os("solaris", unixos, 6);
+
+    if (res != -1)
+        printf("%s\n", unixos[res]);
+    else
+        puts("string doesn't find");
     
     return(EXIT_SUCCESS);
 }
@@ -23,10 +28,10 @@ int main(void) {
 int lookup_os(char const *const osname, char const *oslist[], int const size) {
     char const **osp;
 
-    for (osp = oslist; osp != NULL; osp++) {
+    for (osp = oslist; osp < oslist + size; osp++) {
         if (strcmp(osname, *osp) == 0)
             return osp - oslist;
     }
 
-    return -11;
+    return(-1);
 }
