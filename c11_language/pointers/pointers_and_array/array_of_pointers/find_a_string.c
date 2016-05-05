12a83aea2152d7efcf203d5993faa1c23ae5e763
B3h3m0th <behemoth@autistici.org>
2016-05-05 18:11:06 +0200

    lookup_os() function implementation added

--- c11_language/pointers/pointers_and_array/array_of_pointers/find_a_string.c
+++ c11_language/pointers/pointers_and_array/array_of_pointers/find_a_string.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int lookup_os(char const *const osname, char const *oslist[], int const size);
 
@@ -10,10 +11,22 @@ int main(void) {
         "Debian",
         "Gentoo",
         "Arch",
-        "Solaris"
+        "Solaris",
+        NULL
     };
+
+    lookup_os("OpenBSD", unixos, 6);
     
     return(EXIT_SUCCESS);
 }
 
 int lookup_os(char const *const osname, char const *oslist[], int const size) {
+    char const **osp;
+
+    for (osp = oslist; osp != NULL; osp++) {
+        if (strcmp(osname, *osp) == 0)
+            return osp - oslist;
+    }
+
+    return -11;
+}
