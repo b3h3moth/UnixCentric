91b78103e39d65d59b4cf994783d9c036875c1c7
B3h3m0th <behemoth@autistici.org>
2016-05-07 16:26:19 +0200

    copy string both calloc() + strcpy() and with pointer addresses

--- c11_language/pointers/pointers_and_array/array_of_pointers/copy_strings.c
+++ c11_language/pointers/pointers_and_array/array_of_pointers/copy_strings.c
@@ -1,22 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(void) {
     char *names[] = {
         "francesca",
         "claudia",
-        "giovanna",
+        "xxx",
         "isabella",
-        "x",
+        "xxx",
         "deborah",
         NULL
     };
 
     char name[] = "rossella";
 
-    /* Il modo migliore per copiare una stringa allorquando si lavora con
-    array of pointers e' di salvare semplicemente il puntatore della nuova
-    stringa */
+    /* Per copiare la stringa 'name' - tecnicamente e' unarray di caratteri -
+    all' interno di un campo 'names' si puo' procedere mediante l'allocazione
+    dinamica della memoria, nello specifico con la funzione calloc() che
+    consente anche l'inizializzazione a 0 di ciascun elemento. */
+    names[2] = calloc(strlen(name)+1, sizeof(char));
+    strcpy(names[2], name);
+
+    /* Tuttavia il modo migliore per copiare una stringa, allorquando si lavora
+    con array of puntatori, e' di salvare semplicemente il puntatore della 
+    nuova stringa. */
     names[4] = &name[0];
 
     return(EXIT_SUCCESS);
