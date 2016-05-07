a140a5a4089f33793ce59f37707bf4f3edc2ac58
B3h3m0th <behemoth@autistici.org>
2016-05-07 18:00:29 +0200

    **first and **last variables added

--- c11_language/pointers/full_programs/palindrome.c
+++ c11_language/pointers/full_programs/palindrome.c
@@ -2,5 +2,16 @@
 #include <stdlib.h>
 
 int main(void) {
+    char **first, **last;
+
+    char *strings[] = {
+        "aaabbbcccbbbaaa",
+        "abababbababa",
+        "qwertyaggagaagytrewq",
+        "assaassaassasa",
+    };
+
+    first = strings;
+
     return(EXIT_SUCCESS);
 }
