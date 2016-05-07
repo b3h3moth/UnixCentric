de7690becb7577b47fdda8fdf9a338ecf1f0c7e8
B3h3m0th <behemoth@autistici.org>
2016-05-07 18:06:00 +0200

    macro AP_SIZE to get size of strings

--- c11_language/pointers/full_programs/palindrome.c
+++ c11_language/pointers/full_programs/palindrome.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define AP_SIZE ( sizeof(strings) / sizeof(strings[0]) )
+
 int main(void) {
     char **first, **last;
 
@@ -12,6 +14,9 @@ int main(void) {
     };
 
     first = strings;
+
+    for (int i=0; i<AP_SIZE; i++) {
+    }
 
     return(EXIT_SUCCESS);
 }
