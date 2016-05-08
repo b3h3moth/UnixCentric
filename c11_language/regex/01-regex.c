149690bd65cd8d6527a89590450026cee9268ce1
B3h3m0th <behemoth@autistici.org>
2016-05-08 16:25:46 +0200

    bug fixes into status returns

--- c11_language/regex/01-regex.c
+++ c11_language/regex/01-regex.c
@@ -2,43 +2,42 @@
 #include <stdlib.h>
 #include <regex.h>
 
-int regex_match (char *pattern, const char *default_string);
+int regex_match(char *pattern, const char *default_string);
 
-int main(int argc, char *argv[])
-{
+/* Lo scopo del programma e' l'individuazione di un pattern all'interno di una
+stringa. */
+
+int main(void) {
     char *default_string = "C Language Programming";
-    char *pattern = "guag";
+    char *pattern = "gra";
     
-    if (regex_match (pattern, default_string))
-		printf ("La ricerca del pattern \'%s\' ha avuto successo; "
-				"trovata corrispondenza nella stringa \'%s\'\n", pattern, default_string);
+    if (regex_match(pattern, default_string) == 0)
+		printf ("Pattern \'%s\' found into the string \'%s\'",  \
+                pattern, default_string);
 	else
-		printf ("La ricerca del pattern \'%s\' non ha avuto successo; "
-				"nessuna corrispondenza nella stringa \'%s\'\n",
-				pattern, default_string);
+		printf ("Pattern \'%s\' not found into the string \'%s\'",  \
+                pattern, default_string);
 
     return(EXIT_SUCCESS);
 }
 
-int regex_match (char *pattern, const char *default_string)
-{
+int regex_match(char *pattern, const char *default_string) {
     int status;
     regex_t preg;
 
-    status = regcomp (&preg, pattern, REG_EXTENDED|REG_NOSUB);
+    status = regcomp(&preg, pattern, REG_EXTENDED | REG_NOSUB);
     
 	if (status != 0)
-        return (EXIT_FAILURE);
+        return(EXIT_FAILURE);
     
 	status = regexec(&preg, default_string, (size_t) 0, NULL, 0);
     
-	if (status != 0)
-      {
+	if (status != 0) {
         regfree (&preg);
-        return (EXIT_FAILURE);
-      }
+        return(EXIT_FAILURE);
+    }
 
-    regfree (&preg);
-    
-	return(EXIT_SUCCESS);
+    regfree(&preg);
+
+    return status;
 }
