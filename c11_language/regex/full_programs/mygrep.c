3316d998e972009e3d6c058032084eb2bac8bf4a
B3h3m0th <behemoth@autistici.org>
2016-05-09 12:26:16 +0200

    matchere() implementation and comments about its job

--- c11_language/regex/full_programs/mygrep.c
+++ c11_language/regex/full_programs/mygrep.c
@@ -10,4 +10,16 @@ int main(void) {
 
 // Determina se una stringa di testo soddisfa una regular expression
 int match(char *regexp, char *text) {
+    /* Se la regex inizia con ^, la parte iniziale del testo deve soddisfare
+       la restante parte dell'espressione. */
+    if (regexp[0] == '^')
+        return matchere(regexp + 1, text);
 
+    // Cerca se la stringa e' vuota
+    do {
+        if (matchere(regexp, text))
+            return 1;
+    } while (*text++ != '\0');
+
+    return 0;
+}
