72b809eb0cbb62c0838968335645ec0581c2a79b
B3h3m0th <behemoth@autistici.org>
2016-05-09 12:19:57 +0200

    match() function, check regular expression validity

--- c11_language/regex/full_programs/mygrep.c
+++ c11_language/regex/full_programs/mygrep.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Function Prototypes
+int match(char *regexp, char *text);
+
 int main(void) {
     return(EXIT_SUCCESS);
 }
+
+// Determina se una stringa di testo soddisfa una regular expression
+int match(char *regexp, char *text) {
+
