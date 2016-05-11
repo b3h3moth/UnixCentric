1547c571bce4e5df0d6670e7989f24f343647d9b
B3h3m0th <behemoth@autistici.org>
2016-05-11 12:00:19 +0200

    enhanced program readability

--- c11_language/dynamic_memory_allocation/free/free.c
+++ c11_language/dynamic_memory_allocation/free/free.c
@@ -39,21 +39,5 @@ int main(void) {
     /* Nessun cambiamento nel programma */
     free(NULL);
 
-    /* Si assegna NULL al puntatore rilasciato per evitare comportamenti
-    indefiniti in futuro */
-    ptr = NULL;
-
-    /* Comportamento inaspettato, il puntatore p2 punta ad un indirizzo di 
-    memoria precedentemente rilasciato */
-    int *p1, *p2;
-    p1 = (int*) malloc(sizeof(int));
-    *p1 = 100;
-    p2 = p1;
-    free(p1); /* Si rilascia la memoria ... di entrambi, poiche' p2 = p1 */
-    *p2 = 100; /* Dangling pointer */
-    printf("p2 = %d\n", *p2);   /* Potrebbe anche essere visualizzato il valore
-                                   corretto, tuttavia resta comunque un dato
-                                   non conforme */
-
     return(EXIT_SUCCESS);
 }
