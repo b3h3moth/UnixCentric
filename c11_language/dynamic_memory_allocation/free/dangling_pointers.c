b103d1f0cb771d5ed8db48792ada3bb01e713ffd
B3h3m0th <behemoth@autistici.org>
2016-05-12 19:50:23 +0200

    when occur dangling pointers when invoking free() function

---
+++ c11_language/dynamic_memory_allocation/free/dangling_pointers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Dangling pointers, ovvero quando un puntatore referenzia memoria che e' 
+stata gia' rilasciata. */
+
+int main(void) {
+    int *p1, *p2;
+    p1 = (int*) malloc(sizeof(int));
+    *p1 = 100;
+    p2 = p1;
+    free(p1); /* Si rilascia la memoria ... di entrambi, poiche' p2 = p1 */
+    *p2 = 100; /* Dangling pointer */
+    printf("p2 = %d\n", *p2);   /* Potrebbe anche essere visualizzato il valore
+                                   corretto, tuttavia resta comunque un dato
+                                   non conforme */
+
+    return(EXIT_SUCCESS);
+}
