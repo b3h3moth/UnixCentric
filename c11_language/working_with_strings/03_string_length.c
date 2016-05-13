dc645fd15bcd5ec1b6149f2a4acba980b92bd679
B3h3m0th <behemoth@autistici.org>
2016-05-13 18:18:54 +0200

    rewritten get_strlen()

--- c11_language/working_with_strings/03_string_length.c
+++ c11_language/working_with_strings/03_string_length.c
@@ -3,30 +3,39 @@
 
 #define STR_SIZE    20
 
-int get_strlen(const char str[], int size);
+size_t get_strlen(const char str[]);
 
 int main(int argc, char *argv[]) {
     const char basket_team[STR_SIZE] = "Los Angeles Lakers";
     const char football_team[]       = "Juventus";
+    const char *song                 = "No Woman No cry";
 
-    int len_bteam = get_strlen(basket_team, STR_SIZE);
-    int len_fteam = get_strlen(football_team, STR_SIZE);
+    size_t len_bteam = get_strlen(basket_team);
+    size_t len_fteam = get_strlen(football_team);
+    size_t len_song  = get_strlen(song);
 
     printf("%20s (%2d byte)\n", basket_team, len_bteam);
     printf("%20s (%2d byte)\n", football_team, len_fteam);
+    printf("%20s (%2d byte)\n", song, len_song);
 
     return(EXIT_SUCCESS);
 }
 
-// Calcola la str_len_teamghezza della stringa 'str'
-int get_strlen(const char str[], int size) {
+// Calcola la lunghezza della stringa 'str'
+size_t get_strlen(const char str[]) {
+    /*
     int str_size = 0;
-
-    for (int i=0; i<size; i++) {
+    for (int i=0; i<strlen(str); i++) {
         if (str[i] == '\0')
             break;
         str_size++;
     }
+    */
 
-    return str_size;
+    size_t len;
+
+    for (len=0; *str++ != '\0'; len++)
+        ;
+
+    return len;
 }
