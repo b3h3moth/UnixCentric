090b8b19f5775a90cd30c8971eb0d8916ff7abc0
B3h3m0th <behemoth@autistici.org>
2016-05-14 17:56:03 +0200

    sum_rows() implementation

--- c11_language/functions/functions_and_pointers/multidim_array_as_argument/sum_cols_and_rows.c
+++ c11_language/functions/functions_and_pointers/multidim_array_as_argument/sum_cols_and_rows.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define ROWS    3
+#define COLS    4
+
+// Function Prototypes
+int sum_rows(int arr[][COLS], int rows);
+int sum_cols(int (*arr)[COLS]);
+
 int main(void) {
     return(EXIT_SUCCESS);
+}
+
+int sum_rows(int arr[][COLS], int rows) {
+    static int tot = 0;
+
+    for (int r = 0; r<rows; r++) 
+        for (int c = 0; c<COLS; c++) 
+            tot += arr[r][c];
+
+    return tot;
+}
+
+int sum_cols(int (*arr)[COLS]) {
+    return 0;
 }
