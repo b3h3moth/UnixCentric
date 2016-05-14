f40895291ab8d1a75bd48b4ac4e1e14a797f31cf
B3h3m0th <behemoth@autistici.org>
2016-05-14 18:04:27 +0200

    added sum_all() function prototype to the list

--- c11_language/functions/functions_and_pointers/multidim_array_as_argument/sum_cols_and_rows.c
+++ c11_language/functions/functions_and_pointers/multidim_array_as_argument/sum_cols_and_rows.c
@@ -5,14 +5,29 @@
 #define COLS    4
 
 // Function Prototypes
+int sum_all(int (*arr)[COLS], int rows);
 int sum_rows(int arr[][COLS], int rows);
-int sum_cols(int (*arr)[COLS]);
+int sum_cols(int [][COLS], int rows);
+
+/* Lo scopo del programma e' di provare le invocazioni di una funzione il cui
+parametro formale e' un array multidimensionale e individuare quindi le varie
+signatures possibili. */
 
 int main(void) {
+    int tot;
+    int mdarr[ROWS][COLS] = { 
+        {1,3,5,7},
+        {2,4,6,8},
+        {0,1,2,3}
+    };
+
+    tot = sum_all(mdarr, ROWS);
+    printf("Total rows: %d\n", tot);
+
     return(EXIT_SUCCESS);
 }
 
-int sum_rows(int arr[][COLS], int rows) {
+int sum_all(int arr[][COLS], int rows) {
     static int tot = 0;
 
     for (int r = 0; r<rows; r++) 
