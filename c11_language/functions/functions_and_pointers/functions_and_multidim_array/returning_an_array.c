240ee9ff8ce7294e24c9002436af7bd9d03ab227
B3h3m0th <behemoth@autistici.org>
2016-05-14 19:17:24 +0200

    fixed (*pf())[SIZE], added print((*p)[SIZE]) implementation

--- c11_language/functions/functions_and_pointers/functions_and_multidim_array/returning_an_array.c
+++ c11_language/functions/functions_and_pointers/functions_and_multidim_array/returning_an_array.c
@@ -8,18 +8,34 @@ piuttosto e' possibile far ritornare dalla funzione un puntatore a qualsiasi
 cosa, array compreso. */
 
 int (*pf())[ARR_SIZE];
+void print(int (*p)[ARR_SIZE] );
 
 /* Lo scopo del programma e' di scrivere una funzione che ritorni un puntatore
-a un array di ARR_SIZE elementi. */
+a un array di ARR_SIZE elementi, dopodiche' inizializzare ciascun elemento e
+infine stampare ogni elemento del puntatore ad array. */
 
 int main(void) {
-
     int (*array)[ARR_SIZE] = pf();
+
+    for (int i=0; i<ARR_SIZE; i++)
+        *(*array + i) = i*i;
+
+    print(array);
+
     return(EXIT_SUCCESS);
 }
 
-// Alloca e inizializza a 0 ciascun elemento di un puntatore ad un array
+/* Alloca e inizializza a 0 ciascun elemento di un puntatore ad un array, 
+ritorna un puntatore all'area di memoria di un array di ARR_SIZE elementi. */
 int (*pf())[ARR_SIZE] {
     int (*parr)[ARR_SIZE] = calloc(ARR_SIZE, sizeof(int));
     return parr;
+}
+
+// Stampa ciascun elemento di un puntatore ad array
+void print(int (*p)[ARR_SIZE]) {
+    int *ptmp;
+
+    for (ptmp = *p; ptmp < (*p + ARR_SIZE); ptmp++)
+        printf("%d\n", *ptmp);
 }
