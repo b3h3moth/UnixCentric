62274355fd4b7fd3996e3bd766c3f9613c925de4
B3h3m0th <behemoth@autistici.org>
2016-05-14 18:50:12 +0200

    declaration and implementation of pf(), a function that returns an array of ARR_SIZE pointers to integer

--- c11_language/functions/functions_and_pointers/functions_and_multidim_array/returning_an_array.c
+++ c11_language/functions/functions_and_pointers/functions_and_multidim_array/returning_an_array.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define ARR_SIZE    10
+
+/* Un array non puo' essere utilizzato come valore di ritorno di una funzione, 
+piuttosto e' possibile far ritornare dalla funzione un puntatore a qualsiasi 
+cosa, array compreso. */
+
+int (*pf())[ARR_SIZE];
+
+/* Lo scopo del programma e' di scrivere una funzione che ritorni un puntatore
+a un array di ARR_SIZE elementi. */
+
 int main(void) {
     return(EXIT_SUCCESS);
+}
+
+int (*pf())[ARR_SIZE] {
+    int (*parr)[ARR_SIZE];
+    parr = calloc(ARR_SIZE, sizeof(int));
+    return parr;
 }
