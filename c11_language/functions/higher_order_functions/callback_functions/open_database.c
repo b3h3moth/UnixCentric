3c7d9ac1c835abf40d21c3c9e2485e4ef3a8470f
B3h3m0th <behemoth@autistici.org>
2016-05-18 19:35:25 +0200

    checking command line arguments

--- c11_language/functions/higher_order_functions/callback_functions/open_database.c
+++ c11_language/functions/higher_order_functions/callback_functions/open_database.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sqlite3.h>
 
-int main(void) {
+int main(int argc, char *arg[]) {
+    
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <db> <SQL statement>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
     return(EXIT_SUCCESS);
 }
