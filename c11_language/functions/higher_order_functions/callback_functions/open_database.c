0550257adf118110735897c4e25a736c4b71b39d
B3h3m0th <behemoth@autistici.org>
2016-05-18 19:43:46 +0200

    how to compile the program with sqlite3 library both gcc and Clang

--- c11_language/functions/higher_order_functions/callback_functions/open_database.c
+++ c11_language/functions/higher_order_functions/callback_functions/open_database.c
@@ -2,12 +2,24 @@
 #include <stdlib.h>
 #include <sqlite3.h>
 
-int main(int argc, char *arg[]) {
-    
+int main(int argc, char *argv[]) {
+    sqlite3 *db;
+    int rc;
+    char *err_msg = NULL;
+
     if (argc != 3) {
         fprintf(stderr, "Usage: %s <db> <SQL statement>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
+    // Apertura del database fornito come argomento
+    rc = sqlite3_open(argv[1], &db);
+
+
     return(EXIT_SUCCESS);
 }
+
+/* How to compile the program wih sqlite3 header:
+$   gcc -std=c11 -Wall -pedantic -lsqlite3 open_database.c -o opendb
+$ clang -std=c11 -Wall -pedantic -lsqlite3 open_database.c -o opendb
+*/
