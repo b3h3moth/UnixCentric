082e21bac3870d90440e45ac92adc850d8262287
B3h3m0th <behemoth@autistici.org>
2016-05-20 12:07:31 +0200

    removed unused instructions, added create_array() function definition and implementation

--- c11_language/functions/functions_and_pointers/returning_a_pointer.c
+++ c11_language/functions/functions_and_pointers/returning_a_pointer.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Lo scopo del programma e' di scrivere una funzione che ritorni un puntatore
+int *create_array(int size, int val);
 
-int *create_array(
+/* Lo scopo del programma e' di scrivere una funzione che ritorni un puntatore.
+La memoria sara' allocata mediante la funzione e sara' ritornato l'indirizzo
+della memoria stessa. */
 
 int main(void) {
-    int num1 = 20;
-    int num2 = 30;
-
-    int *val = get_max(&num1, &num2);
-
-    printf("Val: %d\n", *val);
 
     return(EXIT_SUCCESS);
 }
 
-int *get_max(int *x, int *y) {
-    if (*x > *y)
-        return x;
-    else 
-        return y;
+/* La funzione crea un array di dimensione 'size', inizializzando ciascun 
+elemento a 'val'. */
+int *create_array(int size, int val) {
+    int *array = malloc(sizeof(int) * size);
+
+    for (int i=0; i<size; i++)
+        *(array +i) = val;
+
+    return array;
 }
