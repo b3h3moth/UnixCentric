7c3f27d2a9c458792d69e1f32f5ac3b0b702efad
B3h3m0th <behemoth@autistici.org>
2016-05-20 12:23:17 +0200

    replaced subscripts with indirection, code optimization

--- c11_language/functions/functions_and_pointers/returning_a_pointer.c
+++ c11_language/functions/functions_and_pointers/returning_a_pointer.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int *create_array(int size, int val);
+int *create_array(int size);
 
 /* Lo scopo del programma e' di scrivere una funzione che ritorni un puntatore.
 La memoria sara' allocata mediante la funzione e sara' ritornato l'indirizzo
 della memoria stessa. */
 
 int main(void) {
+    int *tmp;
+    int *myarr = create_array(10);
 
+    for (tmp = myarr; tmp < (myarr + 10); tmp++)
+        printf("%d\n", *tmp);
+    
     return(EXIT_SUCCESS);
 }
 
 /* La funzione crea un array di dimensione 'size', inizializzando ciascun 
-elemento a 'val'. */
-int *create_array(int size, int val) {
+elemento con il quadrato dell'indice. Infine ritorna la memoria allocata al
+chiamante, ovvero l'indirizzo del primo elemento. */
+int *create_array(int size) {
     int *array = malloc(sizeof(int) * size);
 
     for (int i=0; i<size; i++)
-        *(array +i) = val;
+        *(array + i) = pow(i);
 
     return array;
 }
