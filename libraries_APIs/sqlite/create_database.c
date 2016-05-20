adf988c191c0933249139b955fdc6e81d4aa2f0c
B3h3m0th <behemoth@autistici.org>
2016-05-20 19:23:14 +0200

    create database, invoke sqlite3_open()

---
+++ libraries_APIs/sqlite/create_database.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sqlite3.h>
+
+/* Lo scopo del programma e' la creazione del database mediante la API 
+specifica, il nome del database stesso e' fornito in input. */
+
+int main(int argc, char *argv[]) {
+    sqlite3 *db;
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <database name>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    // Creazione del database
+    if (sqlite3_open(argv[1], &db)) {
+        fprintf(stderr, "Err. can't create database: %s\n", sqlite3_errmsg(db));
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Database \'%s\' created.\n", argv[1]);
+
+    // Close database connection
+    sqlite3_close(db);
+
+    return(EXIT_SUCCESS);
+}
