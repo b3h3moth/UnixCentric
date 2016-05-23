92c49422183e48aaf70915de45a37d21ae981e02
B3h3m0th <behemoth@autistici.org>
2016-05-23 02:16:27 +0200

    replaced output string

--- libraries_APIs/sqlite/insert_into_table.c
+++ libraries_APIs/sqlite/insert_into_table.c
@@ -35,7 +35,7 @@ int main(int argc, char *argv[]) {
             exit(EXIT_FAILURE);
         }
 
-    puts("Statements successfully stored in a table");
+    puts("Record successfully stored in a table");
 
     // Close database connection
     sqlite3_close(db);
