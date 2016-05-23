68f622dad3f4ac42d3798e687f3f5f7a4f3c07ef
B3h3m0th <behemoth@autistici.org>
2016-05-23 19:10:43 +0200

    get SQLite library version and number

--- libraries_APIs/sqlite/sqlite_libversion.c
+++ libraries_APIs/sqlite/sqlite_libversion.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sqlite3.h>
 
 int main(void) {
+    printf("SQLite library version: %s\n", sqlite3_libversion());
+    printf("SQLite library number: %d\n", sqlite3_libversion_number());
+
     return(EXIT_SUCCESS);
 }
