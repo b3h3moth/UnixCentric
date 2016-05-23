bda37e6aa43dcbdd10679c635a67490e31403b50
B3h3m0th <behemoth@autistici.org>
2016-05-23 19:34:06 +0200

    added the program's purpose, removed const from sizeof

--- c11_language/pointers/pointers_and_array/array_of_pointers/qsort_array_of_pointers_to_strings.c
+++ c11_language/pointers/pointers_and_array/array_of_pointers/qsort_array_of_pointers_to_strings.c
@@ -8,6 +8,9 @@ static int compare(const void *str1, const void *str2) {
     return strcmp (*(const char **) str1, *(const char **) str2);
 }
 
+/* Lo scopo del programma e' di ordinare un array di puntatori a stringhe
+mediante la funzione qsort(), dichiara in stdlib.h */
+
 int main(void) {
     int i;
     const char *country[] = {
@@ -25,7 +28,7 @@ int main(void) {
         "Jamaica", "Ghana",
     };
     
-    qsort (country, ARR_SIZE, sizeof(const char *), compare);
+    qsort (country, ARR_SIZE, sizeof(char *), compare);
     
     for (i=0; i<ARR_SIZE; i++) {
         printf ("[%.2d %12s]\n", i, country[i]);
