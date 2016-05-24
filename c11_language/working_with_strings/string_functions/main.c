f3ddc5617604c2c8d97f92024e1bfe6146b4be1a
B3h3m0th <behemoth@autistici.org>
2016-05-24 12:04:15 +0200

    testing my_strncat() function

--- c11_language/working_with_strings/string_functions/main.c
+++ c11_language/working_with_strings/string_functions/main.c
@@ -6,11 +6,16 @@
 int main(void) {
     char *src = "Take a walk on the wild side";
     char *dest = malloc(sizeof(src)+1);
+    char skill_list[] = "C, C+,+ AWK, Perl, Networking, SysAdministration, ";
+    char update_skill[] = "Network Security, Secure Coding.";
     
     my_strncpy(dest, src,  strlen(src)+1);
+    my_strncat(skill_list, update_skill,  strlen(update_skill)+1);
     
     printf(" src: %s\n", src);
     printf("dest: %s\n", dest);
+
+    printf("%s\n", skill_list);
 
     return(EXIT_SUCCESS);
 }
