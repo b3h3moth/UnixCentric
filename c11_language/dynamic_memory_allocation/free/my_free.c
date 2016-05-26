5bb67d988d9380f03fb75e38efa13db7f60754a0
B3h3m0th <behemoth@autistici.org>
2016-05-26 11:27:58 +0200

    how my_free works

--- c11_language/dynamic_memory_allocation/free/my_free.c
+++ c11_language/dynamic_memory_allocation/free/my_free.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Lo scopo del programma e' di scrivere una nuova funzione free() seguendo
+specifici accorgimenti legati alla sicurezza */
+
 int main(void) {
     return(EXIT_SUCCESS);
 }
