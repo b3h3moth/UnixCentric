e0fedab0eb729ca19c560c36b5bb678fcfaaa253
B3h3m0th <behemoth@autistici.org>
2016-05-26 11:37:35 +0200

    fixed my_free()

--- c11_language/dynamic_memory_allocation/free/my_free.c
+++ c11_language/dynamic_memory_allocation/free/my_free.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Function Prototype
+void my_free(void **pptr);
+
 /* Lo scopo del programma e' di scrivere una nuova funzione free() seguendo
 specifici accorgimenti legati alla sicurezza */
 
 int main(void) {
+    int val = 10;
+    int *pv = malloc(sizeof(int));
+    *pv = 100;
+
+    printf("%d\n", *pv);
+    my_free((void **)&pv);
+
     return(EXIT_SUCCESS);
+}
+
+void my_free(void **pptr) {
+    if (pptr != NULL && *pptr != NULL) {
+        free(*pptr);
+        *pptr = NULL;
+    }
 }
