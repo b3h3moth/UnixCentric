ea156036630d92f22b22d5f2b92a630c5b488154
B3h3m0th <behemoth@autistici.org>
2016-05-31 20:29:55 +0200

    added an array of strings to store the strings to processing

--- c11_language/functions/functions_and_pointers/function_pointer/declaring_function_pointer.c
+++ c11_language/functions/functions_and_pointers/function_pointer/declaring_function_pointer.c
@@ -5,10 +5,20 @@
 size_t process_string(char *str);
 
 int main(void) {
-    size_t (*fplen)(char *);
-    char *str = "The Art of Programming in C";
-    fplen = process_string;
-    printf("The length of \"%s\" is: %d\n", str, fplen(str));
+    size_t (*fpstrlen)(char *);
+    char *pstr[] = {
+        "The art of C programming",
+        "C++ is a good choice for OOP",
+        "Functional programming is beautiful and Lisp is a good think",
+        "AWK was initially developer in 1977 ...",
+        "...by Aho, Weinberger and Kernighan",
+        "UNIX is an ideal development environment"
+    };
+
+    fpstrlen = process_string;
+
+    for (int i = 0; i < (sizeof(pstr)/sizeof(pstr[0])); i++)
+        printf("The length of \"%s\" is: %d\n", pstr[i], fpstrlen(pstr[i]));
     
     return(EXIT_SUCCESS);
 }
