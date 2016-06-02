8ecd1d377b29600aa5a6f978178b7c9b0111a2cd
B3h3m0th <behemoth@autistici.org>
2016-06-02 18:17:54 +0200

    first step, initialize the library

--- libraries_APIs/sqlite/library_initialization.c
+++ libraries_APIs/sqlite/library_initialization.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sqlite.h>
+#include <sqlite3.h>
 
 int main(void) {
+    /* Prima di qualsiasi altra funzione, la libreria dovrebbe essere 
+    inizializzata, questa tuttavia pur essendo una procedura raccomandata
+    e' superata dalla maggior parte delle funzione stesse che lo fanno
+    automaticamente. */
+    if (sqlite3_initialize() == SQLITE_OK)
+        printf("Library \'%s\' initialized\n", sqlite3_libversion());
+
+    sqlite3_shutdown();
     return(EXIT_SUCCESS);
 }
