619abb409ddffa873522981c781dbff8d36caad0
B3h3m0th <behemoth@autistici.org>
2016-06-02 18:21:30 +0200

    freed the library through sqlite3_shutdown()

--- libraries_APIs/sqlite/library_initialization.c
+++ libraries_APIs/sqlite/library_initialization.c
@@ -3,13 +3,18 @@
 #include <sqlite3.h>
 
 int main(void) {
+
     /* Prima di qualsiasi altra funzione, la libreria dovrebbe essere 
-    inizializzata, questa tuttavia pur essendo una procedura raccomandata
-    e' superata dalla maggior parte delle funzione stesse che lo fanno
+    inizializzata, questa pratica tuttavia pur essendo raccomandata
+    e' superata dalla maggior parte delle 'core API' stesse che lo fanno
     automaticamente. */
+    
     if (sqlite3_initialize() == SQLITE_OK)
         printf("Library \'%s\' initialized\n", sqlite3_libversion());
 
-    sqlite3_shutdown();
+    // Rilascia tutte le risorse allocate nell'inizializzazione
+    if (sqlite3_shutdown() == SQLITE_OK)
+        printf("Library freed\n");
+
     return(EXIT_SUCCESS);
 }
