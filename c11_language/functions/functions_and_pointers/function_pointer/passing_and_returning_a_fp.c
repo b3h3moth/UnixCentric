1f756b0a09d5b0e00acd1a57b5195861ecad9b52
B3h3m0th <behemoth@autistici.org>
2016-06-07 12:01:51 +0200

    function pointer data type property

--- c11_language/functions/functions_and_pointers/function_pointer/passing_and_returning_a_fp.c
+++ c11_language/functions/functions_and_pointers/function_pointer/passing_and_returning_a_fp.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Nuovo tipo di dato 'fp', un puntatore a funzione che accetta due parametri
+di tipo intero, e ritorna un intero */
 typedef int (*fp)(int, int);
 
 // Functinn Prototype
@@ -19,8 +21,6 @@ int sum(int val1, int val2) {
 
 // Sottrae due interi
 int sub(int val1, int val2) {
-    if (val1 < val2)
-        return -1;
     return val1 - val2;
 }
 
