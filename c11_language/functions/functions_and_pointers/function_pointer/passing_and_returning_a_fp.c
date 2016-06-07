1aeb6b8d477b01e2a7889723af36b71177ad895c
B3h3m0th <behemoth@autistici.org>
2016-06-07 11:50:37 +0200

    add() sum() added

--- c11_language/functions/functions_and_pointers/function_pointer/passing_and_returning_a_fp.c
+++ c11_language/functions/functions_and_pointers/function_pointer/passing_and_returning_a_fp.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Functinn Prototype
+int sum(int val1, int val2);
+int sub(int val1, int val2);
+
 int main(void) {
     return(EXIT_SUCCESS);
+}
+
+// Somma due interi
+int sum(int val1, int val2) {
+    return val1 + val2;
+}
+
+// Sottrae due interi
+int sub(int val1, int val2) {
+    if (val1 < val2)
+        return -1;
+    return val1 - val2;
 }
