5d5f63773abb196213dad173265b8343f6ab79da
B3h3m0th <behemoth@autistici.org>
2016-06-08 20:00:37 +0200

    get storage class, declaration type and column name

--- libraries_APIs/sqlite/05_get_storage_class_code.c
+++ libraries_APIs/sqlite/05_get_storage_class_code.c
@@ -9,8 +9,53 @@ int main(void) {
     sqlite3         *db = NULL;
     sqlite3_stmt    *stmt = NULL;
     int             res = 0;
-    char            *sql_str = "SELECT msg FROM note order by time;
-    int             flags SQLITE_OPEN_READONLY;
+    int             rc = 0;
+    char            *sql_str = "SELECT * FROM note order by time";
+    int             flags = SQLITE_OPEN_READONLY;
+
+    // Inizializzazione della libreria
+    if (sqlite3_initialize() != SQLITE_OK) {
+        fprintf(stderr, "Err. Unable to initialize the library.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Creazione della connessione al database
+    res = sqlite3_open_v2("1.db", &db, flags, NULL);
+
+    if (res != SQLITE_OK) {
+        fprintf(stderr, "Err. can't create database: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        exit(EXIT_FAILURE);
+    }
+
+    // Creazione della "Prepared Statement".
+    if (sqlite3_prepare_v2(db, sql_str,-1, &stmt, NULL) != SQLITE_OK) {
+        fprintf(stderr, "Err. Unable to create Prepared Statement.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // 
+    for (int i=0; i<sqlite3_column_count(stmt); i++) {
+        printf("Column: %4s | Storage Class: %i | Declared As: %s\n", \
+               sqlite3_column_name(stmt, i), sqlite3_column_type(stmt, i), \
+               sqlite3_column_decltype(stmt, i));
+    }
+
+    rc = sqlite3_step(stmt);
+
+    // Rilascio delle risorse applicate alla 'Prepared Statement'
+    if (sqlite3_finalize(stmt) != SQLITE_OK) {
+        fprintf(stderr, "Err. Unable to finalize.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Close database connection
+    if (sqlite3_close_v2(db) != SQLITE_OK) {
+        fprintf(stderr, "Err. Unable to close connection.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    sqlite3_shutdown();
 
     return(EXIT_SUCCESS);
 }
