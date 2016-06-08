9c3ef9ba5f42eaa9aff5a913ea5e52d5ec06dbb9
B3h3m0th <behemoth@autistici.org>
2016-06-08 19:19:36 +0200

    added sqlite3 header, db, flags and statement variables

--- libraries_APIs/sqlite/05_get_storage_class_code.c
+++ libraries_APIs/sqlite/05_get_storage_class_code.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sqlite3.h>
 
 /* Lo scopo del programma e' di individuare il tipo di dato all'interno dei
 record di un database, mediante la classe di memorizzazione (Storage Class). */
 
 int main(void) {
+    sqlite3         *db = NULL;
+    sqlite3_stmt    *stmt = NULL;
+    int             res = 0;
+    char            *sql_str = "SELECT msg FROM note order by time;
+    int             flags SQLITE_OPEN_READONLY;
+
     return(EXIT_SUCCESS);
 }
