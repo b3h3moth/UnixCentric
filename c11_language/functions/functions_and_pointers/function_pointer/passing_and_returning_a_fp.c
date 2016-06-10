a7f3d2eefa416d4d7c8b56e31137fa57b2a5576a
B3h3m0th <behemoth@autistici.org>
2016-06-10 12:06:11 +0200

    added mol() function definition and implementation

--- c11_language/functions/functions_and_pointers/function_pointer/passing_and_returning_a_fp.c
+++ c11_language/functions/functions_and_pointers/function_pointer/passing_and_returning_a_fp.c
@@ -8,14 +8,16 @@ typedef int (*fp)(int, int);
 // Functinn Prototype
 int sum(int val1, int val2);
 int sub(int val1, int val2);
+int mol(int val1, int val2);
 fp select(char op);
 int eval(char op, int val1, int val2);
 
 // Lo scopo del programma e' di creare esempio usabile di puntatore a funzione
 
 int main(void) {
-    printf("%d\n", eval('+', 10, 20));
-    printf("%d\n", eval('-', 10, 20));
+    printf("sum: %d\n", eval('+', 10, 20));
+    printf("sub: %d\n", eval('-', 10, 20));
+    printf("mol: %d\n", eval('*', 10, 20));
 
     return(EXIT_SUCCESS);
 }
@@ -29,13 +31,19 @@ int sum(int val1, int val2) {
 int sub(int val1, int val2) {
     return val1 - val2;
 }
- 
+
+// Moltiplica due interi
+int mol(int val1, int val2) {
+    return val1 * val2;
+}
+
 /* Se op contiene il segno '+' ritorna la funzione sum(), se invece contiene
 il segno '-' ritorna la funzione sub() */
 fp select(char op) {
     switch(op) {
         case '+': return sum;
         case '-': return sub;
+        case '*': return mol;
     }
 
     return 0;
