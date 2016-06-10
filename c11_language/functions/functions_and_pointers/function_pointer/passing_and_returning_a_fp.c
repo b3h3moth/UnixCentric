2a6d6e8699b4fb053008d19a3db715fa5949e2e2
B3h3m0th <behemoth@autistici.org>
2016-06-10 11:57:10 +0200

    added function comments

--- c11_language/functions/functions_and_pointers/function_pointer/passing_and_returning_a_fp.c
+++ c11_language/functions/functions_and_pointers/function_pointer/passing_and_returning_a_fp.c
@@ -11,6 +11,8 @@ int sub(int val1, int val2);
 fp select(char op);
 int eval(char op, int val1, int val2);
 
+// Lo scopo del programma e' di creare esempio usabile di puntatore a funzione
+
 int main(void) {
     printf("%d\n", eval('+', 10, 20));
     printf("%d\n", eval('-', 10, 20));
@@ -27,7 +29,9 @@ int sum(int val1, int val2) {
 int sub(int val1, int val2) {
     return val1 - val2;
 }
-    
+ 
+/* Se op contiene il segno '+' ritorna la funzione sum(), se invece contiene
+il segno '-' ritorna la funzione sub() */
 fp select(char op) {
     switch(op) {
         case '+': return sum;
@@ -37,6 +41,7 @@ fp select(char op) {
     return 0;
 }
 
+// Valuta i parametri a seconda del contesto
 int eval(char op, int val1, int val2) {
     fp operation = select(op);
     return operation(val1, val2);
