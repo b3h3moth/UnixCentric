429ff04bebe33b04895c8a15ec7762f773b66738
B3h3m0th <behemoth@autistici.org>
2016-06-16 19:15:40 +0200

    SQLite bound parameters, new file added

---
+++ libraries_APIs/sqlite/07_bound_parameters_1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sqlite3.h>
+
+int main(int argc, char *argv[]) {
+    sqlite3 *db = NULL;
+    int flags = SQLITE_OPEN_CREATE | SQLITE_OPEN_READWRITE;
+    int rc = 0;
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <database name>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    // Library initialization
+    sqlite3_initialize();
+
+    // Open database connection
+    rc = sqlite3_open_v2(argv[1], &db, flags, NULL);
+
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "Err. can't create database: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        exit(EXIT_FAILURE);
+    }
+
+
+    // Close database connection
+    sqlite3_close_v2(db);
+    // Shutdown library initialization
+    sqlite3_shutdown();
+
+    return(EXIT_SUCCESS);
+}
