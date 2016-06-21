85ecb8664efcc4a9e0cdec9b776a9d1e34c6e29e
B3h3m0th <behemoth@autistici.org>
2016-06-21 19:13:30 +0200

    POSIX Compilant solution with fstat()

--- c11_language/input_output/get_filesize.c
+++ c11_language/input_output/get_filesize.c
@@ -3,8 +3,16 @@
 #include <sys/stat.h>
 
 /* Lo scopo del programma e' di restituire la grandezza di un file regolare in
-modo sicuro */
+modo sicuro.
+
+Soluzione POSIX compilant*/
 
 int main(void) {
+    struct stat stbuf;
+    off_t size;
+    char *buf;
+    int fd;
+
+    fd = open("
     return(EXIT_SUCCESS);
 }
