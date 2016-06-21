c0a07c4750bc76d7449aa6ccdb8a86f64bebb714
B3h3m0th <behemoth@autistici.org>
2016-06-21 19:11:31 +0200

    added the program's purpose

--- c11_language/input_output/get_filesize.c
+++ c11_language/input_output/get_filesize.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 
+/* Lo scopo del programma e' di restituire la grandezza di un file regolare in
+modo sicuro */
+
 int main(void) {
     return(EXIT_SUCCESS);
 }
