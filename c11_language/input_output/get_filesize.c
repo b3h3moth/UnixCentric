049984fe060645379c347291ef5e698ea9d47e4d
B3h3m0th <behemoth@autistici.org>
2016-06-21 19:16:03 +0200

    fcntl.h header added

--- c11_language/input_output/get_filesize.c
+++ c11_language/input_output/get_filesize.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <fcntl.h>
 
 /* Lo scopo del programma e' di restituire la grandezza di un file regolare in
-modo sicuro.
+modo sicuro. Soluzione POSIX compilant. */
 
-Soluzione POSIX compilant*/
-
-int main(void) {
+int main(int argc, char *argv[]) {
     struct stat stbuf;
     off_t size;
     char *buf;
     int fd;
 
-    fd = open("
+    fd = open(argv[1], O_RDONLY);
     return(EXIT_SUCCESS);
 }
