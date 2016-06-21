52fcddf00a07602edb118293f5f39c55a3c02413
B3h3m0th <behemoth@autistici.org>
2016-06-21 20:26:28 +0200

    print each adress of pointer and pointer to pointer

--- c11_language/pointers/pointer_to_pointer.c
+++ c11_language/pointers/pointer_to_pointer.c
@@ -13,5 +13,14 @@ int main(void) {
     pptr punta a 'val'. */
     int **pptr = &ptr;
 
+    printf("              Address of val: %p <-\n", (void*)&val);
+    printf("              Address of ptr: %p <--\n", (void*)&ptr);
+    printf("             Address of pptr: %p\n", (void*)&pptr);
+    printf(" The address stored into ptr: %p <-\n", (void*)ptr);
+    printf("The address stored into pptr: %p <--\n", (void*)pptr);
+    printf("            The value of val: %d\n", val);
+    printf("    The value pointed by ptr: %d\n", *ptr);
+    printf("   The value pointed by pptr: %d\n", **pptr);
+
     return(EXIT_SUCCESS);
 }
