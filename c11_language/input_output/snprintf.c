f78dda7eb5ec75c9a5074244d2602f51a3f65e92
B3h3m0th <behemoth@autistici.org>
2016-06-22 19:53:25 +0200

    init_buffer() function implementation and prototype

--- c11_language/input_output/snprintf.c
+++ c11_language/input_output/snprintf.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+enum {BUF_SIZE = 19};
+
+// Function Prototypes
+void init_buffer(char *buf, size_t size);
+
 /*
 HEADER: int snprintf(char *str, size_t size, const char *format, ...);
 
@@ -13,5 +18,17 @@ RETURNS: Il numero dei caratteri scritti in 'str', escluso '\0'.
 */
 
 int main(void) {
+    char buf[BUF_SIZE];
+
+    init_buffer(buf, BUF_SIZE);
+
     return(EXIT_SUCCESS);
+}
+
+// Inizializza un vettore di 'size' elementi
+void init_buffer(char *buf, size_t size) {
+    int i;
+
+    for (i=0; i<size; i++)
+        *(buf + i) = i*i;
 }
