81af5203c48ab76da898e5bc9527cb63124dee16
B3h3m0th <behemoth@autistici.org>
2016-06-22 19:41:47 +0200

    snprintf() signature and semantics

--- c11_language/input_output/snprintf.c
+++ c11_language/input_output/snprintf.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* 
+int snprintf(char *str, size_t size, const char *format, ...);
+
+La funzione snprintf() redirige al massimo 'size' byte dell'output della 
+printf() nel buffer 'str', 'format' si occupa di gestire il formato - %s, %d, 
+%f etc. - e infine '...' sono gli argomenti opzionali. */
+
 int main(void) {
     return(EXIT_SUCCESS);
 }
