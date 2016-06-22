a0b0804e5f6d087d5ef7068e120ca197e8f48c7d
B3h3m0th <behemoth@autistici.org>
2016-06-22 19:47:05 +0200

    what snprintf() returns

--- c11_language/input_output/snprintf.c
+++ c11_language/input_output/snprintf.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* 
-int snprintf(char *str, size_t size, const char *format, ...);
+/*
+HEADER: int snprintf(char *str, size_t size, const char *format, ...);
 
-La funzione snprintf() redirige al massimo 'size' byte dell'output della 
-printf() nel buffer 'str', 'format' si occupa di gestire il formato - %s, %d, 
-%f etc. - e infine '...' sono gli argomenti opzionali. */
+SEMANTICS: La funzione snprintf() redirige al massimo 'size' byte dell'output 
+della printf() nel buffer 'str', compreso '\0'; 'format' si occupa di gestire 
+il formato della stringa all'interno del buffer 'str' - %s, %d, %f etc.; 
+infine '...' sono gli argomenti opzionali.
+
+RETURNS: Il numero dei caratteri scritti in 'str', escluso '\0'.
+*/
 
 int main(void) {
     return(EXIT_SUCCESS);
