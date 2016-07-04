7769ad083235c47e03da2fbad7230267513c34a3
B3h3m0th <behemoth@autistici.org>
2016-07-04 20:12:41 +0200

    The program's purpose

--- c11_language/dynamic_memory_allocation/realloc/realloc_array_of_characters.c
+++ c11_language/dynamic_memory_allocation/realloc/realloc_array_of_characters.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Lo scopo del programma e' l'implementazione della funzione get_line() che
+dopo la lettura di caratteri dallo stdin, li assegna a un buffer, ma poiche' il
+totale dei caratteri stesso non e' noto, si utilizza la funzione realloc() per
+assegnare lo spazio necessario. */
+
 int main(void) {
     return(EXIT_SUCCESS);
 }
