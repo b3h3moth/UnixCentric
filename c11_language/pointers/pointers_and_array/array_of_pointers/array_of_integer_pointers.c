a432013dbbaab6ae4db4e89d8c7584f317bfade6
B3h3m0th <behemoth@autistici.org>
2016-07-08 20:20:37 +0200

    we declare an array of integer pointers to ARR_SIZE size

--- c11_language/pointers/pointers_and_array/array_of_pointers/array_of_integer_pointers.c
+++ c11_language/pointers/pointers_and_array/array_of_pointers/array_of_integer_pointers.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+enum { ARR_SIZE = 5 };
+
 /* The program's purpose is to declare and initialize an array of pointers
 to integers, we use both the pointer notation and the subscript notation. */
 
 int main(void) {
+    int *array[ARR_SIZE];
+
     return(EXIT_SUCCESS);
 }
