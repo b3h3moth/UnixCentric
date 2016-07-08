051a722aed412c3a05858856176524c6117b8f26
B3h3m0th <behemoth@autistici.org>
2016-07-08 20:29:59 +0200

    fix subscript notation

--- c11_language/pointers/pointers_and_array/array_of_pointers/array_of_integer_pointers.c
+++ c11_language/pointers/pointers_and_array/array_of_pointers/array_of_integer_pointers.c
@@ -4,13 +4,21 @@
 enum { ARR_SIZE = 5 };
 
 /* The program's purpose is to declare and initialize an array of pointers
-to integers, we use both the pointer notation and the subscript notation. */
+to integers with dereference and subscript notations. */
 
 int main(void) {
     int *array[ARR_SIZE];
+    int *arr[ARR_SIZE];
 
-    /* Subscript notation:
-    Memory allocation and initilization for each array member */
+    // Memory allocation and initilization for each array member
+    
+    // Subscript notation
+    for (int i=0; i<ARR_SIZE; i++) {
+        array[i] = malloc(sizeof(int));
+        *array[i] = i*i;
+    }
+
+    // Dereferencing
     for (int i=0; i<ARR_SIZE; i++) {
         array[i] = malloc(sizeof(int));
         *array[i] = i*i;
