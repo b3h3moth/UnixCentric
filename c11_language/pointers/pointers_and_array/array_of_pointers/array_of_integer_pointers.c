c8f9354f81c00d19ab81faf69b9b6ea22e36ba97
B3h3m0th <behemoth@autistici.org>
2016-07-08 20:19:32 +0200

    how program works, subscript/pointer notations

--- c11_language/pointers/pointers_and_array/array_of_pointers/array_of_integer_pointers.c
+++ c11_language/pointers/pointers_and_array/array_of_pointers/array_of_integer_pointers.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* The program's purpose is to declare and initialize an array of pointers
+to integers, we use both the pointer notation and the subscript notation. */
+
 int main(void) {
     return(EXIT_SUCCESS);
 }
