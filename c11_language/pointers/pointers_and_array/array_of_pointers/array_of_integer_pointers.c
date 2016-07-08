79866793a1dcc074ee26e0b243c0e0c72cd8e6d4
B3h3m0th <behemoth@autistici.org>
2016-07-08 20:24:57 +0200

    subscript notation to memory allocation and init

--- c11_language/pointers/pointers_and_array/array_of_pointers/array_of_integer_pointers.c
+++ c11_language/pointers/pointers_and_array/array_of_pointers/array_of_integer_pointers.c
@@ -9,5 +9,12 @@ to integers, we use both the pointer notation and the subscript notation. */
 int main(void) {
     int *array[ARR_SIZE];
 
+    /* Subscript notation:
+    Memory allocation and initilization for each array member */
+    for (int i=0; i<ARR_SIZE; i++) {
+        array[i] = malloc(sizeof(int));
+        *array[i] = i*i;
+    }
+
     return(EXIT_SUCCESS);
 }
