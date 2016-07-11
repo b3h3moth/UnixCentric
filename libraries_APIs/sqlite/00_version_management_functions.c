4163a13a8f3d0367f0dc45486b223590ff7188eb
B3h3m0th <behemoth@autistici.org>
2016-07-11 19:51:59 +0200

    SQLite version management functions

--- libraries_APIs/sqlite/00_version_management_functions.c
+++ libraries_APIs/sqlite/00_version_management_functions.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <sqlite3.h>
 
+/* Lo scopo del programma e' la presentazione di alcune funzioni utili per
+esrapolare alcuni dati relativi alla libreria SQLite in uso */
+
 int main(void) {
     printf("SQLite library version: %s\n", sqlite3_libversion());
     printf("SQLite library number: %d\n", sqlite3_libversion_number());
