ac389483dff0b9a5b62c853a579fe91b7287b447
B3h3m0th <behemoth@autistici.org>
2016-07-13 20:36:16 +0200

    initilize each row of the jagged array through a compund literals

--- c11_language/array/jagged_array.c
+++ c11_language/array/jagged_array.c
@@ -7,5 +7,11 @@ di tipo array multidimensionale le cui colonne hanno dimensioni differenti. */
 int main(void) {
     // Dichiarazione di un array di puntatori a interi
     int (*(array[])) = {
+        // Ciascuna riga si inizializza mediante i 'compund literals'
+        (int[]) { 10, 20},
+        (int[]) { 30, 40, 50},
+        (int[]) { 60, 70, 80, 90},
+        (int[]) { 100 }
+    };
     return(EXIT_SUCCESS);
 }
