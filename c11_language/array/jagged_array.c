439ac3527095512c934e73794c28706e6d07b14e
B3h3m0th <behemoth@autistici.org>
2016-07-13 20:32:04 +0200

    declared an array of pointers to integers

--- c11_language/array/jagged_array.c
+++ c11_language/array/jagged_array.c
@@ -5,5 +5,7 @@
 di tipo array multidimensionale le cui colonne hanno dimensioni differenti. */
 
 int main(void) {
+    // Dichiarazione di un array di puntatori a interi
+    int (*(array[])) = {
     return(EXIT_SUCCESS);
 }
