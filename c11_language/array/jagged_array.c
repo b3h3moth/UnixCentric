a2fcc7377b5808d97386eec7b65f0d82c7515e89
B3h3m0th <behemoth@autistici.org>
2016-07-13 20:27:19 +0200

    replaced rows with columns

--- c11_language/array/jagged_array.c
+++ c11_language/array/jagged_array.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /* Uno 'Jagged array', letteralmente array seghettato, e' una struttura dati
-di tipo array multidimensionale le cui righe hanno dimensioni differenti. */
+di tipo array multidimensionale le cui colonne hanno dimensioni differenti. */
 
 int main(void) {
     return(EXIT_SUCCESS);
