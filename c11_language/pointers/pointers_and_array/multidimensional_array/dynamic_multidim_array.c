a8a4fa2bc49fbede80cf103acffc9a4cbcfe164c
B3h3m0th <behemoth@autistici.org>
2016-07-16 20:10:52 +0200

    allocate memory for each array element

--- c11_language/pointers/pointers_and_array/multidimensional_array/dynamic_multidim_array.c
+++ c11_language/pointers/pointers_and_array/multidimensional_array/dynamic_multidim_array.c
@@ -5,8 +5,14 @@
 multidimensionale dinamico. */
 
 int main(void) {
-    int rows = 2, cols = 5;
+    int rows = 2, cols = 5, i;
+
+    // Alloca la memoria per l'array
     int **matrix = malloc(rows * sizeof(int *));
+
+    // Alloca la memoria per ciascun elemento
+    for (i=0; i<rows; i++)
+        *(matrix + i) = malloc(cols * sizeof(int));
 
     return(EXIT_SUCCESS);
 }
