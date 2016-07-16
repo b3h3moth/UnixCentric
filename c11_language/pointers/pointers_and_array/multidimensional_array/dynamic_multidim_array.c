9cc9285c97d4269a3e7e16b9dcd3e7d6261546b1
B3h3m0th <behemoth@autistici.org>
2016-07-16 19:59:59 +0200

    the program's purpose

--- c11_language/pointers/pointers_and_array/multidimensional_array/dynamic_multidim_array.c
+++ c11_language/pointers/pointers_and_array/multidimensional_array/dynamic_multidim_array.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Lo scopo del programma e' la definizione ed implementazione di un array
+multidimensionale dinamico. */
+
 int main(void) {
     return(EXIT_SUCCESS);
 }
