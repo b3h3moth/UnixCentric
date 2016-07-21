fbf70e01a89f927c14a54531cf8a40be77f6926c
B3h3m0th <behemoth@autistici.org>
2016-07-21 18:07:06 +0200

    invoce HMAC(), with sha1() hash engine

--- libraries_APIs/cryptography/OpenSSL/HMAC_hash_string.c
+++ libraries_APIs/cryptography/OpenSSL/HMAC_hash_string.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <openssl/hmac.h>
 
 // The program's purpose is how to hash a string through OpenSSL hash function.
@@ -8,6 +9,9 @@ int main(void) {
     char key[] = "A1B2C3B4C5";
     char data[] = "OpenSSL Hash function";
     unsigned char *dgst = NULL;
+
+    dgst = HMAC(EVP_sha1(), key, strlen(key), (unsigned char *)data, \
+                strlen(data), NULL, NULL);
 
     return(EXIT_SUCCESS);
 }
