52d9df6823780261bc7eaaecf4a5105b316e7381
B3h3m0th <behemoth@autistici.org>
2016-07-21 16:32:48 +0200

    sqlite3_config() semantics added. Configuration option and so on

--- libraries_APIs/sqlite/09_setup_global_configuration.c
+++ libraries_APIs/sqlite/09_setup_global_configuration.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Allorquando si lavora con un programma che contiene la libreria SQLite e in 
+altri contesti dove serve una configurazione della libreria specifica, 
+puo' risultare utile avere una configurazione globale della libreria stessa. 
+A tale scopo e' stata implemenata l'interfaccia sqlite3_config(int, ...).
+
+Nota: Nella maggior parte dei casi la configurazione di default risulta essere
+      piu' che sufficiente.
+ 
+L'interfaccia sqlite3_config() deve essere invocata prima dell'inizializzazione
+della libreria con sqlite3_initialize() e dopo lo shutdown della stessa 
+mediante sqlite3_shutdown(), altrimensi sara' resituito l'errore SQLITE_MISUSE.
+
+Il primo argomento della funzione e' un intero che determina le opzioni di
+configurazione della libreria SQLite, gli argomenti successivi dipendono dal 
+primo argomento. Allorquando l'opzione di configurazione va a buon fine 
+ritorna SQLITE_OK, altrimento un codice d'errore.
+*/
+
 int main(void) {
     return(EXIT_SUCCESS);
 }
