03c5c73d79af21cbc812fe01dc7c43cd7d80a79c
B3h3m0th <behemoth@autistici.org>
2016-07-21 18:03:58 +0200

    declared key, data and digest

--- libraries_APIs/cryptography/OpenSSL/HMAC_hash_string.c
+++ libraries_APIs/cryptography/OpenSSL/HMAC_hash_string.c
@@ -5,5 +5,9 @@
 // The program's purpose is how to hash a string through OpenSSL hash function.
 
 int main(void) {
+    char key[] = "A1B2C3B4C5";
+    char data[] = "OpenSSL Hash function";
+    unsigned char *dgst = NULL;
+
     return(EXIT_SUCCESS);
 }
