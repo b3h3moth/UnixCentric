79fa85a7a31012cbe9e18b3bba73ccb17dcc709b
B3h3m0th <behemoth@autistici.org>
2016-07-24 19:41:21 +0200

    fopen() file

--- IO_Standard_Library/get_filesize_with_ftell.c
+++ IO_Standard_Library/get_filesize_with_ftell.c
@@ -2,5 +2,14 @@
 #include <stdlib.h>
 
 int main(void) {
+    FILE *fp;
+    int fsize = 0;
+
+    fp = fopen(argv[1], "rb");
+    if (fp == NULL) {
+        fprintf(stderr, "Err. Cannot open image: %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
     return(EXIT_SUCCESS);
 }
