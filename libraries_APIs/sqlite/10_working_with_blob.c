63fec7aa094c3ba733bf1807ba0f5abb7e44912d
B3h3m0th <behemoth@autistici.org>
2016-07-24 19:23:34 +0200

    checking stream with ferror()

--- libraries_APIs/sqlite/10_working_with_blob.c
+++ libraries_APIs/sqlite/10_working_with_blob.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <sqlite3.h>
 
-/* Lo scopo del programma e' di creare un database per l'inserimento di 
-immagini */
+/* Lo scopo del programma e' di inserire all'interno di un database una
+immagine - tipo di dato blob - ottenuta in input. */
 
 int main(void) {
     sqlite3 *db = NULL;
@@ -23,7 +23,7 @@ int main(void) {
     // Per i tipi di dato 'blob' si deve lavorare con file binary
     fp = fopen(argv[1], "rb");
     if (fp == NULL) {
-        fprintf(stderr, "Cannot open image: %s\n", argv[1]);
+        fprintf(stderr, "Err: Cannot open image: %s\n", argv[1]);
         exit(EXIT_FAILURE);
     }
 
@@ -31,7 +31,12 @@ int main(void) {
     necessario calcolare il peso dell'immagine e si verifica che il risultato
     sia privo di errori. */
     fseek(fp, 0, SEEK_END);
-    
-    
+
+    if (ferror(fp)) {
+        fprintf(stderr, "Err: fseek() failed.\n");
+        fclose(fp);
+        exit(EXIT_FAILURE);
+    }
+
     return(EXIT_SUCCESS);
 }
