35889a9cc1ebb4c89b4897267a975c8f12ee26ff
B3h3m0th <behemoth@autistici.org>
2016-07-24 19:04:46 +0200

    sqlite3 headere and program's semnatics

--- libraries_APIs/sqlite/10_working_with_blob.c
+++ libraries_APIs/sqlite/10_working_with_blob.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sqlite.h>
+
+/* Lo scopo del programma e' di creare un database per l'inserimento di 
+immagini */
 
 int main(void) {
     return(EXIT_SUCCESS);
