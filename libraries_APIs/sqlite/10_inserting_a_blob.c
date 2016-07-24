9341cf885b6b022bb0a7a9e1a5392e3c3be2cba0
B3h3m0th <behemoth@autistici.org>
2016-07-24 20:10:41 +0200

    open sqlite database, sqlite3_open_v2()

--- libraries_APIs/sqlite/10_inserting_a_blob.c
+++ libraries_APIs/sqlite/10_inserting_a_blob.c
@@ -11,6 +11,7 @@ int main(int argc, char *argv[]) {
     sqlite3 *db = NULL;
     sqlite3_stmt *stmt = NULL;
     FILE *fp = NULL;
+    int flags = SQLITE_OPEN_READWRITE;
     int flen = 0;
     int size = 0;
     int rc = 0;
@@ -54,7 +55,7 @@ int main(int argc, char *argv[]) {
     char image[flen+1];
 
     // Lettura dal file pointer e inizializzazione dell'array
-    int size = fread(image, 1, flen, fp);
+    size = fread(image, 1, flen, fp);
 
     if (ferror(fp)) {
         fprintf(stderr, "Err. fread() failed: %s\n", strerror(errno));
@@ -65,7 +66,7 @@ int main(int argc, char *argv[]) {
     // Chiusura del file handler
     fclose(fp);
 
-    sql
+    rc = sqlite3_open_v2("test.db", &db, flags, NULL);
 
     return(EXIT_SUCCESS);
 }
