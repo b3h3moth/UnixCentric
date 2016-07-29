73411429f3d3e527d2769ce3bfefd86a63af7c38
B3h3m0th <behemoth@autistici.org>
2016-07-29 19:14:07 +0200

    create SDL window, screen weight/height are set

---
+++ libraries_APIs/SDL/01_window.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL2/SDL.h>
+
+// Lo scopo del programma e' di creare una finestra di base
+
+int main(int argc, char *argv[]) {
+    const int scr_width = 320;
+    const int scr_height = 400;
+    
+    // Inizializzazione della libreria SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        fprintf(stderr, "Cannot initialize SDL: %s\n", SDL_GetError());
+        exit(EXIT_FAILURE);
+    }
+
+    // Close the library
+    SDL_Quit();
+    
+    return(EXIT_SUCCESS);
+}
