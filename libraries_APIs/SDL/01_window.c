588bd8709558b4d1fff6aeaec5fb31349d78b220
B3h3m0th <behemoth@autistici.org>
2016-07-29 19:28:35 +0200

    create SDL window by using SDL_CreateWindow()

--- libraries_APIs/SDL/01_window.c
+++ libraries_APIs/SDL/01_window.c
@@ -14,8 +14,17 @@ int main(int argc, char *argv[]) {
     // Inizializzazione della libreria SDL
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         fprintf(stderr, "Cannot initialize SDL: %s\n", SDL_GetError());
-        exit(EXIT_FAILURE);
-    }
+    } else {
+        /* Creazione della finestra con specifici parametri
+        - titolo della finestra;
+        - la posizione x della finestra;
+        - la posizione y della finestra;
+        - la lunghezza della finestra;
+        - l'altezza della finestra;
+        - flags a disposizione della finestra, in questo caso indica che la
+          finestra sia visibile */
+        window = SDL_CreateWindow("Take a Walk on The Wild Side", \
+                0, 0, scr_width, scr_height, SDL_WINDOW_SHOWN);
 
     // Close the library
     SDL_Quit();
