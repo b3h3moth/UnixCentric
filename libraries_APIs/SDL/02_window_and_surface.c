92414ca3ca02111747cefacc55e60030f505f78c
B3h3m0th <behemoth@autistici.org>
2016-07-29 20:38:05 +0200

    closed if branch

--- libraries_APIs/SDL/02_window_and_surface.c
+++ libraries_APIs/SDL/02_window_and_surface.c
@@ -36,6 +36,7 @@ int main(int argc, char *argv[]) {
 
             // Update della superficie
             SDL_UpdateWindowSurface(window);
+        }
     }
 
     // Pausa di 5000 millisecondi (5 secondi)
