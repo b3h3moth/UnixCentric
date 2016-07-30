178a3e4b7dd9169531c216d668e0289bfa0e9b9b
B3h3m0th <behemoth@autistici.org>
2016-07-30 18:33:34 +0200

    added infos about SDL create win

--- libraries_APIs/SDL/01_window.c
+++ libraries_APIs/SDL/01_window.c
@@ -14,6 +14,7 @@ int main(int argc, char *argv[]) {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         fprintf(stderr, "Err. Cannot initialize SDL: %s\n", SDL_GetError());
     } else {
+        
         /* Creazione della finestra con specifici parametri
         - titolo della finestra;
         - la posizione x della finestra;
@@ -21,13 +22,17 @@ int main(int argc, char *argv[]) {
         - la lunghezza della finestra;
         - l'altezza della finestra;
         - flags a disposizione della finestra, in questo caso indica che la
-          finestra sia visibile */
+          finestra sia visibile.
+        
+          La finestra - window - rappresenta la risoluzione del gioco, lo 
+        stato iniziale, ma ancora non c'e' nulla di visibile, se non il titolo
+        della finestra stessa. Un flag finale peraltro puo' essere settato a
+        SDL_WINDOW_FULLSCREEN_DESKTOP, che sovrascriverebbe ogni valore 
+        preesistente. */
         window = SDL_CreateWindow("Take a Walk on The Wild Side",   \
                 SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,     \
                 scr_width, scr_height, SDL_WINDOW_SHOWN);
-        /* La finestra - window - rappresenta la risoluzione del gioco, lo 
-        stato iniziale, ma ancora non c'e' nulla di visibile, se non il titolo
-        della finestra stessa. */
+
         if (window == NULL)
             fprintf(stderr, "Err. Could not create window: %s\n", SDL_GetError());
     }
