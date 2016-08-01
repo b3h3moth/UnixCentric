68c8db94a56bc10c53828d87425e5c6f139e9964
B3h3m0th <behemoth@autistici.org>
2016-08-01 19:31:55 +0200

    window, rendere, surface. Each step one by one

---
+++ libraries_APIs/SDL/02_window_renderer.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL2/SDL.h>
+
+/* Lo scopo del programma e' di creare una finestra - window - con una
+superficie 2D - surface - rettangolare */
+
+int main(int argc, char *argv[]) {
+    const int scr_width = 640;
+    const int scr_height = 480;
+    // La finestra
+    SDL_Window *window = NULL;
+    SDL_Renderer *renderer = NULL;
+    
+    // Inizializzazione della libreria SDL
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+        fprintf(stderr, "Err. Cannot initialize SDL: %s\n", SDL_GetError());
+        return(1);
+    } else {
+        window = SDL_CreateWindow("Take a Walk on The Wild Side",   \
+                SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,     \
+                scr_width, scr_height, SDL_WINDOW_SHOWN);
+
+        if (window == NULL) {
+            fprintf(stderr, "Err. Could not create window: %s\n", SDL_GetError());
+            return 1;
+        } else 
+            renderer = SDL_CreateRenderer(window, -1, 0);
+    }
+        
+    SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
+
+    SDL_RenderClear(renderer);
+
+    SDL_RenderPresent(renderer);
+
+    // Pausa di 5000 millisecondi (5 secondi)
+    SDL_Delay(5000);
+
+    // Distrugge il rendering
+    SDL_DestroyRenderer(renderer);
+
+    // Distrugge la finestra
+    SDL_DestroyWindow(window);
+
+    // Chiusura della libreria
+    SDL_Quit();
+    
+    return(EXIT_SUCCESS);
+}
