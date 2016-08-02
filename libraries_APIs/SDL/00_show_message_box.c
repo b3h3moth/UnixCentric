634a908defb2b81892873231d359059ba1353581
B3h3m0th <behemoth@autistici.org>
2016-08-02 13:51:55 +0200

    show a simple message box

---
+++ libraries_APIs/SDL/00_show_message_box.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL2/SDL.h>
+
+// Lo scopo del programma e' di visualizzare un semplice 'message box'
+
+int main(void) {
+    return(EXIT_SUCCESS);
+}
