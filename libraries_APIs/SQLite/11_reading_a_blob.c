aaab43c0e45b2232bf322610364d48d01f526dc0
B3h3m0th <behemoth@autistici.org>
2016-08-05 19:52:18 +0200

    get blob size

--- libraries_APIs/SQLite/11_reading_a_blob.c
+++ libraries_APIs/SQLite/11_reading_a_blob.c
@@ -13,23 +13,17 @@ int main(int argc, char *argv[]) {
     sqlite3        *db = NULL;
     sqlite3_stmt   *stmt = NULL;
     sqlite3_blob   *blob = NULL;
-    sqlite3_int64  row_id = 0;
     FILE           *fp = NULL;
     int            flags_create = SQLITE_OPEN_READWRITE;
-    long           flen = 0;
     int            rc = 0;
-    int            bytes_read = 0;
-    int            offset = 0;
-    void           *data = NULL;
-    char           *err_msg = NULL;
-    char           *sql = "SELECT data FROM blobs";
+    char           *sql = "SELECT data FROM blobs where id = 1";
 
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <db name>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    fp = fopen(argv[1], "wb");
+    fp = fopen("test.jpg", "wb");
     if (fp == NULL) {
         puts("cannot open");
         return 1;
@@ -56,45 +50,19 @@ int main(int argc, char *argv[]) {
 
     // esecuzione della 'prepared statement' e scrittura 
     rc = sqlite3_step(stmt);
-    if (rc != SQLITE_DONE) {
+    // Il totale dei byte scritti, ovvero il peso del dato binario
+    int file_size = sqlite3_blob_bytes(blob);
+    if (rc != SQLITE_OK) {
         fprintf(stderr, "Err. Ins. Statement failed: %d-%s\n", \
                 rc, sqlite3_errmsg(db));
         exit(EXIT_FAILURE);
     }
 
-    // Apertura di un 'BLOB Handle' su 'blob'
-    rc = sqlite3_blob_open(db, "main", "blobs", "data", row_id, 1, &blob);
-    if (rc != SQLITE_OK) {
-        fprintf(stderr, "Err. BLOB Handling failed: %d-%s\n", \
-                rc, sqlite3_errmsg(db));
-        exit(EXIT_FAILURE);
-    }
-
-    data = malloc(DATA_SIZE);
-
-    // Scrive i dati binari di tipo BLOB
-    while ((bytes_read = fread(data, 1, DATA_SIZE, fp))) {
-        rc = sqlite3_blob_read(blob, data, bytes_read, offset);
-        if (rc != SQLITE_OK) {
-            fprintf(stderr, "Err. Writing BLOB: %d-%s\n", \
-                    rc, sqlite3_errmsg(db));
-            exit(EXIT_FAILURE);
-        }
-        offset += bytes_read;
-    }
-
-    // Il totale dei byte scritti, ovvero il peso del dato binario
-    int file_size = sqlite3_blob_bytes(blob);
-     
     sqlite3_blob_close(blob);
     sqlite3_finalize(stmt);
     sqlite3_close(db);
 
-    printf("DB: \'%s\'\nBLOB data: \'%s\' | %d byte\n", \
-            argv[1], argv[2], file_size);
-
     fclose(fp);
-    free(data);
 
     return(EXIT_SUCCESS);
 }
