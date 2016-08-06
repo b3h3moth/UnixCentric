a55ff7812c6b5564230c8b4975132e9d0d25d2ee
B3h3m0th <behemoth@autistici.org>
2016-08-06 18:09:19 +0200

    added variable to store blob data

--- libraries_APIs/SQLite/full_programs/reading_and_writing_blob.c
+++ libraries_APIs/SQLite/full_programs/reading_and_writing_blob.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <sqlite3.h>
 
 int main(int argc, char *argv[]) {
@@ -8,12 +11,16 @@ int main(int argc, char *argv[]) {
     // Il filename del database
     char const *db_filename = NULL;
     // Il file da salvare o da recuperare dal database
-    chat const *sr_file = NULL;
+    char const *sr_file = NULL;
     // 1 = salvare; 0 = recuperare
     int is_store = 0; 
     // Il file descriptor
     int fd = 0;
     // Il peso del file da inserire nel database
-    struct stat file_size = 0;
+    struct stat file_size = {0};
     // Il peso del tipo di dato blob
     int blob_size = 0;
+    unsigned char *z_blob = NULL;
+
+    return(EXIT_SUCCESS);
+}
