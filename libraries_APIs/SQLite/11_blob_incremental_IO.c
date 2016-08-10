a708467d2b52b302b5814c96f3ff0751a0038278
B3h3m0th <behemoth@autistici.org>
2016-08-10 21:19:19 +0200

    init SQLite library, prepare prepared statement

--- libraries_APIs/SQLite/11_blob_incremental_IO.c
+++ libraries_APIs/SQLite/11_blob_incremental_IO.c
@@ -1,6 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sqlite3.h>
 
-int main(void) {
+/* Lo scopo del programma e' la manipolazioni del tipo di dato BLOB mediante
+I/O incrementale */
+
+int main(int argc, char *argv[]) {
+    sqlite3      *db = NULL;
+    sqlite3_stmt *stmt = NULL;
+    int          res = 0;
+    int          flags = SQLITE_OPEN_READWRITE;
+    char         *sql_str = "SELECT data FROM blobs WHERE key = ?"
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <database name>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    // Inizializzazione della libreria
+    if (sqlite3_initialize() != SQLITE_OK) {
+        fprintf(stderr, "Err. Unable to initialize the library.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Creazione della connessione al database */
+    res = sqlite3_open_v2(argv[1], &db, flags, NULL);
+
+    if (res != SQLITE_OK) {
+        fprintf(stderr, "Err. can't create database: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        exit(EXIT_FAILURE);
+    }
+
+    // Creazione della "Prepared Statement".
+    if (sqlite3_prepare_v2(db, sql_str,-1, &stmt, NULL) != SQLITE_OK) {
+        fprintf(stderr, "Err. Unable to create Prepared Statement.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // L'esecuzione del codice VDBE
+    if (sqlite3_step(stmt) == SQLITE_DONE)
+        printf("... Statement successfully executed: %s\n", sql_str);
+
+    // Rilascio della prepared statement
+    if (sqlite3_finalize(stmt) == SQLITE_OK)
+        puts("... Prepared Statemend destroyed.");
+
+    // Close database connection
+    if (sqlite3_close_v2(db) == SQLITE_OK)
+        puts("... Closed database connection. ");
+
+    sqlite3_shutdown();
+
     return(EXIT_SUCCESS);
 }
