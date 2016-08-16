b2ca1dde91c7244c3ce154da338f3d4ef228fb31
B3h3m0th <behemoth@autistici.org>
2016-08-16 11:00:13 +0200

    added callback() function

---
+++ libraries_APIs/SQLite/convenience_wrappers/read_database_records_2.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sqlite3.h>
+
+// Function Prototype
+int callback(void *data, int cols, char **fields, char **col_names) {
+
+/* Lo scopo del programma e' di leggere i record di una tabella mediante la
+funzione di callback, il nome del database e' fornito come argomento. */
+
+int main(int argc, char *argv[]) {
+    sqlite3 *db = NULL;
+    char    **record = NULL;
+    char    *sql_query = NULL;
+    char    *err = NULL;
+    int     rc = 0;
+    int     i, j, nrows, ncols;
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <database_name.db>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    // Creazione del database
+    if (sqlite3_open(argv[1], &db)) {
+        fprintf(stderr, "Err. can't create database: %s\n", sqlite3_errmsg(db));
+        exit(EXIT_FAILURE);
+    }
+
+    // Comando SQL per la lettura dei dati da una tabella
+    sql_query = "SELECT * from note ORDER by id";
+
+    // Lettura dei record mediante la funzione sqlite3_get_table()
+    rc = sqlite3_get_table(db, sql_query, &record, &nrows, &ncols, &err);
+    if (rc != SQLITE_OK) {
+        if (err != NULL) {
+            fprintf(stderr, "Err. can'texecute sql query: \'%s\'\n", err);
+            sqlite3_close(db);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+
+    // Close database connection
+    sqlite3_close(db);
+
+    return(EXIT_SUCCESS);
+}
+
+int callback(void *data, int cols, char **fields, char **col_names) {
+    for (i=0; i<nrows; i++) {
+        for (j=0; j<ncols; j++) {
+            fprintf(stdout, "%s | ", record[(i+1) * ncols + j]);
+        }
+        fputc('\n', stdout);
+    }
+}
