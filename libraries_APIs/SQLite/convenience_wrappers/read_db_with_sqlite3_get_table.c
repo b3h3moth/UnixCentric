16e72120c92e6af97b48648ec1b12c988bf475fd
B3h3m0th <behemoth@autistici.org>
2016-08-16 17:29:19 +0200

    sqlite3_get_table() semantics

--- libraries_APIs/SQLite/convenience_wrappers/read_db_with_sqlite3_get_table.c
+++ libraries_APIs/SQLite/convenience_wrappers/read_db_with_sqlite3_get_table.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include <sqlite3.h>
 
-/* Lo scopo del programma e' di leggere i record di una tabella avvalendosi
+/* Lo scopo del programma e' di leggere i result di una tabella avvalendosi
 della funzione sqlite3_get_table(), il nome del database e' fornito come 
 argomento. */
 
 int main(int argc, char *argv[]) {
     sqlite3 *db = NULL;
-    char    **record = NULL;
+    char    **result = NULL;
     char    *sql_query = NULL;
     char    *err = NULL;
     int     rc = 0;
@@ -28,20 +28,29 @@ int main(int argc, char *argv[]) {
     // Comando SQL per la lettura dei dati da una tabella
     sql_query = "SELECT * from song ORDER by id";
 
-    // Lettura dei record mediante la funzione sqlite3_get_table()
-    rc = sqlite3_get_table(db, sql_query, &record, &nrows, &ncols, &err);
+    /* Lettura dei record mediante la funzione sqlite3_get_table().
+    Usando tale funzione non ci si deve preoccupare di creare una callback().*/
+    rc = sqlite3_get_table(db, sql_query, &result, &nrows, &ncols, &err);
     if (rc != SQLITE_OK) {
         fprintf(stderr, "Err. can'texecute sql query: \'%s\'\n", err);
         sqlite3_free(err);
     }
 
-    // Stampa dei record
+    // Stampa ciascun record, riga per riga
     for (i=0; i<nrows; i++) {
+        
+        // colonna per colonna
         for (j=0; j<ncols; j++) {
-            fprintf(stdout, "%s | ", record[(i+1) * ncols + j]);
+            /* I primi quattro elementi contengono i nomi delle colonne, per
+            cui si salta direttamente al contenuto dei record stessi */
+            fprintf(stdout, "%2s, ", result[(i+1) * ncols + j]);
         }
+        
         fputc('\n', stdout);
     }
+
+    // Rilascio della memoria relativa ai record contenuti in result.
+    sqlite3_free_table(result);
 
     // Close database connection
     sqlite3_close(db);
