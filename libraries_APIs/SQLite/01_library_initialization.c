ae161dd5765f05c851f30530438ff339380e04c9
B3h3m0th <behemoth@autistici.org>
2016-08-20 10:40:55 +0200

    How initialize the SQLite library

--- libraries_APIs/SQLite/01_library_initialization.c
+++ libraries_APIs/SQLite/01_library_initialization.c
@@ -4,10 +4,18 @@
 
 int main(void) {
 
-    /* Prima di qualsiasi altra funzione, la libreria dovrebbe essere 
-    inizializzata, questa pratica tuttavia pur essendo raccomandata
-    e' superata dalla maggior parte delle 'core API' stesse che lo fanno
-    automaticamente. */
+    /* Prima di invocare una qualsiasi funzione, la libreria dovrebbe essere 
+    inizializzata, da notare che questa pratica pur essendo raccomandata
+    e' superata dalla maggior parte delle 'core APIs' che lo fanno
+    automaticamente, come ad esempio la funzione sqlite3_open().
+    
+    A rigor di logica quindi non dovrebbe essere utilizzata. Tuttavia non e'
+    cosi', poiche' per garantire la massima portabilita' dei programmi sarebbe 
+    il caso di inizializzare la libreria esplicitamente, inoltre i futuri
+    rilasci di SQLite lo prevederanno, quindi e' buona norma farlo. 
+    
+    Pertanto, nei programmi scritti nel presente repository la libreria sara'
+    inizializzata e rilasciata mediante le appositie funzioni. */
     
     if (sqlite3_initialize() == SQLITE_OK)
         printf("Library \'%s\' initialized\n", sqlite3_libversion());
