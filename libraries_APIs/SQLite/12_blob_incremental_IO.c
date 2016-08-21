4363920772f2253abd074647661aa6e1f1602b7c
B3h3m0th <behemoth@autistici.org>
2016-08-21 18:50:39 +0200

    fixed incremental I/O routines

--- libraries_APIs/SQLite/12_blob_incremental_IO.c
+++ libraries_APIs/SQLite/12_blob_incremental_IO.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sqlite3.h>
 
 /* Lo scopo del programma e' la manipolazioni del tipo di dato BLOB mediante
@@ -8,14 +9,11 @@ I/O incrementale */
 int main(int argc, char *argv[]) {
     sqlite3 *db = NULL;
     sqlite3_stmt *stmt = NULL;
-    sqlite3_blob *blob = NULL;
     FILE *fblob = NULL;
     int res = 0;
     int blob_size = 0;
     int flags = SQLITE_OPEN_READWRITE;
-    sqlite3_int64 rowid = 0;
     char *blob_data = NULL;
-    char *err_msg = 0;
     char *sql_data = "SELECT data FROM blobs WHERE id = 1";
 
     if (argc < 3) {
@@ -48,19 +46,15 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    sqlite3_blob_open(db, "main", "blobs", "data", 1, 0, &blob);
-
-    blob_size = sqlite3_blob_bytes(blob);
-    blob_data = malloc(blob_size);
-    sqlite3_blob_read(blob, blob_data, blob_size, 0);
-    sqlite3_blob_close(blob);
+    if (sqlite3_step(stmt) == SQLITE_ROW) {
+        blob_size = sqlite3_column_bytes(stmt, 0);
+        blob_data = malloc(blob_size);
+        memcpy(blob_data, sqlite3_column_blob(stmt, 0), blob_size);
+    }
 
     fblob=fopen(data_name, "w");
     fwrite(blob_data, blob_size, 1, fblob);
     fclose(fblob);
-    
-    if (sqlite3_step(stmt) == SQLITE_DONE)
-        printf("... Statement successfully executed: %s\n", sql_data);
 
     // Rilascio della prepared statement
     if (sqlite3_finalize(stmt) == SQLITE_OK)
