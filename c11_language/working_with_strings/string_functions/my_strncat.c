5beaf4b3b4aa8b1701df64730181693ce08f19f6
B3h3m0th <behemoth@autistici.org>
2016-08-25 23:08:06 +0200

    fixed for loop to improve security

--- c11_language/working_with_strings/string_functions/my_strncat.c
+++ c11_language/working_with_strings/string_functions/my_strncat.c
@@ -1,15 +1,13 @@
 #include <string.h>
 #include "my_strfunc.h"
 
-char *my_strncat(char *dest, const char *src, size_t n) {
+void my_strncat(char *dest, const char *src, size_t n) {
     size_t i;
     size_t len_dest = strlen(dest);
 
-    for (i=0; i<n && src[i] != '\0'; i++)
+    for (i=0; src[i] && (i < n-1) != '\0'; i++)
         dest[len_dest + i] = src[i];
     
     // Add null-terminated character
     dest[len_dest + i] = '\0';
-
-    return dest;
 }
