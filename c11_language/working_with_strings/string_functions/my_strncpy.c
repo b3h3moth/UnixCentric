b4c533f9faca4f107ef48e0a6f828dd12d692f43
B3h3m0th <behemoth@autistici.org>
2016-08-25 22:58:56 +0200

    fixed for loop to copy string

--- c11_language/working_with_strings/string_functions/my_strncpy.c
+++ c11_language/working_with_strings/string_functions/my_strncpy.c
@@ -1,13 +1,12 @@
 #include "my_strfunc.h"
 
-char *my_strncpy(char *dest, const char *src, size_t n) {
+void *my_strncpy(char *dest, const char *src, size_t n) {
     size_t i;
 
-    for (i=0; i<n && src[i] != '\0'; i++)
+    for (i=0; i<n && src[i] && (i < n -1); i++)
         dest[i] = src[i];
     
-    for ( ; i<n; i++)
-        dest[i] = '\0';
+    dest[i] = '\0';
 
     return dest;
 }
