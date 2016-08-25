69646a5ac00f98f080276cfe584fa2692bb19028
B3h3m0th <behemoth@autistici.org>
2016-08-25 22:58:29 +0200

    my_strncpy() returns 'void'

--- c11_language/working_with_strings/string_functions/my_strfunc.h
+++ c11_language/working_with_strings/string_functions/my_strfunc.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 
-char *my_strncpy(char *dest, const char *src, size_t n);
+void *my_strncpy(char *dest, const char *src, size_t n);
 char *my_strncat(char *dest, const char *src, size_t n);
 
 #endif
