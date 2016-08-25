5c0e49bf1ac534a65125ec8ebd3c1a78152a91e0
B3h3m0th <behemoth@autistici.org>
2016-08-25 23:06:39 +0200

    minor updates

--- c11_language/working_with_strings/string_functions/my_strncpy.c
+++ c11_language/working_with_strings/string_functions/my_strncpy.c
@@ -3,7 +3,7 @@
 void my_strncpy(char *dest, const char *src, size_t n) {
     size_t i;
 
-    for (i=0; i<n && src[i] && (i < n -1); i++) {
+    for (i=0; i<n && src[i] && (i < n-1); i++) {
         dest[i] = src[i];
     }
     
