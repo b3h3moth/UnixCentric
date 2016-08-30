6ce55df1e8ee515a4f34e8491cf403030c01b3d4
B3h3m0th <behemoth@autistici.org>
2016-08-30 19:20:46 +0200

    clean the buffer

--- IO_Unbuffered/full_programs/write_strings_to_file.c
+++ IO_Unbuffered/full_programs/write_strings_to_file.c
@@ -7,46 +7,46 @@
 
 #define BUF_SIZE	128
 
-/*
- Il programma riceve delle stringhe in input, dopodiche' sono copiate nel file
- filename.
-*/
-
+// Function Prototype
 void ins_str_to_file(char b[], int fdw);
 
-int main(int argc, char *argv[], char *envp[])
-{
-   char buf[BUF_SIZE];
-   int fd1;
-   char *filename = "test.txt";
+/* Lo scopo del programma e' di scrivere una serie di stringhe, mediante la
+funzione 'ins_str_to_file()', in un file. Le stringhe sono inserite input. */
 
-   if ( (fd1 = open(filename, O_WRONLY | O_CREAT, 0755)) < 0) {
-      fprintf(stderr, "Err: (%d) - '%s'\n", errno, strerror(errno));
-      exit(errno);
-   }
 
-   ins_str_to_file(buf, fd1);
-   close(fd1);
+int main(int argc, char *argv[]){
+    char buf[BUF_SIZE];
+    int fd1;
+    char *filename = "test.txt";
 
-   return(EXIT_SUCCESS);
+    if ( (fd1 = open(filename, O_WRONLY | O_CREAT, 0755)) < 0) {
+       fprintf(stderr, "Err: (%d) - '%s'\n", errno, strerror(errno));
+       exit(errno);
+    }
+
+    ins_str_to_file(buf, fd1);
+    
+    close(fd1);
+
+    return(EXIT_SUCCESS);
 }
 
-void ins_str_to_file(char b[], int fdw)
-{
-   int i;
-   int n_read;
-   printf("Immettere il testo (digitare X per uscire)\n");
+void ins_str_to_file(char b[], int fdw) {
+    int i;
+    int n_read;
+    printf("Immettere il testo (digitare X per uscire)\n");
 
-   /* Pulizia del buffer */
-   for (i=0; i<BUF_SIZE; i++)
-      b[i] = 0;
+    // Clean the buffer
+    for (i=0; i<BUF_SIZE; i++)
+        b[i] = 0;
 
-   while ((n_read = read(STDIN_FILENO, b, BUF_SIZE)) > 0) {
-      if (b[n_read-2] == 'X')
-      	 break;
-      if (write(fdw, b, n_read) != n_read) {
-      	 fprintf(stderr, "Err: (%d) - %s\n", errno, strerror(errno));
-	 exit(errno);
-      }
-   }
+    while ((n_read = read(STDIN_FILENO, b, BUF_SIZE)) > 0) {
+        if (b[n_read-2] == 'X')
+            break;
+        
+        if (write(fdw, b, n_read) != n_read) {
+      	    fprintf(stderr, "Err: (%d) - %s\n", errno, strerror(errno));
+            exit(errno);
+        }
+    }
 }
