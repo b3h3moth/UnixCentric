68ea5e24fae58db59af9820f5a90bf74118f75b8
B3h3m0th <behemoth@autistici.org>
2016-08-31 17:18:30 +0200

    security issue, added null terminator character

--- IO_Unbuffered/02_read-1.c
+++ IO_Unbuffered/02_read-1.c
@@ -15,36 +15,40 @@ RETURNS   : Il numero dei byte letti in caso di successo, -1 in caso di errore
 --------------------------------------------------------------------------------
 */
 
+// Lo scopo del programma e' la lettura di un file
 
-int main(int argc, char *argv[], char *envp[])
-{
-   int fd, n;
-   char buf[MAX_BUF];
+int main(int argc, char *argv[]) {
+    int fd, n;
+    char buf[MAX_BUF+1];
 
-   if (argc < 2) {
-      fprintf(stderr, "Uso: %s <file name>\n", argv[0]);
-      exit(EXIT_FAILURE);
-   }
+    if (argc < 2) {
+        fprintf(stderr, "Uso: %s <file name>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
 
-   if ((fd = open(argv[1], O_RDONLY)) < 0) {
-      fprintf(stderr, "Err. open file\n");
-      exit(EXIT_FAILURE);
-   }
+    if ((fd = open(argv[1], O_RDONLY)) < 0) {
+        fprintf(stderr, "Err. open file\n");
+        exit(EXIT_FAILURE);
+    }
 
-   /* la funzione read(), legge MAX_BUF byte dal file descriptor 'fd' e li pone,
-   senza alcuna conversione, in buf; ritorna il numero di byte letti, oppure -1
-   in caso di fallimento. */
-   if ((n = read(fd,buf, MAX_BUF)) < 0) {
-      fprintf(stderr, "Err. read file\n");
-      exit(EXIT_FAILURE);
-   }
+    /* La funzione read(), legge MAX_BUF byte dal file descriptor 'fd' e li 
+    pone, senza alcuna conversione, in buf; ritorna il numero di byte letti, 
+    oppure -1 in caso di fallimento. */
+    if ((n = read(fd ,buf, MAX_BUF)) < 0) {
+        fprintf(stderr, "Err. read file\n");
+        exit(EXIT_FAILURE);
+    }
 
-   if (write (STDOUT_FILENO, buf, n) < 0) {
-      fprintf(stderr, "Err. write file\n");
-      exit(EXIT_FAILURE);
-   }
+    /* E' sempre una buona pratica, allorquando non si lavora con dati binari,
+    inserire il carattere 'null-terminato' finale */
+    buf[n] = '\0';
 
-   close(fd);
+    if (write (STDOUT_FILENO, buf, n) < 0) {
+        fprintf(stderr, "Err. write file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    close(fd);
 
    return(EXIT_SUCCESS);
 }
