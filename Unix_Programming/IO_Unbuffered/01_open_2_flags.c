33503613626a08d8a981dd99c71c5f245a71c012
B3h3m0th <behemoth@autistici.org>
2016-09-01 18:16:51 +0200

    removed error handling from close()

--- Unix_Programming/IO_Unbuffered/01_open_2_flags.c
+++ Unix_Programming/IO_Unbuffered/01_open_2_flags.c
@@ -8,25 +8,25 @@
 
 /* Costanti relative ai bit di permesso sui file
 ------------------------------------------------
-S_ISUID | 04000 |
-S_ISGID | 02000 |
-S_ISVTX | 01000 |
---------+-------+
-S_IRUSR |  0400 |
-S_IWUSR |  0200 |
-S_IXUSR |  0100 |
---------+-------+
-S_IRGRP |   040 |
-S_IWGRP |   020 |
-S_IXGRP |   010 |
---------+-------+
-S_IROTH |    04 |
-S_IWOTH |    02 |
-S_IXOTH |    01 |
---------+-------+
-S_IRWXU |  0700 |
-S_IRWXG |    70 |
-S_IRWXO |    07 |
+S_ISUID | 04000 | Set User ID
+S_ISGID | 02000 | Set Group ID
+S_ISVTX | 01000 | Sticky bit
+--------+-------+--------------
+S_IRUSR |  0400 | User Read
+S_IWUSR |  0200 | User Write
+S_IXUSR |  0100 | User Execute
+--------+-------+--------------
+S_IRGRP |   040 | Group Read
+S_IWGRP |   020 | Group Write
+S_IXGRP |   010 | Group Execute
+--------+-------+--------------
+S_IROTH |    04 | Other Read
+S_IWOTH |    02 | Other Write
+S_IXOTH |    01 | Other Execute
+--------+-------+--------------
+S_IRWXU |  0700 | User  R/W/X
+S_IRWXG |   070 | Group R/W/X
+S_IRWXO |    07 | Other R/W/X
 
 */
 
@@ -43,7 +43,7 @@ int main(int argc, char *argv[]) {
 
     file_flags = O_RDWR | O_CREAT | O_TRUNC;
 
-    /* Permessi sono stati impostati in ottale a 0755 */
+    /* Permessi impostati al valore ottale 0755 */
     file_perms = S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH;
 
     if (argc != 2) {
@@ -56,10 +56,7 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    if ((close(input_fd)) < 0) {
-        fprintf(stderr, "Err. close(%s)\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
+    close(input_fd);
 
     return(EXIT_SUCCESS);
 }
