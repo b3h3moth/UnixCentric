cd98ae02f83590dd282353769b212eddabca1126
B3h3m0th <behemoth@autistici.org>
2016-09-08 19:42:29 +0200

    useless code, removed

--- Unix_Programming/IO_Standard_Library/06_setvbuf.c
+++ Unix_Programming/IO_Standard_Library/06_setvbuf.c
@@ -7,30 +7,13 @@
 
 int main(void) {
    char buf[MAX_BUF];
-   
-   if(setvbuf(stdout, buf, _IOFBF, sizeof buf)) {
-      fprintf(stderr, "Err(%s) cambiamento buffer stodut\n", strerror(errno));
-      return EXIT_FAILURE;
+
+   fprintf(stdout, "Buffering ...\n");
+
+   if (setvbuf(stdout, buf, _IOFBF, sizeof buf)) {
+      fprintf(stderr, "Err. Set buffering failed: %s\n", strerror(errno));
+      exit(EXIT_FAILURE);
    }
-   
-   printf("The art ");
-   fflush(stdout);
-   
-   if(setvbuf(stdout, NULL, _IONBF, 0)) {
-      fprintf(stderr, "Err(%s) cambiamento buffer stodut\n", strerror(errno));
-      return EXIT_FAILURE;
-   }
-   
-   printf("of *nix ");
-   
-   if(setvbuf(stdout, buf, _IOLBF, sizeof buf)) {
-      fprintf(stderr, "Err(%s) cambiamento buffer stodut\n", strerror(errno));
-      return EXIT_FAILURE;
-   
-   }
-   
-   printf("programming");
-   putchar('\n');
    
    return EXIT_SUCCESS;
 }
