0dc9c48c40c675e8cc815f54f5dc1ffbe6767055
B3h3m0th <behemoth@autistici.org>
2016-09-09 18:59:35 +0200

    main() arguments and sys/stat header

--- Unix_Programming/IO_Standard_Library/06_setvbuf.c
+++ Unix_Programming/IO_Standard_Library/06_setvbuf.c
@@ -2,13 +2,14 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include <sys/stat.h>
 
 #define MAX_BUF 512
 
 /* La funzione 'int setvbuf(FILE *stream, char *buf, int mode, size_t size)' 
 definisce la bufferizzazione dello stream. */
 
-int main(void) {
+int main(int argc, char *argv[]) {
     FILE *fp = fopen(argv[1], "r");
 
     if (fp == NULL) {
