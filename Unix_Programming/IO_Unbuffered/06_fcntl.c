aabe5bbfa28ee2d40a4ac269540df1561ef51d12
B3h3m0th <behemoth@autistici.org>
2016-09-10 12:06:46 +0200

    fcntl() semantics

--- Unix_Programming/IO_Unbuffered/06_fcntl.c
+++ Unix_Programming/IO_Unbuffered/06_fcntl.c
@@ -6,26 +6,28 @@
 #include <fcntl.h>
 
 /*
-HEADER    : <fcntl.h>
-PROTOTYPE : int fcntl(int fd, int cmd, ... < arg > );
-SEMANTICS : La funzione fcntl() esegue diverse operazioni sul file descriptor 
-            'fd' a seconda del valore di 'cmd'; il terzo argomento 'arg' puo' 
-	        indicare un intero oppure un puntatore alla struttura struct flock.
-RETURNS   : I valori di ritorno sono diversi a seconda dell'operazione eseguita,
-            sempre -1 in caso di errore
---------------------------------------------------------------------------------
-La funzione fcntl() puo' modificare le proprieta' di un file descriptor, nel
-caso specifico 'fd', tuttavia il numero degli argomenti, il valore di ritorno
-e gli errori sono strettamente legati al valore della variabile intera 'cmd',
-che puo' assumere i seguenti valori:
+HEADER   : <fcntl.h>
+PROTOTYPE: int fcntl(int fd, int cmd, ... < arg > );
+La funzione fcntl() e' una sorta di punto di accesso per l'esecuzione di 
+diverse operazioni sul file descriptor. Il primo argomento 'fd' e' un file
+descriptor aperto, il secondo argomento 'cmd' indica il tipo di operazione da
+eseguire, il terzo argomento 'arg' e' opzionale, se utilizzarlo o meno e' 
+determinato da 'cmd'.
+I valori di ritorno sono diversi a seconda dell'operazione eseguita, in caso di
+errore ritorna sempre -1.
 
-F_DUPFD , duplica il file descriptor;
-F_GETFD , ritorna il valore del file descriptor flag;
-F_SETFD , setta il valore del file descpritor flag;
-F_GETFL , ritorna il valore del file status flag;
-F_SETFL , setta il valore del file status flag;
-F_GETLK , ritorna informazioni sul record locking;
-F_SETLK , setta informazioni sul record locking;
+La funzione fcntl() puo' modificare le proprieta' di un file descriptor, 
+tuttavia il numero degli argomenti, il valore di ritorno, errori compresi, 
+sono strettamente legati al valore della variabile intera 'cmd', che puo' 
+assumere i seguenti valori:
+
+F_DUPFD, duplica il file descriptor;
+F_GETFD, ritorna il valore del file descriptor flag;
+F_SETFD, setta il valore del file descpritor flag;
+F_GETFL, ritorna il valore del file status flag;
+F_SETFL, setta il valore del file status flag;
+F_GETLK, ritorna informazioni sul record locking;
+F_SETLK, setta informazioni sul record locking;
 F_SETLKW, come sopra, non ritorna subito ma attende il rilascio del blocco;
 F_GETOWN, ritorna il PID o il PGID, preposto alla ricezione dei segnali SIGURS;
 F_SETOWN, setta il PID o il PGID, preposto alla ricezione dei segnali SIGURS;
