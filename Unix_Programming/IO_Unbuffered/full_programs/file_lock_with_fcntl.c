e7d67d963915e7ae7d0b2aa5f9c652798052baf8
B3h3m0th <behemoth@autistici.org>
2016-09-10 12:13:29 +0200

    write lock by using fcnlt()

---
+++ Unix_Programming/IO_Unbuffered/full_programs/file_lock_with_fcntl.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Lo scopo del  programma e' la realizzazione di un file 'write lock'
+utilizzando la funzione fcntl() */
+
+int main(void) {
+    return(EXIT_SUCCESS);
+}
