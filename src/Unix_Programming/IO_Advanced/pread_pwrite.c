641a71b72223b64f29fe8e53a084c790e8b12200
B3h3m0th <behemoth@autistici.org>
2016-09-15 20:24:05 +0200

    declare input output variables about file descriptors, offset and size

--- src/Unix_Programming/IO_Advanced/pread_pwrite.c
+++ src/Unix_Programming/IO_Advanced/pread_pwrite.c
@@ -3,5 +3,10 @@
 #include <unistd.h>
 
 int main(void) {
+    char *buf;
+    int in_fd, out_fd, nread;
+    off_t in_offst, out_offst;
+    size_t in_size, out_size;
+
     return(EXIT_SUCCESS);
 }
