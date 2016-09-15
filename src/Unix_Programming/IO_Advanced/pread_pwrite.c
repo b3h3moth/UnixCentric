95cb1c8d0a4a3c3753957ca9c5137111b960a9ad
B3h3m0th <behemoth@autistici.org>
2016-09-15 22:21:28 +0200

    command line arguments

--- src/Unix_Programming/IO_Advanced/pread_pwrite.c
+++ src/Unix_Programming/IO_Advanced/pread_pwrite.c
@@ -8,5 +8,11 @@ int main(void) {
     off_t in_offst, out_offst;
     size_t in_size, out_size;
 
+    if (argc != 7) {
+        fprintf(stderr, "Usage: %s dd <input_file> <input_offset> <input size>"
+                " <output file> <output offset> <output size", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
     return(EXIT_SUCCESS);
 }
