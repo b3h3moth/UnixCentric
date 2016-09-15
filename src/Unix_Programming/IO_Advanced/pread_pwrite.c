de3fddbeaba28e0372e23e1334291e02af3c0bad
B3h3m0th <behemoth@autistici.org>
2016-09-15 22:48:57 +0200

    setup I/O size

--- src/Unix_Programming/IO_Advanced/pread_pwrite.c
+++ src/Unix_Programming/IO_Advanced/pread_pwrite.c
@@ -25,15 +25,17 @@ int main(int argc, char *argv[]) {
 
     out_fd = open(argv[1], O_WRONLY);
     if (out_fd < 0) {
-        fprintf(stderr, "Unable to write %s: %s\n", argv[1], strerror(errno));
+        fprintf(stderr, "Unable to write %s: %s\n", argv[4], strerror(errno));
         exit(EXIT_FAILURE);
     }
 
     // Setup input output offset
     in_offst = strtol(argv[2], NULL, 10);
-    out_offst = strtol(argv[3], NULL, 10);
+    out_offst = strtol(argv[5], NULL, 10);
 
-    out_offst = strtol(argv[3], NULL, 10);
+    // Set up input output size
+    in_size = strtol(argv[3], NULL, 10);
+    out_size = strtol(argv[6], NULL, 10);
 
     return(EXIT_SUCCESS);
 }
