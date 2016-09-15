fa7f80b4c0a91de2956cd5d831ce3afa1add6fc6
B3h3m0th <behemoth@autistici.org>
2016-09-15 22:26:31 +0200

    open input file

--- src/Unix_Programming/IO_Advanced/pread_pwrite.c
+++ src/Unix_Programming/IO_Advanced/pread_pwrite.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
 
-int main(void) {
+int main(int argc, char *argv[]) {
     char *buf;
     int in_fd, out_fd, nread;
     off_t in_offst, out_offst;
@@ -10,7 +13,13 @@ int main(void) {
 
     if (argc != 7) {
         fprintf(stderr, "Usage: %s dd <input_file> <input_offset> <input size>"
-                " <output file> <output offset> <output size", argv[0]);
+                " <output file> <output offset> <output size>", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    in_fd = open(argv[1], O_RDONLY);
+    if (in_fd < 0) {
+        fprintf(stderr, "Unable to open %s: %s\n", argv[1], strerror(errno));
         exit(EXIT_FAILURE);
     }
 
