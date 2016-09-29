f8ec284f71085a144ff1b49e91f2a0ab88d75863
B3h3m0th <behemoth@autistici.org>
2016-09-29 19:07:43 +0200

    iovec structure is set

--- src/Unix_Programming/IO_Advanced/readv.c
+++ src/Unix_Programming/IO_Advanced/readv.c
@@ -2,7 +2,10 @@
 #include <stdlib.h>
 #include <sys/uio.h>
 
+enum { NBUF = 3 };
+
 int main(void) {
+    struct iovec iov[NBUF] = {0};
     const char *const *buf[] = {0};
 
     return(EXIT_SUCCESS);
