08564ab102e9c64c549b9b6a041806bb0409e6b8
B3h3m0th <behemoth@autistici.org>
2016-10-01 19:31:53 +0200

    open file descriptor

--- src/Unix_Programming/IO_Advanced/truncate.c
+++ src/Unix_Programming/IO_Advanced/truncate.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/stat.h>
 
-int main(void) {
+int main(int argc, char *argv[]) {
+    int fd;
+    char *filename;
+    int flags = O_WRONLY | O_CREAT;
+    int mode = S_IRWXU | S_IXGRP | S_IRWXG | S_IROTH | S_IXOTH;
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <filename>.\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    filename = argv[1];
+
+    fd = open(filename, flags, mode);
+    if (fd == -1) {
+        fprintf(stderr, "Err. open() failed: %s.\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    close(fd);
     return(EXIT_SUCCESS);
 }
