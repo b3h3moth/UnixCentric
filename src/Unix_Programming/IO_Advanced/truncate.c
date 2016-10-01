93cf6eeb0fb9dbdb24c23f4e09689bcb1c7bda7d
B3h3m0th <behemoth@autistici.org>
2016-10-01 19:51:50 +0200

    copy input file /etc/fstab into output file

--- src/Unix_Programming/IO_Advanced/truncate.c
+++ src/Unix_Programming/IO_Advanced/truncate.c
@@ -5,26 +5,38 @@
 #include <errno.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+
+enum { SIZE = 512, FILE_SIZE = 2025 };
 
 int main(int argc, char *argv[]) {
-    int fd;
-    char *filename;
-    int flags = O_WRONLY | O_CREAT;
-    int mode = S_IRWXU | S_IXGRP | S_IRWXG | S_IROTH | S_IXOTH;
+    int in_fd, out_fd, nr;
+    char buf[FILE_SIZE];
+    int in_flags = O_RDONLY;
+    int out_flags = O_WRONLY | O_CREAT;
+    int out_mode = S_IRWXU | S_IXGRP | S_IRWXG | S_IROTH | S_IXOTH;
 
-    if (argc != 2) {
+    if (argc != 3) {
         fprintf(stderr, "Usage: %s <filename>.\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    filename = argv[1];
-
-    fd = open(filename, flags, mode);
-    if (fd == -1) {
-        fprintf(stderr, "Err. open() failed: %s.\n", strerror(errno));
+    in_fd = open(argv[1], in_flags);
+    if (in_fd == -1) {
+        fprintf(stderr, "Err. open() input failed: %s.\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
 
-    close(fd);
+    out_fd = open(argv[2], out_flags, out_mode);
+    if (out_fd == -1) {
+        fprintf(stderr, "Err. open() output failed: %s.\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    if ((nr = read(in_fd, buf, FILE_SIZE)) != -1)
+        write(out_fd, buf, FILE_SIZE);
+
+    close(in_fd);
+    close(out_fd);
     return(EXIT_SUCCESS);
 }
