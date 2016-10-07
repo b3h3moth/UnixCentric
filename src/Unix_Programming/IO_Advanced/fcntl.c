4291398bafce27ec713347c2689e782261181c2b
B3h3m0th <behemoth@autistici.org>
2016-10-07 20:39:37 +0200

    testing file descriptor with fcntl()

--- src/Unix_Programming/IO_Advanced/fcntl.c
+++ src/Unix_Programming/IO_Advanced/fcntl.c
@@ -5,47 +5,18 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-/*
-HEADER   : <fcntl.h>
-PROTOTYPE: int fcntl(int fd, int cmd, ... < arg > );
-La funzione fcntl() e' una sorta di punto di accesso per l'esecuzione di 
-diverse operazioni sul file descriptor. Il primo argomento 'fd' e' un file
-descriptor aperto, il secondo argomento 'cmd' indica il tipo di operazione da
-eseguire, il terzo argomento 'arg' e' opzionale, se utilizzarlo o meno e' 
-determinato da 'cmd'.
-I valori di ritorno sono diversi a seconda dell'operazione eseguita, in caso di
-errore ritorna sempre -1.
+/* Lo scopo del programma e' di fornire informazioni sul file descriptor aperto
+mediante la funzione fcntl()
 
-La funzione fcntl() puo' modificare le proprieta' di un file descriptor, 
-tuttavia il numero degli argomenti, il valore di ritorno, errori compresi, 
-sono strettamente legati al valore della variabile intera 'cmd', che puo' 
-assumere i seguenti valori:
-
-F_DUPFD, duplica il file descriptor;
-F_GETFD, ritorna il valore del file descriptor flag;
-F_SETFD, setta il valore del file descpritor flag;
-F_GETFL, ritorna il valore del file status flag;
-F_SETFL, setta il valore del file status flag;
-F_GETLK, ritorna informazioni sul record locking;
-F_SETLK, setta informazioni sul record locking;
-F_SETLKW, come sopra, non ritorna subito ma attende il rilascio del blocco;
-F_GETOWN, ritorna il PID o il PGID, preposto alla ricezione dei segnali SIGURS;
-F_SETOWN, setta il PID o il PGID, preposto alla ricezione dei segnali SIGURS;
-F_GETSIG, ritorna il segnale inviato quando l'input o l'output e' disponibile;
-F_SETSIG, setta il segnale inviato quando l'input o l'output e' disponibile;
-F_SETLEASE, imposta o rimuove un file lease; 
-F_GETLEASE, setta un file lease;
-F_NOTIFY, attiva un meccanismo di notifica.
-
-Una delle operazioni piu' importanti realizzabili con fcntl() e' il blocco in 
-lettura o in scrittura di un file descriptor. */
+$ ./a.out 0 < /dev/fd/0
+$ ./a.out 1 < /dev/fd/0
+*/
 
 int main(int argc, char *argv[]) {
     int val;
     
     if (argc < 2) {
     	fprintf(stderr, "Usage: %s <N file descriptor>\n", argv[0]);
-	    /* es: ./a.out 0 < /etc/fstab */
 	    exit(EXIT_FAILURE);
     }
 
