0a8ec1a63f8338e0674a686578680124c9bd4856
B3h3m0th <behemoth@autistici.org>
2016-10-13 19:44:12 +0200

    info about edata, etext, end extern variables

--- src/Unix_Programming/Process_Control/memory_layout_addresses.c
+++ src/Unix_Programming/Process_Control/memory_layout_addresses.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Su diverse implementazioni UNIX l'ambiente di programmazione C fornisce
+alcune variabili globali per ottenere l'indirizzo del successivo byte di
+specifici segmenti del layout della memoria di un processo. Essi sono:
+- etext, la fine del segmento 'text';
+- edata, la fine del segmento 'data' (dati inizializzati);
+-   end, la fine del segmento 'bss' (dati non inizializzati).
+*/
+
 extern char etext, edata, end;
 int main(void) {
     printf("%x %x %x\n", (int)&etext, (int)&edata, (int)&end);
