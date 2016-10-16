95c45acda137eb1b1cc3443779838c23f1f53d55
B3h3m0th <behemoth@autistici.org>
2016-10-16 18:43:53 +0200

    Process Environment List, fixed

--- src/Unix_Programming/Process_Environment/03_environment_list.c
+++ src/Unix_Programming/Process_Environment/03_environment_list.c
@@ -2,19 +2,19 @@
 #include <stdlib.h>
 
 /*
-Ogni programma ha a dispozione, oltre ai parametri passati alla funzione main(),
-una "environment list", ovvero un puntatore ad un vettore di stringhe, 
-all'interno del quale ciascuna stringa (null-terminated) indica una variabile 
-d'ambiente, nella forma "nome = valore"; ogni variabile d'ambiente contiene 
+Ogni programma ha a disposizione, oltre ai parametri passati alla funzione 
+main(), una 'environment list', ovvero un puntatore ad un vettore di array
+NULL-terminated, all'interno del quale ciascuna stringa indica una variabile 
+d'ambiente nella forma "nome = valore". Ogni variabile d'ambiente contiene 
 specifiche informazioni sull'ambiente in cui il programma sta girando.
 
 Le variabili d'ambiente sono settate, generalmente, al login, tuttavia 
 l'implementazione dipende essclusivamente dal sistema in uso; POSIX si occupa
-della definizione delle variabili d'ambiente e non ISO C.
+della definizione delle variabili d'ambiente, per cui non ISO C.
 
-E' possibile accedere alla lista delle variabili d'ambiente "environment list"
-mediante la variabile (globale) esterna 'environ', oppure utilizzando un terzo 
-argomento della funzione main(), envp[], altamente sconsigliato sia da ISO C 
+E' possibile accedere alla lista delle variabili d'ambiente 'environment list'
+mediante la variabile globale esterna 'environ', oppure utilizzando un terzo 
+argomento della funzione main(), *envp[], altamente sconsigliato sia da ISO C 
 sia da POSIX.
 
 Vi sono diversi comandi che consentono di ottenere la lista delle variabili
