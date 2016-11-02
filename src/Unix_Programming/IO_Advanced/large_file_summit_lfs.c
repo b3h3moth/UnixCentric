aa4084c0d29b75a992b0609c1fe4027e2ef8f872
B3h3m0th <behemoth@autistici.org>
2016-11-02 20:33:21 +0100

    stat, fcntl headers and command line parameters added

--- src/Unix_Programming/IO_Advanced/large_file_summit_lfs.c
+++ src/Unix_Programming/IO_Advanced/large_file_summit_lfs.c
@@ -1,9 +1,10 @@
+#define _LARGEFILE64_SOURCE // Working with large files
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
-#define _LARGEFILE64_SOURCE
-
-int main(void) {
+int main(int argc, char *argv[]) {
 
     return(EXIT_SUCCESS);
 }
