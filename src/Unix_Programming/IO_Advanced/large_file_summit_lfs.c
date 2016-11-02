617e0fa2ade618e93496514df3cff13e6799de1c
B3h3m0th <behemoth@autistici.org>
2016-11-02 20:48:00 +0100

    open64() large file

--- src/Unix_Programming/IO_Advanced/large_file_summit_lfs.c
+++ src/Unix_Programming/IO_Advanced/large_file_summit_lfs.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <string.h>
+#include <errno.h>
 #include <sys/stat.h>
 
 int main(int argc, char *argv[]) {
@@ -13,6 +15,11 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    fd = open64(argv[1], O_RDWR 
+    fd = open64(argv[1], O_RDWR | O_CREAT);
+    if (fd == -1) {
+        fprintf(stderr, "Failed to open file: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
     return(EXIT_SUCCESS);
 }
