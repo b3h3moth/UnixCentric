c15d30ed12da8e0f15854a470784061a6d38ff13
B3h3m0th <behemoth@autistici.org>
2016-11-07 20:24:39 +0100

    testing user_name_from_id()

--- src/Unix_Programming/Users_and_Groups/full_programs/main.c
+++ src/Unix_Programming/Users_and_Groups/full_programs/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include "user_group_convertions.h"
 
 int main(void) {
+    uid_t uid = getuid();
+    char *my_uid = user_name_from_id(uid);
+    printf("My username is: %s\n", my_uid);
+
     return(EXIT_SUCCESS);
 }
