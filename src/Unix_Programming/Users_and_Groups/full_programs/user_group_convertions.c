3e825a120efcbbe78ddeae6c1ca000b87fa516d8
B3h3m0th <behemoth@autistici.org>
2016-11-07 20:39:54 +0100

    userid_from_username() implementation

--- src/Unix_Programming/Users_and_Groups/full_programs/user_group_convertions.c
+++ src/Unix_Programming/Users_and_Groups/full_programs/user_group_convertions.c
@@ -14,11 +14,32 @@ char *username_from_userid(uid_t uid) {
     if (pwd == NULL) {
         if (errno == 0) {
             fprintf(stderr, "Account Not Found.\n");
-            return NULL;
+            return -1;
         } else {
             fprintf(stderr, "Err. getpwuid() Failed.\n");
-            return NULL;
+            return -1;
         }
     } else
         return pwd->pw_name;
+}
+
+// Converte user name in user ID
+uid_t userid_from_username(const char *username) {
+    struct passwd *pwd;
+    uid_t uid;
+
+    if (username == NULL || username == '\0')
+        return -1;
+
+    pwd = getpwnam(username);
+    if (pwd == NULL) {
+        if (errno == 0) {
+            fprintf(stderr, "Account Not Found.\n");
+            return -1;
+        } else {
+            fprintf(stderr, "Err. getpwnam() Failed.\n");
+            return -1;
+        }
+    } else
+        return pwd->pw_uid;
 }
