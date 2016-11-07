d6349547fa8b0a32296813d066b81296cfe7347c
B3h3m0th <behemoth@autistici.org>
2016-11-07 21:28:24 +0100

    testing userid_from_username()

--- src/Unix_Programming/Users_and_Groups/full_programs/main.c
+++ src/Unix_Programming/Users_and_Groups/full_programs/main.c
@@ -5,8 +5,11 @@
 
 int main(void) {
     uid_t uid = getuid();
-    char *my_uid = username_from_userid(uid);
-    printf("My username is: %s\n", my_uid);
+    char *my_user_name = username_from_userid(uid);
+    uid_t my_uid = userid_from_username(my_user_name);
+    
+    printf("My user name is: %s\n", my_user_name);
+    printf("My user ID is: %d\n", my_uid);
 
     return(EXIT_SUCCESS);
 }
