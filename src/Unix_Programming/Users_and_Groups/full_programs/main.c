d6ad7695b43487241e5ab778c1f0297d4849ecfa
B3h3m0th <behemoth@autistici.org>
2016-11-08 19:01:09 +0100

    testing groupname_from_groupid()

--- src/Unix_Programming/Users_and_Groups/full_programs/main.c
+++ src/Unix_Programming/Users_and_Groups/full_programs/main.c
@@ -7,9 +7,11 @@ int main(void) {
     uid_t uid = getuid();
     char *my_user_name = username_from_userid(uid);
     uid_t my_uid = userid_from_username(my_user_name);
+    char *my_group_name = groupname_from_groupid(uid);
     
     printf("My user name is: %s\n", my_user_name);
     printf("My user ID is: %d\n", my_uid);
+    printf("My group name is: %s\n", my_group_name);
 
     return(EXIT_SUCCESS);
 }
