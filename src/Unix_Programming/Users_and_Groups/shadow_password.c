4b567a6f1372e4a966cef5b35582089bee6c2085
B3h3m0th <behemoth@autistici.org>
2016-11-09 22:50:52 +0100

    shadow password informations

--- src/Unix_Programming/Users_and_Groups/shadow_password.c
+++ src/Unix_Programming/Users_and_Groups/shadow_password.c
@@ -4,17 +4,21 @@
 #include <string.h>
 #include <unistd.h>
 #include <shadow.h>
+#include <sys/types.h>
 
 /*
-Nel file /etc/passwd, come si puo' facilmente notare, il secondo campo relativo
-alla password e' criptato, tale file oltre alla password stessa contiene diverse
-informazioni relative agli account del sistema; i file di riferimento sono:
-- /etc/shadow ;
-- /etc/gshadow ;
-- /etc/shadow- (backup).
+Nel file /etc/passwd il secondo campo relativo alle password e' solitamente
+criptato, per cui contiene solo il carattere 'x', la password invece viene
+salvata nel file /etc/shadow, che oltre alla password crittata degli account
+contiene anche diverse informazioni relative agli account stessi del sistema.
 
-Il file /etc/shadow non e' "world readable", pertanto solo 'root' puo' leggerlo 
-e modificarlo, contiene generalmente 9 campi:
+I file di riferimento sono:
+- /etc/shadow
+- /etc/gshadow
+- /etc/shadow- (backup)
+
+Il file /etc/shadow non e' "world readable", pertanto solo l'utente 'root' puo'
+leggerlo, contiene generalmente 9 campi:
 1 - user login name;
 2 - encrypted password;
 3 - date of last password change;
@@ -46,14 +50,14 @@ tra cui:
 HEADER    : <shadow.h>
 PROTOTYPE : struct spwd *getspnam(const char *name);
             struct spwd *getspent(void);
-	    void setspent(void);
-	    void endspent(void);
+	        void setspent(void);
+	        void endspent(void);
 SEMANTICS : La funzione getspnam() ritorna un puntatore alla struttura
             contenente il campo relativo alla corrispondenza 'name'; la funzione
-	    getspent() ritorna un puntatore alla prossima voce dello shadow
-	    password database; la funzione setspent() riavvolge lo shadow 
-	    password database; la funzione endspent() chiude lo shadow password
-	    database dopo che tutte le operazioni si sono cosnluse.
+	        getspent() ritorna un puntatore alla prossima voce dello shadow
+	        password database; la funzione setspent() riavvolge lo shadow 
+	        password database; la funzione endspent() chiude lo shadow password
+	        database dopo che tutte le operazioni si sono cosnluse.
 RETURNS   : Un puntatore alla struttura spwd in caso di successo, NULL in caso 
             di errore
 --------------------------------------------------------------------------------
