8f83c82654d383bc58c476edf1c7b225c2a4dc57
B3h3m0th <behemoth@autistici.org>
2016-11-10 18:38:56 +0100

    allocate memory for the username

--- src/C/libraries/cryptography/crypt/check_passwd.c
+++ src/C/libraries/cryptography/crypt/check_passwd.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
+#include <string.h>
 
 int main(void) {
+    char *username;
     long login_max = 0;
 
     // Get the maximum size of a username on the host (tipically 256)
     login_max = sysconf(_SC_LOGIN_NAME_MAX);
     if (login_max < 0)
         login_max = 256;
+
+    username = malloc(login_max);
+    if (username == NULL) {
+        fprintf(stderr, "Err.malloc() failed: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
 
     return(EXIT_SUCCESS);
 }
