5e037243f9b1656ea709e1abbfd60f1a98540415
B3h3m0th <behemoth@autistici.org>
2016-11-10 18:25:44 +0100

    Get the maximum size of a username on the host, sysconf()

--- src/C/libraries/cryptography/crypt/check_passwd.c
+++ src/C/libraries/cryptography/crypt/check_passwd.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main(void) {
+    long login_max = 0;
+
+    // Get the maximum size of a username on the host
+    login_max = sysconf(_SC_LOGIN_NAME_MAX);
+
     return(EXIT_SUCCESS);
 }
