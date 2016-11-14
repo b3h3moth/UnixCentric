8c24b726009855144ad0162db290c2f0c2ee869b
B3h3m0th <behemoth@autistici.org>
2016-11-14 19:37:46 +0100

    minor updates about program's semantics

--- src/Unix_Programming/Process_Credentials/get_real_and_effective_IDs.c
+++ src/Unix_Programming/Process_Credentials/get_real_and_effective_IDs.c
@@ -4,13 +4,14 @@
 #include <string.h>
 #include <unistd.h>
 
-/* Lo scopo del programma e' di stampare le credenziali di un processo mediante
-funzioni specifiche. */
+/* Lo scopo del programma e' di stampare le credenziali di un processo, nello
+specifico, oltre al PID e PPID, i 'Real' e 'Effective' ID. */
 
 int main(void) {
 
    printf("                  Process-ID (PID): %d\n", getpid());
    printf("          Parent Process-ID (PPID): %d - shell\n", getppid());
+
    printf("           Real User-ID (real-UID): %d\n", getuid());
    printf(" Effective User-ID (effective-UID): %d\n", geteuid());
    printf("          Real Group-ID (real-GID): %d\n", getgid());
