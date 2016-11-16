7996833eff973a54113f672f9789655a98081dd7
B3h3m0th <behemoth@autistici.org>
2016-11-17 00:38:03 +0100

    how program works, its semantics

--- src/Unix_Programming/Process_Credentials/setuid.c
+++ src/Unix_Programming/Process_Credentials/setuid.c
@@ -5,6 +5,11 @@
 #include <unistd.h>
 #include <pwd.h>
 
+/* Lo scopo del programma e' di mostrare come un file 'riservato' possa essere
+letto da un programma con i setuid bits attivati. Pertanto dopo la fase di 
+compilazione, sia i permessi sia il proprietario dell'esseguibile devono essere 
+modificati attivando i setuid bits, utilizzando chmod e chown. */
+
 int main(int argc, char *argv[]) {
     FILE *fp;
 	int c;
