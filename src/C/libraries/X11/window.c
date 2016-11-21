7951ded16f8ff6e32dc49e5d7a399f2cfe949f27
B3h3m0th <behemoth@autistici.org>
2016-11-21 20:24:56 +0100

    create a window with X11, basics

---
+++ src/C/libraries/X11/window.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <X11/X.h>
+#include <X11/Xlib.h>
+#include <unistd.h>
+
+/* Lo scopo del programma e' la creazione di una 'window'.
+
+Per compilare il programma:
+$ gcc (clang) window.c -L/usr/X11R6/lib -lX11 -std=c11 -Wall -pedantic
+*/
+
+int main(void) {
+    Display *display;
+	Window window, rootwindow;
+	int screen;
+
+	display = XOpenDisplay(NULL);
+	screen = DefaultScreen(display);
+	rootwindow = RootWindow(display, screen);
+	window = XCreateSimpleWindow(display, rootwindow, 0, 0, 100, 100, 1, 0, 0);
+	XMapWindow(display, window);
+	XFlush(display);
+
+	sleep(3);
+	XCloseDisplay(display);
+	return(EXIT_SUCCESS);
+}
