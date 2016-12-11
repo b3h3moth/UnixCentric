2a954835a5004d00914cf758b7325e7e48dca314
B3h3m0th <behemoth@autistici.org>
2016-12-11 13:27:45 +0100

    removed comments about gettimeoftheday()

--- src/Unix_Programming/Date_and_Time/calendar_time_2_gettimeoftheday.c
+++ src/Unix_Programming/Date_and_Time/calendar_time_2_gettimeoftheday.c
@@ -5,37 +5,8 @@
 #include <sys/time.h>
 #include <unistd.h>
 
-/*
-HEADER    : <sys/time.h>
-PROTOTYPE : int gettimeofday(struct timeval *tv, struct timezone *tz);
-SEMANTICS : La funzione gettimeofday() salva nella struttura timeval 'tv' la 
-            data corrente, a partire da  "Epoch" e puntata da 'tz.
-RETURNS   : 0 In caso di successo, -1 in caso di errore
---------------------------------------------------------------------------------
-Nota: Le funzione utilizza una struttura 'timeval' e una struttura 'timezone'
-definite in <sys/time.h>, il secondo parametro tuttavia e' obsoleto e deve 
-essere sempre posto a NULL.
 
-Precisione al microsecondo:
-
-struct timeval {
-     long tv_sec;	"Secondi"
-     long tv_usec;	"Microsecondi"
-};
-
-Precisione al nanosecondo:
-
-struct timespec {
-     time_t tv_sec;	"Secondi"
-     long   tv_nsec;	"Nanosecondi"
-};
-
-Tale funzione puo' essere indicata per verificare i tempi di reazione di un
-programma o di un pezzo di codice e con opportuni accorgimenti anche per
-stampare la data corrente.
-*/
-
-int main(int argc, char *argv[]) {
+int main(void){
    struct timeval start, end;
    double time_elapsed;
   
