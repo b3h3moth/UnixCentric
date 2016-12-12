12332c78f2ed6e8ff4380cfdae2300c54221749a
B3h3m0th <behemoth@autistici.org>
2016-12-12 19:59:06 +0100

    removed main() parameters

--- src/Unix_Programming/Date_and_Time/ctime.c
+++ src/Unix_Programming/Date_and_Time/ctime.c
@@ -4,32 +4,25 @@
 #include <string.h>
 #include <time.h>
 
-/*
-HEADER    : <time.h>
-PROTOTYPE : char *ctime(const time_t *pt);
-SEMANTICS : La funzione ctime() partendo da un parametro di tipo time_t 'pt'
-            produce una stringa null-terminated, simile al comando 'date'
-RETURNS   : Il puntatore alla stringa in caso di successo, NULL in caso di 
-            errore
---------------------------------------------------------------------------------
-*/
+/* Lo scopo del programma e' di convertire un tipo time_t in una stringa
+mediante l'utilizzo della funzione ctime() */
 
-int main(int argc, char *argv[]) {
-   time_t t;	
-   char *str_time;	
+int main(void) {
+    time_t t;	
+    char *str_time;	
    
-   if (time(&t) < 0) {
-      fprintf(stderr, "Err.(%s) getting time()\n", strerror(errno));
-      exit(EXIT_FAILURE);
-   }
+    if (time(&t) < 0) {
+       fprintf(stderr, "Err.(%s) getting time()\n", strerror(errno));
+       exit(EXIT_FAILURE);
+    }
 
-   /* La conversione in stringa, partendo da un valore di tipo time_t */
-   if ((str_time = ctime(&t)) == NULL) {
-      fprintf(stderr, "Err.(%s) str conversion: ctime()\n", strerror(errno));
-      exit(EXIT_FAILURE);
-   }
+    /* La conversione in stringa, partendo da un valore di tipo time_t */
+    if ((str_time = ctime(&t)) == NULL) {
+       fprintf(stderr, "Err.(%s) str conversion: ctime()\n", strerror(errno));
+       exit(EXIT_FAILURE);
+    }
 
-   printf("%s", str_time);
-
-   return(EXIT_SUCCESS);
+    printf("%s", str_time);
+    
+    return(EXIT_SUCCESS);
 }
