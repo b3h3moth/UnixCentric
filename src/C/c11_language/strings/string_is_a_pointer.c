a1fc4f2a3bad89eab5dd39f9751760ed18f9f7d3
B3h3m0th <behemoth@autistici.org>
2016-12-31 19:02:29 +0100

    moved string at the top of the main()

--- src/C/c11_language/strings/string_is_a_pointer.c
+++ src/C/c11_language/strings/string_is_a_pointer.c
@@ -2,5 +2,15 @@
 #include <stdlib.h>
 
 int main(void) {
+
+    /* E' possibile verificare che una stringa, in C, e' essa stessa un
+    puntatore */
+    char *p =  "A String in C is a Poiner itself";
+
+    for (int i = 0; *(p + i) != '\0'; ++i)
+        printf("%c",  *(p + i) );
+
+    // Non valido con un array numerico.
+
     return(EXIT_SUCCESS);
 }
