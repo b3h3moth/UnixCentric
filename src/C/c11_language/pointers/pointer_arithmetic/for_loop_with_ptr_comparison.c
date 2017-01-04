8abe48fcd96f07b8f420c88da09d111cbed043ee
B3h3m0th <behemoth@autistici.org>
2017-01-04 20:32:15 +0100

    pointer and array, the essence of deep friendship

--- src/C/c11_language/pointers/pointer_arithmetic/for_loop_with_ptr_comparison.c
+++ src/C/c11_language/pointers/pointer_arithmetic/for_loop_with_ptr_comparison.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Lo scopo del programma e' di verificare l'aritmetica dei puntatori, nel caso
+di una comparazione tra gli stessi, nello specifico in un ciclo for */
+
 int main(void) {
     int vec[] = { 100, 200, 300, 400, 500, 600, 700, 800, 900 };
     int *ptr;
+
+    /* Le similitudini tra puntatori ed array vengono fuori in
+    tutta la loro essenza allorquando, nel ciclo for, sono messi
+    a confronto gli indirizzi di memoria del puntatore 'ptr' e del
+    vettore 'vec'. */
 
     for (ptr = vec; ptr <= vec+8; ptr++)
         printf("%d, ", *ptr);
