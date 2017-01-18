1be9d9c7ff6562043472b5a9cce62d5028824b70
B3h3m0th <behemoth@autistici.org>
2017-01-18 19:34:34 +0100

    fixed printf conversion with %zu, lang references, macro

--- src/C/c11_language/strings/06_vector_of_strings_1.c
+++ src/C/c11_language/strings/06_vector_of_strings_1.c
@@ -4,50 +4,56 @@
 
 // Nota "Vector of strings" e "array of pointers" sono sinonimi
 
-#define MAX_STR 10
-#define ARRAY_SIZE 8
+#define STR_LEN 10
+#define ARR_SIZE 7
 
 int main(int argc, char *argv[]) {
    /* E' possibile omettere il numero di righe poichè sara automaticamente
    determinato dal numero degli elementi presenti, è necessario invece inserire
-   il numero delle colonne, in questo caso ARRAY_SIZE.
+   il numero delle colonne, in questo caso STR_LEN.
 
    Nei casi in cui la stringa di caratteri dovesse risultare di minor
-   lunghezza rispetto a MAX_STR, al resto delle celle sarà inserito il
+   lunghezza rispetto a STR_Len, al resto delle celle sarà inserito il
    'null character, vale a dire '\0'.
 
    C'è però un problema evidente, ovver solo 3 stringhe di caratteri
    hanno nomi sufficientemente lunghi, per il resto ci sarà spreco di spazio.
    */
-   char planets[][ARRAY_SIZE] = {"plutone", "nettuno", "giove", "mercurio",
+   char planets[][STR_LEN] = {"plutone", "nettuno", "giove", "mercurio",
                                 "marte", "urano", "terra"};
+
+   printf("Size of 'planets': %zu\n", sizeof(planets));
+   for (int i=0; i<ARR_SIZE; i++)
+      printf("A: %d:%10s (%zu byte)\n", i, planets[i], strlen(planets[i]));
 
    /* Per ovviare allo spreco di byte, come nel caso precedente, ed essere
    anche piu' efficienti, e' consigliabile implementare un puntatore ad un
    array di stringhe, dove ciascuna stringa sara' esattamente delle dimensioni
    richieste. */
-   char *pianeti[ARRAY_SIZE] = {"plutone", "nettuno", "giove", "mercurio",
+   char *pianeti[ARR_SIZE] = {"plutone", "nettuno", "giove", "mercurio",
                                 "marte", "urano", "terra"};
 
-   for (int i=0; i<ARRAY_SIZE-1; i++)
-      printf("A: %d:%10s (%d byte)\n", i, pianeti[i], strlen(pianeti[i]));
+   printf("Size of 'pianeti': %zu\n", sizeof(pianeti));
+   for (int i=0; i<ARR_SIZE; i++)
+      printf("A: %d:%10s (%zu byte)\n", i, pianeti[i], strlen(pianeti[i]));
 
+   puts("\n(dynamic array of pointers)");
    int dim;
-   printf("Grandezza del vettore: ");
+   printf("Please type how many planet do you want: ");
    scanf("%d", &dim);
    char *dyn_planets[dim];
-   char temp[MAX_STR];
+   char temp[STR_LEN];
 
    /* Creazione dinamica di un vettore di stringhe */
    for (int i=0; i<dim; i++) {
-      printf("inserisci il pianeta '%d': ", i);
+      printf("type the planet's name '%d': ", i);
       scanf("%10s", temp);
       dyn_planets[i] = malloc(strlen(temp) * sizeof(char));
       strcpy(dyn_planets[i], temp);
    }
 
    for(int y=0; y<dim; y++)
-      printf("B: %d:%10s (%d byte)\n", y, dyn_planets[y], strlen(dyn_planets[y]));
+      printf("B: %d:%10s (%zu byte)\n", y, dyn_planets[y], strlen(dyn_planets[y]));
 
    return(EXIT_SUCCESS);
 }
