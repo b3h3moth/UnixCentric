aea41d6d0a2cea947de10a95dd07dbd495bcb305
B3h3m0th <behemoth@autistici.org>
2017-05-07 18:38:05 +0200

    removed pathconf() routine

--- src/Unix_Programming/System_Informations_System_Limits/limits_sysconf_pathconf.c
+++ src/Unix_Programming/System_Informations_System_Limits/limits_sysconf_pathconf.c
@@ -5,23 +5,17 @@
 #include <unistd.h>
 
 static void pr_sysconf(char *msg, int name);
-static void pr_pathconf(char *msg, char *path, int name);
 
 /* Lo scopo del programma e' di fornire diverse informazioni circa le
 "limitazioni" del sistema in uso. Da notare le costanti passate alle funzioni,
-quelle che iniziano con _SC sono processate da sysconf(), invece quelle che
-iniziano con _PC sono passate a pathconf() o fpathconf().*/
+quelle che iniziano con _SC sono processate da sysconf(). */
 
-int main (int argc, char *argv[]) {
-
-    if (argc != 2) {
-    	printf ("Usage: %s <dirname>", argv[0]);
-	    exit(EXIT_FAILURE);
-    }
+int main(void) {
 
     pr_sysconf("ARG_MAX                   =", _SC_ARG_MAX);
     pr_sysconf("CHILD_MAX                 =", _SC_CHILD_MAX);
     pr_sysconf("clock ticks/second        =", _SC_CLK_TCK);
+
     pr_sysconf("NGROUPS_MAX               =", _SC_NGROUPS_MAX);
     pr_sysconf("OPEN_MAX                  =", _SC_OPEN_MAX);
 #ifdef	_SC_STREAM_MAX
@@ -33,15 +27,6 @@ int main (int argc, char *argv[]) {
     pr_sysconf("_POSIX_JOB_CONTROL        =", _SC_JOB_CONTROL);
     pr_sysconf("_POSIX_SAVED_IDS          =", _SC_SAVED_IDS);
     pr_sysconf("_POSIX_VERSION            =", _SC_VERSION);
-    pr_pathconf("MAX_CANON                 =", "/dev/tty", _PC_MAX_CANON);
-    pr_pathconf("MAX_INPUT                 =", "/dev/tty", _PC_MAX_INPUT);
-    pr_pathconf("_POSIX_VDISABLE           =", "/dev/tty", _PC_VDISABLE);
-    pr_pathconf("LINK_MAX                  =", argv[1], _PC_LINK_MAX);
-    pr_pathconf("NAME_MAX                  =", argv[1], _PC_NAME_MAX);
-    pr_pathconf("PATH_MAX                  =", argv[1], _PC_PATH_MAX);
-    pr_pathconf("PIPE_BUF                  =", argv[1], _PC_PIPE_BUF);
-    pr_pathconf("_POSIX_NO_TRUNC           =", argv[1], _PC_NO_TRUNC);
-    pr_pathconf("_POSIX_CHOWN_RESTRICTED   =", argv[1], _PC_CHOWN_RESTRICTED);
 
     exit(EXIT_SUCCESS);
 }
@@ -54,28 +39,11 @@ static void pr_sysconf(char *msg, int name)
 
     if ((val = sysconf (name)) < 0) {
     	if (errno != 0) {
-	        perror ("sysconf error");
+	        perror("sysconf error");
 	        exit(EXIT_FAILURE);
 	    }
 
-        fputs (" (not defined)\n", stdout);
+        fputs(" (not defined)\n", stdout);
     } else
-    	printf (" %ld\n", val);
-}
-
-static void pr_pathconf(char *msg, char *path, int name)
-{
-    long val;
-    fputs (msg, stdout);
-    errno = 0;
-
-    if ((val = pathconf (path, name)) < 0) {
-    	if (errno != 0) {
-	        perror ("pathconf error");
-	        exit(EXIT_FAILURE);
-	}
-
-        fputs (" (no limit)\n", stdout);
-    } else
-    	printf (" %ld\n", val);
+    	printf(" %ld\n", val);
 }
