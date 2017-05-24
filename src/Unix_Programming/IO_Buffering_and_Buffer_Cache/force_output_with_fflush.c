e471b098231480412d00c9e609b20a3a65693031
B3h3m0th <behemoth@autistici.org>
2017-05-24 19:52:38 +0200

    added info about buffer cache

--- src/Unix_Programming/IO_Buffering_and_Buffer_Cache/force_output_with_fflush.c
+++ src/Unix_Programming/IO_Buffering_and_Buffer_Cache/force_output_with_fflush.c
@@ -2,14 +2,24 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/* Lo scopo del programma e' di forzare la scrittura dell'output mediante
+l'utilizzo della funzione fflush(). */
+
 int main(void) {
 
-    printf("take a walk on the wild side, by: ");
+    /* la funzione write() invia i propri dati direttamente al buffer cache del
+    kernel, la printf() invece attende che il buffer di userspace sia pieno
+    prima di inviare i dati al buffer cache del kernel per essere scritti.
+
+    Va da sè che, nel caso specifico, l'output sara' invertito */
+    printf("Take a walk on the wild side, by: ");
     write(1, "Lou Reed.\n", 10);
 
     fputc('\n', stdout);
 
-    printf("Stand up for your rights, by: ");
+    /* Per risolvere il problema sopra citato si utilizza la fflush() per
+    scrivere immediatamente i dati in output. */
+    printf("Get-up stand-up, stand-up for your rights, by: ");
     fflush(stdout);
     write(1, "Bob Marley.\n", 12);
 
