cb8b100837cefaa3d98bc2ce5138bfc3ee63e316
B3h3m0th <behemoth@autistici.org>
2017-05-25 20:42:27 +0200

    fix how buffer cache works

--- src/Unix_Programming/IO_Buffering_and_Buffer_Cache/force_output_with_fflush.c
+++ src/Unix_Programming/IO_Buffering_and_Buffer_Cache/force_output_with_fflush.c
@@ -7,18 +7,20 @@ l'utilizzo della funzione fflush(). */
 
 int main(void) {
 
-    /* la funzione write() invia i propri dati direttamente al buffer cache del
-    kernel, la printf() invece attende che il buffer di userspace sia pieno
-    prima di inviare i dati al buffer cache del kernel per essere scritti.
+    /* La funzione write() invia i propri dati direttamente alla buffer cache
+    del kernel, la printf() invece attende che il buffer di userspace sia pieno
+    prima di inviare i dati alla medesima buffer cache del kernel per essere
+    scritti.
 
-    Va da sè che, nel caso specifico, l'output sara' invertito */
+    Va da sè che, nel caso specifico, l'output scritto sul terminale potrebbe
+    subire delle variazioni consistenti. */
     printf("Take a walk on the wild side, by: ");
     write(1, "Lou Reed.\n", 10);
 
     fputc('\n', stdout);
 
-    /* Per risolvere il problema sopra citato si utilizza la fflush() per
-    scrivere immediatamente i dati in output. */
+    /* Per risolvere il problema sopra esposto si utilizza la fflush() per
+    scaricare i dati, ovvero il buffer, in output. */
     printf("Get-up stand-up, stand-up for your rights, by: ");
     fflush(stdout);
     write(1, "Bob Marley.\n", 12);
