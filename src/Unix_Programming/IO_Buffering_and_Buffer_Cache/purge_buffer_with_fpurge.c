9590de498e6b96ee81c231655c499dfa1774c0fe
B3h3m0th <behemoth@autistici.org>
2017-05-26 19:43:58 +0200

    fix ascii

--- src/Unix_Programming/IO_Buffering_and_Buffer_Cache/purge_buffer_with_fpurge.c
+++ src/Unix_Programming/IO_Buffering_and_Buffer_Cache/purge_buffer_with_fpurge.c
@@ -3,19 +3,19 @@
 #include <unistd.h>
 #include <stdio_ext.h>
 
-/* Lo scopo del programma è di scartare l'output pendente mediante
+/* Lo scopo del programma e' di scartare l'output pendente mediante
 l'utilizzo della funzione __fpurge().
 
-La funzione __fpurge() non è ne' standard ne' portabile, è stata introdotta in
-Solaris, presente nella libreria glibc.*/
+La funzione __fpurge() non e' ne' standard ne' portabile, e' stata introdotta
+in Solaris, presente nella libreria glibc.*/
 
 int main(void) {
 
     /* A differenza della funzione fflush() che forza la scrittura del buffer
     cache, la funzione __fpurge() pulisce il buffer dello stream fornito.
 
-    Nel caso specifico sarà scartato tutto l'output non ancora scritto, per cui
-    la funzione printf() non produrrà alcun output. */
+    Nel caso specifico sara' scartato tutto l'output non ancora scritto, per
+    cui la funzione printf() non produrra' alcunche' */
 
     printf("Get-up stand-up, stand-up for your rights, by: ");
     __fpurge(stdout);
