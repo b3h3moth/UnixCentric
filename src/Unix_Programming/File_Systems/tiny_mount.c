829ea57d4dc465b9b749baadd3227fb410812615
B3h3m0th <behemoth@autistici.org>
2017-06-01 20:14:37 +0200

    tiny mount() skeleton

---
+++ src/Unix_Programming/File_Systems/tiny_mount.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+
+/*
+HEADER    : 
+PROTOTYPE : 
+SEMANTICS : 
+RETURNS   : 0 in caso di successo, -1 in caso di errore
+--------------------------------------------------------------------------------
+*/
+
+int main(int argc, char *argv[]) {
+    return(EXIT_SUCCESS);
+}
