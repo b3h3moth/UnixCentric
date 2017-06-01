79501a58da84f634d19fa95fc53bc88783f97f68
B3h3m0th <behemoth@autistici.org>
2017-06-01 20:16:18 +0200

    fix UP skeleton

--- src/Unix_Programming/File_Systems/tiny_mount.c
+++ src/Unix_Programming/File_Systems/tiny_mount.c
@@ -1,17 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
-#include <string.h>
-#include <unistd.h>
 
-/*
-HEADER    : 
-PROTOTYPE : 
-SEMANTICS : 
-RETURNS   : 0 in caso di successo, -1 in caso di errore
---------------------------------------------------------------------------------
-*/
-
-int main(int argc, char *argv[]) {
+int main(void) {
     return(EXIT_SUCCESS);
 }
