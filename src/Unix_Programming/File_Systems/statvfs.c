953eddbaaf2eff90993128958ee3fecdeb0a93c2
B3h3m0th <behemoth@autistici.org>
2017-06-15 17:45:29 +0200

    the program's purpose

--- src/Unix_Programming/File_Systems/statvfs.c
+++ src/Unix_Programming/File_Systems/statvfs.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Lo scopo del programma e' di ottenere informazioni su un file system montato.
+Il device relativo al file siystem viene fornito in input. */
+
 int main(int argc, char *argv[]) {
     return(EXIT_SUCCESS);
 }
