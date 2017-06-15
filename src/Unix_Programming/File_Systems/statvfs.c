6948a0bffc506925fa40408006d21ab3d4270fe3
B3h3m0th <behemoth@autistici.org>
2017-06-15 18:10:09 +0200

    statvfs header and variable

--- src/Unix_Programming/File_Systems/statvfs.c
+++ src/Unix_Programming/File_Systems/statvfs.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/statvfs.h>
 
 /* Lo scopo del programma e' di ottenere informazioni su un file system montato.
 Il device relativo al file siystem viene fornito in input. */
@@ -8,6 +9,7 @@ Il device relativo al file siystem viene fornito in input. */
 int main(int argc, char *argv[]) {
     char *pathname = NULL;
     size_t len = 0;
+    struct statvfs *device = NULL;
 
     if (argc != 2) {
         fprintf(stdout, "Usage: %s 'pathname'\n", argv[0]);
@@ -17,6 +19,7 @@ int main(int argc, char *argv[]) {
     len = strlen(argv[1]) + 1;
     pathname = malloc(len * sizeof(char));
     strncpy(pathname, argv[1], len);
+
 
     return(EXIT_SUCCESS);
 }
