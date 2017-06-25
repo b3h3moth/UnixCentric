3f6b9dfda7598f56560b1b0ed1d5301d29057885
B3h3m0th <behemoth@autistici.org>
2017-06-25 19:59:18 +0200

    argument control added

--- src/Unix_Programming/File_System/full_programs/removing_link.c
+++ src/Unix_Programming/File_System/full_programs/removing_link.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <string.h>
 #include <sys/stat.h>
 
 enum {CMD_SZ=200, BUF_SZ=1024};
@@ -11,7 +12,10 @@ int main(int argc, char *argv[]) {
     char cmd[CMD_SZ];   /* Comando da eseguire via shell */
     char buf[BUF_SZ];   /* Random byte da scrivere nel file */
 
-
+    if (argc < 2 || strcmp(argv[1], "--help") || strcmp(argv[1], "-h") == 0) {
+        fprintf(stderr, "Usage: %s tmpfile [num 1kb blocks]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
 
     exit(EXIT_SUCCESS);
 }
