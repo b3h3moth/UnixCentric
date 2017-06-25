99f2487a756ea25ea91e7355426084942f73196a
B3h3m0th <behemoth@autistici.org>
2017-06-25 19:54:13 +0200

    local variables added

--- src/Unix_Programming/File_System/full_programs/removing_link.c
+++ src/Unix_Programming/File_System/full_programs/removing_link.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+enum {CMD_SZ=200, BUF_SZ=1024};
 
 int main(int argc, char *argv[]) {
+    int fd, n_blks;
+    char cmd[CMD_SZ];   /* Comando da eseguire via shell */
+    char buf[BUF_SZ];   /* Random byte da scrivere nel file */
+
+
+
     exit(EXIT_SUCCESS);
 }
