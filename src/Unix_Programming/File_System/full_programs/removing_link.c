f2a3c4712a13db61d6fd87fe4b98d06c8fca36e9
B3h3m0th <behemoth@autistici.org>
2017-06-25 20:11:55 +0200

    open fd and fills n_blocks

--- src/Unix_Programming/File_System/full_programs/removing_link.c
+++ src/Unix_Programming/File_System/full_programs/removing_link.c
@@ -5,6 +5,9 @@
 #include <string.h>
 #include <sys/stat.h>
 
+#define MODES (O_WRONLY | O_CREAT | O_EXCL)
+#define PERMS (S_IRUSR | S_IWUSR)
+
 enum {CMD_SZ=200, BUF_SZ=1024};
 
 int main(int argc, char *argv[]) {
@@ -12,10 +15,21 @@ int main(int argc, char *argv[]) {
     char cmd[CMD_SZ];   /* Comando da eseguire via shell */
     char buf[BUF_SZ];   /* Random byte da scrivere nel file */
 
-    if (argc < 2 || strcmp(argv[1], "--help") || strcmp(argv[1], "-h") == 0) {
-        fprintf(stderr, "Usage: %s tmpfile [num 1kb blocks]\n", argv[0]);
+    if (argc < 2) {
+        if (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) {
+            fprintf(stderr, "Usage: %s tmpfile [num 1kb blocks]\n", argv[0]);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    n_blks = strtol(argv[2], NULL, 10);
+
+    if ((fd = open(argv[1], MODES, PERMS)) == -1) {
+        fprintf(stderr, "Err. %d, open() %s\n", errno, strerror(errno));
         exit(EXIT_FAILURE);
     }
+
+
 
     exit(EXIT_SUCCESS);
 }
