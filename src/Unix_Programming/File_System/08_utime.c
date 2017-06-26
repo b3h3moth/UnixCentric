6d03e5035a34c873d9b161276b7d671cba7a367c
B3h3m0th <behemoth@autistici.org>
2017-06-26 19:52:51 +0200

    fixed usage and blocks

--- src/Unix_Programming/File_System/08_utime.c
+++ src/Unix_Programming/File_System/08_utime.c
@@ -44,63 +44,62 @@ $ ls -lu (il campo dell'orario si riferisce all'ultimo accesso);
 $ ls -lc (il campo dell'orario si riferisce all'ultimo cambiamento di stato).
 */
 
+/*  Lo scopo del programma e' la modifica di uno o piu' file azzerandoli, 
+e successivamente modificare anche il time mediante utime() */
 int main(int argc, char *argv[]) {
-   int fd, i;
-   struct stat bufstat;
-   struct utimbuf bufutime;
+    int fd, i;
+    struct stat bufstat;
+    struct utimbuf bufutime;
 
-   if (argc < 2) {
-      fprintf(stderr, "Uso: %s <filename1>...<filenameN>\n", argv[0]);
-      exit(EXIT_FAILURE);
-   }
+    if (argc < 2) {
+       fprintf(stderr, "Usage: %s <filename1>...<filenameN>\n", argv[0]);
+       fprintf(stderr, "Be careful, each file will be truncated to zero.\n");
+       exit(EXIT_FAILURE);
+    }
 
-   /*
-    La funzione stat() verifica la presenza del file ricevuto in input, qualora
-    dovesse filare tutto liscio esso passa alla successiva funzione open(), che
-    provvede ad aprirlo in lettura e scrittura, nonche', con O_TRUNC, ne azzera
-    il contenuto; il file pertanto e' stato acceduto e modificato, per cui, in
-    teoria, dovrebbe essere modificato sia l'orario di accesso sia l'orario di
-    modifica.
-   */
-   for (i=1; i<argc; i++) {
-      if (stat(argv[i], &bufstat) < 0) {
-      	 fprintf(stderr, "Err.:(%d) - %s on %s\n", errno, strerror(errno), argv[i]);
-	 continue;
-      }
+    /* La funzione stat() verifica la presenza del file ricevuto in input, 
+    in caso di esito positivo la funzione open() lo aprira' in lettura e 
+    scrittura, nonche', mediante il flag O_TRUNC, ne azzerera' il contenuto. 
+    
+    Il file pertanto essendo stato acceduto e modificato, dovrebbe vedersi 
+    modificato sia l'orario di accesso sia l'orario di modifica. */
+    for (i=1; i<argc; i++) {
+        if (stat(argv[i], &bufstat) < 0) {
+            fprintf(stderr, "Err.:(%d) - %s on %s\n", errno, strerror(errno), 
+                    argv[i]);
+	    continue;
+        }
 
-      if ((fd = open(argv[i], O_RDWR | O_TRUNC)) < 0) {
-      	 fprintf(stderr, "Err.:(%d) - %s on %s\n", errno, strerror(errno), argv[i]);
-	 continue;
-      }
+        if ((fd = open(argv[i], O_RDWR | O_TRUNC)) < 0) {
+      	     fprintf(stderr, "Err.:(%d) - %s on %s\n", errno, strerror(errno),
+                     argv[i]);
+	    continue;
+        }
 
-      close(fd);
+        close(fd);
 
-      /*
-       Nonostante il file sia stato azzerato, le date di accesso e di modifica
-       non saranno toccate, o meglio, i rispettivi campi saranno impostati con
-       l'orario di creazione del file stessi, per cui "apparentemente"
-       resteranno immutati.
-      */
-      bufutime.actime = bufstat.st_atime;
-      bufutime.modtime =  bufstat.st_mtime;
+    /* Nonostante il file sia stato azzerato, le date di accesso e di modifica
+    non saranno toccate, o meglio, i rispettivi campi saranno impostati con
+    l'orario di creazione del file stessi, per cui "apparentemente"
+    resteranno immutati.*/
+        bufutime.actime = bufstat.st_atime;
+        bufutime.modtime =  bufstat.st_mtime;
 
-      /*
-       Le date ora sono resettate al valore attuale, tuttavia solo la data
-       relativa al cambiamento di stato sara' resettata, le altre due, come si
-       e' mostrato pocanzi, non saranno toccate.
-      */
-      if (utime(argv[i], &bufutime) < 0) {
-      	 fprintf(stderr, "Err.:(%d) - %s on %s\n", errno, strerror(errno), argv[i]);
-	 continue;
-      }
+    /* Le date ora sono resettate al valore attuale, tuttavia solo la data
+    relativa al cambiamento di stato sara' resettata, le altre due, come si
+    e' mostrato pocanzi, non saranno toccate. */
+        if (utime(argv[i], &bufutime) < 0) {
+      	     fprintf(stderr, "Err.:(%d) - %s on %s\n", errno, strerror(errno),
+                     argv[i]);
+	    continue;
+        }
 
-      /*
-       Per verificare il buon esito e' sufficiente eseguire i seguenti passi:
-       $ >FILE_1 && >FILE_2
-       $ stat FILE_*
-       $ ./a.out FILE_1 FILE_2
-       Si notera' che i campi Access e Modify non sono stato modificati.
-      */
+    /* Per verificare il buon esito e' sufficiente eseguire i seguenti passi:
+    $ >FILE_1 && >FILE_2
+    $ stat FILE_*
+    $ ./a.out FILE_1 FILE_2
+    Si notera' che i campi Access e Modify non sono stato modificati.
+    */
    }
 
    return(EXIT_SUCCESS);
