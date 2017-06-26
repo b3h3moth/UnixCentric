d60e872fae55afcf3071f21df1d442c04fd3f041
B3h3m0th <behemoth@autistici.org>
2017-06-26 19:20:27 +0200

    fixed readlink() data and buf

--- src/Unix_Programming/File_System/07_symbolic_link.c
+++ src/Unix_Programming/File_System/07_symbolic_link.c
@@ -4,8 +4,10 @@
 #include <errno.h>
 #include <string.h>
 #include <fcntl.h>
+#include <sys/stat.h>
 
 #define BUF_SIZE 1024
+#define PERMS (S_IRWXU | S_IRGRP | S_IROTH)
 
 /*
 HEADER    : <unistd.h>
@@ -16,25 +18,25 @@ PROTOTYPE : int symlink(const char *actualpath, const char symlinkpath);
 
 SEMANTICS : symlink() crea una nuova voce di directory 'symlinkpath', essa punta
             ad 'actualpath'; readlink() consente la lettura del symbolic link
-	    'pathname' nel buffer 'buf' di dimensione 'bufsize'.
+	        'pathname' nel buffer 'buf' di dimensione 'bufsize'.
 
 RETURNS   : symlink()  - 0 In caso di successo, -1 in caso di errore
             readlink() - il numero dei byte letti in caso di successo, 0 in caso
-	    di errore.
---------------------------------------------------------------------------------
+	        di errore.
 */
 
 int main(int argc, char *argv[]) {
    int fd;
    char buf[BUF_SIZE];
+   ssize_t buf_len;
 
    if (argc < 3) {
-      fprintf(stderr, "Uso: %s <origine> <symbolic-link>\n", argv[0]);
+      fprintf(stderr, "Usage: %s <default file> <symbolic-link>\n", argv[0]);
       exit(EXIT_FAILURE);
    }
 
    /* Crea il file di origine */
-   if ((fd = open(argv[1], O_CREAT, S_IRWXU | S_IRGRP | S_IROTH)) < 0) {
+   if ((fd = open(argv[1], O_CREAT, PERMS)) < 0) {
       fprintf(stderr, "Err.:(%d) - %s: %s\n", errno, strerror(errno), argv[1]);
       exit(EXIT_FAILURE);
    }
@@ -46,15 +48,17 @@ int main(int argc, char *argv[]) {
       exit(EXIT_FAILURE);
    }
 
-   if (readlink(argv[2], buf, BUF_SIZE) < 0 ) {
+   buf_len = readlink(argv[2], buf, BUF_SIZE - 1);
+   if (buf_len == -1) {
       fprintf(stderr, "Err.:(%d) - Read symbolic-link: %s\n", errno, 
       	      strerror(errno));
       exit(EXIT_FAILURE);
    }
+   buf[buf_len] = '\0';
 
-   printf("                       Origine: '%s'\n", argv[1]);
-   printf("                 Symbolic-link: '%s'\n", argv[2]);
-   printf("Il contenuto del symbolic-link: '%s'\n", buf);
+   printf("         default file: '%s'\n", argv[1]);
+   printf("        symbolic-link: '%s'\n", argv[2]);
+   printf("symbolic-link content: '%s'\n", buf);
 
    close(fd);
 
