d52d690ea2da0ac6882c09967342b9eed4a91491
B3h3m0th <behemoth@autistici.org>
2017-06-27 19:53:05 +0200

    replaced parameter with DIR struct

---
+++ src/Unix_Programming/File_System/11_read_dir_1_fdopendir.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <fcntl.h>
+
+int main(int argc, char *argv[]) {
+   int dfd;
+   DIR *dir;
+   struct dirent *dp;
+
+   if (argc < 2) {
+      fprintf(stderr, "Uso: %s <dirname>\n", argv[0]);
+      exit(EXIT_FAILURE);
+   }
+   
+   dfd = open(argv[1], O_RDONLY);
+
+   /* fdopendir() apre la directory specificata dal file descriptor 
+   precedentemente aperto */
+   if ((dir = fdopendir(dfd)) == NULL) {
+      fprintf(stderr, "Err.:(%d) - %s: %s\n", errno, strerror(errno), argv[1]);
+      exit(EXIT_SUCCESS);
+   }
+
+   /* Si eliminano dalla stampa le directory predefinite '.' e '..' */
+   while ((dp = readdir(dir)) != NULL) {
+      if (strcmp(dp->d_name, ".") == 0 || strcmp(dp->d_name, "..") == 0)
+      	 continue;
+      else
+      	 printf("%s\n", dp->d_name);
+   }
+   
+
+   while ((closedir(dir) == -1) & (errno == EINTR))
+      ;
+
+   exit(EXIT_SUCCESS);
+}
