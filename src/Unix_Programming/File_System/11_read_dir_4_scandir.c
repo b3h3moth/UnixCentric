863d89e28d4ab133f3165cb4e170c6048bcf3e81
B3h3m0th <behemoth@autistici.org>
2017-06-28 20:10:15 +0200

    fixed scandir() code

--- src/Unix_Programming/File_System/11_read_dir_4_scandir.c
+++ src/Unix_Programming/File_System/11_read_dir_4_scandir.c
@@ -4,30 +4,31 @@
 #include <string.h>
 #include <dirent.h>
 
-/* Lo scopo del programma e' di fornire la lista dei file in modo ordinato 
-grazie all'utilizzo della funzione scandir() */
+/* Lo scopo del programma e' di fornire la lista dei file di una directory, 
+ottenuta in input come paramentro, in modo ordinato, grazie all'utilizzo della 
+funzione scandir() */
 
 int main(int argc, char *argv[]) {
-   int n, i;
-   struct dirent **namelist;
+    int n, i;
+    struct dirent **namelist;
 
-   if (argc < 2) {
-      fprintf(stderr, "Uso: %s <dirname>\n", argv[0]);
-      exit(EXIT_FAILURE);
-   }
+    if (argc < 2) {
+       fprintf(stderr, "Uso: %s <dirname>\n", argv[0]);
+       exit(EXIT_FAILURE);
+    }
 
-   n = scandir(argv[1], &namelist, 0, alphasort);
-   if (n < 0) {
-       fprintf(stderr, "Err.: %d scandir(); %s\n", errno, strerror(errno));
-       return(EXIT_FAILURE);
-   } else {
-       for (i = 0; i < n; i++) {
-           printf("%s\n", namelist[i]->d_name);
-           free(namelist[i]);
-       }
-   }
-   
-   free(namelist);
+    n = scandir(argv[1], &namelist, 0, alphasort);
+    if (n < 0) {
+        fprintf(stderr, "Err.: %d scandir(); %s\n", errno, strerror(errno));
+        return(EXIT_FAILURE);
+    } else {
+        for (i = 0; i < n; i++) {
+            printf("%s\n", namelist[i]->d_name);
+            free(namelist[i]);
+        }
+    }
 
-   exit(EXIT_SUCCESS);
+       
+    free(namelist);
+    exit(EXIT_SUCCESS);
 }
