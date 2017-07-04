af72d763c7c5aa4e2027162a4d85d90c3e0e540d
B3h3m0th <behemoth@autistici.org>
2017-07-04 19:41:16 +0200

    read a  buf of events, display contents of each structure

--- src/Unix_Programming/File_Events_Monitoring/01_init.c
+++ src/Unix_Programming/File_Events_Monitoring/01_init.c
@@ -2,10 +2,16 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include <limits.h>         /* NAME_MAX */
 #include <sys/inotify.h>
+
+#define BUF_LEN (10 * (sizeof(struct inotify_event) + NAME_MAX + 1))
 
 int main(int argc, char *argv[]) {
     int intf_fd, i, wd;
+    ssize_t nread;
+    char buf[BUF_LEN], *p;
+    struct inotify_event *event;
 
     if (argc < 2) {
         fprintf(stderr, "Usage: %s <pathname,...>\n", argv[0]);
@@ -30,6 +36,31 @@ int main(int argc, char *argv[]) {
         }
 
         printf("Wathing %s using watch descriptor (wd) %d\n", argv[i], wd);
+    }
+
+    for (;;) {
+        /* Lettura di un buffer di eventi dal file descriptor inotify */
+        nread = read(intf_fd, buf, BUF_LEN);
+        if (nread == -1) {
+            fprintf(stderr, "returned 0 from inotify read(), it's not good\n");
+            return;
+        }
+
+        if (nread == -1) {
+            fprintf(stderr, "Err. %d read(); %s\n", errno, strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+
+        printf("Read %ld bytes from inotify fd\n", (long)nread);
+
+        /* Chiamata della funzione  display_events() per visualizzare il
+        contenuto di ciascun 'struct inotify_event' all'interno del buffer. */
+        for(p = buf; p < (buf + nread); ) {
+            event = (struct inotify_event *) p;
+            display_events(event);
+
+            p += sizeof(struct inotify_event) + event->len;
+        }
     }
 
     return(EXIT_SUCCESS);
