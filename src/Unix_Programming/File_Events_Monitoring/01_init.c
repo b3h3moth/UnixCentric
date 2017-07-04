78ad4b70751929e65fcb549a08611e4b99289c60
B3h3m0th <behemoth@autistici.org>
2017-07-04 19:19:17 +0200

    add file to watch with inotify_add_watch()

--- src/Unix_Programming/File_Events_Monitoring/01_init.c
+++ src/Unix_Programming/File_Events_Monitoring/01_init.c
@@ -5,7 +5,7 @@
 #include <sys/inotify.h>
 
 int main(int argc, char *argv[]) {
-    int intf_fd;
+    int intf_fd, i, wd;
 
     if (argc < 2) {
         fprintf(stderr, "Usage: %s <pathname,...>\n", argv[0]);
@@ -17,6 +17,18 @@ int main(int argc, char *argv[]) {
     if (intf_fd == -1) {
         fprintf(stderr, "Err: %d inotify_init(); %s\n", errno, strerror(errno));
         exit(EXIT_FAILURE);
+    }
+
+    /* Aggiunta dei file da monitorare */
+    for (i=1; i<argc; i++) {
+        /* Sono monitorati tutti gli eventi riguardanti l'input */
+        wd = inotify_add_watch(intf_fd, argv[i], IN_ALL_EVENTS);
+        if (wd == -1) {
+            fprintf(stderr, "Usage: %s <pathname,...>\n", argv[0]);
+            exit(EXIT_FAILURE);
+        }
+
+        printf("Wathing %s using watch descriptor (wd) %d\n", argv[i], wd);
     }
 
     return(EXIT_SUCCESS);
