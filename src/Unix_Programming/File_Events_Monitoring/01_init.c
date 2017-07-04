1075071e1ed93dbd4046ce4a7b8065b339d0d0be
B3h3m0th <behemoth@autistici.org>
2017-07-04 20:15:53 +0200

    fixed buf with __attribute__((aligned(8))

--- src/Unix_Programming/File_Events_Monitoring/01_init.c
+++ src/Unix_Programming/File_Events_Monitoring/01_init.c
@@ -39,7 +39,7 @@ static void display_events(struct inotify_event *j) {
 int main(int argc, char *argv[]) {
     int intf_fd, i, wd;
     ssize_t nread;
-    char buf[BUF_LEN];
+    char buf[BUF_LEN] __attribute__((aligned(8)));
     char *p;
     struct inotify_event *event;
 
