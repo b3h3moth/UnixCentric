ea36e699c4e7f1967c4dedfbec3ddd3338f7f8d0
B3h3m0th <behemoth@autistici.org>
2017-07-07 18:32:27 +0200

    bit mask (EVENTS) declared into macro

--- src/Unix_Programming/File_Events_Monitoring/nonitoring_dir.c
+++ src/Unix_Programming/File_Events_Monitoring/nonitoring_dir.c
@@ -6,8 +6,9 @@
 #include <sys/types.h>
 #include <sys/inotify.h>
 
-#define EVENT_SIZE  ( sizeof (struct inotify_event) )
-#define BUF_LEN     ( 1024 * ( EVENT_SIZE + 16 ) )
+#define EVENT_SIZE  (sizeof(struct inotify_event))
+#define BUF_LEN     (1024 * (EVENT_SIZE + 16))
+#define EVENTS      (IN_MODIFY | IN_CREATE | IN_DELETE)
 
 int main(int argc, char *argv[]){
     int length, i = 0;
@@ -23,8 +24,7 @@ int main(int argc, char *argv[]){
 		exit(EXIT_FAILURE);
 	}
 
-	wd = inotify_add_watch(fd, "/home/strike",
-			       IN_MODIFY | IN_CREATE | IN_DELETE);
+	wd = inotify_add_watch(fd, argv[1],
 	length = read(fd, buf, BUF_LEN);
 
 	if (length < 0) {
