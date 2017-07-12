ef9d625998112526efcf6b73490fc532c90d5b16
B3h3m0th <behemoth@autistici.org>
2017-07-12 19:01:55 +0200

    minor update

--- src/Unix_Programming/Signals/02_signal.c
+++ src/Unix_Programming/Signals/02_signal.c
@@ -8,7 +8,7 @@
 /*
 L'interfaccia piu' semplice relativa alla gestione dei segnali nei sistemi unix-
 like e' la funzione signal(); tale funzione e' standard ISO C, il quale pero'
-non  considera ne' il multitasking, ne' il process group, ne' il terminal I/O e 
+non  considera ne' il multitasking, ne' il process group, ne' il terminal I/O e
 cosi' via, pertanto la funzione signal() e' diventata nel tempo inutile nei
 sistemi unix-like, e poiche' la semantica differisce totalmente da un sistema
 all'altro si consiglia di adoperare la funzione sigaction().
@@ -17,41 +17,41 @@ La funzione signal() e' trattata per puro scopo didattico.
 
 HEADER    : <signal.h>
 PROTOTYPE : typedef void (*sighandler_t)(int);
-            sighandler_t signal(int signum, sighandler_t handler); 
-SEMANTICS : La funzione signal() associa o installa la funzione di gestione 
+            sighandler_t signal(int signum, sighandler_t handler);
+SEMANTICS : La funzione signal() associa o installa la funzione di gestione
             'handler' al segnale 'signum'
 RETURNS   : Il gestore precedente in caso di successo, SIG_ERR in caso di errore
 --------------------------------------------------------------------------------
 Nota: si e' utilizzata la typedef per rendere il prototipo della funzione
       meno ostico da leggere ed interpretare, il tipo di dato sighandler_t e'
-      una estensione GNU delle glibc. 
-      
+      una estensione GNU delle glibc.
+
 Il prototipo della funzione e' caratterizzato da due argomenti:
 - 'signum', un intero indicante il signal number;
-- 'handler' un puntatore ad una funzione void che riceve come argomento un 
-   intero. Il kernel puo' essere informato su una funzione deputata al 
+- 'handler' un puntatore ad una funzione void che riceve come argomento un
+   intero. Il kernel puo' essere informato su una funzione deputata al
    trattamento del segnale, tale funzione e' definita "signal handler", gestore
    del segnale. Non restituisce alcun risultato.
 
-Il valore della funzione handler puo' essere: 
+Il valore della funzione handler puo' essere:
 - l'indirizzo della funzione da chiamare al verificarsi del segnale;
 - la costante SIG_IGN, per ignorare il segnale;
 - la costante SIG_DFL, per l'azione di default del segnale.
 
 Se si specifica l'indirizzo della funzione si sta parlando di "signal handler" o
-di "signal-catching function" - gestore del segnale o funzione per la cattura 
+di "signal-catching function" - gestore del segnale o funzione per la cattura
 di un segnale.
 
 La funzione restituisce l'indirizzo dell'azione precedente, che puo' essere
-salvato per poi essere riutilizzato in un secondo tempo, ovvero quando il 
+salvato per poi essere riutilizzato in un secondo tempo, ovvero quando il
 processo tornera' dalla funzione di gestione del segnale e proprio nel punto in
 cui era avvenuta l'eccezione.
 
 Dura nel tempo l'associazione segnale/handler?
 ----------------------------------------------
-Talune implementazioni di Unix, tra cui BSD e SyVr.3, prevedono che l'azione 
-rimanga installata anche dopo la ricezione del segnale, in altre invece, come 
-SysV prime versioni, dopo l'attivazione dell'handler si ripristina 
+Talune implementazioni di Unix, tra cui BSD e SyVr.3, prevedono che l'azione
+rimanga installata anche dopo la ricezione del segnale, in altre invece, come
+SysV prime versioni, dopo l'attivazione dell'handler si ripristina
 automaticamente l'azione di default. In questi casi, per riagganciare il segnale
 all'handler e' necessario reinstallare nuovamente il segnale nel corpo dell'
 handler stesso.
@@ -66,19 +66,19 @@ $ ./a.out &
 
 Con il comando kill si inviano i primi due segnali:
 $ kill -USR1 5960
-Ricevuto il segnale: SIGUSR1 
+Ricevuto il segnale: SIGUSR1
 $ kill -USR2 5960
 Ricevuto il segnale: SIGUSR2
 
-Ora si porta il programma da background a foreground, e poiche' vi e' un ciclo 
-infinito il programma restera' in esecuzione, in attesa di essere terminato; 
+Ora si porta il programma da background a foreground, e poiche' vi e' un ciclo
+infinito il programma restera' in esecuzione, in attesa di essere terminato;
 si prova a terminarlo con la combinazione di tasti Control-C
 $ fg %1
 ./a.out
 ^C Ricevuto il segnale: SIGINT
 ^C Ricevuto il segnale: SIGINT
 
-Come si puo' ben notare il segnale SIGINT (^C) e' stato intercettato, a questo 
+Come si puo' ben notare il segnale SIGINT (^C) e' stato intercettato, a questo
 punto per terminarlo non resta altro da fare che stopparlo nuovamente
 con Control-Z, dopodiche' terminarlo con il comando kill e il segnale SIGKILL:
 
@@ -95,17 +95,17 @@ int main(int argc, char *argv[]) {
     ai tasti ^C. */
     if (signal(SIGUSR1, signal_handler) == SIG_ERR) {
     	fprintf(stderr, "Err.(%s), can't catch SIGUSR1\n", strerror(errno));
-	exit(EXIT_FAILURE);
+	    exit(EXIT_FAILURE);
     }
 
     if (signal(SIGUSR2, signal_handler) == SIG_ERR) {
     	fprintf(stderr, "Err.(%s), can't catch SIGUSR2\n", strerror(errno));
-	exit(EXIT_FAILURE);
+	    exit(EXIT_FAILURE);
     }
 
     if (signal(SIGINT, signal_handler) == SIG_ERR) {
     	fprintf(stderr, "Err.(%s), can't catch SIGINT\n", strerror(errno));
-	exit(EXIT_FAILURE);
+	    exit(EXIT_FAILURE);
     }
 
     /* La funzione pause() pone in attesa il processo in attesa di un segnale */
@@ -124,9 +124,9 @@ static void signal_handler(int signum)
 	    fprintf(stderr, "Err.(%s), can't catch SIGUSR1\n", strerror(errno));
 	    exit(EXIT_FAILURE);
 	}
-    	
+
 	printf("Ricevuto il segnale: SIGUSR1\n");
-    
+
     } else if (signum == SIGUSR2)
     	printf("Ricevuto il segnale: SIGUSR2\n");
     else if (signum == SIGINT)
