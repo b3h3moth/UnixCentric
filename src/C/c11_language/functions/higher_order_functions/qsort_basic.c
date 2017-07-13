c12ada1790181cb98bed1ee6144bab782487b1aa
B3h3m0th <behemoth@autistici.org>
2017-07-13 19:46:52 +0200

    MAX_LINES and pline declared

---
+++ src/C/c11_language/functions/higher_order_functions/qsort_basic.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINES 1000      /* massimo numero di righe ordinabili */
+
+char *pline[MAX_LINES];     /* puntatori alle righe */
+
+int main(int argc, char *argv[]) {
+    return(EXIT_SUCCESS);
+}
