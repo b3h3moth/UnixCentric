e5055eaa7d04494b4a589e89fa37a160ec5dc4d2
B3h3m0th <behemoth@autistici.org>
2017-07-13 19:48:38 +0200

    nlines, num declared

--- src/C/c11_language/functions/higher_order_functions/qsort_basic.c
+++ src/C/c11_language/functions/higher_order_functions/qsort_basic.c
@@ -7,5 +7,7 @@
 char *pline[MAX_LINES];     /* puntatori alle righe */
 
 int main(int argc, char *argv[]) {
+    int nlines = 0;         /* righe in ingresso lette */
+    int num = 0;            /* ordinamento numerico = 1 */
     return(EXIT_SUCCESS);
 }
