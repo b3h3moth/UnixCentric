515511d4cdc0ba7645b1138b3d15650c764f9e55
B3h3m0th <behemoth@autistici.org>
2017-07-13 19:55:01 +0200

    check command line, invoke read/write funcs

--- src/C/c11_language/functions/higher_order_functions/qsort_basic.c
+++ src/C/c11_language/functions/higher_order_functions/qsort_basic.c
@@ -8,6 +8,18 @@ char *pline[MAX_LINES];     /* puntatori alle righe */
 
 int main(int argc, char *argv[]) {
     int nlines = 0;         /* righe in ingresso lette */
-    int num = 0;            /* ordinamento numerico = 1 */
+    int numeric_sort = 0;   /* ordinamento numerico = 1 */
+
+    /* Verifica argomenti in input e settaggio dell'ordinamento numerico */
+    if (argc > 1 && strcmp(argv[1], "-n") == 0)
+        numeric_sort = 1;
+    if ((nlines = readlines(pline, MAX_LINES)) >= 0) {
+        writelines(pline, nlines);
+        return(EXIT_SUCCESS);
+    } ekse {
+        printf("so many input data types to sort.\n");
+        return(EXIT_FAILURE);
+    }
+
     return(EXIT_SUCCESS);
 }
