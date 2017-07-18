7cd447842d1312a72f8ea2ef42b97e4879a9d6c4
B3h3m0th <behemoth@autistici.org>
2017-07-18 19:18:26 +0200

    qsort() redefined

--- src/C/c11_language/functions/higher_order_functions/qsort_basic.c
+++ src/C/c11_language/functions/higher_order_functions/qsort_basic.c
@@ -9,20 +9,24 @@ static char allocbuf[ALLOC_SIZE];   /* vettore di memoria */
 static char *palloc = allocbuf;     /* primo elemento libero */
 char *pline[MAX_LINES];             /* puntatori alle righe */
 
+/* Function Prototypes */
 int read_lines(char *pline[], int maxlines);
 void write_lines(char *pline[], int nlines);
 int get_line(char str[], int lim);
+void quicksort(char *vec[], int left, int right);
+void swap(char *vec[], int i, int j);
 char *alloc(int n);
 void afree(char *p);
 
 int main(int argc, char *argv[]) {
     int nlines = 0;         /* righe in ingresso lette */
-    int numeric_sort = 0;   /* ordinamento numerico = 1 */
+    int numeric_sort;   /* ordinamento numerico = 1 */
 
     /* Verifica argomenti in input e settaggio dell'ordinamento numerico */
     if (argc > 1 && strcmp(argv[1], "-n") == 0)
         numeric_sort = 1;
     if ((nlines = read_lines(pline, MAX_LINES)) >= 0) {
+        quicksort(pline, 0, nlines-1);
         write_lines(pline, nlines);
         return(EXIT_SUCCESS);
     } else {
@@ -72,6 +76,35 @@ int get_line(char str[], int lim) {
     str[i] = '\0';
 
     return i;
+}
+
+/* La funzione quicksort() ordina un vettore di puntatori a carattere, ciascun
+carattere e' il primo di una stringa. */
+void quicksort(char *vec[], int left, int right) {
+    int i, last;
+    void swap(char *vec[], int i, int j);
+
+    /* se vec contiene meno di due caratteri ritorna nulla */
+    if (left >= right)
+        return;
+
+    swap(vec, left, (left + right)/2);
+    last = left;
+    for (i= left+1; i<=right; i++)
+        if (strcmp(vec[i], vec[left]) < 0)
+            swap(vec, ++last, i);
+    swap(vec, left, last);
+    quicksort(vec, left, last-1);
+    quicksort(vec, last+1, right);
+}
+
+/* La funzione swap() scambia di post vec[i] con vec[j] */
+void swap(char *vec[], int i, int j) {
+    char *tmp;
+
+    tmp = vec[i];
+    vec[i] = vec[j];
+    vec[j] = tmp;
 }
 
 /* La funzione alloc() restituisce un puntatore a n caratteri */
