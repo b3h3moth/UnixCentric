b823b8dbca17946fd8ed937f9854032a9ac447a8
B3h3m0th <behemoth@autistici.org>
2017-08-01 16:21:38 +0200

    fixed unrecognized command line option -std=c11

--- src/C/representing_information/addressing_and_byte_ordering/show_bytes.c
+++ src/C/representing_information/addressing_and_byte_ordering/show_bytes.c
@@ -3,7 +3,8 @@
 
 // Returns each byte in hexadecimal format
 void show_bytes(byte_pointer start, int len) {
-    for (int i=0; i<len; i++)
+    int i;
+    for (i=0; i<len; i++)
         printf("%.2x ", start[i]);
 
     printf("\n");
