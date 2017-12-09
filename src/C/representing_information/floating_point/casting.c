34ee90364a287e2aca2d1a7172f2923ee0e9c389
B3h3m0th <behemoth@autistici.org>
2017-12-09 18:48:48 +0100

    fix printf() notation

--- src/C/representing_information/floating_point/casting.c
+++ src/C/representing_information/floating_point/casting.c
@@ -3,8 +3,12 @@
 
 int main(void) {
     int ival = 32769;
-    float fval = (float)ival;
 
-    printf("from int %d to float %lf (IEEE 754: %e)\n", ival, fval, fval);
+    // Explicit casting from int to float
+    float fval = ival;
+
+    printf("From int: \'%d\'\n"
+           "     to float: \'%f\'\n"
+           "     IEEE 754: \'%e\'\n", ival, fval, fval);
     return(EXIT_SUCCESS);
 }
