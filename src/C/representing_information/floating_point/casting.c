ec0641e125d3df7f9258cd2d090b41b22e80e227
B3h3m0th <behemoth@autistici.org>
2017-12-09 19:04:41 +0100

    float to integer, casting

--- src/C/representing_information/floating_point/casting.c
+++ src/C/representing_information/floating_point/casting.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 int main(void) {
-    int ival = 32769;
 
     // Explicit casting from int to float
-    float fval = ival;
+    int ival1 = 32769;
+    float fval1 = (float)ival1;
 
     printf("From int: \'%d\'\n"
            "     to float: \'%f\'\n"
-           "     IEEE 754: \'%e\'\n", ival, fval, fval);
+           "     IEEE 754: \'%e\'\n", ival1, fval1, fval1);
+
+    // Explicit casting from int to float
+    float fval2 = 4147483650.0;
+    int ival2 = (int)fval2;
+    printf("From float: \'%f\'\n"
+           "     IEEE 754: \'%e\'\n"
+           "     to int: \'%d\' INT_MAX: %d\n", fval2, fval2, ival2, INT_MAX);
+
     return(EXIT_SUCCESS);
 }
