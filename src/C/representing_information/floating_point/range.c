ccdd7e41bdceb6475da340b3af504070261e38c8
B3h3m0th <behemoth@autistici.org>
2017-12-11 19:37:27 +0100

    replacved IEEE 754 with exponential notation

--- src/C/representing_information/floating_point/range.c
+++ src/C/representing_information/floating_point/range.c
@@ -6,9 +6,9 @@ int main(void) {
 
     printf("Single precision, data type \'float\'\n"
            "\tmin value: %f\n"
-           "\tIEEE 754 \'scientific notation\': %e\n"
+           "\texponential notation: %e\n"
            "\tmax value: %f\n"
-           "\tIEEE 754 \'scientific notation\': %e\n", 
+           "\texponential notation: %e\n", 
            FLT_MIN, FLT_MIN, FLT_MAX, FLT_MAX);
 
     return(EXIT_SUCCESS);
