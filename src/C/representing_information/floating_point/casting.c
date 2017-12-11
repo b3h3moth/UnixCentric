8c6db6a2623c594e2aefa407ed9900d420da3d01
B3h3m0th <behemoth@autistici.org>
2017-12-11 20:46:16 +0100

    conversion from int to double

--- src/C/representing_information/floating_point/casting.c
+++ src/C/representing_information/floating_point/casting.c
@@ -4,16 +4,19 @@
 
 int main(void) {
 
-    /* Explicit casting from int to float */
     int ival1 = 32769;
+    float fval2 = 4147483650.0;
+    float fval2a = -4147483650.0;
+
+    /* Explicit casting from int to float */
     float fval1 = (float)ival1;
 
-    /* Explicit casting from int to float */
-    float fval2 = 4147483650.0;
+    /* Explicit casting from float to int */
     int ival2 = (int)fval2;
-
-    float fval2a = -4147483650.0;
     int ival2a = (int)fval2a;
+
+    /* Explicit casting from int to double */
+    double dval1 = (double)ival1;
 
     printf("From int: \'%d\'\n"
            "     to float: \'%f\'\n"
@@ -29,6 +32,11 @@ int main(void) {
            "     IEEE 754, exponential notation: \'%e\'\n"
            "     to int: \'%d\' INT_MIN: %d\n", 
            fval2a, fval2a, ival2a, INT_MIN);
+
+    printf("From int: \'%d\'\n"
+           "     to double: \'%lf\'\n"
+           "     IEEE 754, exponential notation: \'%e\'\n", 
+           ival1, dval1, dval1);
 
     return(EXIT_SUCCESS);
 }
