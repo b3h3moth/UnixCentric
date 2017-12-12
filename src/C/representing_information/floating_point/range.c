63513c99aa87b7dc114647ab16e057d25b0c3425
B3h3m0th <behemoth@autistici.org>
2017-12-12 13:48:06 +0100

    double precision ranging

--- src/C/representing_information/floating_point/range.c
+++ src/C/representing_information/floating_point/range.c
@@ -11,5 +11,12 @@ int main(void) {
            "\texponential notation: %e\n", 
            FLT_MIN, FLT_MIN, FLT_MAX, FLT_MAX);
 
+    printf("Double precision, data type \'double\'\n"
+           "\tmin value: %f\n"
+           "\texponential notation: %e\n"
+           "\tmax value: %f\n"
+           "\texponential notation: %e\n", 
+           DBL_MIN, DBL_MIN, DBL_MAX, DBL_MAX);
+
     return(EXIT_SUCCESS);
 }
