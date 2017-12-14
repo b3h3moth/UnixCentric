0e5804f01cf208ea224e4d8edfb151ffb4ed5d1c
B3h3m0th <behemoth@autistici.org>
2017-12-14 16:48:10 +0100

    improved function scope

--- src/C/representing_information/integer/integer_arithmetic/comparision.c
+++ src/C/representing_information/integer/integer_arithmetic/comparision.c
@@ -4,8 +4,9 @@
 unsigned char is_zero(unsigned char val);
 unsigned char is_equal(unsigned char x, unsigned char y);
 
-/* La comparazione tra due 'unsigned char' per verificare se sono uguali,
-mediante shifting, AND e XOR logico. */
+/* Si esegue la comparazione tra due 'unsigned char' senza adoperare
+operatori di confronto, ma solo con tecniche di shifting, AND e XOR 
+logico. */
 
 int main(void) {
     unsigned char val1 = 252;
@@ -19,18 +20,20 @@ int main(void) {
     return(EXIT_SUCCESS);
 }
 
+/* Verifica se il parametro passato come argomentoo e' zero o meno */
 unsigned char is_zero(unsigned char val) {
 
-    return( (val & (1<<7)) >> 7 |
-            (val & (1<<6)) >> 6 |
-            (val & (1<<5)) >> 5 |
-            (val & (1<<4)) >> 4 |
-            (val & (1<<3)) >> 3 |
-            (val & (1<<2)) >> 2 |
-            (val & (1<<1)) >> 1 | 
-            (val & 1)) & 1;
+    return ~((val & (1<<7)) >> 7 |
+             (val & (1<<6)) >> 6 |
+             (val & (1<<5)) >> 5 |
+             (val & (1<<4)) >> 4 |
+             (val & (1<<3)) >> 3 |
+             (val & (1<<2)) >> 2 |
+             (val & (1<<1)) >> 1 | 
+             (val & 1)) & 1;
 }
 
+/* Verifica se i due parametri sono uguali o meno */
 unsigned char is_equal(unsigned char x, unsigned char y) {
     return(is_zero(x ^ y));
 }
