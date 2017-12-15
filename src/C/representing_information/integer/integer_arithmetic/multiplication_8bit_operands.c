cef0965ad58ad33f640f13dab18cf58b1a00c73c
B3h3m0th <behemoth@autistici.org>
2017-12-16 00:45:11 +0100

    8 bit operands return 16 bit, multip. function

---
+++ src/C/representing_information/integer/integer_arithmetic/multiplication_8bit_operands.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Lo scopo della funzione e' di moltiplicare due operandi unsigned char da 
+8 bit e di ritornare un unsigned short da 16 bit */
+
+unsigned short
+
+int main(void) {
+    return(EXIT_SUCCESS);
+}
