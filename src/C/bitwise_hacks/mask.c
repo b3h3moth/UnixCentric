f64fc3205a3281d9ce891c6956da57fee610c7c4
B3h3m0th <behemoth@autistici.org>
2017-12-17 19:35:51 +0100

    how bitmask works

--- src/C/bitwise_hacks/mask.c
+++ src/C/bitwise_hacks/mask.c
@@ -7,10 +7,12 @@ int main(void) {
     /* Una maschera e' una costante o una variabile, e' utilizzata per estrarre
     i bit desiderati da una variabile o da un'espressione */
 
-    int mask = 1; // 00000000 00000000 00000000 00000001
+    int mask = 1; /* 00000000 00000000 00000000 00000001 */
+    int i;
 
-    // Si utilizza la maschera per determinare il bit meno significativo
-    for (int i=0; i<LEN; i++)
+    /* Si utilizza la maschera per determinare se il bit meno significativo
+    e' 0 oppure 1 */
+    for (i=0; i<LEN; i++)
         printf("%d & mask(%d) = %d\n", i, mask, i & mask);
 
     return(EXIT_SUCCESS);
