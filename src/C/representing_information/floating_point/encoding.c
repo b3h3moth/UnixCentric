f56eeab9ace283939aa192e495bd902434a6c14a
B3h3m0th <behemoth@autistici.org>
2018-05-07 19:37:22 +0200

    encoding floating point

--- src/C/representing_information/floating_point/encoding.c
+++ src/C/representing_information/floating_point/encoding.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <float.h>
 
-int main(int argc, char *argv[]) {
 int main(void) {
-    return 0;
+
+    printf("Property   \tfloat \t\tdouble\t\t\tlong double\n");
+    printf("mantissa:  \t%d    \t\t%d    \t\t\t%d\n", FLT_MANT_DIG, DBL_MANT_DIG, LDBL_MANT_DIG);
+    printf("dec.digits:\t%d    \t\t%d    \t\t\t%d\n", FLT_DIG, DBL_DIG, LDBL_DIG);
+    printf("max value: \t%9.5e \t%18.14e \t%22.17Le\n", FLT_MAX, DBL_MAX, LDBL_MAX);
+    printf("min value: \t%9.5e \t%18.14e \t%22.17Le\n", FLT_MIN, DBL_MIN, LDBL_MIN);
+
     return(EXIT_SUCCESS);
 }
