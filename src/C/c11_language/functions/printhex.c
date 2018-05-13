55b2a0f7519385de8d5250ff306130c6dbd5d0c1
B3h3m0th <behemoth@autistici.org>
2018-05-13 18:59:25 +0200

    print hex, working with memory addresses

---
+++ src/C/c11_language/functions/printhex.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void printhex(void *p, int size);
+
+int main(void) {
+    float f = -317.125;
+    int i = -1013018624;
+    char c = 'A';
+    double d = 1.2345e-14;
+
+    printf("Value:%12f - hex: ", f); printhex(&f, sizeof(f));
+    printf("Value:%12d - hex: ", i); printhex(&i, sizeof(i));
+    printf("Value:%12c - hex: ", c); printhex(&c, sizeof(c));
+    printf("Value:%12g - hex: ", d); printhex(&c, sizeof(d));
+
+    return(EXIT_SUCCESS);
+}
+
+/* It prints each actual parameter like an hex number */
+void printhex(void *p, int size) {
+    /* A pointer to void can't be used in expressions, so the best
+     way to print a memory address is a cast, one byte at a time */
+    unsigned char *ip = (unsigned char *)p;   /* First byte address */
+
+    printf("0x");
+    for (int i=0; i<size; i++) {
+        printf("%02x", *ip);
+        *ip++;
+    }
+    printf("\n");
+}
