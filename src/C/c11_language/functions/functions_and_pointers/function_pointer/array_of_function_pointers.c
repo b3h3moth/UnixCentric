b04ed942c8133bcf766eab4a6dcd1d097a21cb6d
B3h3m0th <behemoth@autistici.org>
2018-05-15 17:57:04 +0200

    select_symbol() removed, dv() added

--- src/C/c11_language/functions/functions_and_pointers/function_pointer/array_of_function_pointers.c
+++ src/C/c11_language/functions/functions_and_pointers/function_pointer/array_of_function_pointers.c
@@ -4,14 +4,14 @@
 typedef int (*fp)(int, int);
 
 // Dichiarazione di un array di puntatori funzione
-fp fparr[128] = {NULL};
-// Istruzione equivalente: int (*operation[128])(int, int) = { NULL };
+fp fparr[4] = {NULL};
+// Istruzione equivalente: int (*operation[3])(int, int) = { NULL };
 
 // Function Prototypes
 int sum(int val1, int val2);
 int sub(int val1, int val2);
 int mol(int val1, int val2);
-fp select_symbol(char op);
+int dv(int val1, int val2);
 int eval(int op, int val1, int val2);
 void initialize_op(void);
 
@@ -20,9 +20,11 @@ a funzione */
 
 int main(void) {
     initialize_op();
-    printf("%d\n", eval('+', 10, 20));
-    printf("%d\n", eval('-', 10, 20));
-    printf("%d\n", eval('*', 10, 20));
+    printf("100+20=%d\n", eval('+', 100, 20));
+    printf("100-20=%d\n", eval('-', 100, 20));
+    printf("100*20=%d\n", eval('*', 100, 20));
+    printf("100/20=%d\n", eval('/', 100, 20));
+    printf("100/0=%d\n",  eval('/', 100, 0));
     return(EXIT_SUCCESS);
 }
 
@@ -41,23 +43,16 @@ int mol(int val1, int val2) {
     return val1 * val2;
 }
 
+int dv(int val1, int val2) {
+    return (val2 == 0) ? 0 : (val1 / val2);
+}
+
 // Assegna la funzione adatta a ciascun operatore
 void initialize_op(void) {
     fparr['+'] = sum;
     fparr['-'] = sub;
     fparr['*'] = mol;
-}
-
-/* Se op contiene il segno '+' ritorna la funzione sum(), se invece contiene
-il segno '-' ritorna la funzione sub() */
-fp select_symbol(char op) {
-    switch(op) {
-        case '+': return sum;
-        case '-': return sub;
-        case '*': return mol;
-    }
-
-    return 0;
+    fparr['/'] = dv;
 }
 
 // Valuta i parametri a seconda del contesto
