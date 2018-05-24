6e9bff470d580142ff84bdb9ffe952942be7d18c
B3h3m0th <behemoth@autistici.org>
2018-05-24 17:39:55 +0200

    added more test

--- src/C/interfaces/Buffer/main.c
+++ src/C/interfaces/Buffer/main.c
@@ -7,36 +7,33 @@ int main(void) {
     Buffer *buf2 = buf_alloc(10, "test2");
     Buffer *buf3 = buf_alloc(10, "test3");
     Buffer *buf4 = buf_alloc(10, "test4");
-    Buffer *buf5 = buf_alloc(10, "test5");
-    Buffer *buf6c;
 
     init_random(buf1);
     print(buf1);
-   
-    init_str(buf2, "ABCDEFGHIL");
+
+    init_str(buf2, "ABCDEFGHI");
     print(buf2);
 
     buf_copy(buf3, buf2);
     print(buf3);
+
+    Buffer *buf5 = buf_concatenate(buf1, buf2, "testconcat");
+    print(buf5);
 
     buf_inverse_copy(buf4, buf1);
     print(buf4);
 
     printf("buf1 has %d occurences of 'A'\n", count(buf1, 'A'));
     printf("buf2 has %d occurences of 'H'\n", count(buf2, 'H'));
- 
-    concatenate(buf6c, buf1, buf2);
-    print(buf6c);
 
     reset(buf2);
-    printf("buf2 has %d occurences of 'H'\n", count(buf2, 'H'));
+
 
     buf_dealloc(buf1);
     buf_dealloc(buf2);
     buf_dealloc(buf3);
     buf_dealloc(buf4);
     buf_dealloc(buf5);
-    buf_dealloc(buf6c);
 
     return(EXIT_SUCCESS);
 }
