58957155d0bdac21a8495952b1c6267df7580048
B3h3m0th <behemoth@autistici.org>
2018-05-29 13:55:18 +0200

    testing findinList() func. with a for loop

--- src/C/Algorithms_and_Data_Structures/data_structures/linked-list/linear_linked_list/main.c
+++ src/C/Algorithms_and_Data_Structures/data_structures/linked-list/linear_linked_list/main.c
@@ -4,7 +4,9 @@
 
 int main(void) {
     char *myfile = "lll.txt";
+    int i;
     typeList llst = NULL;
+
     addNode(&llst, 3);
     addNode(&llst, 2);
     addNode(&llst, 1);
@@ -19,12 +21,12 @@ int main(void) {
     if (emptyList(llst))
         fprintf(stdout, "B. The list is empty\n");
     addNode(&llst, 10);
-    addNode(&llst, 100);
-    addNode(&llst, 1000);
-    addNode(&llst, 10000);
+    addNode(&llst, 1);
+    addNode(&llst, 9);
+    addNode(&llst, 7);
     writeNodeF(myfile, llst);
     /* $ cat lll.txt */
-
-
+    for (i=0; i<=10; i++) 
+        printf("find %.2d: %d\n", i, findinList(llst, i));
     return(EXIT_SUCCESS);
 }
