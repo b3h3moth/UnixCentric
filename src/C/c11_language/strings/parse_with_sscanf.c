55e35f766808daf7c063f50d1aaff58a228b3f2a
B3h3m0th <behemoth@autistici.org>
2018-06-03 19:19:11 +0200

    minor fix

--- src/C/c11_language/strings/parse_with_sscanf.c
+++ src/C/c11_language/strings/parse_with_sscanf.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* The program's purpose is to test sscanf() function  */
+
 int main(void) {
-    const char str[] = "1975/03/03 06:00:00;info1;info2;info3";
+    const char str[] = "1982/08/12 21:30:37;Unix;Operating;System";
     const char *ptr = str;
     char field[20];
     int n;
 
-    while(sscanf(ptr, "%31[^;]%n", field, &n) == 1) {
+    while(sscanf(ptr, "%19[^;]%n", field, &n) == 1) {
         printf("Filed = %s\n", field);
         ptr += n;
-        if (*ptr != ';')
+        if (*ptr != ';') /* ';' is the field separator */
             break;
         ++ptr;
     }
