5c5fc2ae51fbc8b1df6a969c791b2e12b906f3d3
B3h3m0th <behemoth@autistici.org>
2018-06-06 23:06:45 +0200

    fix code, added stdlib.h

--- src/C/representing_information/addressing_and_byte_ordering/endianess/endianess.c
+++ src/C/representing_information/addressing_and_byte_ordering/endianess/endianess.c
@@ -1,35 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 char* Bits(char c);
 int AmBigEndian();
-  /* create a segment of static memory with the right data */
+
+/* create a segment of static memory with the right data */
   char a[4] = {'A','B','C','D'};
-/* function main ------------------------------------------- */
-int main()
-{
-  char* b = a;            /* b points to the beginning of a */
-  short* s = (short*) s;  /* s points to the beginning of a */
-  int* p = (int*) a;      /* p points to the beginning of a */
-  float* f = (float*) a;  /* f points to the beginning of a */
-  if (AmBigEndian())
-    printf("I am big endian\n");
-  else
-    printf("I am little endian\n");
-  /* show the data as a sequence of bits */
-  printf("%s",Bits(a[0]));
-  printf("%s",Bits(a[1]));
-  printf("%s",Bits(a[2]));
-  printf("%s",Bits(a[3]));
-  putchar('\n');
-  /* show the data as 4 characters */
-  printf("'%c','%c','%c','%c'\n",*b,*(b+1),*(b+2),*(b+3));
-  /* show the data as 2 short integers */
-  printf("%d,%d\n",*s,*(s+1));
-  /* show the data as 1 integer */
-  printf("%d\n",*p);
-  /* show the data as 1 float */
-  printf("%f\n",*f);
-   return 0;
-}/*end main*/
+
+int main(void) {
+    char* b = a;            /* b points to the beginning of a */
+    short* s = (short*) s;  /* s points to the beginning of a */
+    int* p = (int*) a;      /* p points to the beginning of a */
+    float* f = (float*) a;  /* f points to the beginning of a */
+    
+    if (AmBigEndian())
+        printf("I am big endian\n");
+    else
+        printf("I am little endian\n");
+  
+    /* show the data as a sequence of bits */
+    printf("%s",Bits(a[0]));
+    printf("%s",Bits(a[1]));
+    printf("%s",Bits(a[2]));
+    printf("%s",Bits(a[3]));
+    putchar('\n');
+    
+    /* show the data as 4 characters */
+    printf("'%c','%c','%c','%c'\n",*b,*(b+1),*(b+2),*(b+3));
+  
+    /* show the data as 2 short integers */
+    printf("%d,%d\n",*s,*(s+1));
+  
+    /* show the data as 1 integer */
+    printf("%d\n",*p);
+  
+    /* show the data as 1 float */
+    printf("%f\n",*f);
+
+    return(EXIT_SUCCESS);
+}
+
 /* function Bits ------------------------------------------- */
 char* Bits(char c)
 {
