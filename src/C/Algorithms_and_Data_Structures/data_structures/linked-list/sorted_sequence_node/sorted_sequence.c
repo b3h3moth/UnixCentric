2092f40b9452a79b6758eec92199901d7c7b0fa2
B3h3m0th <behemoth@autistici.org>
2018-06-07 20:31:20 +0200

    init() function to initialize SSN

--- src/C/Algorithms_and_Data_Structures/data_structures/linked-list/sorted_sequence_node/sorted_sequence.c
+++ src/C/Algorithms_and_Data_Structures/data_structures/linked-list/sorted_sequence_node/sorted_sequence.c
@@ -3,4 +3,6 @@
 
 sorted_Sequence *init(void) {
     sorted_Sequence *s = (sorted_Sequence*)malloc(sizeof(sorted_Sequence));
+    s->head = (sorted_Sequence_Node*)malloc(sizeof(sorted_Sequence_Node));
+    s->head->next = NULL; /* Node generator */
 }
