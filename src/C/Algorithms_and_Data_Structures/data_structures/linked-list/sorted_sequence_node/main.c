9a1153e7597c26d58fe0dd5bde6150b38e97aed4
B3h3m0th <behemoth@autistici.org>
2018-06-08 13:40:08 +0200

    testing ins_First()

--- src/C/Algorithms_and_Data_Structures/data_structures/linked-list/sorted_sequence_node/main.c
+++ src/C/Algorithms_and_Data_Structures/data_structures/linked-list/sorted_sequence_node/main.c
@@ -3,7 +3,11 @@
 #include "sorted_sequence.h"
 
 int main() {
+    /* Initialize the SSN to NULL */
     sorted_Sequence *ssn = init();
+    /* Add the first node */
+    ins_First(ssn, 15);
+    printf("%d\n", ssn->head->next->data);
 
     return(EXIT_SUCCESS);
 }
