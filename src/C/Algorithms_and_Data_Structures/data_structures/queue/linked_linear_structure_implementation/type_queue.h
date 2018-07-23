a04e14c93484a6937a5233823398ef0149b01aa1
B3h3m0th <behemoth@autistici.org>
2018-07-23 18:26:27 +0200

    queue, linked linear structure implementation

---
+++ src/C/Algorithms_and_Data_Structures/data_structures/queue/linked_linear_structure_implementation/type_queue.h
@@ -0,0 +1,26 @@
+#ifndef TYPE_QUEUE_H
+#define TYPE_QUEUE_H
+
+typedef struct node_LLS node_LLS;
+typedef struct typeQueue typeQueue;
+
+struct node_LLS {
+    int data;
+    node_LLS *next;
+};
+
+struct typeQueue {
+    node_LLS *head;
+    node_LLS *tail;
+};
+
+
+/* Queue data structure, Linked Linear Structure (LLS) implementation.
+- A pointer to node_LLS 'head' is a pointer to the generator node;
+- A pointer to node_LLS 'tail' is a pointer to the last node;
+- Each node (node_LLS) includes:
+    - the filed data, to save an integer value:
+    - the field next, a pointer to the next node of the queue.
+*/
+
+#endif
