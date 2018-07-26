aba7368649bd0060bcdc5b6037445ada7327e716
B3h3m0th <behemoth@autistici.org>
2018-07-26 19:23:12 +0200

    minor updates

--- src/C/Algorithms_and_Data_Structures/data_structures/queue/linked_linear_structure_implementation/type_queue.c
+++ src/C/Algorithms_and_Data_Structures/data_structures/queue/linked_linear_structure_implementation/type_queue.c
@@ -5,7 +5,7 @@
 typeQueue_LLS *init() {
     typeQueue_LLS *q = (typeQueue_LLS *)malloc(sizeof(typeQueue_LLS));
     node_LLS *n = (node_LLS *)malloc(sizeof(node_LLS));
-    q->head = n;        /* The head and the tail of the queue */
-    q->tail = n;        /*      point to the same node, */
-    n->next = NULL;     /*      the generator node. */
+    q->head = n;        /* The head and the tail of the queue... */
+    q->tail = n;        /* ...point to the same node. */
+    n->next = NULL;     /* the generator node. Now the queue is empty. */
 }
