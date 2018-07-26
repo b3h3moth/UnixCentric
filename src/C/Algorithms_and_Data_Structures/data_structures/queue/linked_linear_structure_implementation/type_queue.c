970aa606c67a618a1caa07a0f70d2ed92a598b1a
B3h3m0th <behemoth@autistici.org>
2018-07-26 19:50:22 +0200

    empty queue checking

--- src/C/Algorithms_and_Data_Structures/data_structures/queue/linked_linear_structure_implementation/type_queue.c
+++ src/C/Algorithms_and_Data_Structures/data_structures/queue/linked_linear_structure_implementation/type_queue.c
@@ -2,10 +2,16 @@
 #include "type_queue.h"
 
 /* Queue initialization */
-typeQueue_LLS *init() {
+typeQueue_LLS *init(void) {
     typeQueue_LLS *q = (typeQueue_LLS *)malloc(sizeof(typeQueue_LLS));
     node_LLS *n = (node_LLS *)malloc(sizeof(node_LLS));
     q->head = n;        /* The head and the tail of the queue... */
     q->tail = n;        /* ...point to the same node. */
     n->next = NULL;     /* the generator node. Now the queue is empty. */
+    return q;
+}
+
+/* Check if the queue is empty */
+int emptyQueue(typeQueue_LLS q) {
+    return q.head->next == NULL;
 }
