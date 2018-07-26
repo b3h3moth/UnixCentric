d0c0c878749307b89b7779f5358af46158481512
B3h3m0th <behemoth@autistici.org>
2018-07-26 19:18:19 +0200

    typeQueue_LLS init() added

---
+++ src/C/Algorithms_and_Data_Structures/data_structures/queue/linked_linear_structure_implementation/type_queue.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include "type_queue.h"
+
+/* Queue initialization */
+typeQueue_LLS *init() {
+    typeQueue_LLS *q = (typeQueue_LLS *)malloc(sizeof(typeQueue_LLS));
+    node_LLS *n = (node_LLS *)malloc(sizeof(node_LLS));
+    q->head = n;        /* The head and the tail of the queue */
+    q->tail = n;        /*      point to the same node, */
+    n->next = NULL;     /*      the generator node. */
+}
