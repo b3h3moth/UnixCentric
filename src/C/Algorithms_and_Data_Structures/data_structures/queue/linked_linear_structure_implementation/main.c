d524a0abc4b2a19c47c5d67dc359784710b45a0d
B3h3m0th <behemoth@autistici.org>
2018-07-27 12:04:26 +0200

    insQueue() tested in main()

--- src/C/Algorithms_and_Data_Structures/data_structures/queue/linked_linear_structure_implementation/main.c
+++ src/C/Algorithms_and_Data_Structures/data_structures/queue/linked_linear_structure_implementation/main.c
@@ -3,7 +3,11 @@
 #include "type_queue.h"
 
 int main(void) {
+    int i;
     typeQueue_LLS *myqq = init();
+
+    for (i = 0; i<10; i++)
+        insQueue(myqq, i*i);
 
     if (emptyQueue(*myqq))
         printf("The queue is empty\n");
