9a5c3cf687dc932b3dc33e4eba785ad5c77bd260
B3h3m0th <behemoth@autistici.org>
2018-09-10 18:32:39 +0200

    added node and type LLS

--- src/C/Algorithms_and_Data_Structures/data_structures/linked_list/personLLS.c
+++ src/C/Algorithms_and_Data_Structures/data_structures/linked_list/personLLS.c
@@ -3,16 +3,36 @@
 #include <string.h>
 #include <time.h>
 
+typedef struct person Person;
+typedef struct node_lls NodeLLS;
+typedef struct type_lls TypeLLS;
+
 struct person {
     char firstname[16];
     char lastname[16];
     int age;
 };
 
-typedef struct person Person;
+struct node_lls {
+    Person info;
+    NodeLLS *next;
+};
+
+struct type_lls {
+    NodeLLS *head;  /* Pointer to the first element of the Linear Linked List */
+    int size;       /* How many nodes the Linked Linear Structure contains */
+};
 
 /* Constant PersonError is useful when an error occurs */
 Person PersonError = {"","",0};
+
+/* Function prototypes */
+void print(Person p);
+int equal(Person p1, Person p2);
+
+int main() {
+    return(EXIT_SUCCESS);
+}
 
 /* Print full person */
 void print(Person p) {
@@ -23,8 +43,4 @@ void print(Person p) {
 int equal(Person p1, Person p2) {
     return (strcmp(p1.firstname, p2.firstname) == 0) &&
            (strcmp(p1.lastname, p2.lastname) == 0) && (p1.age == p2.age);
-}
-
-int main() {
-    return(EXIT_SUCCESS);
 }
